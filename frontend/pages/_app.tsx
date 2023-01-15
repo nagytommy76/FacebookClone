262a281a69a45cfb981e0b7740db9e45d491d7d3
nagytommy76 <nagytommy76@gmail.com>
2023-01-15 15:23:49 +0100

    Added a themeProvider to the app

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,14 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Work_Sans } from '@next/font/google'
 
+import { ThemeProvider } from '@emotion/react'
+import theme from '../src/theme'
 import Layout from '../components/Layout/Layout'
 
 const work = Work_Sans({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
    return (
-      <Layout className={work.className}>
-         <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+         <Layout className={work.className}>
+            <Component {...pageProps} />
+         </Layout>
+      </ThemeProvider>
    )
 }
---
+++ frontend/src/theme.tsx
@@ -0,0 +1,31 @@
+import { Work_Sans } from '@next/font/google'
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
+
+export const work_sans = Work_Sans({
+   weight: ['300', '400', '500', '700'],
+   subsets: ['latin'],
+   display: 'swap',
+   fallback: ['Helvetica', 'Arial', 'sans-serif'],
+})
+
+// Create a theme instance.
+const theme = createTheme({
+   palette: {
+      mode: 'dark',
+      primary: {
+         main: '#556cd6',
+      },
+      secondary: {
+         main: '#19857b',
+      },
+      error: {
+         main: red.A400,
+      },
+   },
+   typography: {
+      fontFamily: work_sans.style.fontFamily,
+   },
+})
+
+export default theme
