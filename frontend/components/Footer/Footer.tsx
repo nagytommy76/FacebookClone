9d52497c7370940b05fa476380703cade3e133c2
nagytommy76 <nagytommy76@gmail.com>
2023-01-17 20:15:18 +0100

    Working on the footer

--- frontend/components/Footer/Footer.tsx
+++ frontend/components/Footer/Footer.tsx
@@ -2,25 +2,40 @@ import styled from '@emotion/styled'
 
 import Typography from '@mui/material/Typography'
 import Paper from '@mui/material/Paper'
+import Stack from '@mui/material/Stack'
+import IconButton from '@mui/material/IconButton'
+
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import GitHubIcon from '@mui/icons-material/GitHub'
 
 const Footer = () => {
    return (
-      <Paper elevation={0} square>
-         <StyledFooter>
-            <Typography>Készítette: © Nagy Tamás 2023</Typography>
-            <p>social ikonok helye!!!</p>
-         </StyledFooter>
-      </Paper>
+      <StyledFooter elevation={2}>
+         <Typography variant='h6'>Készítette: © Nagy Tamás 2023</Typography>
+         <Stack direction='row' spacing={0} pt={1}>
+            <IconButton aria-label='linkedIn' size='large'>
+               <LinkedInIcon fontSize='large' />
+            </IconButton>
+            <IconButton aria-label='github' size='large'>
+               <GitHubIcon fontSize='large' />
+            </IconButton>
+         </Stack>
+      </StyledFooter>
    )
 }
 
 export default Footer
 
-const StyledFooter = styled('footer')({
+const StyledFooter = styled(Paper)({
+   // backgroundColor: '#222',
+   // color: '#FFF',
    width: '100%',
-   height: '80px',
+   padding: '1rem 0',
+
    display: 'flex',
    flexDirection: 'column',
    alignItems: 'center',
    justifyContent: 'center',
 })
+
+const StyledSocialRow = styled('span')({})
