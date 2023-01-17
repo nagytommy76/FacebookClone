a75f0e9e6091c8e94ff67d0c1d32e52cb4360ec1
nagytommy76 <nagytommy76@gmail.com>
2023-01-17 20:15:58 +0100

    Added textfields (email/pass) without any functionality

--- frontend/components/Auth/Login/Login.tsx
+++ frontend/components/Auth/Login/Login.tsx
@@ -1,10 +1,27 @@
+import Link from 'next/link'
+
 import styled from '@emotion/styled'
 import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+
+import TextField from '@mui/material/TextField'
 
 const Login = () => {
    return (
       <StyledLoginContainer>
-         <StyledPaper elevation={3}>Login</StyledPaper>
+         <StyledPaper elevation={3}>
+            <TextField id='email' label='E-mail vagy felhasználó' variant='outlined' fullWidth />
+            <TextField id='password' label='Jelszó' type='password' variant='outlined' fullWidth />
+            <Button variant='contained' fullWidth>
+               Bejelentkezés
+            </Button>
+            <p>Elfelejtetted a jelszavad?</p>
+            <Divider />
+            <Button variant='contained' color='success' fullWidth>
+               <Link href='/register'>Új fiók létrehozása</Link>
+            </Button>
+         </StyledPaper>
       </StyledLoginContainer>
    )
 }
@@ -20,5 +37,11 @@ const StyledLoginContainer = styled('section')({
 })
 
 const StyledPaper = styled(Paper)({
-   padding: 16,
+   padding: 25,
+   width: '400px',
+   height: '340px',
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: 'center',
+   justifyContent: 'space-between',
 })
