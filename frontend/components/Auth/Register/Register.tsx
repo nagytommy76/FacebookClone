ffd65a479215494b21b0d355427f9d0bc32cd910
nagytommy76 <nagytommy76@gmail.com>
2023-01-18 19:59:32 +0100

    Created the text fields without any functionality

--- frontend/components/Auth/Register/Register.tsx
+++ frontend/components/Auth/Register/Register.tsx
@@ -1,8 +1,28 @@
+import { StyledAuthContainer, StyledRegisterPaper } from '../Styles'
+
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+
+import DateOfBirth from './Includes/DateOfBirth'
+
 const Register = () => {
    return (
-      <div>
-         <h1>Regisztrációs oldal</h1>
-      </div>
+      <StyledAuthContainer>
+         <StyledRegisterPaper elevation={3}>
+            <h1>Regisztráció</h1>
+            <Stack direction='row' spacing={1}>
+               <TextField id='surname' label='Vezetéknév' variant='outlined' />
+               <TextField id='firstName' label='Keresztnév' variant='outlined' />
+            </Stack>
+            <TextField id='email' label='E-mail cím' variant='outlined' fullWidth />
+            <TextField id='password' label='Jelszó' type='password' variant='outlined' fullWidth />
+            <DateOfBirth />
+            <Button variant='contained' color='success' fullWidth>
+               Regisztráció
+            </Button>
+         </StyledRegisterPaper>
+      </StyledAuthContainer>
    )
 }
 
