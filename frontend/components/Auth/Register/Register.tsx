4e20747f0507804617e74cddf4824c4ae0e07221
nagytommy76 <nagytommy76@gmail.com>
2023-01-21 14:56:39 +0100

    Using refs to store input data

--- frontend/components/Auth/Register/Register.tsx
+++ frontend/components/Auth/Register/Register.tsx
@@ -1,5 +1,6 @@
-import { StyledAuthContainer, StyledRegisterPaper } from '../Styles'
+import { useRef } from 'react'
 
+import { StyledAuthContainer, StyledRegisterPaper } from '../Styles'
 import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
@@ -7,18 +8,37 @@ import TextField from '@mui/material/TextField'
 import DateOfBirth from './Includes/DateOfBirth'
 
 const Register = () => {
+   const sureNameRef = useRef<HTMLInputElement>(null)
+   const firstNameRef = useRef<HTMLInputElement>(null)
+   const emailRef = useRef<HTMLInputElement>(null)
+   const passwordRef = useRef<HTMLInputElement>(null)
+
+   const handleRegisterSend = () => {
+      console.log(sureNameRef.current?.value)
+      console.log(firstNameRef.current?.value)
+      console.log(emailRef.current?.value)
+      console.log(passwordRef.current?.value)
+   }
+
    return (
       <StyledAuthContainer>
          <StyledRegisterPaper elevation={3}>
             <h1>Regisztráció</h1>
             <Stack direction='row' spacing={1}>
-               <TextField id='surname' label='Vezetéknév' variant='outlined' />
-               <TextField id='firstName' label='Keresztnév' variant='outlined' />
+               <TextField inputRef={sureNameRef} id='surname' label='Vezetéknév' variant='outlined' />
+               <TextField inputRef={firstNameRef} id='firstName' label='Keresztnév' variant='outlined' />
             </Stack>
-            <TextField id='email' label='E-mail cím' variant='outlined' fullWidth />
-            <TextField id='password' label='Jelszó' type='password' variant='outlined' fullWidth />
+            <TextField inputRef={emailRef} id='email' label='E-mail cím' variant='outlined' fullWidth />
+            <TextField
+               inputRef={passwordRef}
+               id='password'
+               label='Jelszó'
+               type='password'
+               variant='outlined'
+               fullWidth
+            />
             <DateOfBirth />
-            <Button variant='contained' color='success' fullWidth>
+            <Button onClick={handleRegisterSend} variant='contained' color='success' fullWidth>
                Regisztráció
             </Button>
          </StyledRegisterPaper>
