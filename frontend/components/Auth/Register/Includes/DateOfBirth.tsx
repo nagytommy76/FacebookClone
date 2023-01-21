b347a3766e41d67f1090d63934b8f9ca7b65c126
nagytommy76 <nagytommy76@gmail.com>
2023-01-21 14:57:00 +0100

    Using the month/year hook

--- frontend/components/Auth/Register/Includes/DateOfBirth.tsx
+++ frontend/components/Auth/Register/Includes/DateOfBirth.tsx
@@ -1,4 +1,5 @@
 import useGetYears from './Hook/useGetYears'
+import useGetMonth from './Hook/useGetMonth'
 
 import Stack from '@mui/material/Stack'
 import InputLabel from '@mui/material/InputLabel'
@@ -8,6 +9,7 @@ import Select, { SelectChangeEvent } from '@mui/material/Select'
 
 const DateOfBirth = () => {
    const years = useGetYears()
+   const months = useGetMonth()
    return (
       <Stack direction='row' spacing={1} width='100%'>
          <FormControl fullWidth>
@@ -17,6 +19,7 @@ const DateOfBirth = () => {
                id='demo-simple-select'
                // value={age}
                label='Születési év'
+               MenuProps={{ PaperProps: { sx: { maxHeight: 300 } } }}
                // onChange={handleChange}
             >
                {years.map((year, index) => (
@@ -35,9 +38,11 @@ const DateOfBirth = () => {
                label='Születési év'
                // onChange={handleChange}
             >
-               <MenuItem value={10}>Ten</MenuItem>
-               <MenuItem value={20}>Twenty</MenuItem>
-               <MenuItem value={30}>Thirty</MenuItem>
+               {Object.entries(months).map((month) => (
+                  <MenuItem key={month[0]} value={month[0]}>
+                     {month[1]}
+                  </MenuItem>
+               ))}
             </Select>
          </FormControl>
          <FormControl fullWidth>
