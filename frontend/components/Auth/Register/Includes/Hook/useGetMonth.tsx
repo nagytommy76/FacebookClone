d49c569f229db2debca847cf85c68f3b7eae979a
nagytommy76 <nagytommy76@gmail.com>
2023-01-21 14:55:52 +0100

    Created a getMont hook

---
+++ frontend/components/Auth/Register/Includes/Hook/useGetMonth.tsx
@@ -0,0 +1,18 @@
+const useGetMonth = () => {
+   return {
+      january: 'Január',
+      february: 'Február',
+      march: 'Március',
+      april: 'Április',
+      may: 'Május',
+      june: 'Június',
+      july: 'Július',
+      august: 'Augusztus',
+      september: 'Szeptember',
+      october: 'Október',
+      november: 'November',
+      december: 'December',
+   }
+}
+
+export default useGetMonth
