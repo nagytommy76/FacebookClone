b3330242f984d875ddf659ea6d1cc571318f0e58
nagytommy76 <nagytommy76@gmail.com>
2023-01-28 13:40:02 +0100

    Modified the route to /register and post method

--- backend/api/user/user.ts
+++ backend/api/user/user.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express'
 
 const router = Router()
 
-router.get('/', (request: Request, response: Response) => {
+router.post('/register', (request: Request, response: Response) => {
    return response.json({ msg: 'Felhasználók oldal' })
 })
 
