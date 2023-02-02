3901e1ccb87b15e546cb3442bd226903750c6bcb
nagytommy76 <nagytommy76@gmail.com>
2023-02-02 09:42:34 +0100

    Changed useRef to useState & using onerror fn

--- frontend/components/Auth/Register/Register.tsx
+++ frontend/components/Auth/Register/Register.tsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from 'react'
-import { AxiosError, axiosInstance as axios, isAxiosError } from '../../../axiosSetup/AxiosInstance'
+import { useState } from 'react'
+import { axiosInstance as axios, isAxiosError } from '../../../axiosSetup/AxiosInstance'
 import { GenderTypes, IDateOfBirth, IInputValues, InputValues } from './Includes/Types'
 
 import { useMutation } from '@tanstack/react-query'
@@ -14,9 +14,9 @@ import GenderRadio from './Includes/GenderRadio'
 
 const Register = () => {
    const [sureName, setSureName] = useState<IInputValues>(InputValues)
-   const firstNameRef = useRef<HTMLInputElement>(null)
-   const emailRef = useRef<HTMLInputElement>(null)
-   const passwordRef = useRef<HTMLInputElement>(null)
+   const [firstName, setFirstName] = useState<IInputValues>(InputValues)
+   const [email, setEmail] = useState<IInputValues>(InputValues)
+   const [password, setPassword] = useState<IInputValues>(InputValues)
    const [dateOfBirth, setDateOfBirth] = useState<IDateOfBirth>({
       day: '',
       month: '',
@@ -27,21 +27,33 @@ const Register = () => {
    const handleRegisterSend = async () => {
       return await axios.post('/auth/register', {
          sureName: sureName.value,
-         firstName: firstNameRef.current?.value,
-         email: emailRef.current?.value,
-         password: passwordRef.current?.value,
+         firstName: firstName.value,
+         email: email.value,
+         password: password.value,
          dateOfBirth,
          gender,
       })
    }
 
-   const { mutate, error, isError } = useMutation({
+   const { mutate } = useMutation({
       mutationKey: ['register'],
       mutationFn: handleRegisterSend,
+      onError(error) {
+         if (isAxiosError(error)) {
+            const errorResponse = error.response?.data.errors as IInputValues[]
+            console.log(errorResponse)
+            errorResponse.map((value) => {
+               switch (value.param) {
+                  case 'email':
+                     setEmail((prevValue) => {
+                        return { ...prevValue, isError: true, msg: value.msg, param: value.param }
+                     })
+                     break
+               }
+            })
+         }
+      },
    })
-   if (isError && isAxiosError(error)) {
-      // console.log(error.response?.data.errors)
-   }
 
    return (
       <StyledAuthContainer>
@@ -49,11 +61,20 @@ const Register = () => {
             <h1>Regisztráció</h1>
             <Stack direction='row' spacing={1}>
                <TextField value={sureName.value} id='surname' label='Vezetéknév' variant='outlined' />
-               <TextField inputRef={firstNameRef} id='firstName' label='Keresztnév' variant='outlined' />
+               <TextField value={firstName.value} id='firstName' label='Keresztnév' variant='outlined' />
             </Stack>
-            <TextField inputRef={emailRef} id='email' label='E-mail cím' variant='outlined' fullWidth />
             <TextField
-               inputRef={passwordRef}
+               value={email.value}
+               onChange={(e) => setEmail({ ...email, value: e.target.value })}
+               error={email.isError}
+               helperText={email.msg}
+               id='email'
+               label='E-mail cím'
+               variant='outlined'
+               fullWidth
+            />
+            <TextField
+               value={password.value}
                id='password'
                label='Jelszó'
                type='password'
