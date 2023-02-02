5e1b71765b804ac0d63ef552801e229df8380a1e
nagytommy76 <nagytommy76@gmail.com>
2023-02-02 09:32:54 +0100

    Added a length check to email field

--- backend/api/user/validators/registerValidator.ts
+++ backend/api/user/validators/registerValidator.ts
@@ -1,7 +1,12 @@
 import { body, CustomValidator } from 'express-validator'
 
 export const ValidateRegister = [
-   body('email').isEmail().normalizeEmail().withMessage('Az email cím nem megfelelő formátumú!'),
+   body('email')
+      .isLength({ min: 2 })
+      .withMessage('Nem lehet üres amező!')
+      .isEmail()
+      .normalizeEmail()
+      .withMessage('Az email cím nem megfelelő formátumú vagy üres a mező!'),
    body('sureName').trim().isLength({ min: 1 }).withMessage('A Vezetéknév mező kitöltése kötelező!'),
    body('firstName').trim().isLength({ min: 1 }).withMessage('A Keresztnév mező kitöltése kötelező!'),
    // body('firstPassword').custom(checkPasswordsEquality),
