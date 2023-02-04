6ae287d6101a0822280cead5b04944a2e4b6fa0d
nagytommy76 <nagytommy76@gmail.com>
2023-02-04 16:17:07 +0100

    Moved the state & mutation to a custom hook

--- frontend/components/Auth/Register/Register.tsx
+++ frontend/components/Auth/Register/Register.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import { axiosInstance as axios, isAxiosError } from '../../../axiosSetup/AxiosInstance'
-import { GenderTypes, IDateOfBirth, IInputValues, InputValues } from './Includes/Types'
-
-import { useMutation } from '@tanstack/react-query'
+import useRegisterMutate from './Hooks/useRegisterMutate'
 
 import { StyledAuthContainer, StyledRegisterPaper } from '../Styles'
 import Stack from '@mui/material/Stack'
@@ -13,59 +9,42 @@ import DateOfBirth from './Includes/DateOfBirth'
 import GenderRadio from './Includes/GenderRadio'
 
 const Register = () => {
-   const [sureName, setSureName] = useState<IInputValues>(InputValues)
-   const [firstName, setFirstName] = useState<IInputValues>(InputValues)
-   const [email, setEmail] = useState<IInputValues>(InputValues)
-   const [password, setPassword] = useState<IInputValues>(InputValues)
-   const [dateOfBirth, setDateOfBirth] = useState<IDateOfBirth>({
-      day: '',
-      month: '',
-      year: '',
-   })
-   const [gender, setGender] = useState<GenderTypes>('male')
-
-   const handleRegisterSend = async () => {
-      return await axios.post('/auth/register', {
-         sureName: sureName.value,
-         firstName: firstName.value,
-         email: email.value,
-         password: password.value,
-         dateOfBirth,
-         gender,
-      })
-   }
-
-   const { mutate } = useMutation({
-      mutationKey: ['register'],
-      mutationFn: handleRegisterSend,
-      onError(error) {
-         if (isAxiosError(error)) {
-            const errorResponse = error.response?.data.errors as IInputValues[]
-            console.log(errorResponse)
-            errorResponse.map((value) => {
-               switch (value.param) {
-                  case 'email':
-                     setEmail((prevValue) => {
-                        return { ...prevValue, isError: true, msg: value.msg, param: value.param }
-                     })
-                     break
-               }
-            })
-         }
-      },
-   })
+   const {
+      registerMutate,
+      dateOfBirth,
+      email,
+      firstName,
+      gender,
+      password,
+      sureName,
+      setDateOfBirth,
+      setGender,
+      setAnyTextStateValues,
+   } = useRegisterMutate()
 
    return (
       <StyledAuthContainer>
          <StyledRegisterPaper elevation={3}>
             <h1>Regisztráció</h1>
             <Stack direction='row' spacing={1}>
-               <TextField value={sureName.value} id='surname' label='Vezetéknév' variant='outlined' />
-               <TextField value={firstName.value} id='firstName' label='Keresztnév' variant='outlined' />
+               <TextField
+                  value={sureName.value}
+                  onChange={(e) => setAnyTextStateValues(e, 'surename')}
+                  id='surname'
+                  label='Vezetéknév'
+                  variant='outlined'
+               />
+               <TextField
+                  value={firstName.value}
+                  onChange={(e) => setAnyTextStateValues(e, 'firstname')}
+                  id='firstName'
+                  label='Keresztnév'
+                  variant='outlined'
+               />
             </Stack>
             <TextField
                value={email.value}
-               onChange={(e) => setEmail({ ...email, value: e.target.value })}
+               onChange={(e) => setAnyTextStateValues(e, 'email')}
                error={email.isError}
                helperText={email.msg}
                id='email'
@@ -75,6 +54,7 @@ const Register = () => {
             />
             <TextField
                value={password.value}
+               onChange={(e) => setAnyTextStateValues(e, 'password')}
                id='password'
                label='Jelszó'
                type='password'
@@ -83,7 +63,7 @@ const Register = () => {
             />
             <DateOfBirth dateOfBirth={dateOfBirth} setDateOfBirth={setDateOfBirth} />
             <GenderRadio gender={gender} setGender={setGender} />
-            <Button onClick={() => mutate()} variant='contained' color='success' fullWidth>
+            <Button onClick={() => registerMutate()} variant='contained' color='success' fullWidth>
                Regisztráció
             </Button>
          </StyledRegisterPaper>
