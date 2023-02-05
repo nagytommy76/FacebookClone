ad092159859d325db3b18d30fcb273843186802c
nagytommy76 <nagytommy76@gmail.com>
2023-02-05 12:07:05 +0100

    Added an error msg field

--- frontend/components/Auth/Register/Includes/DateOfBirth.tsx
+++ frontend/components/Auth/Register/Includes/DateOfBirth.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import useGetYears from './Hook/useGetYears'
 import useGetMonth from './Hook/useGetMonth'
 import useGetDays from './Hook/useGetDays'
+import useDateOfBirth from './Hook/useDateOfBirth'
 import { IDateOfBirth } from './Types'
 
 import Stack from '@mui/material/Stack'
+import FormHelperText from '@mui/material/FormHelperText'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
-import useDateOfBirth from './Hook/useDateOfBirth'
 
 const DateOfBirth: React.FC<{
    dateOfBirth: IDateOfBirth
@@ -21,55 +22,58 @@ const DateOfBirth: React.FC<{
    const { handleDayChange, handleMontChange, handleYearChange } = useDateOfBirth(setDateOfBirth)
 
    return (
-      <Stack direction='row' spacing={1} width='100%'>
-         <FormControl fullWidth>
-            <InputLabel id='demo-simple-select-label'>Születési év</InputLabel>
-            <Select
-               labelId='demo-simple-select-label'
-               id='year'
-               value={dateOfBirth.year}
-               label='Születési év'
-               MenuProps={{ PaperProps: { sx: { maxHeight: 300 } } }}
-               onChange={handleYearChange}>
-               {years.map((year, index) => (
-                  <MenuItem key={index} value={year}>
-                     {year}
-                  </MenuItem>
-               ))}
-            </Select>
-         </FormControl>
-         <FormControl fullWidth>
-            <InputLabel id='demo-simple-select-label'>Hónap</InputLabel>
-            <Select
-               labelId='demo-simple-select-label'
-               id='month'
-               value={dateOfBirth.month}
-               label='Hónap'
-               onChange={handleMontChange}>
-               {months.map((month, index) => (
-                  <MenuItem key={index} value={index}>
-                     {month}
-                  </MenuItem>
-               ))}
-            </Select>
-         </FormControl>
-         <FormControl fullWidth>
-            <InputLabel id='demo-simple-select-label'>Nap</InputLabel>
-            <Select
-               labelId='demo-simple-select-label'
-               id='days'
-               value={dateOfBirth.day}
-               label='Nap'
-               MenuProps={{ PaperProps: { sx: { maxHeight: 300 } } }}
-               onChange={handleDayChange}>
-               {days.map((day) => (
-                  <MenuItem key={day} value={day}>
-                     {day}
-                  </MenuItem>
-               ))}
-            </Select>
-         </FormControl>
-      </Stack>
+      <>
+         <Stack direction='row' spacing={1} width='100%'>
+            <FormControl fullWidth error={dateOfBirth.isError}>
+               <InputLabel id='demo-simple-select-label'>Születési év</InputLabel>
+               <Select
+                  labelId='demo-simple-select-label'
+                  id='year'
+                  value={dateOfBirth.year}
+                  label='Születési év'
+                  MenuProps={{ PaperProps: { sx: { maxHeight: 300 } } }}
+                  onChange={handleYearChange}>
+                  {years.map((year, index) => (
+                     <MenuItem key={index} value={year}>
+                        {year}
+                     </MenuItem>
+                  ))}
+               </Select>
+            </FormControl>
+            <FormControl fullWidth error={dateOfBirth.isError}>
+               <InputLabel id='demo-simple-select-label'>Hónap</InputLabel>
+               <Select
+                  labelId='demo-simple-select-label'
+                  id='month'
+                  value={dateOfBirth.month}
+                  label='Hónap'
+                  onChange={handleMontChange}>
+                  {months.map((month, index) => (
+                     <MenuItem key={index} value={index}>
+                        {month}
+                     </MenuItem>
+                  ))}
+               </Select>
+            </FormControl>
+            <FormControl fullWidth error={dateOfBirth.isError}>
+               <InputLabel id='demo-simple-select-label'>Nap</InputLabel>
+               <Select
+                  labelId='demo-simple-select-label'
+                  id='days'
+                  value={dateOfBirth.day}
+                  label='Nap'
+                  MenuProps={{ PaperProps: { sx: { maxHeight: 300 } } }}
+                  onChange={handleDayChange}>
+                  {days.map((day) => (
+                     <MenuItem key={day} value={day}>
+                        {day}
+                     </MenuItem>
+                  ))}
+               </Select>
+            </FormControl>
+         </Stack>
+         <FormHelperText error={dateOfBirth.isError}>{dateOfBirth.msg}</FormHelperText>
+      </>
    )
 }
 
