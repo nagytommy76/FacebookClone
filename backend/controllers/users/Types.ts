6098e282a2dbd7542724284d36e233db5ba8537b
nagytommy76 <nagytommy76@gmail.com>
2023-02-11 16:08:32 +0100

    Created a Register request interface

--- backend/controllers/users/Types.ts
+++ backend/controllers/users/Types.ts
@@ -1,15 +1,30 @@
 import { Model, ObjectId } from 'mongoose'
+import { Request } from 'express'
+export interface IRegisterRequest extends Request {
+   body: {
+      email: string
+      sureName: string
+      firstName: string
+      password: string
+      gender: 'male' | 'female'
+      dateOfBirth: { day: number; month: number; year: number }
+   }
+}
 
 export interface UserTypes {
    _id: ObjectId
+   email: string
    firstName: string
    sureName: string
    password: string
    isEmailConfirmed: boolean
+   dateOfBirth: { day: number; month: number; year: number }
+   gender: 'male' | 'female'
    iat?: number
    exp?: number
 }
 
 export interface UserModel extends Model<UserTypes> {
-   register(email: string): Promise<void>
+   checkRegisterEmail(email: string): Promise<void>
+   encryptPassword(nativePass: string): Promise<string>
 }
