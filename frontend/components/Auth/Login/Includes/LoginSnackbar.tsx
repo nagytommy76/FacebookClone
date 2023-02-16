1fa8e9fc363306e30a2c3b5d1129a0c48efb3886
nagytommy76 <nagytommy76@gmail.com>
2023-02-16 19:35:12 +0100

    Created a separated login snackbar component

---
+++ frontend/components/Auth/Login/Includes/LoginSnackbar.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+
+import Fade from '@mui/material/Fade'
+import Snackbar from '@mui/material/Snackbar'
+
+const LoginSnackbar = () => {
+   const [isRegister, setIsRegister] = useState<{ isSuccess: boolean; msg: string | string[] }>({
+      isSuccess: false,
+      msg: '',
+   })
+   const router = useRouter()
+
+   useEffect(() => {
+      const { isRegisterSuccess, msg } = router.query
+      if (isRegisterSuccess && msg) setIsRegister({ isSuccess: Boolean(isRegisterSuccess), msg })
+   }, [router.query.isRegisterSuccess])
+
+   return (
+      <Snackbar
+         open={isRegister.isSuccess}
+         onClose={() => {}}
+         TransitionComponent={Fade}
+         message={isRegister.msg}
+      />
+   )
+}
+
+export default LoginSnackbar
