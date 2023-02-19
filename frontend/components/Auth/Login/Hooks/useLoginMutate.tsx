4e28179addd66b2d5be044a439f878700386783f
nagytommy76 <nagytommy76@gmail.com>
2023-02-19 11:51:33 +0100

    Handling the error messages

--- frontend/components/Auth/Login/Hooks/useLoginMutate.tsx
+++ frontend/components/Auth/Login/Hooks/useLoginMutate.tsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { useMutation } from '@tanstack/react-query'
-import { axiosInstance as axios } from '../../../../axiosSetup/AxiosInstance'
-import { IInputValues } from '../../Register/Includes/Types'
+import { axiosInstance as axios, AxiosResponse, isAxiosError } from '../../../../axiosSetup/AxiosInstance'
+import { IInputValues, InputValues } from '../../Register/Includes/Types'
 
-const useLoginMutate = (email: IInputValues, password: IInputValues) => {
-   const handleLoginSend = async (event: React.FormEvent) => {
+const useLoginMutate = () => {
+   const [email, setEmail] = useState<IInputValues>(InputValues)
+   const [password, setPassword] = useState<IInputValues>(InputValues)
+
+   const handleSetEmail = (event: ChangeEvent<HTMLInputElement>) =>
+      setEmail((prevValue) => {
+         return { ...prevValue, value: event.target.value }
+      })
+   const handleSetPassword = (event: ChangeEvent<HTMLInputElement>) =>
+      setPassword((prevValue) => {
+         return { ...prevValue, value: event.target.value }
+      })
+
+   const handleLoginSend = async (event: React.FormEvent): Promise<AxiosResponse<{ msg: string }>> => {
       event.preventDefault()
       return await axios.post('/auth/login', {
          email: email.value,
@@ -12,15 +24,48 @@ const useLoginMutate = (email: IInputValues, password: IInputValues) => {
       })
    }
 
-   const { mutate, isLoading } = useMutation({
+   const { mutate, isLoading, data } = useMutation({
       mutationKey: ['login'],
       mutationFn: handleLoginSend,
       onSuccess: (data) => {
          console.log(data.data)
       },
+      onError: (error) => {
+         if (isAxiosError(error)) {
+            const errorResponse = error.response?.data.errors as IInputValues[]
+            console.log(errorResponse)
+            errorResponse.map((value) => {
+               if (value.param === 'email') {
+                  setEmail((prevValues) => {
+                     return {
+                        ...prevValues,
+                        msg: value.msg,
+                        isError: true,
+                     }
+                  })
+               } else {
+                  setPassword((prevValues) => {
+                     return {
+                        ...prevValues,
+                        msg: value.msg,
+                        isError: true,
+                     }
+                  })
+               }
+            })
+         }
+      },
    })
 
-   return { loginMutate: mutate, isLoading }
+   return {
+      email,
+      password,
+      handleSetEmail,
+      handleSetPassword,
+      loginMutate: mutate,
+      isLoading,
+      data,
+   }
 }
 
 export default useLoginMutate
