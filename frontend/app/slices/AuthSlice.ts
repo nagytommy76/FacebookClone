fd4b3bd100fa31d29d1dfe9826f14eae09e56a0c
nagytommy76 <nagytommy76@gmail.com>
2023-02-25 20:42:54 +0100

    Added the first auth slice

---
+++ frontend/app/slices/AuthSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export const AuthSlice = createSlice({
+   name: 'auth',
+   initialState: {},
+   reducers: {},
+})
+
+export default AuthSlice.reducer
