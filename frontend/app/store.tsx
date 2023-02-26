3495c9e4c1f4170b29b623867091879ccb118c33
nagytommy76 <nagytommy76@gmail.com>
2023-02-26 14:29:20 +0100

    Imported the tokenSlice & exported the useAppDispatch hook

--- frontend/app/store.tsx
+++ frontend/app/store.tsx
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch } from 'react-redux'
 
 import AuthSlice from './slices/AuthSlice'
+import TokenSlice from './slices/TokenSlice'
 
 export const store = configureStore({
    reducer: {
       auth: AuthSlice,
+      token: TokenSlice,
    },
 })
 
@@ -12,3 +15,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = useDispatch // Export a hook that can be reused to resolve types
