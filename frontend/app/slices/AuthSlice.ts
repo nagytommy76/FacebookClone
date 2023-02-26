dcb3026de25fd25c294f8209191d5f6b1ae0295a
nagytommy76 <nagytommy76@gmail.com>
2023-02-26 11:54:46 +0100

    Added the reducers & initial state to the slice

--- frontend/app/slices/AuthSlice.ts
+++ frontend/app/slices/AuthSlice.ts
@@ -1,10 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
+interface IAuthState {
+   userId: string
+   userName: string
+   isLoggedIn: boolean
+}
+
+const initialState: IAuthState = {
+   userId: '',
+   userName: '',
+   isLoggedIn: false,
+}
+
 export const AuthSlice = createSlice({
    name: 'auth',
-   initialState: {},
-   reducers: {},
+   initialState,
+   reducers: {
+      setUserId: (state, action: PayloadAction<string>) => {
+         state.userId = action.payload
+      },
+      setUserName: (state, action: PayloadAction<string>) => {
+         state.userName = action.payload
+      },
+      setIsLoggedIn: (state, action: PayloadAction<boolean>) => {
+         state.isLoggedIn = action.payload
+      },
+      setLogoutUser: (state) => {
+         state.userId = ''
+         state.userName = ''
+         state.isLoggedIn = false
+      },
+   },
 })
 
+export const { setUserId, setUserName, setIsLoggedIn, setLogoutUser } = AuthSlice.actions
 export default AuthSlice.reducer
