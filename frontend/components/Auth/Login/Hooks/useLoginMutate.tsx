3a719606d839ca8c2f522dec5b9c13463c0fa901
nagytommy76 <nagytommy76@gmail.com>
2023-02-26 14:28:47 +0100

    Using the custom hooks

--- frontend/components/Auth/Login/Hooks/useLoginMutate.tsx
+++ frontend/components/Auth/Login/Hooks/useLoginMutate.tsx
@@ -1,7 +1,11 @@
 import { useMutation } from '@tanstack/react-query'
-import { axiosInstance as axios, AxiosResponse, isAxiosError } from '../../../../axiosSetup/AxiosInstance'
+
+import useOnError from './useOnError'
+import useMutationFn from './useMutationFn'
+import useOnSuccess from './useOnSuccess'
+
 import type { Dispatch, SetStateAction } from 'react'
-import type { IInputValues, ILoginData } from '../../Register/Includes/Types'
+import type { IInputValues } from '../../Register/Includes/Types'
 
 const useLoginMutate = (
    email: string,
@@ -9,47 +13,15 @@ const useLoginMutate = (
    setEmail: Dispatch<SetStateAction<IInputValues>>,
    setPassword: Dispatch<SetStateAction<IInputValues>>
 ) => {
-   const handleLoginSend = async (event: React.FormEvent): Promise<AxiosResponse<ILoginData>> => {
-      event.preventDefault()
-      return await axios.post('/auth/login', {
-         email,
-         password,
-      })
-   }
-
-   const onSuccessFn = (data: AxiosResponse<ILoginData>) => {
-      // https://www.youtube.com/watch?v=ss-_S1Vyxa0&ab_channel=SonnySangha
-   }
+   const onError = useOnError(setEmail, setPassword)
+   const handleLoginSend = useMutationFn(email, password)
+   const onSuccess = useOnSuccess()
 
    const { mutate, isLoading, data } = useMutation({
       mutationKey: ['login'],
       mutationFn: handleLoginSend,
-      onSuccess: onSuccessFn,
-      onError: (error) => {
-         if (isAxiosError(error)) {
-            const errorResponse = error.response?.data.errors as IInputValues[]
-            console.log(errorResponse)
-            errorResponse.map((value) => {
-               if (value.param === 'email') {
-                  setEmail((prevValues) => {
-                     return {
-                        ...prevValues,
-                        msg: value.msg,
-                        isError: true,
-                     }
-                  })
-               } else {
-                  setPassword((prevValues) => {
-                     return {
-                        ...prevValues,
-                        msg: value.msg,
-                        isError: true,
-                     }
-                  })
-               }
-            })
-         }
-      },
+      onSuccess,
+      onError,
    })
 
    return {
