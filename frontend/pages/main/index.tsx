99cb2bdbba4121d2866ef5ba76a478f433317df4
nagytommy76 <nagytommy76@gmail.com>
2023-02-28 19:56:38 +0100

    Created a basic main page

---
+++ frontend/pages/main/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+   return (
+      <div>
+         <h1>Ez egy védett oldal és csak access tokennel láthatom!</h1>
+      </div>
+   )
+}
+
+export default index
