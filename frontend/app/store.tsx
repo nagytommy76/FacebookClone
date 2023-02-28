8c9e34fddb185f8cbeaba0c46b0b79aa57be1f6a
nagytommy76 <nagytommy76@gmail.com>
2023-02-28 19:45:10 +0100

    Added the useAppselector constant

--- frontend/app/store.tsx
+++ frontend/app/store.tsx
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import type { TypedUseSelectorHook } from 'react-redux'
 
 import AuthSlice from './slices/AuthSlice'
 import TokenSlice from './slices/TokenSlice'
@@ -17,3 +18,4 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
 export const useAppDispatch: () => AppDispatch = useDispatch // Export a hook that can be reused to resolve types
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
