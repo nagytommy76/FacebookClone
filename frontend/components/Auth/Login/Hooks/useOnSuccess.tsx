675b620d25329860e5616cc3145071dcb0e5282d
nagytommy76 <nagytommy76@gmail.com>
2023-03-02 19:25:44 +0100

    Moving the user to main page

--- frontend/components/Auth/Login/Hooks/useOnSuccess.tsx
+++ frontend/components/Auth/Login/Hooks/useOnSuccess.tsx
@@ -1,12 +1,15 @@
 import { useAppDispatch } from '../../../../app/store'
 import { setAccessToken } from '../../../../app/slices/TokenSlice'
+import { useRouter } from 'next/router'
 import { setUserName, setIsLoggedIn, setUserId } from '../../../../app/slices/AuthSlice'
 
 import type { AxiosResponse } from 'axios'
 import type { ILoginData } from '../../Register/Includes/Types'
 
 const useOnSuccess = () => {
+   const router = useRouter()
    const dispatch = useAppDispatch()
+
    const onSuccessFn = (data: AxiosResponse<ILoginData>) => {
       // https://www.youtube.com/watch?v=ss-_S1Vyxa0&ab_channel=SonnySangha
       // Sikeres a belépés ->
@@ -16,6 +19,7 @@ const useOnSuccess = () => {
       dispatch(setUserName(data.data.userName))
       dispatch(setUserId(data.data.userId))
       dispatch(setIsLoggedIn(true))
+      router.push('/main')
    }
    return onSuccessFn
 }
