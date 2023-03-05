7fb61328cbf4c0425e3f954233d5adbdc606874f
nagytommy76 <nagytommy76@gmail.com>
2023-03-05 12:16:22 +0100

    Added the refresh-token api route

--- backend/api/user/user.ts
+++ backend/api/user/user.ts
@@ -2,12 +2,14 @@ import { Router } from 'express'
 import { ValidateRegister, ValidateLogin } from './validators/authValidator'
 import { authValidationMiddleware } from '../../middlewares/authValidationMiddleware'
 
-import { loginUserController } from '../../controllers/users/users'
+import { loginUserController, checkRefreshTokenValidityController } from '../../controllers/users/users'
 import { registerUserController } from '../../controllers/users/register'
 
 const router = Router()
 
 router.post('/register', ValidateRegister, authValidationMiddleware, registerUserController)
 router.post('/login', ValidateLogin, authValidationMiddleware, loginUserController)
+
+router.post('/refresh-token', checkRefreshTokenValidityController)
 
 module.exports = router
