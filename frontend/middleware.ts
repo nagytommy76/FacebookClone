d28a5ab444ee150391774564e32cf3e5c114d40e
nagytommy76 <nagytommy76@gmail.com>
2023-03-05 15:59:28 +0100

    Setting cookies in the middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,10 +3,9 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-   // return NextResponse.redirect(new URL('/about-2', request.url))
    const refreshToken = request.cookies.get('refreshToken')?.value
    if (!refreshToken) return NextResponse.redirect(new URL('/login', request.url))
-   //  Ha van access token megnézem, hogy valid-e axios-szal
+   //  Ha van access token megnézem, hogy valid-e fetch-el, axios nem müxik
    const response = await fetch('http://localhost:5050/api/auth/refresh-token', {
       method: 'POST',
       headers: {
@@ -15,10 +14,25 @@ export async function middleware(request: NextRequest) {
       body: JSON.stringify({ refreshToken }),
    })
    // Lejárt a refreshToken
-   if (response.status === 403) return NextResponse.redirect(new URL('/login', request.url))
+   if (response.status === 403 || response.status === 401) {
+      let redirectResponse = NextResponse.redirect(new URL('/login', request.url))
+      redirectResponse.cookies.delete('accessToken')
+      redirectResponse.cookies.delete('refreshToken')
+      return redirectResponse
+   }
    const newAccessToken = await response.json()
-   // console.log(newAccessToken)
-   return NextResponse.next()
+
+   const nextResponse = NextResponse.next()
+
+   nextResponse.cookies.set({
+      name: 'accessToken',
+      value: newAccessToken,
+      secure: true,
+      httpOnly: true,
+      sameSite: 'none',
+      expires: new Date(Date.now() + 60000),
+   })
+   return nextResponse
 }
 
 export const config = {
