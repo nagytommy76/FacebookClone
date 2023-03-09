bfd45050a2e0085df5a7f50aeed15fa43a6ad410
nagytommy76 <nagytommy76@gmail.com>
2023-03-09 20:10:58 +0100

    Fixed a function description

--- backend/controllers/users/helpers/convertMilsec.ts
+++ backend/controllers/users/helpers/convertMilsec.ts
@@ -2,10 +2,6 @@ import ms from 'ms'
 
 /**
  * @param time string. "1day", "1y"
- * @param format (60000, { long: true })          "1 minute"
-            ms(2 * 60000, { long: true })         "2 minutes"
-            ms(-3 * 60000, { long: true })        "-3 minutes"
-            ms(ms('10 hours'), { long: true })    "10 hours" 
  * @returns number, 10000 millisec
  */
 export const convertStringToMillisec = (time: string): number => {
