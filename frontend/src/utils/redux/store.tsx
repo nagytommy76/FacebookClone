59c5eee026b7676c14f063472a6e33dccf6bb5b4
nagytommy76 <nagytommy76@gmail.com>
2023-03-12 15:03:26 +0100

    Installed & set up redux-presist

--- frontend/src/utils/redux/store.tsx
+++ frontend/src/utils/redux/store.tsx
@@ -2,14 +2,34 @@ import { configureStore } from '@reduxjs/toolkit'
 import { useDispatch, useSelector } from 'react-redux'
 import type { TypedUseSelectorHook } from 'react-redux'
 
+import {
+   persistReducer,
+   persistStore,
+   FLUSH,
+   REHYDRATE,
+   PAUSE,
+   PERSIST,
+   PURGE,
+   REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import AuthSlice from './slices/AuthSlice'
 import TokenSlice from './slices/TokenSlice'
+import ThemeSlice from './slices/ThemeSlice'
 
 export const store = configureStore({
    reducer: {
-      auth: AuthSlice,
       token: TokenSlice,
+      auth: persistReducer({ key: 'Auth', storage }, AuthSlice),
+      theme: persistReducer({ key: 'Theme', storage }, ThemeSlice),
    },
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+         serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+         },
+      }),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
@@ -17,5 +37,6 @@ export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 
+export const persistor = persistStore(store)
 export const useAppDispatch: () => AppDispatch = useDispatch // Export a hook that can be reused to resolve types
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
