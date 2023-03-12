2a4fcda8e4171c30b1ea3122ac2dd132ee0b9ac5
nagytommy76 <nagytommy76@gmail.com>
2023-03-12 11:30:53 +0100

    Created a theme switch component

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -1,29 +1,27 @@
 import Link from 'next/link'
 import { useAppSelector } from '../../app/redux/store'
 
+import ThemeSwitch from './Includes/ThemeSwitch'
+
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
 
 const Navbar = () => {
    const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
    return (
       <AppBar position='sticky'>
          <Toolbar>
-            <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
-               <MenuIcon />
-            </IconButton>
             <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-               News
+               Facebook 2.0
             </Typography>
             {!isLoggedIn && (
                <Link href='/login'>
                   <Button color='inherit'>Belépés</Button>
                </Link>
             )}
+            <ThemeSwitch />
          </Toolbar>
       </AppBar>
    )
