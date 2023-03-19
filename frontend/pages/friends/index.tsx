7fa9c6430e470cef2766ff639619b77f4123d445
nagytommy76 <nagytommy76@gmail.com>
2023-03-19 10:41:23 +0100

    Created the friends/groups pages

---
+++ frontend/pages/friends/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Friends = () => {
+   return (
+      <div>
+         <h1>Barátok komponens, szintén refreshTokennel látható</h1>
+      </div>
+   )
+}
+
+export default Friends
---
+++ frontend/pages/groups/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Groups = () => {
+   return (
+      <div>
+         <h1>Csoportok oldal, védett....</h1>
+      </div>
+   )
+}
+
+export default Groups
