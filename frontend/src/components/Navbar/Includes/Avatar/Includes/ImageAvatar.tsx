56923c6fd50ab6ba2b527e52e44584deaccc78f8
nagytommy76 <nagytommy76@gmail.com>
2023-03-21 18:46:54 +0100

    Added a displayText prop

--- frontend/src/components/Navbar/Includes/Avatar/Includes/ImageAvatar.tsx
+++ frontend/src/components/Navbar/Includes/Avatar/Includes/ImageAvatar.tsx
@@ -4,11 +4,14 @@ import Avatar from '@mui/material/Avatar'
 
 import OwnProfile from '../../../../../../public/sajat.jpg'
 
-const ImageAvatar = () => {
+const ImageAvatar: React.FC<{ displayText?: string }> = ({ displayText }) => {
    return (
-      <Avatar sx={{ bgcolor: 'orange', width: 45, height: 45 }}>
-         <Image width={45} height={45} alt='Profile picture' src={OwnProfile} />
-      </Avatar>
+      <>
+         <Avatar sx={{ bgcolor: 'orange', width: 45, height: 45 }}>
+            <Image width={45} height={45} alt='Profile picture' src={OwnProfile} />
+         </Avatar>
+         <p>{displayText}</p>
+      </>
    )
 }
 
