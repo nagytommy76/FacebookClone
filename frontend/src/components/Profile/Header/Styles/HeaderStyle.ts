cd6f8e62761d47d8d516b55a34c5fc8af4b749f7
nagytommy76 <nagytommy76@gmail.com>
2023-03-23 19:32:07 +0100

    Created separated stlyes for the different components

---
+++ frontend/src/components/Profile/Header/Styles/HeaderStyle.ts
@@ -0,0 +1,31 @@
+import { styled } from '@mui/system'
+import Image from 'next/image'
+
+import Paper from '@mui/material/Paper'
+
+export const ProfileHeader = styled(Paper)({
+   marginTop: 16,
+   padding: 20,
+   width: '100%',
+   height: '200px',
+   display: 'flex',
+   flexDirection: 'row',
+   alignItems: 'center',
+})
+
+export const HeaderImage = styled(Image)({
+   width: '168px',
+   height: '168px',
+   borderRadius: '50%',
+   marginRight: '18px',
+})
+
+export const HeadTitleSection = styled('div')({
+   height: '65%',
+   flexGrow: 1,
+   display: 'flex',
+   flexDirection: 'column',
+   justifyContent: 'space-around',
+})
+
+export const ProfileModifySection = styled('div')({ flexGrow: 0, alignSelf: 'flex-end' })
