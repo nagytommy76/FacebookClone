3e3e3d6d5dd90bb0ae7a27c0bc7a6feddb21c14f
nagytommy76 <nagytommy76@gmail.com>
2023-03-25 15:09:59 +0100

    Lifted up the NavTabs component & added the Body component

--- frontend/src/components/Profile/ProfileMe.tsx
+++ frontend/src/components/Profile/ProfileMe.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import dynamic from 'next/dynamic'
 import { StyledMeContainer } from './Styles/Style'
 
+import NavTabs from './Header/NavTabs/NavTabs'
 const ProfileHeader = dynamic(() => import('./Header/Header'))
+const Body = dynamic(() => import('./Body/Body'))
 
 const ProfileMe = () => {
+   const [tabValue, setTabValue] = useState<number>(0)
+
+   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+      setTabValue(newValue)
+   }
+
    return (
       <StyledMeContainer>
-         <ProfileHeader />
+         <ProfileHeader>
+            <NavTabs handleTabChange={handleTabChange} tabValue={tabValue} />
+         </ProfileHeader>
+         <Body tabValue={tabValue} />
       </StyledMeContainer>
    )
 }
