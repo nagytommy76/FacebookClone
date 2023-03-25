ffb7211e4c3aad8951262e0073f18ed04cb38a7d
nagytommy76 <nagytommy76@gmail.com>
2023-03-25 15:11:31 +0100

    Modified the color of the tabs & underline

--- frontend/src/components/Profile/Header/Styles/HeaderStyle.ts
+++ frontend/src/components/Profile/Header/Styles/HeaderStyle.ts
@@ -1,11 +1,13 @@
-import { styled } from '@mui/system'
+import { styled } from '@mui/material'
 import Image from 'next/image'
 
+import Tab from '@mui/material/Tab'
+import Tabs from '@mui/material/Tabs'
 import Paper from '@mui/material/Paper'
 
 export const ProfileHeader = styled(Paper)({
    marginTop: 16,
-   padding: 20,
+   padding: '20px 20px 0px 20px',
    width: '100%',
    minHeight: '200px',
 })
@@ -32,3 +34,11 @@ export const HeadTitleSection = styled('div')({
 })
 
 export const ProfileModifySection = styled('div')({ flexGrow: 0, alignSelf: 'flex-end' })
+
+export const StyledTab = styled(Tab)({
+   textTransform: 'none',
+   fontSize: '16px',
+   '&.Mui-selected': { color: '#087aea' },
+})
+
+export const StyledTabs = styled(Tabs)({ '& .MuiTabs-indicator': { backgroundColor: '#087aea' } })
