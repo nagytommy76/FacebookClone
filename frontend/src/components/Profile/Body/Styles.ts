cea20db985bc7c41e6b15b1f55c476d8af404ae8
nagytommy76 <nagytommy76@gmail.com>
2023-03-25 15:08:55 +0100

    Created a style file for the body

---
+++ frontend/src/components/Profile/Body/Styles.ts
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material'
+
+import Paper from '@mui/material/Paper'
+
+export const BodyPaper = styled(Paper)({
+   marginTop: 16,
+   padding: 8,
+   width: '100%',
+})
