4bf5950f66e4e1e16ece2b17a83e41a1317e9b14
nagytommy76 <nagytommy76@gmail.com>
2023-03-26 12:58:51 +0200

    Using the AboutMe component & import it with dynamic

--- frontend/src/components/Profile/Body/Body.tsx
+++ frontend/src/components/Profile/Body/Body.tsx
@@ -1,33 +1,10 @@
 import React from 'react'
+import dynamic from 'next/dynamic'
 
 import { BodyPaper } from './Styles'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
 
-interface TabPanelProps {
-   children?: React.ReactNode
-   index: number
-   value: number
-}
-
-function TabPanel(props: TabPanelProps) {
-   const { children, value, index, ...other } = props
-
-   return (
-      <div
-         role='tabpanel'
-         hidden={value !== index}
-         id={`simple-tabpanel-${index}`}
-         aria-labelledby={`simple-tab-${index}`}
-         {...other}>
-         {value === index && (
-            <Box sx={{ p: 3 }}>
-               <Typography>{children}</Typography>
-            </Box>
-         )}
-      </div>
-   )
-}
+import TabPanel from './Includes/TabPanel'
+const AboutMeComponent = dynamic(() => import('./AboutMe/AboutMe'))
 
 const Body: React.FC<{ tabValue: number }> = ({ tabValue }) => {
    return (
@@ -36,7 +13,9 @@ const Body: React.FC<{ tabValue: number }> = ({ tabValue }) => {
             Bejegyzések jönnek ide
          </TabPanel>
          <TabPanel value={tabValue} index={1}>
-            Névjegy jön ide, iskola, munkahelyek stb
+            <React.Suspense fallback={<h2>Töltés...</h2>}>
+               <AboutMeComponent />
+            </React.Suspense>
          </TabPanel>
          <TabPanel value={tabValue} index={2}>
             Ismerősök
