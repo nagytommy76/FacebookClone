d88991f183fa1ce9ba4a042452d5ebf338d35dbc
nagytommy76 <nagytommy76@gmail.com>
2023-03-31 19:54:52 +0200

    Created a Posts component

---
+++ frontend/src/components/Profile/Body/Posts/Posts.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { StyledPostContainer } from './PostStyles'
+
+const Posts = () => {
+   return (
+      <>
+         <h1>Ide jönnek a postok, és a bevitel</h1>
+         <h4>Ide jön egy mi jár a fejedban input mező (add comment)</h4>
+         <h5>Valamint a már meglévő commentek listája</h5>
+         <p>Minden külön Paper-ben</p>
+      </>
+   )
+}
+
+export default Posts
