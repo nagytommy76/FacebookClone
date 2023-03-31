c8d123036b788db29155ff53a8cb3b90deffa763
nagytommy76 <nagytommy76@gmail.com>
2023-03-31 19:57:05 +0200

    Removed the BodyPaper custom style & loading the Posts component

--- frontend/src/components/Profile/Body/Body.tsx
+++ frontend/src/components/Profile/Body/Body.tsx
@@ -1,15 +1,14 @@
 import dynamic from 'next/dynamic'
 
-import { BodyPaper } from './Styles'
-
 import TabPanel from './Includes/TabPanel'
 const AboutMeComponent = dynamic(() => import('./AboutMe/AboutMe'), { loading: () => <h2>Töltés...</h2> })
+const PostsComponent = dynamic(() => import('./Posts/Posts'), { loading: () => <h2>Töltés!!!4444...</h2> })
 
 const Body: React.FC<{ tabValue: number }> = ({ tabValue }) => {
    return (
-      <BodyPaper elevation={1}>
+      <>
          <TabPanel value={tabValue} index={0}>
-            Bejegyzések jönnek ide
+            <PostsComponent />
          </TabPanel>
          <TabPanel value={tabValue} index={1}>
             <AboutMeComponent />
@@ -20,7 +19,7 @@ const Body: React.FC<{ tabValue: number }> = ({ tabValue }) => {
          <TabPanel value={tabValue} index={3}>
             Fényképek jönnek ide
          </TabPanel>
-      </BodyPaper>
+      </>
    )
 }
 
