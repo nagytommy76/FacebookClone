d3ab5035aaea4c041a17c43afda8dad27df66129
nagytommy76 <nagytommy76@gmail.com>
2023-04-01 19:58:06 +0200

    Loading dynamically the AddPost component

--- frontend/src/components/Profile/Body/Posts/Posts.tsx
+++ frontend/src/components/Profile/Body/Posts/Posts.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import dynamic from 'next/dynamic'
 
-import { StyledPostContainer } from './PostStyles'
+const AddPostComponent = dynamic(() => import('./AddPost/AddPost'), { loading: () => <h1>Töltés</h1> })
 
 const Posts = () => {
    return (
       <>
-         <h1>Ide jönnek a postok, és a bevitel</h1>
+         <AddPostComponent />
          <h4>Ide jön egy mi jár a fejedban input mező (add comment)</h4>
          <h5>Valamint a már meglévő commentek listája</h5>
          <p>Minden külön Paper-ben</p>
