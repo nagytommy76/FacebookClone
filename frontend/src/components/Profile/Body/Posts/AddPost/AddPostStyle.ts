1a46376cae4da21305cc6ccda12fa4156ff97a6c
nagytommy76 <nagytommy76@gmail.com>
2023-04-01 19:57:11 +0200

    Creadet a style file for AddPost component

---
+++ frontend/src/components/Profile/Body/Posts/AddPost/AddPostStyle.ts
@@ -0,0 +1,15 @@
+import { styled } from '@mui/material'
+
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+
+export const AddPostStyle = styled(Paper)({
+   marginTop: 16,
+   marginBottom: 16,
+   height: '70px',
+})
+
+export const CustomAddPostButton = styled(Button)({
+   width: '80%',
+   borderRadius: '20px',
+})
