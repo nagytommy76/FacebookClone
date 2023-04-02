d6ba2203c3f4461337de35f5bf051fd97e337834
nagytommy76 <nagytommy76@gmail.com>
2023-04-02 13:31:21 +0200

    Loading dynamically the AddPost component & created open state

--- frontend/src/components/Profile/Body/Posts/AddPost/AddPost.tsx
+++ frontend/src/components/Profile/Body/Posts/AddPost/AddPost.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
+import dynamic from 'next/dynamic'
+import ProfileImage from '../../../../../../public/sajat.jpg'
 
-import { AddPostStyle, CustomAddPostButton } from './AddPostStyle'
+import { AddPostStyle, CustomAddPostButton, CustomNextImage } from './AddPostStyle'
+
+const AddPostDialog = dynamic(() => import('./AddDialog/AddDialog'), {
+   loading: () => <h1>Töltés, majd csinálni egy suspense-t</h1>,
+})
 
 const AddPost = () => {
+   const [open, setOpen] = useState<boolean>(false)
+
+   const handleClickOpen = () => {
+      setOpen(true)
+   }
+
+   const handleClose = () => {
+      setOpen(false)
+   }
+
    return (
-      <AddPostStyle>
-         <CustomAddPostButton variant='text' color='primary'>
-            Mi jár a fejedben?
-         </CustomAddPostButton>
-      </AddPostStyle>
+      <>
+         <AddPostStyle>
+            {/* Ez itt majd dinamikus lesz */}
+            <CustomNextImage src={ProfileImage} alt='Sajat kép' />
+            <CustomAddPostButton onClick={handleClickOpen} disableRipple variant='text' color='primary'>
+               Mi jár a fejedben?
+            </CustomAddPostButton>
+         </AddPostStyle>
+         <AddPostDialog handleCloseAddDialog={handleClose} openAddDialog={open} />
+      </>
    )
 }
 
