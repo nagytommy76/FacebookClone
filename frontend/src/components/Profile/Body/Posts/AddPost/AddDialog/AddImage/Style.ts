6cf125f3170c9fd624dbd0bd877c676ed64c75ef
nagytommy76 <nagytommy76@gmail.com>
2023-04-07 16:33:06 +0200

    Created a style file for Label,input,Image

---
+++ frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/AddImage/Style.ts
@@ -0,0 +1,43 @@
+import { styled } from '@mui/material'
+import Image from 'next/image'
+
+export const StyledImagePreviewContainer = styled('div')({
+   height: '50%',
+   display: 'flex',
+   flexDirection: 'column',
+})
+
+export const StyledImagePreview = styled(Image)({
+   flexGrow: 1,
+   marginBottom: '.2rem',
+   objectFit: 'cover',
+})
+
+export const StyledImageInputField = styled('input')({
+   opacity: 0,
+   width: 0.1,
+   height: 0.1,
+   position: 'absolute',
+})
+
+export const StyledLabelAsButton = styled('label')(({ theme }) => ({
+   position: 'relative',
+   width: '210px',
+   height: '45px',
+   borderRadius: '5px',
+
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'center',
+
+   background: theme.palette.info.main,
+   boxShadow: '0 4px 7px rgba(0,0,0,0.4)',
+   color: theme.palette.text.primary,
+
+   cursor: 'pointer',
+   transition: 'box-shadow .2s ease',
+
+   ['&:hover']: {
+      boxShadow: '0 8px 14px rgba(0,0,0,0.4)',
+   },
+}))
