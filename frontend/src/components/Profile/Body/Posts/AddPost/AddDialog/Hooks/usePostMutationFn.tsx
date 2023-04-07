80c220c923fd1f609a5bd1b668f5029a913be46f
nagytommy76 <nagytommy76@gmail.com>
2023-04-07 17:02:59 +0200

    postedPicturesPath prop is required

--- frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/usePostMutationFn.tsx
+++ frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/usePostMutationFn.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { axiosInstance as axios, AxiosResponse } from '../../../../../../../utils/axiosSetup/AxiosInstance'
 
-const usePostMutationFn = (description: string, postedPicturesPath?: FileList | null) => {
+const usePostMutationFn = (description: string, postedPicturesPath: FileList | null) => {
    const handleAddPostSend = async (event: React.FormEvent): Promise<AxiosResponse> => {
       event.preventDefault()
+      if (postedPicturesPath !== null) console.log(postedPicturesPath[0].name)
       return await axios.post('/post/save-post', {
          description,
          postedPicturesPath,
