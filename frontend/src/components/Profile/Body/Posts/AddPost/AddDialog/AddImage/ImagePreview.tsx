d628f30e3423916eb964719ab1bb1193a7764441
nagytommy76 <nagytommy76@gmail.com>
2023-04-07 16:34:41 +0200

    Separated the ImagePreview container

---
+++ frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/AddImage/ImagePreview.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { StyledImagePreview, StyledImagePreviewContainer } from './Style'
+
+const ImagePreview: React.FC<{ selectedFilePreview: string[] }> = ({ selectedFilePreview }) => {
+   return (
+      <StyledImagePreviewContainer>
+         {selectedFilePreview.map((filePreview, index) => (
+            <StyledImagePreview key={index} width={550} height={200} alt={filePreview} src={filePreview} />
+         ))}
+      </StyledImagePreviewContainer>
+   )
+}
+
+export default ImagePreview
