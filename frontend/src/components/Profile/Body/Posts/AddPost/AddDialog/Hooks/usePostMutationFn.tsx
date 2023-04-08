d46f6e66cdaa610b525bff811a81f5144f8c29a9
nagytommy76 <nagytommy76@gmail.com>
2023-04-08 20:02:21 +0200

    Using the UploadToFirebase hook

--- frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/usePostMutationFn.tsx
+++ frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/usePostMutationFn.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { axiosInstance as axios, AxiosResponse } from '../../../../../../../utils/axiosSetup/AxiosInstance'
 
+import useUploadFirebase from './useUploadFirebase'
+
 const usePostMutationFn = (description: string, postedPicturesPath: FileList | null) => {
+   const handleImageUploadToFirebase = useUploadFirebase(postedPicturesPath as FileList)
+
    const handleAddPostSend = async (event: React.FormEvent): Promise<AxiosResponse> => {
       event.preventDefault()
-      if (postedPicturesPath !== null) console.log(postedPicturesPath[0].name)
+      if (postedPicturesPath !== null) {
+         await handleImageUploadToFirebase()
+         console.log(postedPicturesPath[0].name)
+      }
       return await axios.post('/post/save-post', {
          description,
          postedPicturesPath,
