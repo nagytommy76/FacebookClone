b5cc771defdbab76332b980a9c465cc5f8543f49
nagytommy76 <nagytommy76@gmail.com>
2023-04-09 12:30:55 +0200

    Checking the image is added

--- frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/usePostMutationFn.tsx
+++ frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/usePostMutationFn.tsx
@@ -4,17 +4,17 @@ import { axiosInstance as axios, AxiosResponse } from '../../../../../../../util
 import useUploadFirebase from './useUploadFirebase'
 
 const usePostMutationFn = (description: string, postedPicturesPath: FileList | null) => {
-   const handleImageUploadToFirebase = useUploadFirebase(postedPicturesPath as FileList)
-
    const handleAddPostSend = async (event: React.FormEvent): Promise<AxiosResponse> => {
       event.preventDefault()
-      if (postedPicturesPath !== null) {
-         await handleImageUploadToFirebase()
-         console.log(postedPicturesPath[0].name)
+      let uploadedImagesPaths = null
+      if (postedPicturesPath) {
+         const handleMultipleImageUploadToFirebase = useUploadFirebase(postedPicturesPath)
+         uploadedImagesPaths = await handleMultipleImageUploadToFirebase()
       }
+
       return await axios.post('/post/save-post', {
          description,
-         postedPicturesPath,
+         postedPicturesPath: uploadedImagesPaths,
       })
    }
    return handleAddPostSend
