5dbddd9f79ed2f8d18c04f714a6d1845f324ee64
nagytommy76 <nagytommy76@gmail.com>
2023-04-09 11:41:49 +0200

    Uploading multiple images and sending back the path as string[]

--- frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/useUploadFirebase.tsx
+++ frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/useUploadFirebase.tsx
@@ -1,4 +1,4 @@
-import { ref, uploadBytes } from 'firebase/storage'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 import { firebaseStorage } from '../../../../../../../utils/firebase/firebase'
 import { v4 } from 'uuid'
 
@@ -6,16 +6,30 @@ import { useAppSelector } from '../../../../../../../utils/redux/store'
 
 const useUploadFirebase = (uploadImage: FileList) => {
    const userId = useAppSelector((state) => state.auth.userId)
-   const handleImageUploadToFirebase = async () => {
+
+   const handleMultipleImageUploadToFirebase = async () => {
+      const uploadedImagesArray: string[] = []
+      for (let index = 0; index < uploadImage.length; index++) {
+         const singleImageFile = uploadImage[index]
+         const imagesPath = await handleSingleImageUploadToFirebase(singleImageFile)
+         console.log(imagesPath)
+         uploadedImagesArray.push(imagesPath as string)
+      }
+      return uploadedImagesArray
+   }
+
+   const handleSingleImageUploadToFirebase = async (singleImageFile: File) => {
       try {
-         const imageReference = ref(firebaseStorage, `${userId}/${uploadImage[0].name + v4()}`)
-         const imageResult = await uploadBytes(imageReference, uploadImage[0])
-         console.log(imageResult)
+         const imageReference = ref(firebaseStorage, `${userId}/${v4()}_${singleImageFile.name}`)
+         await uploadBytes(imageReference, singleImageFile)
+         const currentImageLink = await getDownloadURL(imageReference)
+
+         return currentImageLink
       } catch (error) {
          console.log(error)
       }
    }
-   return handleImageUploadToFirebase
+   return handleMultipleImageUploadToFirebase
 }
 
 export default useUploadFirebase
