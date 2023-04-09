dff63168199bf1555479d2e88f9246261848a1e2
nagytommy76 <nagytommy76@gmail.com>
2023-04-09 13:04:46 +0200

    Check and generate new accessToken & added to the user route

--- backend/api/user/user.ts
+++ backend/api/user/user.ts
@@ -2,7 +2,11 @@ import { Router } from 'express'
 import { ValidateRegister, ValidateLogin } from './validators/authValidator'
 import { authValidationMiddleware } from '../../middlewares/authValidationMiddleware'
 
-import { checkRefreshTokenValidityController, logoutUserController } from '../../controllers/users/users'
+import {
+   checkRefreshTokenValidityController,
+   checkAccessTokenValidityController,
+   logoutUserController,
+} from '../../controllers/users/users'
 import { loginUserController } from '../../controllers/users/login'
 import { registerUserController } from '../../controllers/users/register'
 
@@ -13,5 +17,6 @@ router.post('/login', ValidateLogin, authValidationMiddleware, loginUserControll
 router.post('/logout', logoutUserController)
 
 router.post('/refresh-token', checkRefreshTokenValidityController)
+router.post('/generate-access-token', checkAccessTokenValidityController)
 
 module.exports = router
--- backend/controllers/users/users.ts
+++ backend/controllers/users/users.ts
@@ -1,6 +1,7 @@
 import { Response, Request } from 'express'
 import jwt from 'jsonwebtoken'
-import { REFRESH_TOKEN_SECRET } from '../../config/endpoints.config'
+import { REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET } from '../../config/endpoints.config'
+import { ACCESS_TOKEN_EXPIRES_IN_MILLISEC } from './helpers/signtTokens'
 
 export const checkRefreshTokenValidityController = (req: Request, res: Response) => {
    // Ide a refresh token kell
@@ -15,6 +16,31 @@ export const checkRefreshTokenValidityController = (req: Request, res: Response)
    } catch (error) {
       res.status(500).json(error)
    }
+}
+// Ezt fogom elérni majd az axios interceptorból
+export const checkAccessTokenValidityController = (req: Request, res: Response) => {
+   const accessToken = req.cookies?.accessToken as string | undefined
+   if (!accessToken) return res.sendStatus(401)
+   try {
+      jwt.verify(accessToken, ACCESS_TOKEN_SECRET, (error, decoded: any) => {
+         if (error) return res.status(403).json({ errorMessage: 'accessToken expired' })
+         const newAccessToken = jwt.sign(
+            {
+               userId: decoded.userId,
+               email: decoded.email,
+               iat: decoded.iat,
+               exp: decoded.exp,
+            },
+            ACCESS_TOKEN_SECRET
+         )
+         res.cookie('accessToken', newAccessToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: ACCESS_TOKEN_EXPIRES_IN_MILLISEC,
+         }).sendStatus(201)
+      })
+   } catch (error) {}
 }
 
 export const logoutUserController = async (req: Request, res: Response) => {
