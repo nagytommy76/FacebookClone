6244d50d35a1973ca7a58f62c25085ce756dd0c0
nagytommy76 <nagytommy76@gmail.com>
2023-04-11 19:38:05 +0200

    Created the save post functionality

--- backend/controllers/posts/savePost.ts
+++ backend/controllers/posts/savePost.ts
@@ -6,8 +6,18 @@ import { IPostRequest } from './PostTypes'
 /**
  * a userId az accessToken decoded fog jönni
  */
-export const savePostController = (req: IPostRequest, res: Response) => {
-   console.log(req.user)
+export const savePostController = async (req: IPostRequest, res: Response) => {
    const { description, postedPicturesPath } = req.body
-   res.status(200).json({ description, postedPicturesPath })
+
+   try {
+      const createdPost = await PostModel.create({
+         userId: req.user?.userId,
+         description,
+         postedPicturesPath,
+      })
+
+      res.status(201).json({ createdPost })
+   } catch (error) {
+      res.status(500).json(error)
+   }
 }
