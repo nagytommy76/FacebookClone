9b501c2612388824f7b1604c4073d6825904772f
nagytommy76 <nagytommy76@gmail.com>
2023-04-11 18:33:02 +0200

    Finished the axios interceptor functionality

--- frontend/src/utils/axiosSetup/AxiosInstance.tsx
+++ frontend/src/utils/axiosSetup/AxiosInstance.tsx
@@ -23,20 +23,18 @@ const AxiosSetupProvider: React.FC<{ children: React.ReactElement }> = ({ childr
    const logout = useLogout()
 
    useEffect(() => {
-      console.count('UseEffect: ')
       axiosInstance.interceptors.response.use(
          (response) => {
             return response
          },
          async (error) => {
             // Ebben az esetben nincs accessToken a cookie-ban, ezért kell egy új -->
-            console.log(error.response.data.errorMessage)
             if (
                error.response.status === 404 &&
                error.response.data.errorMessage === 'refreshToken not found'
             ) {
-               console.log('Nincs refresh token, kilépés')
-               return await logout()
+               await logout()
+               return await Promise.reject(error)
             }
             if (
                error.config &&
@@ -48,10 +46,8 @@ const AxiosSetupProvider: React.FC<{ children: React.ReactElement }> = ({ childr
                // Ekkor kell egy új accessToken (Forbidden) / 403 error, tehát lejárt az accessToken
                try {
                   await axiosInstance.post('/auth/generate-access-token')
-                  console.count('lefut???::::: ')
                   return axios.request(error.config)
                } catch (error) {
-                  console.count('sima catch ág: ')
                   // ha itt hiba van lejárt a refreshToken
                   if (
                      isAxiosError(error) &&
@@ -59,15 +55,11 @@ const AxiosSetupProvider: React.FC<{ children: React.ReactElement }> = ({ childr
                      error.response?.data.errorMessage === 'refreshToken expired'
                   ) {
                      // Ki kell léptetni a usert
-                     console.count('lefutok, alkalommal: ')
                      await logout()
+                     return await Promise.reject(error)
                   }
                }
             }
-            // else {
-            //    console.log('Else logout fut leeeee')
-            //    await logout()
-            // }
          }
       )
    }, [])
