4e3512683049efb5fc258413f803611b289f40bd
nagytommy76 <nagytommy76@gmail.com>
2023-04-13 19:45:25 +0200

    Added an OnSuccess fn

--- frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/usePostMutate.tsx
+++ frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/usePostMutate.tsx
@@ -1,18 +1,20 @@
 import { useMutation } from '@tanstack/react-query'
 import usePostMutationFn from './usePostMutationFn'
 
-const usePostMutate = (description: string, postedPicturesPath: FileList | null) => {
+const usePostMutate = (description: string, postedPicturesPath: FileList | null, handleClose: () => void) => {
    const handlePostSend = usePostMutationFn(description, postedPicturesPath)
 
-   const { mutate, isLoading, data } = useMutation({
+   const { mutate, isLoading } = useMutation({
       mutationKey: ['post'],
       mutationFn: handlePostSend,
+      onSuccess: async () => {
+         handleClose()
+      },
    })
 
    return {
       postMutate: mutate,
       isLoading,
-      data,
    }
 }
 
