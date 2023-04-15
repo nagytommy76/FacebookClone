60bb535bace70b7d79f897df947706df872755b6
nagytommy76 <nagytommy76@gmail.com>
2023-04-15 15:13:16 +0200

    Added a handleSnackOpenIfSuccess fn to open the snackbar

--- frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/usePostMutate.tsx
+++ frontend/src/components/Profile/Body/Posts/AddPost/AddDialog/Hooks/usePostMutate.tsx
@@ -1,14 +1,22 @@
 import { useMutation } from '@tanstack/react-query'
 import usePostMutationFn from './usePostMutationFn'
 
-const usePostMutate = (description: string, postedPicturesPath: FileList | null, handleClose: () => void) => {
+const usePostMutate = (
+   description: string,
+   postedPicturesPath: FileList | null,
+   handleClose: () => void,
+   handleSnackOpenIfSuccess: () => void
+) => {
    const handlePostSend = usePostMutationFn(description, postedPicturesPath)
 
    const { mutate, isLoading } = useMutation({
       mutationKey: ['post'],
       mutationFn: handlePostSend,
       onSuccess: async () => {
-         handleClose()
+         handleSnackOpenIfSuccess()
+         setTimeout(() => {
+            handleClose()
+         }, 5000)
       },
    })
 
