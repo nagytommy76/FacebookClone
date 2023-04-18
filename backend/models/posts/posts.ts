c3b4ecb45de8c9ecac52bfc506b8631f9a31b76b
nagytommy76 <nagytommy76@gmail.com>
2023-04-18 19:49:34 +0200

    Added timestamps both user/post schema

--- backend/models/posts/posts.ts
+++ backend/models/posts/posts.ts
@@ -17,25 +17,27 @@ const likes = {
    required: false,
 }
 
-const PostsSchema = new Schema({
-   userId: { type: Schema.Types.ObjectId, ref: 'User' },
-   description: String,
-   postedPicturesPath: { type: [String], required: false },
-   postedAt: { type: Date, default: Date.now() },
-   likes,
-   comments: {
-      type: [
-         {
-            userId: { type: Schema.Types.ObjectId, ref: 'User' },
-            answer: { type: String, required: true },
-            answeredAt: { type: Date, required: true },
-            parentCommentId: { type: String, required: false, default: null },
-            commentDepth: { type: Number, required: true, default: 1 },
-            likes,
-         },
-      ],
-      required: false,
+const PostsSchema = new Schema(
+   {
+      userId: { type: Schema.Types.ObjectId, ref: 'User' },
+      description: String,
+      postedPicturesPath: { type: [String], required: false },
+      likes,
+      comments: {
+         type: [
+            {
+               userId: { type: Schema.Types.ObjectId, ref: 'User' },
+               answer: { type: String, required: true },
+               answeredAt: { type: Date, required: true },
+               parentCommentId: { type: String, required: false, default: null },
+               commentDepth: { type: Number, required: true, default: 1 },
+               likes,
+            },
+         ],
+         required: false,
+      },
    },
-})
+   { timestamps: true }
+)
 
 export const Posts = model('Posts', PostsSchema)
--- backend/models/user/user.ts
+++ backend/models/user/user.ts
@@ -2,40 +2,43 @@ import { Schema, model } from 'mongoose'
 import type { UserModel, IUserTypes } from '../../controllers/users/Types'
 import { UserStatics } from './statics'
 
-const UserSchema = new Schema<IUserTypes, UserModel>({
-   firstName: String,
-   sureName: String,
-   isEmailConfirmed: {
-      type: Boolean,
-      default: false,
-   },
-   email: {
-      type: String,
-      required: [true, 'Kérek egy email címet!'],
-      unique: true,
-   },
-   password: {
-      type: String,
-      required: [true, 'Adjon meg egy jelszót!'],
-      minlength: [4, 'a jelszó min. 4 karakter legyen!'],
-   },
-   friends: { type: [{ userId: Schema.Types.ObjectId }] },
-   userDetails: {
-      profilePicturePath: String,
-      birthTown: String,
-      homeTown: String,
-      relationShip: { type: { isAlone: Boolean, inRelation: Boolean }, required: false },
-      gender: {
+const UserSchema = new Schema<IUserTypes, UserModel>(
+   {
+      firstName: String,
+      sureName: String,
+      isEmailConfirmed: {
+         type: Boolean,
+         default: false,
+      },
+      email: {
          type: String,
-         required: true,
+         required: [true, 'Kérek egy email címet!'],
+         unique: true,
       },
-      dateOfBirth: {
-         type: Date,
-         required: true,
+      password: {
+         type: String,
+         required: [true, 'Adjon meg egy jelszót!'],
+         minlength: [4, 'a jelszó min. 4 karakter legyen!'],
       },
+      friends: { type: [{ userId: Schema.Types.ObjectId }] },
+      userDetails: {
+         profilePicturePath: String,
+         birthTown: String,
+         homeTown: String,
+         relationShip: { type: { isAlone: Boolean, inRelation: Boolean }, required: false },
+         gender: {
+            type: String,
+            required: true,
+         },
+         dateOfBirth: {
+            type: Date,
+            required: true,
+         },
+      },
+      posts: { type: Schema.Types.ObjectId, ref: 'Posts' },
    },
-   posts: { type: Schema.Types.ObjectId, ref: 'Posts' },
-})
+   { timestamps: true }
+)
 
 UserStatics(UserSchema)
 
