86a8b9cdebff8c70ec077470dac8544d21867439
nagytommy76 <nagytommy76@gmail.com>
2023-04-22 15:52:24 +0200

    Created a state for the like types

--- frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Like/Like.tsx
+++ frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Like/Like.tsx
@@ -1,27 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Button from '@mui/material/Button'
-import Tooltip from '@mui/material/Tooltip'
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt'
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
-import { FooterSectionStyle, ButtonGroupStyle, CustomTooltipTitle } from '../Styles'
+import { CustomTooltipTitle, ButtonGroupStyle } from './Styles'
 
 import Reactions from './Reactions'
+import type { LikeTypes } from '../../Types'
 
 const Like = () => {
+   const [like, setLike] = useState<LikeTypes | undefined>(undefined)
+   const handleSetLike = (likeType: LikeTypes) => {
+      // Ide majd egy usemutation jön, illetve controlled close a tooltipre
+      // A gombot pedig a like típusa szerint animálni!
+      console.log(likeType)
+      setLike(likeType)
+   }
+
    return (
-      <FooterSectionStyle>
-         <ButtonGroupStyle>
-            <CustomTooltipTitle open={true} placement='top' enterDelay={500} title={<Reactions />}>
-               <Button fullWidth startIcon={<ThumbUpOffAltIcon />}>
-                  Like
-               </Button>
-            </CustomTooltipTitle>
-            <Button fullWidth startIcon={<ChatBubbleOutlineIcon />}>
-               Hozzászólás
+      <ButtonGroupStyle>
+         <CustomTooltipTitle placement='top' title={<Reactions setLike={handleSetLike} />}>
+            <Button fullWidth startIcon={<ThumbUpOffAltIcon />}>
+               Like
             </Button>
-         </ButtonGroupStyle>
-      </FooterSectionStyle>
+         </CustomTooltipTitle>
+         <Button fullWidth startIcon={<ChatBubbleOutlineIcon />}>
+            Hozzászólás
+         </Button>
+      </ButtonGroupStyle>
    )
 }
 
--- frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Styles.ts
+++ frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Styles.ts
@@ -1,8 +1,6 @@
 import { styled } from '@mui/material'
 import Image from 'next/image'
 
-import Tooltip, { tooltipClasses, TooltipProps } from '@mui/material/Tooltip'
-
 export const StyledImageGridContainer = styled('section')({
    width: '100%',
    display: 'grid',
@@ -33,20 +31,4 @@ export const StyledImage = styled(Image)({
 export const FooterSectionStyle = styled('footer')({
    width: '100%',
    padding: '1rem 1rem',
-})
-
-export const ButtonGroupStyle = styled('div')({
-   display: 'flex',
-   flexDirection: 'row',
-   gap: '2px',
-})
-
-// LIke section
-export const CustomTooltipTitle = styled(Tooltip)({
-   backgroundColor: 'transparent',
-   padding: 0,
-   [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: 'transparent',
-      padding: 0,
-   },
 })
