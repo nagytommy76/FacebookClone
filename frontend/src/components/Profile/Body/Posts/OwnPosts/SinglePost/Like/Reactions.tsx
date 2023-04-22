8e3b352a324761e7b02f523e1cedbb52c8b53bb9
nagytommy76 <nagytommy76@gmail.com>
2023-04-22 15:51:48 +0200

    Added a tooltip and an onclick listener to the image

--- frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Like/Reactions.tsx
+++ frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Like/Reactions.tsx
@@ -1,25 +1,46 @@
-import Image from 'next/image'
-import Paper from '@mui/material/Paper'
+import { StyledPaperContainer, StyledImage } from './Styles'
+import Tooltip from '@mui/material/Tooltip'
 
-import LikeSVG from '../../../../../../../assets/like.svg'
+import LikeAnimated from '../../../../../../../assets/likeAnimated.svg'
 import Love from '../../../../../../../assets/love.svg'
 import Care from '../../../../../../../assets/care.svg'
 import Haha from '../../../../../../../assets/haha.svg'
 import Wow from '../../../../../../../assets/wow.svg'
 import Sad from '../../../../../../../assets/sad.svg'
 import Angry from '../../../../../../../assets/angry.svg'
+import { LikeTypes } from '../../Types'
 
-const Reactions = () => {
+const Reactions: React.FC<{ setLike: (likeType: LikeTypes) => void }> = ({ setLike }) => {
    return (
-      <Paper>
-         <Image src={LikeSVG} alt='like' width={40} height={40} />
-         <Image src={Love} alt='like' width={40} height={40} />
-         <Image src={Care} alt='like' width={40} height={40} />
-         <Image src={Haha} alt='like' width={40} height={40} />
-         <Image src={Wow} alt='like' width={40} height={40} />
-         <Image src={Sad} alt='like' width={40} height={40} />
-         <Image src={Angry} alt='like' width={40} height={40} />
-      </Paper>
+      <StyledPaperContainer>
+         <Tooltip title='Tetszik' placement='top' enterDelay={50}>
+            <StyledImage
+               onClick={() => setLike('isLike')}
+               src={LikeAnimated}
+               alt='like'
+               width={40}
+               height={40}
+            />
+         </Tooltip>
+         <Tooltip title='Imádom' placement='top' enterDelay={50}>
+            <StyledImage onClick={() => setLike('isLove')} src={Love} alt='love' width={40} height={40} />
+         </Tooltip>
+         <Tooltip title='Ölelés' placement='top' enterDelay={50}>
+            <StyledImage onClick={() => setLike('isCare')} src={Care} alt='care' width={40} height={40} />
+         </Tooltip>
+         <Tooltip title='Vicces' placement='top' enterDelay={50}>
+            <StyledImage onClick={() => setLike('isHaha')} src={Haha} alt='haha' width={40} height={40} />
+         </Tooltip>
+         <Tooltip title='Hűha' placement='top' enterDelay={50}>
+            <StyledImage onClick={() => setLike('isWow')} src={Wow} alt='wow' width={40} height={40} />
+         </Tooltip>
+         <Tooltip title='Szomorú' placement='top' enterDelay={50}>
+            <StyledImage onClick={() => setLike('isSad')} src={Sad} alt='sad' width={40} height={40} />
+         </Tooltip>
+         <Tooltip title='Dühítő' placement='top' enterDelay={50}>
+            <StyledImage onClick={() => setLike('isAngry')} src={Angry} alt='angry' width={40} height={40} />
+         </Tooltip>
+      </StyledPaperContainer>
    )
 }
 
