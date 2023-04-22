77b22ddac2adfb2012340eca08e47c9f73108842
nagytommy76 <nagytommy76@gmail.com>
2023-04-22 15:49:19 +0200

    Separated the image container

---
+++ frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Includes/ImageContainer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyledImageGridContainer, StyledImage, FirstGridImage } from '../Styles'
+import { IOwnPost } from '../../Types'
+
+const ImageContainer: React.FC<{ singlePost: IOwnPost }> = ({ singlePost }) => {
+   return (
+      <StyledImageGridContainer>
+         {singlePost.postedPicturesPath !== null &&
+            singlePost.postedPicturesPath.map((image, index) =>
+               index === 0 ? (
+                  <FirstGridImage key={index} src={image} alt='Kép' width={500} height={500} />
+               ) : (
+                  <StyledImage key={index} src={image} alt='Kép' width={500} height={500} />
+               )
+            )}
+      </StyledImageGridContainer>
+   )
+}
+
+export default ImageContainer
