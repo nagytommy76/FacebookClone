4ca3bfdfd5edb3a9734a80aafad74fd68e8c9205
nagytommy76 <nagytommy76@gmail.com>
2023-04-23 13:26:14 +0200

    using the 2 custom hooks

--- frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Like/Like.tsx
+++ frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Like/Like.tsx
@@ -1,30 +1,52 @@
 import React, { useState } from 'react'
+import useLikeMutate from './Hooks/useLikeMutate'
+import useButtonColor from './Hooks/useButtonColor'
 
 import Button from '@mui/material/Button'
-import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt'
+
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
 import { CustomTooltipTitle, ButtonGroupStyle } from './Styles'
 
 import Reactions from './Reactions'
 import type { LikeTypes } from '../../Types'
 
-const Like = () => {
+const Like: React.FC<{ postId: string }> = ({ postId }) => {
+   const { likeBtnIcon, likeButtonColor, likeBtnText, setButtonColor } = useButtonColor()
+   const { mutatePostLike } = useLikeMutate()
    const [like, setLike] = useState<LikeTypes | undefined>(undefined)
+
+   const handleLikeBtnClick = () => {
+      if (like === undefined) handleSetLike('isLike')
+      else handleUnsetLike()
+   }
+
    const handleSetLike = (likeType: LikeTypes) => {
       // Ide majd egy usemutation jön, illetve controlled close a tooltipre
       // A gombot pedig a like típusa szerint animálni!
-      console.log(likeType)
+      setButtonColor(likeType)
       setLike(likeType)
+      mutatePostLike({ likeTypeFomInput: likeType, postId })
+   }
+
+   const handleUnsetLike = () => {
+      // ide egy remove post like mutate kell
+      setButtonColor(undefined)
+      setLike(undefined)
    }
 
    return (
       <ButtonGroupStyle>
          <CustomTooltipTitle placement='top' title={<Reactions setLike={handleSetLike} />}>
-            <Button fullWidth startIcon={<ThumbUpOffAltIcon />}>
-               Like
+            <Button
+               sx={{ color: likeButtonColor, textTransform: 'none' }}
+               disableRipple
+               onClick={handleLikeBtnClick}
+               fullWidth
+               startIcon={likeBtnIcon}>
+               {likeBtnText}
             </Button>
          </CustomTooltipTitle>
-         <Button fullWidth startIcon={<ChatBubbleOutlineIcon />}>
+         <Button disableRipple fullWidth startIcon={<ChatBubbleOutlineIcon />}>
             Hozzászólás
          </Button>
       </ButtonGroupStyle>
