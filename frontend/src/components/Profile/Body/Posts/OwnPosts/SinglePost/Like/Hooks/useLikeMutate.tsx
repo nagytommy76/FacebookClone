2b19b8ef34b9589f3fad7f70fc17ce83e6138e68
nagytommy76 <nagytommy76@gmail.com>
2023-04-23 13:24:17 +0200

    Created a custom hook for sending the like

---
+++ frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Like/Hooks/useLikeMutate.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { axiosInstance as axios } from '../../../../../../../../utils/axiosSetup/AxiosInstance'
+import { useMutation } from '@tanstack/react-query'
+import type { LikeTypes } from '../../../Types'
+
+interface ISendLike {
+   likeTypeFomInput: LikeTypes
+   postId: string
+}
+
+const useLikeMutate = () => {
+   const handleSendLike = async ({ likeTypeFomInput, postId }: ISendLike) => {
+      try {
+         return await axios.post('/post/post-like', { reactionType: likeTypeFomInput, postId })
+      } catch (error) {
+         console.log(error)
+      }
+   }
+
+   const { mutate } = useMutation({
+      mutationKey: ['likePost'],
+      mutationFn: handleSendLike,
+   })
+   return { mutatePostLike: mutate }
+}
+
+export default useLikeMutate
