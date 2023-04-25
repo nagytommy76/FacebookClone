6393a16c5d240892802e0962f71776d03b07b8d3
nagytommy76 <nagytommy76@gmail.com>
2023-04-25 19:59:10 +0200

    Saving the likes

--- backend/controllers/posts/likePost.ts
+++ backend/controllers/posts/likePost.ts
@@ -5,16 +5,36 @@ import type { IPostLikeRequest } from './PostTypes'
 export const likePostController = async (request: IPostLikeRequest, response: Response) => {
    const { postId, reactionType } = request.body
    const userId = request.user?.userId as any
-   //    if(!userId) return response.status(404)
+
    try {
       const foundPostToModifyLike = await PostModel.findById(postId)
       if (!foundPostToModifyLike) return response.status(404).json({ msg: 'nincs ilyen poszt' })
-      foundPostToModifyLike.likes.push({ reactionType, userId })
+
+      const userLike = foundPostToModifyLike.likes.find(
+         (like) => like.userId.toString() === userId.toString()
+      )
+      if (userLike) {
+         userLike.reactionType[reactionType] = true
+      } else {
+         foundPostToModifyLike.likes.push({
+            userId,
+            reactionType: {
+               isLike: false,
+               isAngry: false,
+               isCare: false,
+               isHaha: false,
+               isLove: false,
+               isSad: false,
+               isWow: false,
+               [reactionType]: true,
+            },
+         })
+      }
 
       response
          .status(200)
          .json({ msg: foundPostToModifyLike, ember: request.user, mit: request.body.reactionType })
    } catch (error) {
-      response.status(500)
+      response.status(500).json({ msg: 'Internal server error' })
    }
 }
