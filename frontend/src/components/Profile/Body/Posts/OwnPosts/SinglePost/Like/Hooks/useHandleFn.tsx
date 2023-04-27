67a4a99d5612fa1abc0fc806eff6b667d741d06c
nagytommy76 <nagytommy76@gmail.com>
2023-04-27 19:45:45 +0200

    Separated the handle click and send functions

---
+++ frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Like/Hooks/useHandleFn.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import useLikeMutate from './useLikeMutate'
+import type { LikeTypes } from '../../../Types'
+
+const useHandleFn = (setButtonColor: (currentLikeType: LikeTypes | undefined) => void, postId: string) => {
+   const { mutatePostLike } = useLikeMutate()
+   const [like, setLike] = useState<LikeTypes | undefined>(undefined)
+
+   const handleUnsetLike = () => {
+      // ide egy remove post like mutate kell
+      setButtonColor(undefined)
+      setLike(undefined)
+   }
+   const handleLikeBtnClick = () => {
+      if (like === undefined) handleSendPostLike('isLike')
+      else handleUnsetLike()
+   }
+
+   const handleSetLikeAndButtonColor = (likeType: LikeTypes) => {
+      // Ide majd egy usemutation jön, illetve controlled close a tooltipre
+      // A gombot pedig a like típusa szerint animálni!
+      setButtonColor(likeType)
+      setLike(likeType)
+   }
+
+   const handleSendPostLike = (likeType: LikeTypes) => {
+      handleSetLikeAndButtonColor(likeType)
+      mutatePostLike({ likeTypeFomInput: likeType, postId })
+   }
+
+   return {
+      handleLikeBtnClick,
+      handleSendPostLike,
+      handleSetLikeAndButtonColor,
+   }
+}
+
+export default useHandleFn
