19e79d553be1375ef33a87092f4fb180798386c0
nagytommy76 <nagytommy76@gmail.com>
2023-04-29 15:26:15 +0200

    Created a delete like custom hook

---
+++ frontend/src/components/Profile/Body/Posts/OwnPosts/SinglePost/Like/Hooks/useLikeDelete.tsx
@@ -0,0 +1,16 @@
+import { useMutation } from '@tanstack/react-query'
+import { axiosInstance as axios } from '../../../../../../../../utils/axiosSetup/AxiosInstance'
+
+const deleteLikeFn = async (postId: string) => {
+   return await axios.delete('/post/post-like-delete', { data: { postId } })
+}
+
+const useLikeDelete = () => {
+   const { mutate } = useMutation({
+      mutationKey: ['deleteLike'],
+      mutationFn: deleteLikeFn,
+   })
+   return { deleteMutation: mutate }
+}
+
+export default useLikeDelete
