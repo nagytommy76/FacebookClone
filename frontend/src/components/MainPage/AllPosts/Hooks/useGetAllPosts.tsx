98ad99f4e8eaa3d2d7ae805fcad894e67f2540b9
nagytommy76 <nagytommy76@gmail.com>
2023-04-30 10:50:13 +0200

    Created a getAllPosts custom hook

---
+++ frontend/src/components/MainPage/AllPosts/Hooks/useGetAllPosts.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query'
+import { axiosInstance as axios, AxiosResponse } from '../../../../utils/axiosSetup/AxiosInstance'
+import { IPost } from '../../../Profile/Body/Posts/OwnPosts/Types'
+
+const fetchAllQueries = async () => {
+   try {
+      const response = await axios.get('/post/get-posts')
+      return response as AxiosResponse<{ allPosts: IPost[] }>
+   } catch (error) {
+      console.log(error)
+   }
+}
+
+const useGetAllPosts = () => {
+   const { data, isLoading } = useQuery({ queryKey: ['todos'], queryFn: fetchAllQueries })
+   return {
+      isLoading,
+      allPosts: data?.data.allPosts,
+   }
+}
+
+export default useGetAllPosts
