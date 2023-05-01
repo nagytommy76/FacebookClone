dd81db2cc0e98fea761ac76fde8dd84f5701247b
nagytommy76 <nagytommy76@gmail.com>
2023-05-01 12:09:00 +0200

    Using the postHeader component

--- frontend/src/components/Posts/SinglePost/SinglePost.tsx
+++ frontend/src/components/Posts/SinglePost/SinglePost.tsx
@@ -1,27 +1,26 @@
 import React from 'react'
-import moment from 'moment'
+import type { IPost } from '../../Profile/Body/Posts/OwnPosts/Types'
 
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 import Divider from '@mui/material/Divider'
-import type { IOwnPost } from '../../Profile/Body/Posts/OwnPosts/Types'
-import { FooterSectionStyle } from './Styles'
+import { FooterSectionStyle, BodyDescriptionSection } from './Styles'
 
+import PostHeader from './Includes/PostHeader/PostHeader'
 import ImageContainer from './Includes/ImageContainer'
 import Like from './Like/Like'
 
-import { PostHeaderStyle } from './Styles'
-
-const SinglePost: React.FC<{ singlePost: IOwnPost }> = ({ singlePost }) => {
+const SinglePost: React.FC<{ singlePost: IPost }> = ({ singlePost }) => {
    return (
       <Paper sx={{ margin: '1rem 0', pb: '1rem', minHeight: '100px' }}>
-         <PostHeaderStyle>
-            <Typography variant='h6'>
-               {singlePost.userId.firstName} {singlePost.userId.sureName}
-            </Typography>
-            <p>{moment(singlePost.createdAt).format('YYYY MMMM D dddd, kk:mm')}</p>
-            <Typography variant='body1'>{singlePost.description}</Typography>
-         </PostHeaderStyle>
+         <PostHeader
+            profilePicturePath={singlePost.userId.userDetails.profilePicturePath}
+            createdAt={singlePost.createdAt}
+            displayFullName={`${singlePost.userId.firstName} ${singlePost.userId.sureName}`}
+         />
+         <BodyDescriptionSection>
+            <Typography variant='subtitle1'>{singlePost.description}</Typography>
+         </BodyDescriptionSection>
          <ImageContainer singlePost={singlePost} />
          <FooterSectionStyle>
             <Like postLikes={singlePost.likes} postId={singlePost._id} />
