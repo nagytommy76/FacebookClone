0f2693c51eeae2f12c8078caf5e70c33befcc7e5
nagytommy76 <nagytommy76@gmail.com>
2023-05-03 19:32:15 +0200

    Created a Style file for the TooltipTitle component

---
+++ frontend/src/components/Posts/SinglePost/ProfileCard/Styles.ts
@@ -0,0 +1,31 @@
+import { styled } from '@mui/material'
+import Image from 'next/image'
+import Paper from '@mui/material/Paper'
+
+export const StyledPaperContainer = styled(Paper)({
+   minWidth: '370px',
+   minHeight: '150px',
+   display: 'flex',
+   alignItems: 'center',
+})
+
+export const StyledProfileImage = styled(Image)({
+   margin: '1rem',
+   width: 110,
+   height: 110,
+   borderRadius: '50%',
+   objectFit: 'cover',
+})
+
+export const StyledProfileBody = styled('div')({
+   height: '150px',
+   //    padding: '.7rem .7rem .7rem 0',
+   display: 'flex',
+   flexDirection: 'column',
+   justifyContent: 'space-evenly',
+})
+
+export const StyledIconParagraph = styled('p')({
+   fontWeight: 350,
+   fontSize: '16px',
+})
