d53237dcdc6bf13f3fd95c48a2baeaf352db8a47
nagytommy76 <nagytommy76@gmail.com>
2023-05-06 14:03:31 +0200

    Pushing the created post's id to user's posts

--- backend/controllers/posts/savePost.ts
+++ backend/controllers/posts/savePost.ts
@@ -1,5 +1,6 @@
-import { Response } from 'express'
+import { Response, response } from 'express'
 import { Posts as PostModel } from '../../models/posts/posts'
+import { User as UserModel } from '../../models/user/user'
 
 import { IPostRequest } from './PostTypes'
 
@@ -8,14 +9,18 @@ import { IPostRequest } from './PostTypes'
  */
 export const savePostController = async (req: IPostRequest, res: Response) => {
    const { description, postedPicturesPath } = req.body
-
+   const userId = req.user?.userId
+   if (!userId) return response.status(404).json({ msg: 'User not found' })
    try {
+      const postingUser = await UserModel.findById(userId)
       const createdPost = await PostModel.create({
-         userId: req.user?.userId,
+         userId: userId,
          description,
          postedPicturesPath,
       })
 
+      postingUser?.posts.push(createdPost._id)
+      await postingUser?.save()
       res.status(201).json({ createdPost })
    } catch (error) {
       res.status(500).json(error)
