6fbb5c55086079a1345b963d67879d7297be2c9f
nagytommy76 <nagytommy76@gmail.com>
2023-05-07 14:35:41 +0200

    Using the context provider

--- frontend/src/components/Profile/ProfileMe.tsx
+++ frontend/src/components/Profile/ProfileMe.tsx
@@ -1,26 +1,27 @@
 import { useState } from 'react'
 import dynamic from 'next/dynamic'
 import { StyledMeContainer } from './Styles/Style'
-import useGetUserData from './Hooks/useGetUserData'
 
+import ProfileContextProvider from './Context/ProfileContextProvider'
 import NavTabs from './Header/NavTabs/NavTabs'
 const ProfileHeader = dynamic(() => import('./Header/Header'))
 const Body = dynamic(() => import('./Body/Body'))
 
 const ProfileMe = () => {
-   const { userData } = useGetUserData()
    const [tabValue, setTabValue] = useState<number>(0)
    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
       setTabValue(newValue)
    }
 
    return (
-      <StyledMeContainer>
-         <ProfileHeader>
-            <NavTabs handleTabChange={handleTabChange} tabValue={tabValue} />
-         </ProfileHeader>
-         <Body tabValue={tabValue} />
-      </StyledMeContainer>
+      <ProfileContextProvider>
+         <StyledMeContainer>
+            <ProfileHeader>
+               <NavTabs handleTabChange={handleTabChange} tabValue={tabValue} />
+            </ProfileHeader>
+            <Body tabValue={tabValue} />
+         </StyledMeContainer>
+      </ProfileContextProvider>
    )
 }
 
