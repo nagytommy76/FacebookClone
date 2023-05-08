bbc87d9f6e2308b80005ba4054ef7bc6b8e57ffb
nagytommy76 <nagytommy76@gmail.com>
2023-05-08 19:40:30 +0200

    Populating the post's userId field

--- backend/controllers/users/userDetails.ts
+++ backend/controllers/users/userDetails.ts
@@ -6,9 +6,11 @@ export const getUserDetailsWithOwnPosts = async (request: IJWTUserType, response
    const userId = request.user?.userId
    if (!userId) return response.status(404).json({ msg: 'user not found' })
    try {
-      const foundUserWithPosts = await UserModel.find({ _id: userId }).populate('posts')
-      //   console.log(foundUserWithPosts?.populated('Posts'))
-      return response.status(200).json({ foundUserWithPosts })
+      const foundUserWithPosts = await UserModel.findById(userId).populate({
+         path: 'posts',
+         populate: { path: 'userId' },
+      })
+      return response.status(200).json(foundUserWithPosts)
    } catch (error) {
       response.status(500).json({ error, msg: 'internal server error' })
    }
