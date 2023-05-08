bcc5af4b46512b69743dc8370f2b4f5f30dae19e
nagytommy76 <nagytommy76@gmail.com>
2023-05-08 19:38:43 +0200

    Removed the useGetOwnPosts hook and replaced with the context

--- frontend/src/components/Profile/Body/Posts/OwnPosts/OwnPosts.tsx
+++ frontend/src/components/Profile/Body/Posts/OwnPosts/OwnPosts.tsx
@@ -1,5 +1,6 @@
+import { useContext } from 'react'
 import dynamic from 'next/dynamic'
-import useGetPosts from './Hooks/useGetPosts'
+import { ProfileContext } from '../../../Context/ProfileContextProvider'
 import type { IPost } from '../../../../Posts/Types'
 
 const SinglePostComponent = dynamic(() => import('../../../../Posts/SinglePost/SinglePost'), {
@@ -7,13 +8,15 @@ const SinglePostComponent = dynamic(() => import('../../../../Posts/SinglePost/S
 })
 
 const OwnPosts = () => {
-   const { data, isLoading } = useGetPosts()
-   if (isLoading) {
+   const { isDataLoading, userData } = useContext(ProfileContext)
+   if (isDataLoading) {
       return <h1>Ide majd egy Suspense component jön. TÖLTÉS</h1>
    }
+   userData.posts[0].postedPicturesPath
    return (
       <div>
-         {data && data.data.map((post: IPost) => <SinglePostComponent key={post._id} singlePost={post} />)}
+         {userData.posts &&
+            userData.posts.map((post: IPost) => <SinglePostComponent key={post._id} singlePost={post} />)}
       </div>
    )
 }
