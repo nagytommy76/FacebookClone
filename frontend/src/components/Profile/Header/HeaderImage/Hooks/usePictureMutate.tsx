29ddd9ab22ecae0f1e908dd14b5859ab708a150e
nagytommy76 <nagytommy76@gmail.com>
2023-05-13 15:49:37 +0200

    Created a mutation hook

---
+++ frontend/src/components/Profile/Header/HeaderImage/Hooks/usePictureMutate.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useMutation } from '@tanstack/react-query'
+import { axiosInstance as axios } from '../../../../../utils/axiosSetup/AxiosInstance'
+
+const usePictureMutate = () => {
+   const mutation = useMutation({
+      mutationFn: (profilePicturePath: string) => {
+         return axios.post('/user/save-profile-picture', { profilePicturePath })
+      },
+   })
+   return mutation.mutate
+}
+
+export default usePictureMutate
