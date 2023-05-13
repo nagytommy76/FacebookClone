b7f3086c673c4caad553a203fb7d485a92e38640
nagytommy76 <nagytommy76@gmail.com>
2023-05-13 15:51:08 +0200

    Uploading the picture to firebase & using the hooks

--- frontend/src/components/Profile/Header/HeaderImage/HeaderImage.tsx
+++ frontend/src/components/Profile/Header/HeaderImage/HeaderImage.tsx
@@ -1,28 +1,41 @@
-import React, { useState } from 'react'
-import { HeaderImage as HeaderImageStyle } from './Style'
+import React from 'react'
+import useUploadFirebase from '../../../Posts/AddPost/AddDialog/Hooks/useUploadFirebase'
+import useCheckPicture from './Hooks/useCheckPicture'
+import useModalControl from './Hooks/useModalControl'
+import usePictureMutate from './Hooks/usePictureMutate'
 
+import { HeaderImage as HeaderImageStyle } from './Style'
 import Dialog from '@mui/material/Dialog'
 import DialogTitle from '@mui/material/DialogTitle'
 import DialogContent from '@mui/material/DialogContent'
+import DialogActions from '@mui/material/DialogActions'
+import Button from '@mui/material/Button'
+import SendIcon from '@mui/icons-material/Send'
 
 import AddImage from '../../../Base/ImagePreview/AddImage'
 
 const HeaderImage: React.FC<{ profilePicturePath: string }> = ({ profilePicturePath }) => {
-   const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
-   const [uploadedPictures, setUploadedPictures] = useState<FileList | null>(null)
+   const { handleSingleImageUploadToFirebase } = useUploadFirebase()
+   const { isButtonDisabled, setUploadedPictures, uploadedPictures } = useCheckPicture()
+   const { handleCloseDialog, handleOpenDialog, isModalOpen } = useModalControl()
+   const mutationFunction = usePictureMutate()
 
-   const handleSetUploadPictures = (event: React.ChangeEvent<HTMLInputElement>) => {}
-   const handleOpenDialog = () => {
-      setIsModalOpen(true)
-   }
-   const handleCloseDialog = () => {
-      setIsModalOpen(false)
+   const handleSetUploadPictures = async () => {
+      if (!uploadedPictures) return
+      // Először feltöltöm az 1DB képet firebase/userId/profileImage mappába ha ez megvan ->
+      const uploadedPicturePath = await handleSingleImageUploadToFirebase(
+         uploadedPictures.item(0) as File,
+         'profilePicture'
+      )
+      console.log(uploadedPicturePath)
+      // Elküldöm az adatbázisnak a visszakapott url-t
+      mutationFunction(uploadedPicturePath as string)
    }
    return (
       <>
          <HeaderImageStyle
             onClick={handleOpenDialog}
-            src={profilePicturePath}
+            src={profilePicturePath || ''}
             alt='Profil kép'
             width={100}
             height={100}
@@ -30,8 +43,22 @@ const HeaderImage: React.FC<{ profilePicturePath: string }> = ({ profilePictureP
          <Dialog open={isModalOpen} onClose={handleCloseDialog}>
             <DialogTitle>Profilkép feltöltése</DialogTitle>
             <DialogContent>
-               <AddImage setUploadedPictures={setUploadedPictures} uploadedPictures={uploadedPictures} />
+               <AddImage
+                  multiple={false}
+                  setUploadedPictures={setUploadedPictures}
+                  uploadedPictures={uploadedPictures}
+               />
             </DialogContent>
+            <DialogActions>
+               <Button
+                  onClick={handleSetUploadPictures}
+                  disabled={isButtonDisabled}
+                  fullWidth
+                  variant='contained'
+                  endIcon={<SendIcon />}>
+                  Feltöltés
+               </Button>
+            </DialogActions>
          </Dialog>
       </>
    )
