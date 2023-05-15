0810690604e1183c529c9c514ce5497b8dd4a2e6
nagytommy76 <nagytommy76@gmail.com>
2023-05-15 19:54:21 +0200

    Created a state to change the curren profile pictures

--- frontend/src/components/Profile/Header/HeaderImage/Hooks/useGetCurrentPictures.tsx
+++ frontend/src/components/Profile/Header/HeaderImage/Hooks/useGetCurrentPictures.tsx
@@ -1,20 +1,24 @@
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance as axios } from '../../../../../utils/axiosSetup/AxiosInstance'
+import type { IProfilePicture } from '../../../../Posts/Types'
 
 const useGetCurrentPictures = () => {
-   const { data, isLoading } = useQuery({
+   const [currentProfilePictures, setCurrentProfilePictures] = useState<IProfilePicture[]>()
+
+   const { isLoading } = useQuery({
       queryKey: ['currentProfilePic'],
       queryFn: async () => {
          try {
             const response = await axios.get('/user/get-profile-pictures')
-            return response.data as { path: string; isSelected: boolean }[]
+            setCurrentProfilePictures(response.data as { _id: string; path: string; isSelected: boolean }[])
+            return response.data
          } catch (error) {
             return Promise.reject(new Error('some error'))
          }
       },
    })
-   return { currentProfilePictures: data, isLoading }
+   return { currentProfilePictures, isLoading, setCurrentProfilePictures }
 }
 
 export default useGetCurrentPictures
