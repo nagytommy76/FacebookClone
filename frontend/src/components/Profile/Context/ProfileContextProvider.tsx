494e488c724873898199cf3120e73708483b8bbf
nagytommy76 <nagytommy76@gmail.com>
2023-05-17 20:22:06 +0200

    Using the reducer & removed the functions

--- frontend/src/components/Profile/Context/ProfileContextProvider.tsx
+++ frontend/src/components/Profile/Context/ProfileContextProvider.tsx
@@ -1,83 +1,47 @@
-import React, { createContext, useState, useEffect } from 'react'
-import { IUserPopulatedPosts } from '../../Auth/AuthTypes'
+import React, { createContext, useState, useEffect, useReducer } from 'react'
 import useGetUserData from '../Hooks/useGetUserData'
+import UserDetailsReducer, { initialProfileState, InitialState, IBaseListAction } from './ProfileReducer'
 
 interface IProfileContext {
+   profileReducer: InitialState
+   profileDispatch: React.Dispatch<IBaseListAction>
    tabValue: number
    handleTabChange: (event: React.SyntheticEvent, newValue: number) => void
-   userData: IUserPopulatedPosts
-   setUserData: React.Dispatch<React.SetStateAction<IUserPopulatedPosts>>
    isDataLoading: boolean
-   getSelectedProfilePicture: () =>
-      | {
-           path: string
-           isSelected: boolean
-        }
-      | undefined
-}
-
-const userDataObject: IUserPopulatedPosts = {
-   _id: '',
-   createdAt: 0,
-   email: '',
-   firstName: '',
-   friends: [{ userId: '' }],
-   isEmailConfirmed: false,
-   password: '',
-   posts: [],
-   sureName: '',
-   updatedAt: 0,
-   userDetails: {
-      birthTown: '',
-      dateOfBirth: { day: 0, month: 0, year: 0 },
-      gender: 'female',
-      homeTown: '',
-      profilePicturePath: [{ isSelected: false, path: '' }],
-      relationShip: { inRelation: false, isAlone: true },
-      studies: {
-         elementary: { from: 2000, name: '', to: 2000 },
-         highSchool: { from: 2000, name: '', to: 2000 },
-         university: { from: 2000, name: '', to: 2000 },
-      },
-      workPlaces: [{ city: '', companyName: '', from: 2000, post: '', to: 2000 }],
-   },
 }
 
 export const ProfileContext = createContext<IProfileContext>({
-   userData: userDataObject,
-   setUserData: () => {},
+   profileReducer: initialProfileState,
+   profileDispatch: () => {},
    handleTabChange: () => {},
    tabValue: 0,
    isDataLoading: true,
-   getSelectedProfilePicture: () => {
-      return { isSelected: false, path: '' }
-   },
 })
 
 const ProfileContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+   const [profileReducer, profileDispatch] = useReducer(UserDetailsReducer, initialProfileState)
    const [tabValue, setTabValue] = useState<number>(0)
-   const [userData, setUserData] = useState<IUserPopulatedPosts>(userDataObject)
    const { data, isLoading } = useGetUserData()
 
-   const getSelectedProfilePicture = () => {
-      return userData.userDetails.profilePicturePath.find((image) => image.isSelected)
-   }
    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
       setTabValue(newValue)
    }
+
    useEffect(() => {
-      if (data) setUserData(data)
+      if (data) {
+         profileReducer.initialUserDataState = data
+      }
    }, [data])
 
    return (
       <ProfileContext.Provider
          value={{
-            userData,
-            setUserData,
+            profileDispatch,
+            profileReducer,
             handleTabChange,
             tabValue,
             isDataLoading: isLoading,
-            getSelectedProfilePicture,
+            // setUserProfilePicutres,
          }}>
          {children}
       </ProfileContext.Provider>
