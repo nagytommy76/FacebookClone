57834931c02bfd08800df2b6b8b5a2714d02d8a7
nagytommy76 <nagytommy76@gmail.com>
2023-05-20 14:43:10 +0200

    Temporary added a selector function to the PostHeader component

--- frontend/src/components/MainPage/AllPosts/AllPosts.tsx
+++ frontend/src/components/MainPage/AllPosts/AllPosts.tsx
@@ -1,20 +1,37 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 import useGetAllPosts from './Hooks/useGetAllPosts'
-import type { IPost } from '../../Posts/Types'
+import type { IPost, IProfilePicture } from '../../Posts/Types'
 
 const SinglePostComponent = dynamic(() => import('../../Posts/SinglePost/SinglePost'))
 const AddPostComponent = dynamic(() => import('../../Posts/AddPost/AddPost'))
+const PostHeader = dynamic(() => import('../../Posts/SinglePost/Includes/PostHeader/PostHeader'))
 
 const AllPosts = () => {
    const { allPosts, isLoading } = useGetAllPosts()
    if (isLoading) {
       return <h1>Egyelőre így, majd egy suspense conponens kell neki</h1>
    }
+
+   const getSelectedProfilePicture = (profilePictures: IProfilePicture[]) => {
+      return profilePictures.find((image) => image.isSelected)
+   }
+
    return (
       <>
          <AddPostComponent />
-         {allPosts && allPosts.map((post: IPost) => <SinglePostComponent key={post._id} singlePost={post} />)}
+         {allPosts &&
+            allPosts.map((post: IPost) => (
+               <SinglePostComponent key={post._id} singlePost={post}>
+                  <PostHeader
+                     createdAt={post.createdAt}
+                     userInfo={post.userId}
+                     selectSelectedProfilePicture={() =>
+                        getSelectedProfilePicture(post.userId.userDetails.profilePicturePath)
+                     }
+                  />
+               </SinglePostComponent>
+            ))}
       </>
    )
 }
