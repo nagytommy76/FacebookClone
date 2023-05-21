81653fadc011e8fcc94acbe555d6941b943395eb
nagytommy76 <nagytommy76@gmail.com>
2023-05-21 14:56:12 +0200

    Using the PostContext

--- frontend/src/components/MainPage/AllPosts/AllPosts.tsx
+++ frontend/src/components/MainPage/AllPosts/AllPosts.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import dynamic from 'next/dynamic'
-import useGetAllPosts from './Hooks/useGetAllPosts'
+import { PostsContext } from '../Context/PostsContextProvider'
+
 import type { IPost, IProfilePicture } from '../../Posts/Types'
 
 const SinglePostComponent = dynamic(() => import('../../Posts/SinglePost/SinglePost'))
@@ -8,20 +9,15 @@ const AddPostComponent = dynamic(() => import('../../Posts/AddPost/AddPost'))
 const PostHeader = dynamic(() => import('../../Posts/SinglePost/Includes/PostHeader/PostHeader'))
 
 const AllPosts = () => {
-   const { allPosts, isLoading } = useGetAllPosts()
-   if (isLoading) {
-      return <h1>Egyelőre így, majd egy suspense conponens kell neki</h1>
-   }
-
+   const { postsReducer } = useContext(PostsContext)
    const getSelectedProfilePicture = (profilePictures: IProfilePicture[]) => {
       return profilePictures.find((image) => image.isSelected)
    }
-
    return (
       <>
          <AddPostComponent />
-         {allPosts &&
-            allPosts.map((post: IPost) => (
+         {postsReducer.posts &&
+            postsReducer.posts.map((post: IPost) => (
                <SinglePostComponent key={post._id} singlePost={post}>
                   <PostHeader
                      createdAt={post.createdAt}
