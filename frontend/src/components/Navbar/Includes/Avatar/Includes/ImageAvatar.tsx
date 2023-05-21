ca9919d8312937e1a8bf50de7c8f9a3f770b0129
nagytommy76 <nagytommy76@gmail.com>
2023-05-21 11:26:55 +0200

    Querying the currentPic & set the redux state

--- frontend/src/components/Navbar/Includes/Avatar/Includes/ImageAvatar.tsx
+++ frontend/src/components/Navbar/Includes/Avatar/Includes/ImageAvatar.tsx
@@ -1,14 +1,38 @@
 import Image from 'next/image'
+import { useQuery } from '@tanstack/react-query'
+import { AxiosResponse, axiosInstance as axios } from '../../../../../utils/axiosSetup/AxiosInstance'
+import { useAppDispatch, useAppSelector } from '../../../../../utils/redux/store'
+import { setCurrentImage } from '../../../../../utils/redux/slices/AuthSlice'
+import type { IProfilePicture } from '../../../../Posts/Types'
 
 import Avatar from '@mui/material/Avatar'
-
-import OwnProfile from '../../../../../../public/sajat.jpg'
+import OwnProfile from '../../../../../assets/facebook-profile.jpg'
 
 const ImageAvatar: React.FC<{ displayText?: string }> = ({ displayText }) => {
+   const dispatch = useAppDispatch()
+
+   const { isLoading } = useQuery({
+      queryKey: ['getCurrentPicture'],
+      queryFn: async () => {
+         const response = (await axios.get('/user//get-current-picture')) as AxiosResponse<{
+            currentImage: IProfilePicture
+         }>
+         return response.data
+      },
+      onSuccess: (data) => {
+         dispatch(setCurrentImage(data.currentImage))
+      },
+   })
+   const currentPicture = useAppSelector((state) => state.auth.currentImage)
+
    return (
       <>
          <Avatar sx={{ bgcolor: 'orange', width: 45, height: 45 }}>
-            <Image width={45} height={45} alt='Profile picture' src={OwnProfile} />
+            {isLoading ? (
+               <Image width={45} height={45} alt='Profile picture' src={OwnProfile} />
+            ) : (
+               <Image width={45} height={45} alt='Profile picture' src={currentPicture.path} />
+            )}
          </Avatar>
          <p>{displayText}</p>
       </>
