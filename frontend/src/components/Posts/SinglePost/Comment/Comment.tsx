180bd2fb54885665d8007ae01ebb66dc45c35d8b
nagytommy76 <nagytommy76@gmail.com>
2023-05-22 18:14:00 +0200

    Using the styles & working on the text field's underline removal

--- frontend/src/components/Posts/SinglePost/Comment/Comment.tsx
+++ frontend/src/components/Posts/SinglePost/Comment/Comment.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
+import { StyledPaperContainer, StyledTextContainer, StyledTextInput } from './Styles'
 import Collapse from '@mui/material/Collapse'
+import TextField from '@mui/material/TextField'
 
 const Comment: React.FC<{ isCollapsed: boolean }> = ({ isCollapsed }) => {
    return (
       <>
-         <Collapse orientation='vertical' in={isCollapsed}>
-            <h1>Komment hozzáadása for jön ide</h1>
+         <Collapse orientation='vertical' in={true}>
+            <form role='presentation'>
+               <StyledPaperContainer>
+                  <StyledTextContainer>
+                     <StyledTextInput
+                        placeholder='Hozzászólás írása...'
+                        multiline
+                        variant='standard'
+                        fullWidth
+                     />
+                  </StyledTextContainer>
+               </StyledPaperContainer>
+            </form>
          </Collapse>
       </>
    )
