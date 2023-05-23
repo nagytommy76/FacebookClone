40d22e535c749ad639906d0a12a84ac9edce500b
nagytommy76 <nagytommy76@gmail.com>
2023-05-23 15:44:39 +0200

    Created a save post comment controller

---
+++ backend/controllers/posts/postComment.ts
@@ -0,0 +1,31 @@
+import { Response } from 'express'
+import type { IJWTUserType } from '../../middlewares/accessTokenRefresh'
+import { Posts as PostModel } from '../../models/posts/posts'
+
+export const savePostComment = async (request: ISavePostRequest, response: Response) => {
+   const userId = request.user?.userId
+   const { comment, postId } = request.body
+   if (!userId) return response.status(404).json({ msg: 'User not found' })
+   try {
+      const foundPost = await PostModel.findById(postId)
+      foundPost?.comments.push({
+         comment,
+         userId,
+         answeredAt: new Date(),
+         parentCommentId: null,
+         commentDepth: 1,
+         likes: [],
+      })
+      await foundPost?.save()
+      response.status(200).json({ comments: foundPost?.comments })
+   } catch (error) {
+      response.status(500).json({ error })
+   }
+}
+
+interface ISavePostRequest extends IJWTUserType {
+   body: {
+      postId: string
+      comment: string
+   }
+}
