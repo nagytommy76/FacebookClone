0e3784e8662a6f9fb7630832a4d4547844654994
nagytommy76 <nagytommy76@gmail.com>
2023-05-27 17:18:44 +0200

    Created a likeComment controller

--- backend/controllers/posts/postComment.ts
+++ backend/controllers/posts/postComment.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express'
-import type { IJWTUserType } from '../../middlewares/accessTokenRefresh'
 import { Posts as PostModel } from '../../models/posts/posts'
+import type { IJWTUserType } from '../../middlewares/accessTokenRefresh'
+import type { ICommentLikeRequest } from './PostTypes'
 
 export const savePostComment = async (request: ISavePostRequest, response: Response) => {
    const userId = request.user?.userId
@@ -19,6 +20,28 @@ export const savePostComment = async (request: ISavePostRequest, response: Respo
       response.status(200).json({ comments: foundPost?.comments })
    } catch (error) {
       response.status(500).json({ error })
+   }
+}
+
+export const likeCommentController = async (request: ICommentLikeRequest, response: Response) => {
+   const { commentId, postId, reactionType } = request.body
+   const userId = request.user?.userId
+
+   try {
+      // https://www.mongodb.com/community/forums/t/how-to-only-get-the-array-nested-subdocuments-with-that-document-id-and-not-having-to-iterate-through-it/100197
+      const foundPostToModifyLike = await PostModel.find(
+         {
+            _id: postId,
+         },
+         {
+            comments: {
+               $elemMatch: { _id: commentId },
+            },
+         }
+      )
+      response.status(200).json(foundPostToModifyLike)
+   } catch (error) {
+      response.status(500).json(error)
    }
 }
 
