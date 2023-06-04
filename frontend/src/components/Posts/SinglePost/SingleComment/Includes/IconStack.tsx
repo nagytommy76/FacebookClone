cd00dcf2662b9d3d913acefe6d38e0575047e4ec
nagytommy76 <nagytommy76@gmail.com>
2023-06-04 16:34:23 +0200

    Displaying the like icons depending on a boolean prop

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/IconStack.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/IconStack.tsx
@@ -1,19 +1,37 @@
 import React from 'react'
 import useCommentLikeIcons from '../Hooks/useCommentLikeIcons'
-import { IconStackStyle } from '../Styles'
 import type { IOrderedLikesCount } from '../../Like/Types'
 
-const IconStack: React.FC<{ orderedCountedLike: IOrderedLikesCount | null }> = ({ orderedCountedLike }) => {
+import { IconStackStyle } from '../Includes/Reatcions/Styles'
+import { IconStackContainerStyle } from '../Styles'
+import Typography from '@mui/material/Typography'
+
+const IconStack: React.FC<{ orderedCountedLike: IOrderedLikesCount | null; displayRow?: boolean }> = ({
+   orderedCountedLike,
+   displayRow = true,
+}) => {
    if (!orderedCountedLike) return <></>
    const getLikeIconComponent = useCommentLikeIcons()
-   return (
-      <IconStackStyle>
+   return displayRow ? (
+      <>
          {Object.entries(orderedCountedLike).map(
             (likeType, index) =>
                likeType[1] > 0 &&
                index <= 2 && <span key={index}>{getLikeIconComponent(likeType[0] as any)}</span>
          )}
-      </IconStackStyle>
+      </>
+   ) : (
+      <IconStackContainerStyle>
+         {Object.entries(orderedCountedLike).map(
+            (likeType, index) =>
+               likeType[1] > 0 && (
+                  <IconStackStyle key={index}>
+                     {getLikeIconComponent(likeType[0] as any)}{' '}
+                     <Typography variant='caption'>{likeType[1]}</Typography>
+                  </IconStackStyle>
+               )
+         )}
+      </IconStackContainerStyle>
    )
 }
 
