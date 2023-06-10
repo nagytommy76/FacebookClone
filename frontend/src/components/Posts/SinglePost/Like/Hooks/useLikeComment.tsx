ed84fa2129e4f958c1db0df9b4daa0aec719a2cd
nagytommy76 <nagytommy76@gmail.com>
2023-06-10 17:04:28 +0200

    Dispatching the comment when the user clicks the like btn

--- frontend/src/components/Posts/SinglePost/Like/Hooks/useLikeComment.tsx
+++ frontend/src/components/Posts/SinglePost/Like/Hooks/useLikeComment.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { CommentContext } from '../../SingleComment/Context/CommentContext'
+import { CommentActions } from '../../SingleComment/Context/CommentReducer'
 import { AxiosResponse, axiosInstance as axios } from '../../../../../utils/axiosSetup/AxiosInstance'
 import { useMutation } from '@tanstack/react-query'
 import type { IPostLike, LikeTypes } from '../Types'
@@ -11,9 +12,7 @@ interface ICommentLike {
 }
 // Ide kéne egy context-et behívni, (kommentek context), mert ezt a hook-ot csak akkor hívom meg ha !isPostLike
 const useLikeComment = () => {
-   const {
-      commentReducer: { singleComment },
-   } = useContext(CommentContext)
+   const { commentDispatch } = useContext(CommentContext)
    const handleSendLike = async ({ likeTypeFomInput, postId, commentId }: ICommentLike) => {
       try {
          const response = (await axios.post('/post/post-comment-like', {
@@ -21,9 +20,7 @@ const useLikeComment = () => {
             postId,
             commentId,
          })) as AxiosResponse<IPostLike[]>
-         // setComment(response.data)
-         console.log(response.data)
-
+         commentDispatch({ type: CommentActions.SET_COMMENT_LIKE, payload: response.data })
          return response
       } catch (error) {
          console.log(error)
