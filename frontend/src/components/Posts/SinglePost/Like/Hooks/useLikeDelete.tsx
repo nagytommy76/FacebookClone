e0db4c238cc350c86aafedf64b3162dd130dc26d
nagytommy76 <nagytommy76@gmail.com>
2023-06-12 20:25:30 +0200

    Removing the post like by its ID

--- frontend/src/components/Posts/SinglePost/Like/Hooks/useLikeDelete.tsx
+++ frontend/src/components/Posts/SinglePost/Like/Hooks/useLikeDelete.tsx
@@ -1,14 +1,21 @@
+import { useContext } from 'react'
+import { PostContext } from '../../../../MainPage/Context/PostContextProvider'
+import { PostsActions } from '../../../../MainPage/Context/PostReducer'
 import { useMutation } from '@tanstack/react-query'
-import { axiosInstance as axios } from '../../../../../utils/axiosSetup/AxiosInstance'
+import { axiosInstance as axios, AxiosResponse } from '../../../../../utils/axiosSetup/AxiosInstance'
 
 const deleteLikeFn = async (postId: string) => {
-   return await axios.delete('/post/post-like-delete', { data: { postId } })
+   return (await axios.delete('/post/post-like-delete', { data: { postId } })) as AxiosResponse<string>
 }
 
 const useLikeDelete = () => {
+   const { postsDispatch } = useContext(PostContext)
    const { mutate } = useMutation({
       mutationKey: ['deleteLike'],
       mutationFn: deleteLikeFn,
+      onSuccess(data) {
+         if (data.status === 200) postsDispatch({ type: PostsActions.REMOVE_SINGLE_LIKE, payload: data.data })
+      },
    })
    return { deleteMutation: mutate }
 }
