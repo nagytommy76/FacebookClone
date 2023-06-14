179cf7a719de95274144da4d6327a42cc855b943
nagytommy76 <nagytommy76@gmail.com>
2023-06-14 19:30:07 +0200

    Added priority prop ti next Image

--- frontend/src/components/Posts/SinglePost/SingleComment/Hooks/useCommentLikeIcons.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Hooks/useCommentLikeIcons.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import type { LikeTypes } from '../../Like/Types'
 
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt'
-import LikeAnimated from '../../../../../assets/likeAnimated.svg'
+import LikeAnimated from '../../../../../assets/like.svg'
 import Love from '../../../../../assets/love.svg'
 import Care from '../../../../../assets/care.svg'
 import Haha from '../../../../../assets/haha.svg'
@@ -15,19 +15,19 @@ const useCommentLikeIcons = () => {
    const getLikeIconComponent = (currentLikeType: LikeTypes | string) => {
       switch (currentLikeType) {
          case 'isLike':
-            return <Image src={LikeAnimated} alt='like icon' />
+            return <Image priority src={LikeAnimated} alt='like icon' />
          case 'isLove':
-            return <Image src={Love} alt='love icon' />
+            return <Image priority src={Love} alt='love icon' />
          case 'isCare':
-            return <Image src={Care} alt='care icon' />
+            return <Image priority src={Care} alt='care icon' />
          case 'isHaha':
-            return <Image src={Haha} alt='haha icon' />
+            return <Image priority src={Haha} alt='haha icon' />
          case 'isWow':
-            return <Image src={Wow} alt='wow icon' />
+            return <Image priority src={Wow} alt='wow icon' />
          case 'isSad':
-            return <Image src={Sad} alt='sad icon' />
+            return <Image priority src={Sad} alt='sad icon' />
          case 'isAngry':
-            return <Image src={Angry} alt='angry icon' />
+            return <Image priority src={Angry} alt='angry icon' />
          default:
             return <ThumbUpOffAltIcon />
       }
