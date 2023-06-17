cb1f370859bdefd493d75f8dca9b598d16ba4233
nagytommy76 <nagytommy76@gmail.com>
2023-06-17 16:04:17 +0200

    Finished the single post skeleton

--- frontend/src/skeletons/SinglePost/SinglePost.tsx
+++ frontend/src/skeletons/SinglePost/SinglePost.tsx
@@ -2,11 +2,40 @@ import React from 'react'
 
 import Skeleton from '@mui/material/Skeleton'
 import Paper from '@mui/material/Paper'
+import Divider from '@mui/material/Divider'
+
+import {
+   StyledPostHeadSkeleton,
+   StyledFlexContainer,
+   BodySectionPlaceholderStyle,
+   LikeAndCommentSection,
+   StyledAddLikeComment,
+} from './Style'
 
 const SinglePost = () => {
    return (
-      <Paper sx={{ margin: '1rem 0', pb: '.3rem', minHeight: '100px' }}>
-         <Skeleton animation='wave' height={40} width='80%' />
+      <Paper sx={{ margin: '1rem 0', p: '1rem', minHeight: '100px' }}>
+         <StyledPostHeadSkeleton>
+            <StyledFlexContainer>
+               <Skeleton variant='circular' width={50} height={50} />
+               <span>
+                  <Skeleton animation='wave' height={20} width={180} />
+                  <Skeleton animation='wave' height={20} width={180} />
+               </span>
+            </StyledFlexContainer>
+         </StyledPostHeadSkeleton>
+         <BodySectionPlaceholderStyle />
+         {/* <Divider /> */}
+         <LikeAndCommentSection>
+            <Skeleton animation='wave' height={30} width={50} />
+            <Skeleton animation='wave' height={30} width={80} />
+         </LikeAndCommentSection>
+         <Divider />
+         <StyledAddLikeComment>
+            <Skeleton animation='wave' height={50} width={170} />
+            <Skeleton animation='wave' height={50} width={170} />
+         </StyledAddLikeComment>
+         <Divider />
       </Paper>
    )
 }
--- frontend/src/skeletons/SinglePost/Style.ts
+++ frontend/src/skeletons/SinglePost/Style.ts
@@ -1,1 +1,36 @@
 import { styled } from '@mui/material'
+
+export const StyledPostHeadSkeleton = styled('header')(({ theme }) => ({
+   width: '32%',
+   [theme.breakpoints.down('sm')]: {
+      width: '65%',
+   },
+}))
+
+export const StyledFlexContainer = styled('div')(({ theme }) => ({
+   width: '100%',
+   display: 'flex',
+   flexDirection: 'row',
+   alignItems: 'center',
+   justifyContent: 'space-between',
+}))
+
+export const BodySectionPlaceholderStyle = styled('div')({
+   height: '150px',
+})
+
+export const LikeAndCommentSection = styled('div')({
+   margin: '.5rem 0',
+   width: '100%',
+   display: 'flex',
+   flexDirection: 'row',
+   justifyContent: 'space-between',
+})
+
+export const StyledAddLikeComment = styled('div')({
+   margin: '.5rem 0',
+   width: '100%',
+   display: 'flex',
+   flexDirection: 'row',
+   justifyContent: 'space-evenly',
+})
