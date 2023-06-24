dea8d0df75e41747b7a47e6e777a1d9d84b947d8
nagytommy76 <nagytommy76@gmail.com>
2023-06-24 16:44:29 +0200

    Using fill & sizes on Image

--- frontend/src/components/Base/ImageSlider/ImageSlider.tsx
+++ frontend/src/components/Base/ImageSlider/ImageSlider.tsx
@@ -1,34 +1,39 @@
-import React, { useState } from 'react'
-
 import { StyledModalContainer, StyledImageContainer, CloseIconStyle, StyledModalImage } from './Styles'
 import Modal from '@mui/material/Modal'
-// import Fade from '@mui/material/Fade'
 import LeftArrow from './Includes/LeftArrow'
+import RightArrow from './Includes/RightArrow'
+
 const ImageSlider: React.FC<{
    isImgSliderOpen: boolean
    postedPicturesPath: string[]
    currentPicIndex: number
    setIsImgSliderOpen: React.Dispatch<React.SetStateAction<boolean>>
-}> = ({ isImgSliderOpen, postedPicturesPath, currentPicIndex, setIsImgSliderOpen }) => {
-   //    const [currentPic, setCurrentPic] = useState<number>(0)
-   const [direction, setDirection] = useState<'left' | 'up' | 'down' | 'right'>('right')
-   const [isSlide, setIsSlide] = useState<boolean>(true)
-
+   nextImage: () => void
+   previousImage: () => void
+}> = ({
+   isImgSliderOpen,
+   postedPicturesPath,
+   currentPicIndex,
+   setIsImgSliderOpen,
+   nextImage,
+   previousImage,
+}) => {
    return (
       <Modal open={isImgSliderOpen}>
          <StyledModalContainer>
             <CloseIconStyle onClick={() => setIsImgSliderOpen(false)} />
-            {/* <Fade in={isSlide}> */}
             <StyledImageContainer>
                <StyledModalImage
-                  width={1920}
-                  height={1080}
+                  // width={1920}
+                  // height={1080}
+                  sizes='(max-width: 768px) 500px, (max-width: 1200px) 1200px, 100vw'
+                  fill
                   src={postedPicturesPath[currentPicIndex]}
                   alt=''
                />
             </StyledImageContainer>
-            {/* </Fade> */}
-            <LeftArrow />
+            <LeftArrow previousImage={previousImage} />
+            <RightArrow nextImage={nextImage} />
          </StyledModalContainer>
       </Modal>
    )
