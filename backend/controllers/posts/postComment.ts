e2ab907e82714a62d80987fa9353f7299c2a77cd
nagytommy76 <nagytommy76@gmail.com>
2023-06-25 16:19:58 +0200

    Created a PostController class

--- backend/controllers/posts/postComment.ts
+++ backend/controllers/posts/postComment.ts
@@ -1,7 +1,47 @@
 import { Response } from 'express'
 import { Posts as PostModel } from '../../models/posts/posts'
+import BasePostController from './basePost'
 import type { IJWTUserType } from '../../middlewares/accessTokenRefresh'
 import type { ICommentLikeRequest, LikeTypes, IReactionTypes } from './PostTypes'
+
+export default class PostController extends BasePostController {
+   likeCommentController = async (request: ICommentLikeRequest, response: Response) => {
+      const { commentId, postId, reactionType } = request.body
+      const userId = request.user?.userId as string | any
+      try {
+         // https://www.mongodb.com/community/forums/t/how-to-only-get-the-array-nested-subdocuments-with-that-document-id-and-not-having-to-iterate-through-it/100197
+         const foundPostToModifyLike = await this.findPostModelByPostId(postId)
+         if (!foundPostToModifyLike) return response.status(404).json({ msg: 'Post comment not found' })
+
+         const commentLikeIndex = foundPostToModifyLike.comments.findIndex((comment) => {
+            return comment._id?.toString() === commentId.toString()
+         })
+         const userLike = this.findUsersLikeByUserID(
+            foundPostToModifyLike.comments[commentLikeIndex].likes,
+            userId
+         )
+         this.checkUserLike(
+            userLike,
+            reactionType,
+            foundPostToModifyLike.comments[commentLikeIndex].likes,
+            userId
+         )
+
+         await foundPostToModifyLike.save()
+         await foundPostToModifyLike.populate({
+            path: 'comments.userId',
+            select: ['firstName', 'sureName', 'userDetails.profilePicturePath'],
+            match: {
+               'userDetails.profilePicturePath': { $elemMatch: { isSelected: { $eq: true } } },
+            },
+         })
+         response.status(200).json(foundPostToModifyLike.comments[commentLikeIndex].likes)
+      } catch (error) {
+         console.log(error)
+         response.status(500).json({ error })
+      }
+   }
+}
 
 export const savePostComment = async (request: ISavePostRequest, response: Response) => {
    const userId = request.user?.userId
@@ -33,55 +73,6 @@ export const savePostComment = async (request: ISavePostRequest, response: Respo
 
 const findPreviousReactionType = (reactionType: IReactionTypes) => {
    return Object.keys(reactionType).filter((key) => reactionType[key])[0] as LikeTypes
-}
-
-export const likeCommentController = async (request: ICommentLikeRequest, response: Response) => {
-   const { commentId, postId, reactionType } = request.body
-   const userId = request.user?.userId as string | any
-   try {
-      // https://www.mongodb.com/community/forums/t/how-to-only-get-the-array-nested-subdocuments-with-that-document-id-and-not-having-to-iterate-through-it/100197
-      const foundPostToModifyLike = await PostModel.findById(postId)
-      if (!foundPostToModifyLike) return response.status(404).json({ msg: 'Post comment not found' })
-
-      const commentLikeIndex = foundPostToModifyLike.comments.findIndex((comment) => {
-         return comment._id?.toString() === commentId.toString()
-      })
-
-      const userLike = foundPostToModifyLike.comments[commentLikeIndex].likes.find(
-         (like) => like.userId.toString() === userId.toString()
-      )
-      if (userLike) {
-         let previousReaction = findPreviousReactionType(userLike.reactionType)
-         userLike.reactionType[previousReaction] = false
-         userLike.reactionType[reactionType] = true
-      } else {
-         foundPostToModifyLike.comments[commentLikeIndex].likes.push({
-            userId,
-            reactionType: {
-               isLike: false,
-               isAngry: false,
-               isCare: false,
-               isHaha: false,
-               isLove: false,
-               isSad: false,
-               isWow: false,
-               [reactionType]: true,
-            },
-         })
-      }
-      await foundPostToModifyLike.save()
-      await foundPostToModifyLike.populate({
-         path: 'comments.userId',
-         select: ['firstName', 'sureName', 'userDetails.profilePicturePath'],
-         match: {
-            'userDetails.profilePicturePath': { $elemMatch: { isSelected: { $eq: true } } },
-         },
-      })
-      response.status(200).json(foundPostToModifyLike.comments[commentLikeIndex].likes)
-   } catch (error) {
-      console.log(error)
-      response.status(500).json({ error })
-   }
 }
 
 interface ISavePostRequest extends IJWTUserType {
