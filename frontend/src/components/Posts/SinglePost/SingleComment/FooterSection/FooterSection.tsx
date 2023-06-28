64c2d09aa0349433b8d546df8cc2dca458cebe03
nagytommy76 <nagytommy76@gmail.com>
2023-06-28 19:51:38 +0200

    Checking the isDisabled & Check the ref after 200ms activate the cursor

--- frontend/src/components/Posts/SinglePost/SingleComment/FooterSection/FooterSection.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/FooterSection/FooterSection.tsx
@@ -17,12 +17,23 @@ const FooterSection: React.FC<{
       commentReducer: { singleComment },
    } = useContext(CommentContext)
    const currentTime = useMoment(singleComment.answeredAt)
-   const reference = useRef(null)
+   const reference = useRef<null | HTMLInputElement>(null)
    const [isAnswerOpen, setIsAnswerOpen] = useState<boolean>(false)
    const [answerText, setAnswerText] = useState<string>('')
+   const [isSendDisabled, setIsSendDisabled] = useState<boolean>(true)
 
-   const handleSetAnswerOpen = () => setIsAnswerOpen(true)
-   const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => setAnswerText(event.target.value)
+   const handleSetAnswerOpen = () => {
+      setIsAnswerOpen(true)
+      // Azért kell, hogy miután kinyílik a collapse, az után állítsa be.
+      setTimeout(() => {
+         if (reference) reference.current?.focus()
+      }, 200)
+   }
+   const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (event.target.value.length === 0) setIsSendDisabled(true)
+      else setIsSendDisabled(false)
+      setAnswerText(event.target.value)
+   }
 
    return (
       <>
@@ -33,20 +44,19 @@ const FooterSection: React.FC<{
                postId={postId}
                postLikes={singleComment.likes}
             >
-               <StyledCommentAnswerButton ref={reference} onClick={handleSetAnswerOpen}>
-                  Válasz
-               </StyledCommentAnswerButton>
+               <StyledCommentAnswerButton onClick={handleSetAnswerOpen}>Válasz</StyledCommentAnswerButton>
             </Likes>
             <Tooltip arrow title={moment(singleComment.answeredAt).format('YYYY MMMM D dddd, kk:mm')}>
                <span>{currentTime}</span>
             </Tooltip>
          </CommentFooterStyle>
-         <Collapse in={isAnswerOpen}>
+         <Collapse in={isAnswerOpen} timeout={100}>
             <AddCommentBase
-               reference={reference}
+               reference={reference as React.MutableRefObject<null>}
                handleSendComment={() => {}}
                commentText={answerText}
                handleChangeText={handleChangeText}
+               isSendDisabled={isSendDisabled}
             />
          </Collapse>
       </>
