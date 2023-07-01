4dab722b5f2f605b14d63652385f715d4cd39988
nagytommy76 <nagytommy76@gmail.com>
2023-07-01 16:21:48 +0200

    Separated the comment types to another file

---
+++ backend/controllers/posts/types/commentTypes.ts
@@ -0,0 +1,31 @@
+import { ObjectId } from 'mongoose'
+import type { IPostLike } from '../PostTypes'
+
+export interface ICommentAnswer {
+   userId: string
+   comment: string
+   parentCommentId: string
+   commentDepth: number
+   answeredAt: string
+   commentImage: string
+   likes?: IPostLike[]
+}
+
+export interface IPostComment {
+   _id?: string | ObjectId
+   userId:
+      | ObjectId
+      | string
+      | {
+           firstName: string
+           sureName: string
+           userDetails: { profilePicturePath: { _id: string; path: string; isSelected: boolean }[] }
+        }
+   comment: string
+   answeredAt?: Date | string
+   commentImage?: string
+   parentCommentId: string | null
+   commentDepth: number
+   commentAnswers?: ICommentAnswer[]
+   likes: IPostLike[]
+}
