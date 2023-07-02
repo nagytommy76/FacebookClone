3bef8e75799ef45341168247784c959fe99883ec
nagytommy76 <nagytommy76@gmail.com>
2023-07-02 16:10:48 +0200

    Dispatching the ADD_SINGLE_COMMENT_ANSWER in an onSucces fn

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/FooterSection/Hooks/useCreateAnswer.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/FooterSection/Hooks/useCreateAnswer.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useMutation } from '@tanstack/react-query'
-import { axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+import { AxiosResponse, axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+import { CommentContext } from '../../../Context/CommentContext'
+import { CommentActions } from '../../../Context/CommentReducer'
+import { ICommentAnswers } from '@/src/types/LikeTypes'
 
 const useCreateAnswer = (
    postId: string,
@@ -9,21 +12,28 @@ const useCreateAnswer = (
    parentCommentId: string | null,
    commentAnswer: string
 ) => {
+   const { commentDispatch } = useContext(CommentContext)
    const handleMutate = async () => {
-      const response = await axios.post('/post/add-comment-answer', {
+      const response = (await axios.post('/post/add-comment-answer', {
          postId,
          commentId,
          commentDepth,
          parentCommentId,
          answeredAt: new Date(),
          commentAnswer,
-      })
+      })) as AxiosResponse<{ createdCommentAnswer: ICommentAnswers[] }>
       return response
    }
 
    const { mutate } = useMutation({
       mutationKey: ['saveAnswerText'],
       mutationFn: handleMutate,
+      onSuccess(data) {
+         commentDispatch({
+            type: CommentActions.ADD_SINGLE_COMMENT_ANSWER,
+            payload: data.data.createdCommentAnswer,
+         })
+      },
    })
    return mutate
 }
