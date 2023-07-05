a895aeff8db18d9adf3166de6f1e0d7b55519d36
nagytommy76 <nagytommy76@gmail.com>
2023-07-05 19:55:31 +0200

    Simplified the component with the BaseAnswerComponent

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/SingleAnswer/SingleAnswer.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/SingleAnswer/SingleAnswer.tsx
@@ -1,81 +1,30 @@
-import React, { useContext, useEffect } from 'react'
-import moment from 'moment'
-
-import useMoment from '../../Hooks/useMoment'
-import useCreateAnswer from '../Hooks/useCreateAnswer'
+import dynamic from 'next/dynamic'
+import React, { useContext, useEffect, useState } from 'react'
 
 import { CommentContext } from '../../Context/CommentContext'
 import type { ICommentAnswers } from '@/src/types/LikeTypes'
 
-import {
-   StyledCommentPaper,
-   StyledProfileImage,
-   StyledCommentContainer,
-   StyledListElement,
-} from '../../Styles'
-import { CommentFooterStyle, StyledCommentAnswerButton } from '../FooterSection/Styles'
-import Collapse from '@mui/material/Collapse'
-import Tooltip from '@mui/material/Tooltip'
+import AnswerList from '../AnswerList/AnswerList'
 
-import Likes from '../../../Like/Like'
-import AddCommentBase from '@/src/components/Base/AddComment/AddCommentBase'
-import Reactions from '../Reatcions/Reactions'
+const BaseAnswerComponent = dynamic(() => import('../Base/BaseAnswerAndComment'), {
+   loading: () => <h1>BaseAnswerComponent TÖLT</h1>,
+})
 
 const SingleAnswer: React.FC<{ answer: ICommentAnswers }> = ({ answer }) => {
    const {
       commentReducer: { postId },
       getAnswerReplies,
    } = useContext(CommentContext)
-   const currentTime = useMoment(answer.answeredAt)
-   const {
-      answerText,
-      isAnswerOpen,
-      isSendDisabled,
-      reference,
-      answerMutate,
-      handleChangeText,
-      handleSetAnswerOpen,
-   } = useCreateAnswer(answer.commentDepth + 1, answer._id)
+   const [childAnswers, setChildAnswers] = useState<ICommentAnswers[] | null>(null)
 
    useEffect(() => {
-      console.log(getAnswerReplies(answer._id))
-   }, [answer._id, getAnswerReplies])
+      setChildAnswers(getAnswerReplies(answer._id))
+   }, [answer.parentCommentId, getAnswerReplies, answer._id])
 
    return (
-      <StyledCommentContainer>
-         <StyledListElement>
-            <StyledProfileImage
-               src={answer.userId.userDetails.profilePicturePath[0].path}
-               alt='profil'
-               width={20}
-               height={20}
-            />
-            <StyledCommentPaper key={answer._id}>
-               <p>{answer.comment}</p>
-               <Reactions likes={answer.likes as []} />
-            </StyledCommentPaper>
-            <CommentFooterStyle>
-               <Likes commentId={answer._id} isPostLike={false} postId={postId} postLikes={answer.likes}>
-                  <StyledCommentAnswerButton onClick={handleSetAnswerOpen}>Válasz</StyledCommentAnswerButton>
-               </Likes>
-               <Tooltip arrow title={moment(answer.answeredAt).format('YYYY MMMM D dddd, kk:mm')}>
-                  <span>{currentTime}</span>
-               </Tooltip>
-            </CommentFooterStyle>
-            <Collapse in={isAnswerOpen} timeout={100}>
-               <AddCommentBase
-                  reference={reference as React.MutableRefObject<null>}
-                  handleSendComment={() => {
-                     answerMutate()
-                  }}
-                  commentText={answerText}
-                  handleChangeText={handleChangeText}
-                  isSendDisabled={isSendDisabled}
-               />
-            </Collapse>
-            <p>IDE JÖN AZ ANSWERLIST COMPONENT A CHILD REPLIES-AL</p>
-         </StyledListElement>
-      </StyledCommentContainer>
+      <BaseAnswerComponent answer={answer} postId={postId}>
+         {childAnswers ? <AnswerList answer={childAnswers} /> : <></>}
+      </BaseAnswerComponent>
    )
 }
 
