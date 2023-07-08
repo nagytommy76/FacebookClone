59998251b76b7a5767da581dc8cd850ffefc03de
nagytommy76 <nagytommy76@gmail.com>
2023-07-08 14:00:02 +0200

    Using hte BaseCommentComponent

--- frontend/src/components/Posts/SinglePost/SingleComment/SingleComment.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/SingleComment.tsx
@@ -1,50 +1,20 @@
+import dynamic from 'next/dynamic'
 import React, { useContext } from 'react'
 import { CommentContext } from './Context/CommentContext'
 
-import { StyledCommentContainer, StyledProfileImage, StyledListElement, StyledCommentPaper } from './Styles'
-
-import Reactions from './Includes/Reatcions/Reactions'
-import FooterSection from './Includes/FooterSection/FooterSection'
-// import ProfileCard from '../ProfileCard/DetailsTooltipTitle'
-// import CustomTooltipTitle from '../../../Base/CustomTooltipTitle'
+const BaseCommentComponent = dynamic(() => import('./Includes/Base/BaseAnswerAndComment'))
+const AnswerList = dynamic(() => import('./Includes/AnswerList/AnswerList'))
 
 const SingleComment = () => {
    const {
-      commentReducer: { singleComment },
+      commentReducer: { singleComment, postId },
+      parentRootAnswers,
    } = useContext(CommentContext)
 
    return (
-      <StyledCommentContainer>
-         <StyledListElement>
-            <StyledProfileImage
-               src={singleComment.userId.userDetails.profilePicturePath[0].path}
-               alt='profil'
-               width={20}
-               height={20}
-            />
-            <StyledCommentPaper>
-               {/* <CustomTooltipTitle
-                  title={
-                     <ProfileCard
-                        selectSelectedProfilePicture={() => {
-                           return { _id: '', isSelected: false, path: '' }
-                        }}
-                        userInfo={comment.userId.userDetails as any}
-                     />
-                  }>
-                  <p>
-                     {comment.userId.firstName} {comment.userId.sureName}
-                  </p>
-               </CustomTooltipTitle> */}
-               <p>
-                  {singleComment.userId.firstName} {singleComment.userId.sureName}
-               </p>
-               <p>{singleComment.comment}</p>
-               <Reactions likes={singleComment.likes} />
-            </StyledCommentPaper>
-            <FooterSection />
-         </StyledListElement>
-      </StyledCommentContainer>
+      <BaseCommentComponent answer={singleComment} postId={postId}>
+         {parentRootAnswers ? <AnswerList answer={parentRootAnswers} /> : <></>}
+      </BaseCommentComponent>
    )
 }
 
