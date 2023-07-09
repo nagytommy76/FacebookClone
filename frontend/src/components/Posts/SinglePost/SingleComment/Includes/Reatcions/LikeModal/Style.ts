139424f0e6fd5ca10fe1933812ef15109988cf00
nagytommy76 <nagytommy76@gmail.com>
2023-07-09 17:41:39 +0200

    Adde a ModalHeader style

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Reatcions/LikeModal/Style.ts
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Reatcions/LikeModal/Style.ts
@@ -5,10 +5,19 @@ import Paper from '@mui/material/Paper'
 export const StyledModalPaper = styled(Paper)({
    minWidth: 400,
    minHeight: 400,
+   padding: '.75rem 1rem',
    position: 'absolute',
    top: '50%',
    left: '50%',
    transform: 'translate(-50%, -50%)',
 
    borderRadius: 15,
+})
+
+// "HEADER SECTION"
+export const ModalHeader = styled('header')({
+   width: '100%',
+   display: 'flex',
+   flexDirection: 'row',
+   justifyContent: 'space-between',
 })
