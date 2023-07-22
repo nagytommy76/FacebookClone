85b812a5ec7b88b2b6a64c0815a92f2343b48440
nagytommy76 <nagytommy76@gmail.com>
2023-07-22 15:04:21 +0200

    Added a handle function

--- frontend/src/components/Base/AddComment/Includes/EmojiTooltipTitle.tsx
+++ frontend/src/components/Base/AddComment/Includes/EmojiTooltipTitle.tsx
@@ -3,13 +3,18 @@ import React from 'react'
 import Box from '@mui/material/Box'
 import ClickAwayListener from '@mui/base/ClickAwayListener'
 import data from '@emoji-mart/data'
+import {} from 'emoji-mart'
 import Picker from '@emoji-mart/react'
 
 const EmojiTooltipTitle: React.FC<{ handleCloseTooltip: () => void }> = ({ handleCloseTooltip }) => {
+   const handleEmojiSelect = (event: any) => {
+      console.log(event.native)
+   }
+
    return (
       <ClickAwayListener onClickAway={handleCloseTooltip}>
          <div>
-            <Picker data={data} onEmojiSelect={console.log} />
+            <Picker data={data} onEmojiSelect={handleEmojiSelect} />
          </div>
       </ClickAwayListener>
    )
