88b04674fee6affa199954e1c756feb5e4a56f47
nagytommy76 <nagytommy76@gmail.com>
2023-07-26 12:11:33 +0200

    Called the LikePost class

--- backend/api/post/post.ts
+++ backend/api/post/post.ts
@@ -1,27 +1,29 @@
 import { Router } from 'express'
 import { savePostController } from '../../controllers/posts/savePost'
 import { authenticateAccessTokenForApi } from '../../middlewares/accessTokenRefresh'
-import {
-   likePostController,
-   deleteLikeFromPostController,
-   getPostLikesByTypeAndCountController,
-} from '../../controllers/posts/likePost'
+import { deleteLikeFromPostController } from '../../controllers/posts/likePost'
 import { savePostComment } from '../../controllers/posts/postComment'
 import PostCommentController from '../../controllers/posts/postComment'
 import GetPostsController from '../../controllers/posts/getPosts'
+import LikePost from '../../controllers/posts/likePost'
 
 const PostComment = new PostCommentController()
 const GetPosts = new GetPostsController()
+const LikePostClass = new LikePost()
 
 const router = Router()
 // Ide kell egy api route protection (accessTokennel, middleware)
 router.get('/get-posts', authenticateAccessTokenForApi, GetPosts.getAllPosts)
 router.get('/get-user-posts', authenticateAccessTokenForApi, GetPosts.getUsersAllPosts)
-router.post('/get-post-like-count', authenticateAccessTokenForApi, getPostLikesByTypeAndCountController)
+router.post(
+   '/get-post-like-count',
+   authenticateAccessTokenForApi,
+   LikePostClass.getPostLikesByTypeAndCountController
+)
 router.post('/save-post', authenticateAccessTokenForApi, savePostController)
 
 // Likolás
-router.post('/post-like', authenticateAccessTokenForApi, likePostController)
+router.post('/post-like', authenticateAccessTokenForApi, LikePostClass.likePostController)
 router.post('/post-comment-add', authenticateAccessTokenForApi, savePostComment)
 router.post('/post-comment-like', authenticateAccessTokenForApi, PostComment.likeCommentController)
 router.post('/add-comment-answer', authenticateAccessTokenForApi, PostComment.answerToCommentController)
