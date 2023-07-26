376573ae7660794e9779456d577aea95102ce03b
nagytommy76 <nagytommy76@gmail.com>
2023-07-26 12:10:34 +0200

    Moved the functions to the LikePost class

--- backend/controllers/posts/likePost.ts
+++ backend/controllers/posts/likePost.ts
@@ -1,56 +1,51 @@
 import { Response } from 'express'
 import { Posts as PostModel } from '../../models/posts/posts'
-import { Types } from 'mongoose'
-import type {
-   IPostLikeRequest,
-   IPostRemoveLikeRequest,
-   IReactionTypes,
-   LikeTypes,
-   IGetLikesRequest,
-} from './types/PostTypes'
+import type { IPostLikeRequest, IPostRemoveLikeRequest, IGetLikesRequest } from './types/PostTypes'
 
-const findPreviousReactionType = (reactionType: IReactionTypes) => {
-   return Object.keys(reactionType).filter((key) => reactionType[key])[0] as LikeTypes
-}
+import BaseLikeController from './Base/baseLike'
 
-export const likePostController = async (request: IPostLikeRequest, response: Response) => {
-   const { postId, reactionType } = request.body
-   const userId = request.user?.userId as any
+export default class LikePost extends BaseLikeController {
+   getPostLikesByTypeAndCountController = async (request: IGetLikesRequest, response: Response) => {
+      const { postId } = request.body
+      try {
+         const postLikes = await PostModel.findById(postId)
+            .select('likes')
+            .populate({
+               path: 'likes.userId',
+               select: ['firstName', 'sureName', 'userDetails.profilePicturePath.$'],
+               match: {
+                  'userDetails.profilePicturePath': { $elemMatch: { isSelected: { $eq: true } } },
+               },
+            })
+         if (!postLikes) return response.status(404).json({ msg: 'post not found' })
+         const reactionTypes = this.getLikesByReactionType(postLikes.likes)
+         const totalReactionCount = this.countLikeReactions(reactionTypes)
 
-   try {
-      const foundPostToModifyLike = await PostModel.findById(postId)
-      if (!foundPostToModifyLike) return response.status(404).json({ msg: 'nincs ilyen poszt' })
-
-      const userLike = foundPostToModifyLike.likes.find(
-         (like) => like.userId.toString() === userId.toString()
-      )
-
-      if (userLike) {
-         let previousReaction: LikeTypes
-         previousReaction = findPreviousReactionType(userLike.reactionType)
-         userLike.reactionType[previousReaction] = false
-         userLike.reactionType[reactionType] = true
-      } else {
-         foundPostToModifyLike.likes.push({
-            userId,
-            reactionType: {
-               isLike: false,
-               isAngry: false,
-               isCare: false,
-               isHaha: false,
-               isLove: false,
-               isSad: false,
-               isWow: false,
-               [reactionType]: true,
-            },
-         })
+         return response.status(200).json({ reactionTypes, totalReactionCount })
+      } catch (error) {
+         response.status(500).json({ msg: 'Internal server error', error })
       }
+   }
 
-      await foundPostToModifyLike.save()
+   getPostCommentsLikesByTypeAndCountController = async (request: IGetLikesRequest, response: Response) => {}
 
-      response.status(200).json(foundPostToModifyLike.likes)
-   } catch (error) {
-      response.status(500).json({ msg: 'Internal server error' })
+   likePostController = async (request: IPostLikeRequest, response: Response) => {
+      const { postId, reactionType } = request.body
+      const userId = request.user?.userId as any
+
+      try {
+         const foundPostToModifyLike = await this.findPostModelByPostId(postId)
+         if (!foundPostToModifyLike) return response.status(404).json({ msg: 'nincs ilyen poszt' })
+
+         const userLike = this.findUsersLikeByUserID(foundPostToModifyLike.likes, userId)
+         this.checkUserLike(userLike, reactionType, foundPostToModifyLike.likes, userId)
+
+         await foundPostToModifyLike.save()
+
+         response.status(200).json(foundPostToModifyLike.likes)
+      } catch (error) {
+         response.status(500).json({ msg: 'Internal server error', error })
+      }
    }
 }
 
@@ -74,83 +69,4 @@ export const deleteLikeFromPostController = async (request: IPostRemoveLikeReque
    } catch (error) {
       response.status(500).json({ msg: 'Internal server error' })
    }
-}
-
-export const getPostLikesByTypeAndCountController = async (request: IGetLikesRequest, response: Response) => {
-   const { postId } = request.body
-
-   const postLikes = await PostModel.findById(postId)
-      .select('likes')
-      .populate({
-         path: 'likes.userId',
-         select: ['firstName', 'sureName', 'userDetails.profilePicturePath.$'],
-         match: {
-            'userDetails.profilePicturePath': { $elemMatch: { isSelected: { $eq: true } } },
-         },
-      })
-   if (!postLikes) return response.status(404).json({ msg: 'post not found' })
-
-   const reactionTypes: {
-      [index: string]: { count: number; reactors: any[] }
-   } = {}
-   postLikes.likes.map((like) => {
-      Object.entries(like.reactionType).map((keyValue) => {
-         if (like.reactionType[keyValue[0]] === true) {
-            if (reactionTypes[keyValue[0]] === undefined) {
-               reactionTypes[keyValue[0]] = {
-                  count: 1,
-                  reactors: [like.userId],
-               }
-            } else {
-               reactionTypes[keyValue[0]].count++
-               // Hozzáadni a usereket
-               reactionTypes[keyValue[0]].reactors.push(like.userId)
-            }
-         }
-      })
-   })
-
-   return response.status(200).json(reactionTypes)
-}
-export const getPostCommentsLikesByTypeAndCountController = () => {
-   // const post = await PostModel.aggregate([
-   //    { $match: { _id: new Types.ObjectId(postId) } },
-   // {
-   //    $project: {
-   //       comments: {
-   //          $filter: {
-   //             input: '$comments',
-   //             as: 'foundComment',
-   //             cond: { $eq: ['$$foundComment._id', new Types.ObjectId(commentId)] },
-   //          },
-   //       },
-   //    },
-   // },
-   // {
-   //    $unwind: {
-   //       path: '$comments',
-   //    },
-   // },
-   // { $project: { 'comments.likes': 1 } },
-   // {
-   //    $lookup: {
-   //       from: 'users',
-   //       localField: 'comments.likes.userId',
-   //       foreignField: '_id',
-   //       as: 'comments.likes.userId',
-   //    },
-   // },
-   // {
-   //    $group: {
-   //       _id: null,
-   //       isLike: { $addToSet: '$comments.likes.reactionType.isLike' },
-   //       isAngry: { $addToSet: '$comments.likes.reactionType.isAngry' },
-   //    },
-   // },
-   // {
-   //    $unwind: {
-   //       path: '$isAngry',
-   //    },
-   // },
-   // ])
 }
