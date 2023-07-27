9667df00fedc93ffdce9bc8ab7b38ec0ed656998
nagytommy76 <nagytommy76@gmail.com>
2023-07-27 12:37:34 +0200

    Using the IReactionCount interface

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Hooks/useGetComment.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Hooks/useGetComment.tsx
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
 
-const useGetComment = (commentId: string | null, postId: string) => {
+const useGetComment = (commentId: string | null, postId: string, isModalOpen: boolean = false) => {
    const { data } = useQuery({
-      queryKey: ['GetCommentLikeTypes', 'ONLY_COMMENT_REACTIONS', commentId],
+      queryKey: ['GetCommentLikeTypes', commentId],
       queryFn: async () => {
          return await axios.post(`/post/get-comment-like-count`, { commentId, postId })
       },
+      enabled: isModalOpen,
    })
    return { commentLikeCount: data }
 }
--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Hooks/useGetPostLike.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Hooks/useGetPostLike.tsx
@@ -1,14 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
-import { axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+import { AxiosResponse, axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+import type { IReactionCount } from '@/src/types/LikeTypes'
 
-const useGetPostLike = (postId: string) => {
-   const { data } = useQuery({
+const useGetPostLike = (postId: string, isModalOpen: boolean = false) => {
+   const { data, isLoading } = useQuery({
       queryKey: ['GetLikeTypes', postId],
       queryFn: async () => {
-         return await axios.post(`/post/get-post-like-count`, { postId })
+         return (await axios.post(`/post/get-post-like-count`, { postId })) as AxiosResponse<IReactionCount>
       },
+      enabled: isModalOpen,
    })
-   return { postLikeCount: data }
+   return { postLikeCount: data, isLoading }
 }
 
 export default useGetPostLike
