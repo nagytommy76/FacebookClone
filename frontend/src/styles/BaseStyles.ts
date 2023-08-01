cf2aa0c0a7642f511ecd7721daad55e34c40f489
nagytommy76 <nagytommy76@gmail.com>
2023-08-01 13:17:28 +0200

    Created a base style folder & added to the tsconfig

---
+++ frontend/src/styles/BaseStyles.ts
@@ -0,0 +1,10 @@
+import { styled } from '@mui/material'
+import Image from 'next/image'
+
+// Picture style --------------------------------------------
+
+export const StyledProfileImage = styled(Image)({
+   height: 40,
+   width: 40,
+   borderRadius: 50,
+})
