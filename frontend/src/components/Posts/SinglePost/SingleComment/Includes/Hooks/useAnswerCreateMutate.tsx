3f8a6c3737f063275e8328d17a3e37460c06d4a7
nagytommy76 <nagytommy76@gmail.com>
2023-08-06 14:41:49 +0200

    Separated the mutation from createAnswer hook

---
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Hooks/useAnswerCreateMutate.tsx
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react'
+import { useMutation } from '@tanstack/react-query'
+import { AxiosResponse, axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+
+import type { ICommentAnswers } from '@/src/types/LikeTypes'
+import { CommentContext } from '../../Context/CommentContext'
+import { CommentActions } from '../../Context/CommentReducer'
+
+const useAnswerCreateMutate = (
+   commentDepth: number,
+   parentCommentId: string | null,
+   answerText: string,
+   setStatesToDefault: () => void
+) => {
+   const handleCreateMutate = async () => {
+      const response = (await axios.post('/post/add-comment-answer', {
+         postId,
+         commentId: _id,
+         commentDepth,
+         parentCommentId,
+         answeredAt: new Date(),
+         commentAnswer: answerText,
+      })) as AxiosResponse<{ createdCommentAnswer: ICommentAnswers[] }>
+      return response
+   }
+   const {
+      commentDispatch,
+      commentReducer: {
+         postId,
+         singleComment: { _id },
+      },
+   } = useContext(CommentContext)
+
+   const { mutate } = useMutation({
+      mutationKey: ['saveAnswerText'],
+      mutationFn: handleCreateMutate,
+      onSuccess(data) {
+         commentDispatch({
+            type: CommentActions.ADD_SINGLE_COMMENT_ANSWER,
+            payload: data.data.createdCommentAnswer,
+         })
+         setStatesToDefault()
+      },
+   })
+
+   return { saveAnswerMutate: mutate }
+}
+
+export default useAnswerCreateMutate
