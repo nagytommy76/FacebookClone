b5ed88f0eeaca865419d66864d8b2ea49d7368bc
nagytommy76 <nagytommy76@gmail.com>
2023-08-06 13:04:43 +0200

    Delete the comment by its ID

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/Options/Hooks/useRemoveComment.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/Options/Hooks/useRemoveComment.tsx
@@ -3,12 +3,10 @@ import { useMutation } from '@tanstack/react-query'
 import { axiosInstance as axios, AxiosResponse } from '@/src/utils/axiosSetup/AxiosInstance'
 import { PostContext } from '@/src/components/MainPage/Context/PostContextProvider'
 
-import type { IPostComment } from '@/src/types/LikeTypes'
-
 const mutationFunction = async ({ commentId, postId }: { commentId: string; postId: string }) => {
    const response = (await axios.delete('/post/post-comment-delete', {
       data: { commentId, postId },
-   })) as AxiosResponse<{ newComments: IPostComment[] }>
+   })) as AxiosResponse
    return response.data
 }
 
@@ -17,8 +15,8 @@ const useRemoveComment = () => {
    const { mutate } = useMutation({
       mutationKey: ['deleteComment'],
       mutationFn: mutationFunction,
-      onSuccess({ newComments }) {
-         postsDispatch({ type: 'REMOVE_SINGLE_COMMENT', payload: newComments })
+      onSuccess({}, { commentId }) {
+         postsDispatch({ type: 'REMOVE_SINGLE_COMMENT', payload: commentId })
       },
    })
    return mutate
