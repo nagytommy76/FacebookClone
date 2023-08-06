8f462cd80f2bb20e29d6ee06ce76170cc1df561b
nagytommy76 <nagytommy76@gmail.com>
2023-08-06 14:42:27 +0200

    Using the useAnswerCreateMutate

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Hooks/useCreateAnswer.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Hooks/useCreateAnswer.tsx
@@ -1,22 +1,22 @@
-import React, { useContext, useRef, useState } from 'react'
-import { useMutation } from '@tanstack/react-query'
-import { AxiosResponse, axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
-import { CommentContext } from '../../Context/CommentContext'
-import { CommentActions } from '../../Context/CommentReducer'
-import { ICommentAnswers } from '@/src/types/LikeTypes'
+import React, { useRef, useState } from 'react'
+import useAnswerCreateMutate from './useAnswerCreateMutate'
 
 const useCreateAnswer = (commentDepth: number, parentCommentId: string | null) => {
-   const {
-      commentDispatch,
-      commentReducer: {
-         postId,
-         singleComment: { _id },
-      },
-   } = useContext(CommentContext)
    const reference = useRef<null | HTMLInputElement>(null)
    const [isAnswerOpen, setIsAnswerOpen] = useState<boolean>(false)
    const [answerText, setAnswerText] = useState<string>('')
    const [isSendDisabled, setIsSendDisabled] = useState<boolean>(true)
+   const setStatesToDefault = () => {
+      setAnswerText('')
+      setIsSendDisabled(true)
+      setIsAnswerOpen(false)
+   }
+   const { saveAnswerMutate } = useAnswerCreateMutate(
+      commentDepth,
+      parentCommentId,
+      answerText,
+      setStatesToDefault
+   )
 
    const handleSetAnswerOpen = () => {
       setIsAnswerOpen(true)
@@ -31,35 +31,11 @@ const useCreateAnswer = (commentDepth: number, parentCommentId: string | null) =
       setAnswerText(event.target.value)
    }
 
-   const handleMutate = async () => {
-      const response = (await axios.post('/post/add-comment-answer', {
-         postId,
-         commentId: _id,
-         commentDepth,
-         parentCommentId,
-         answeredAt: new Date(),
-         commentAnswer: answerText,
-      })) as AxiosResponse<{ createdCommentAnswer: ICommentAnswers[] }>
-      return response
-   }
-
-   const { mutate } = useMutation({
-      mutationKey: ['saveAnswerText'],
-      mutationFn: handleMutate,
-      onSuccess(data) {
-         commentDispatch({
-            type: CommentActions.ADD_SINGLE_COMMENT_ANSWER,
-            payload: data.data.createdCommentAnswer,
-         })
-         setAnswerText('')
-         setIsSendDisabled(true)
-         setIsAnswerOpen(false)
-      },
-   })
    return {
-      answerMutate: mutate,
+      saveAnswerMutate,
       handleChangeText,
       handleSetAnswerOpen,
+      setAnswerText,
       isAnswerOpen,
       answerText,
       isSendDisabled,
