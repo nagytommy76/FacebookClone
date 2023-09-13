f039535029b7fc1bdc5f62e4ceb2ad1662bfb751
nagytommy76 <nagytommy76@gmail.com>
2023-09-13 19:51:53 +0200

    Moved the likeCommentController

--- backend/controllers/posts/postComment.ts
+++ backend/controllers/posts/postComment.ts
@@ -2,46 +2,8 @@ import { Response } from 'express'
 import { Posts as PostModel } from '../../models/posts/posts'
 import BasePostController from './Base/basePost'
 import type { IJWTUserType } from '../../middlewares/accessTokenRefresh'
-import type { ICommentLikeRequest } from './types/PostTypes'
 
 export default class PostCommentController extends BasePostController {
-   likeCommentController = async (request: ICommentLikeRequest, response: Response) => {
-      const { commentId, postId, reactionType } = request.body
-      const userId = request.user?.userId as string | any
-      try {
-         // https://www.mongodb.com/community/forums/t/how-to-only-get-the-array-nested-subdocuments-with-that-document-id-and-not-having-to-iterate-through-it/100197
-         const foundPostToModifyLike = await this.findPostModelByPostId(postId)
-         if (!foundPostToModifyLike) return response.status(404).json({ msg: 'Post comment not found' })
-
-         const commentLikeIndex = foundPostToModifyLike.comments.findIndex((comment) => {
-            return comment._id?.toString() === commentId.toString()
-         })
-         const userLike = this.findUsersLikeByUserID(
-            foundPostToModifyLike.comments[commentLikeIndex].likes,
-            userId
-         )
-         this.checkUserLike(
-            userLike,
-            reactionType,
-            foundPostToModifyLike.comments[commentLikeIndex].likes,
-            userId
-         )
-
-         await foundPostToModifyLike.save()
-         await foundPostToModifyLike.populate({
-            path: 'comments.userId',
-            select: ['firstName', 'sureName', 'userDetails.profilePicturePath'],
-            match: {
-               'userDetails.profilePicturePath': { $elemMatch: { isSelected: { $eq: true } } },
-            },
-         })
-         response.status(200).json(foundPostToModifyLike.comments[commentLikeIndex].likes)
-      } catch (error) {
-         console.log(error)
-         response.status(500).json({ error })
-      }
-   }
-
    answerToCommentController = async (request: ISavePostCommentAnswerRequest, response: Response) => {
       const userId = request.user?.userId
       if (!userId) return response.status(404).json({ msg: 'User not found' })
