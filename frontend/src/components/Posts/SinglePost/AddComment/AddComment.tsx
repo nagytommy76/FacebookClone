1bf76d1c92d397c5ba09f42a40018748dac15027
nagytommy76 <nagytommy76@gmail.com>
2023-09-17 15:21:04 +0200

    Using empty arrow fns & Using a handleSendComment fn to make the request

--- frontend/src/components/Posts/SinglePost/AddComment/AddComment.tsx
+++ frontend/src/components/Posts/SinglePost/AddComment/AddComment.tsx
@@ -1,9 +1,5 @@
-import React, { useState, useContext } from 'react'
-import { useMutation } from '@tanstack/react-query'
-import { AxiosResponse, axiosInstance as axios } from '@/utils/axiosSetup/AxiosInstance'
-import type { IPostComment } from '@/types/LikeTypes'
-
-import { PostContext } from '../../../MainPage/Context/PostContextProvider'
+import React from 'react'
+import useAddComment from './Hook/useAddComment'
 
 import AddCommentBase from '../SingleComment/Includes/Base/AddComment/AddCommentBase'
 
@@ -11,45 +7,21 @@ const AddComment: React.FC<{
    reference: React.MutableRefObject<null>
    postId: string
 }> = ({ reference, postId }) => {
-   const { postsDispatch } = useContext(PostContext)
-   const [commentText, setCommentText] = useState<string>('')
-   const [isSendDisabled, setIsSendDisabled] = useState<boolean>(true)
-   const { mutate } = useMutation({
-      mutationKey: ['sendPostComment'],
-      mutationFn: async () => {
-         const response = (await axios.post('/post/post-comment-add', {
-            answeredAt: new Date(),
-            comment: commentText,
-            postId,
-         })) as AxiosResponse<{ comments: IPostComment[] }>
-         return response.data
-      },
-      onSuccess: (data) => {
-         postsDispatch({ type: 'ADD_NEW_COMMENT', payload: data.comments })
-      },
-   })
-
-   const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (event.target.value.length === 0) setIsSendDisabled(true)
-      else setIsSendDisabled(false)
-      setCommentText(event.target.value)
-   }
-
-   const handleSendComment = () => {
-      mutate()
-      setCommentText('')
-   }
-
+   const { commentText, isSendDisabled, handleChangeText, handleSendComment } = useAddComment(postId)
    return (
       <>
          <AddCommentBase
-            updateCommentMutate={() => {}}
-            isUpdate={false}
-            handleSendComment={handleSendComment}
-            commentText={commentText}
-            handleChangeText={handleChangeText}
             reference={reference}
+            commentText={commentText}
             isSendDisabled={isSendDisabled}
+            isUpdate={false}
+            isAddPostComment={true}
+            handleChangeText={handleChangeText}
+            handleAddSinglePostComment={handleSendComment}
+            updateCommentMutate={() => {}}
+            commentAnswerId=''
+            handleSendCommentAnswer={() => {}}
+            handleUpdateCommentAnswerMutate={() => {}}
          />
       </>
    )
