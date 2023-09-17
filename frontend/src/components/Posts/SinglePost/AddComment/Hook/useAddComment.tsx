0496e68dfea6706dcf921ce1b5a89b5fcded3403
nagytommy76 <nagytommy76@gmail.com>
2023-09-17 15:20:06 +0200

    Created a AddComment hook to separate the logic from AddComment component

---
+++ frontend/src/components/Posts/SinglePost/AddComment/Hook/useAddComment.tsx
@@ -0,0 +1,40 @@
+import { useContext, useState } from 'react'
+import { useMutation } from '@tanstack/react-query'
+import { AxiosResponse, axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+import { PostContext } from '@/PostContext/PostContextProvider'
+import { IPostComment } from '@/src/types/LikeTypes'
+
+const useAddComment = (postId: string) => {
+   const { postsDispatch } = useContext(PostContext)
+   const [commentText, setCommentText] = useState<string>('')
+   const [isSendDisabled, setIsSendDisabled] = useState<boolean>(true)
+   const { mutate } = useMutation({
+      mutationKey: ['sendPostComment'],
+      mutationFn: async () => {
+         const response = (await axios.post('/post/post-comment-add', {
+            answeredAt: new Date(),
+            comment: commentText,
+            postId,
+         })) as AxiosResponse<{ comments: IPostComment[] }>
+         return response.data
+      },
+      onSuccess: (data) => {
+         postsDispatch({ type: 'ADD_NEW_COMMENT', payload: data.comments })
+      },
+   })
+
+   const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (event.target.value.length === 0) setIsSendDisabled(true)
+      else setIsSendDisabled(false)
+      setCommentText(event.target.value)
+   }
+
+   const handleSendComment = () => {
+      mutate()
+      setCommentText('')
+   }
+
+   return { handleChangeText, handleSendComment, commentText, isSendDisabled }
+}
+
+export default useAddComment
