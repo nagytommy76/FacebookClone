fd7905dc8ed4315ba3005dea5fbd90c01e714366
nagytommy76 <nagytommy76@gmail.com>
2023-09-23 16:37:54 +0200

    Working on the commentAnswer like functionality

--- backend/controllers/posts/like/likeComment.ts
+++ backend/controllers/posts/like/likeComment.ts
@@ -1,6 +1,8 @@
 import { Response } from 'express'
 import BasePostController from '../Base/basePost'
-import { ICommentLikeRequest, ICommentAnswerLikeRequest } from '../types/PostTypes'
+import { Posts as PostModel } from '../../../models/posts/posts'
+import type { ICommentLikeRequest, ICommentAnswerLikeRequest, IPostLike } from '../types/PostTypes'
+import type { ICommentAnswer } from '../types/commentTypes'
 
 export default class CommentLikeController extends BasePostController {
    likeCommentController = async (request: ICommentLikeRequest, response: Response) => {
@@ -42,9 +44,69 @@ export default class CommentLikeController extends BasePostController {
 
    likeCommentAnswerController = async (request: ICommentAnswerLikeRequest, response: Response) => {
       const { commentId, postId, commentAnswerId, reactionType } = request.body
-      const userId = request.user?.userId
+      const userId = request.user?.userId as string
 
-      const foundPostToModifyLike = await this.findPostModelByPostId(postId)
-      if (!foundPostToModifyLike) return response.status(404).json({ msg: 'Post comment not found' })
+      try {
+         const postToModifyLike = await PostModel.findOne({
+            _id: postId,
+            comments: { $elemMatch: { _id: commentId } },
+         }).select(['comments.$'])
+         if (!postToModifyLike) return response.status(404).json({ msg: 'Post comment not found' })
+
+         const commentAnswersLikeIndex = postToModifyLike.comments[0].commentAnswers.findIndex(
+            (commentAnswer) => {
+               return commentAnswer._id?.toString() === commentAnswerId.toString()
+            }
+         )
+
+         const userLike = this.findUsersLikeByUserID(
+            postToModifyLike.comments[0].commentAnswers[commentAnswersLikeIndex].likes,
+            userId
+         )
+         this.checkUserLike(
+            userLike,
+            reactionType,
+            postToModifyLike.comments[0].commentAnswers[commentAnswersLikeIndex].likes,
+            userId
+         )
+         await postToModifyLike.save()
+         response.status(200).json({ postToModifyLike })
+      } catch (error) {
+         console.log(error)
+         response.status(500).json({ msg: 'internal server error', error })
+      }
    }
 }
+
+// const PostToModifyLike = await PostModel.updateOne(
+//    {
+//       _id: postId,
+//       comments: {
+//          $elemMatch: { _id: commentId, 'commentAnswers._id': commentAnswerId },
+//       },
+//    },
+//    {
+//       $push: {
+//          'comments.$[outer].commentAnswers.$[inner].likes': {
+//             userId,
+//             reactionType: {
+//                isLike: false,
+//                isAngry: false,
+//                isCare: false,
+//                isHaha: false,
+//                isLove: false,
+//                isSad: false,
+//                isWow: false,
+//                [reactionType]: true,
+//             },
+//          },
+//       },
+//    },
+//    {
+//       arrayFilters: [{ 'outer._id': commentId }, { 'inner._id': commentAnswerId }],
+//    }
+// )
+
+// https://www.mongodb.com/docs/manual/reference/method/db.collection.updateOne/#examples
+
+// Ezt így kell megcsinálnom
