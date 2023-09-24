7be91f90f4a1436276fc16fd563560155ab5a392
nagytommy76 <nagytommy76@gmail.com>
2023-09-24 13:08:32 +0200

    Fixed the dispatch & passed the likeIdToDelete

--- frontend/src/components/Posts/SinglePost/Like/Hooks/useLikeCommentDelete.tsx
+++ frontend/src/components/Posts/SinglePost/Like/Hooks/useLikeCommentDelete.tsx
@@ -1,9 +1,7 @@
 import { useContext } from 'react'
 import { useMutation } from '@tanstack/react-query'
-import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
-
+import { axiosInstance as axios } from '@/axios/AxiosInstance'
 import { CommentContext } from '@/CommentContext/CommentContext'
-import { IPostLike } from '@/src/types/LikeTypes'
 interface ICommentLikeDelete {
    postId: string
    likeIdToDelete: string
@@ -11,9 +9,9 @@ interface ICommentLikeDelete {
 }
 
 const deleteCommentLikeFn = async ({ postId, commentId, likeIdToDelete }: ICommentLikeDelete) => {
-   return (await axios.delete('/post/post-comment-like-delete', {
+   return await axios.delete('/post/post-comment-like-delete', {
       data: { postId, commentId, likeIdToDelete },
-   })) as AxiosResponse<{ likes: IPostLike[]; removedUserLikesID: string }>
+   })
 }
 
 const useLikeCommentDelete = () => {
@@ -21,9 +19,9 @@ const useLikeCommentDelete = () => {
    const { mutate } = useMutation({
       mutationKey: ['deleteLike'],
       mutationFn: deleteCommentLikeFn,
-      onSuccess(data) {
+      onSuccess(data, variables) {
          if (data.status === 200)
-            commentDispatch({ type: 'REMOVE_SINGLE_COMMENT_LIKE', payload: data.data.removedUserLikesID })
+            commentDispatch({ type: 'REMOVE_SINGLE_COMMENT_LIKE', payload: variables.likeIdToDelete })
       },
    })
    return { deleteCommentLikeMutation: mutate }
