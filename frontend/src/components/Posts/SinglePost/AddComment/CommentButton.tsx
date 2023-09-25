f00e3a6b2ebe4268cd010c4177bbf5defcc8e14f
nagytommy76 <nagytommy76@gmail.com>
2023-09-25 19:57:03 +0200

    using an isTextFieldActive prop to handle the open dialog || add the ref

--- frontend/src/components/Posts/SinglePost/AddComment/CommentButton.tsx
+++ frontend/src/components/Posts/SinglePost/AddComment/CommentButton.tsx
@@ -3,11 +3,17 @@ import React from 'react'
 import Button from '@mui/material/Button'
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
 
-const CommentButton: React.FC<{ commentRef: React.MutableRefObject<null | HTMLInputElement> }> = ({
-   commentRef,
-}) => {
+const CommentButton: React.FC<{
+   commentRef: React.MutableRefObject<null | HTMLInputElement>
+   handleOpenFn: () => void
+   isTextFieldActive?: boolean
+}> = ({ commentRef, handleOpenFn, isTextFieldActive = false }) => {
    const handleClick = () => {
-      if (commentRef) commentRef.current?.focus()
+      if (isTextFieldActive) {
+         commentRef && commentRef.current?.focus()
+      } else {
+         handleOpenFn()
+      }
    }
    return (
       <Button disableRipple fullWidth startIcon={<ChatBubbleOutlineIcon />} onClick={handleClick}>
