3c4218e5594510e3935ab765717249a80f9368ac
nagytommy76 <nagytommy76@gmail.com>
2023-09-25 19:57:38 +0200

    Added a Dialog & working on the comments dispaly

--- frontend/src/components/Posts/SinglePost/SinglePost.tsx
+++ frontend/src/components/Posts/SinglePost/SinglePost.tsx
@@ -13,6 +13,8 @@ import {
    LikeAndCommentContainer,
 } from './Styles'
 
+import Dialog from '@mui/material/Dialog'
+
 const ImageSlideComponent = dynamic(() => import('./Includes/ImageSlide/ImageSlide'))
 const ReactionsContainer = dynamic(
    () => import('./SingleComment/Includes/Reatcions/Container/ReactionsContainer')
@@ -24,43 +26,78 @@ import SingleComment from './SingleComment/SingleComment'
 
 const SinglePost: React.FC<{
    children: React.ReactNode
-}> = ({ children }) => {
+   CommentsComponent?: React.ReactNode
+   isTextFieldActive?: boolean
+}> = ({ children, CommentsComponent, isTextFieldActive = false }) => {
    const {
       postsReducer: { singlePost },
    } = useContext(PostContext)
    const commentRef = useRef(null)
+   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false)
 
    return (
-      <Paper sx={{ margin: '1rem 0', pb: '.3rem', minHeight: '100px' }}>
-         {children}
-         <BodyDescriptionSection>
-            <Typography variant='subtitle1'>{singlePost.description}</Typography>
-         </BodyDescriptionSection>
-         {singlePost.postedPicturesPath && singlePost.postedPicturesPath.length > 0 && (
-            <ImageSlideComponent postedPicturesPath={singlePost.postedPicturesPath} />
-         )}
-         <FooterSectionStyle>
-            <LikeAndCommentContainer>
-               {singlePost.likes.length > 0 && (
-                  <ReactionsContainer likes={singlePost.likes} postId={singlePost._id} />
-               )}
-               {singlePost.comments.length > 0 && <p>{singlePost.comments.length} hozzászólás</p>}
-            </LikeAndCommentContainer>
-            <Divider sx={{ mt: 1, mb: 1 }} />
-            <ButtonGroupStyle>
-               <Like postLikes={singlePost.likes} postId={singlePost._id}>
-                  <CommentButton commentRef={commentRef} />
-               </Like>
-            </ButtonGroupStyle>
-            <Divider sx={{ mt: 1, mb: 1 }} />
-            {singlePost.comments.map((comment) => (
-               <CommentContextProvider key={comment._id} singleComment={comment} postId={singlePost._id}>
-                  <SingleComment />
-               </CommentContextProvider>
-            ))}
-            <AddComment postId={singlePost._id} reference={commentRef} />
-         </FooterSectionStyle>
-      </Paper>
+      <>
+         <Paper sx={{ margin: '1rem 0', pb: '.3rem', minHeight: '100px' }}>
+            {children}
+            <BodyDescriptionSection>
+               <Typography variant='subtitle1'>{singlePost.description}</Typography>
+            </BodyDescriptionSection>
+            {singlePost.postedPicturesPath && singlePost.postedPicturesPath.length > 0 && (
+               <ImageSlideComponent postedPicturesPath={singlePost.postedPicturesPath} />
+            )}
+            <FooterSectionStyle>
+               <LikeAndCommentContainer>
+                  {singlePost.likes.length > 0 && (
+                     <ReactionsContainer likes={singlePost.likes} postId={singlePost._id} />
+                  )}
+                  {singlePost.comments.length > 0 && <p>{singlePost.comments.length} hozzászólás</p>}
+               </LikeAndCommentContainer>
+               <Divider sx={{ mt: 1, mb: 1 }} />
+               <ButtonGroupStyle>
+                  <Like commentId='' postLikes={singlePost.likes} postId={singlePost._id}>
+                     <CommentButton
+                        isTextFieldActive={isTextFieldActive}
+                        handleOpenFn={() => setIsDialogOpen(true)}
+                        commentRef={commentRef}
+                     />
+                  </Like>
+               </ButtonGroupStyle>
+               {CommentsComponent}
+            </FooterSectionStyle>
+         </Paper>
+         <Dialog
+            maxWidth='md'
+            scroll='body'
+            sx={{ width: '100%', margin: 'auto' }}
+            open={isDialogOpen}
+            onClose={() => setIsDialogOpen(false)}
+            aria-labelledby='scroll-dialog-title'
+            aria-describedby='scroll-dialog-description'
+         >
+            <div style={{ width: '800px', overflowX: 'hidden' }}>
+               <SinglePost
+                  isTextFieldActive={true}
+                  CommentsComponent={
+                     <>
+                        <Divider sx={{ mt: 1, mb: 1 }} />
+                        {singlePost.comments.map((comment) => (
+                           <CommentContextProvider
+                              key={comment._id}
+                              singleComment={comment}
+                              postId={singlePost._id}
+                           >
+                              <SingleComment />
+                           </CommentContextProvider>
+                        ))}
+                        <AddComment postId={singlePost._id} reference={commentRef} />
+                     </>
+                  }
+               >
+                  {children}
+               </SinglePost>
+            </div>
+         </Dialog>
+      </>
    )
 }
 
