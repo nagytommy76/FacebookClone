3625066c281a5cefe0f92dade07c87e7ac601e79
nagytommy76 <nagytommy76@gmail.com>
2023-09-25 19:55:49 +0200

    Added a smooth scroll behavior

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
    return (
-      <Html lang='hu'>
+      <Html lang='hu' style={{ scrollBehavior: 'smooth' }}>
          <Head />
          <body>
             <Main />
