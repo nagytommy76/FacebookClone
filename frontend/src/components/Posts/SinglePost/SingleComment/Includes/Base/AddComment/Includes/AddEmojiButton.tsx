4f0c6869a48ecaa886827ea99ddd727b3bcf3fe7
nagytommy76 <nagytommy76@gmail.com>
2023-10-02 19:42:19 +0200

    Passing the setAnswerText to EmojiTooltipTitle

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/AddComment/Includes/AddEmojiButton.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/AddComment/Includes/AddEmojiButton.tsx
@@ -1,20 +1,30 @@
 import React, { useState } from 'react'
 
-import Tooltip from '@mui/material/Tooltip'
 import IconButton from '@mui/material/IconButton'
 import AddReactionIcon from '@mui/icons-material/AddReaction'
 
 import EmojiTooltipTitle from './EmojiTooltipTitle'
 
-const AddEmojiButton = () => {
+import { StyledEmojiTooltip } from '../Styles'
+
+const AddEmojiButton: React.FC<{
+   handleChangeText: (event: React.ChangeEvent<HTMLInputElement>) => void
+   setAnswerText: React.Dispatch<React.SetStateAction<string>>
+}> = ({ handleChangeText, setAnswerText }) => {
    const [openTooltip, setOpenTooltip] = useState<boolean>(false)
    const handleCloseTooltip = () => setOpenTooltip(false)
    const handleOpenTooltip = () => setOpenTooltip(true)
 
    return (
-      <Tooltip
+      <StyledEmojiTooltip
          open={openTooltip}
-         title={<EmojiTooltipTitle handleCloseTooltip={handleCloseTooltip} />}
+         title={
+            <EmojiTooltipTitle
+               setAnswerText={setAnswerText}
+               handleChangeText={handleChangeText}
+               handleCloseTooltip={handleCloseTooltip}
+            />
+         }
          arrow
          placement='top-end'
       >
@@ -29,7 +39,7 @@ const AddEmojiButton = () => {
          >
             <AddReactionIcon fontSize='inherit' />
          </IconButton>
-      </Tooltip>
+      </StyledEmojiTooltip>
    )
 }
 
