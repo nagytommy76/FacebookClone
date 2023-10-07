dcc5bd0b556cf653cfd3115045b0dd694365fda9
nagytommy76 <nagytommy76@gmail.com>
2023-10-07 17:29:10 +0200

    Created an open answers button component

---
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/OpenCommentAnswers/OpenCommentAnswers.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+import Collapse from '@mui/material/Collapse'
+import Button from '@mui/material/Button'
+import SubdirectoryArrowRightIcon from '@mui/icons-material/SubdirectoryArrowRight'
+
+const OpenCommentAnswers: React.FC<{
+   children: React.ReactNode
+   isFirstAnswer: boolean
+   answerLength: number
+}> = ({ answerLength, children, isFirstAnswer }) => {
+   const [isAnswerOpened, setIsAnswerOpened] = useState<boolean>(false)
+   return (
+      <>
+         {isFirstAnswer && (
+            <>
+               <Button
+                  onClick={() => setIsAnswerOpened(true)}
+                  variant='text'
+                  startIcon={<SubdirectoryArrowRightIcon />}
+               >
+                  {answerLength} válasz
+               </Button>
+               <Collapse in={isAnswerOpened}>{children}</Collapse>
+            </>
+         )}
+      </>
+   )
+}
+
+export default OpenCommentAnswers
