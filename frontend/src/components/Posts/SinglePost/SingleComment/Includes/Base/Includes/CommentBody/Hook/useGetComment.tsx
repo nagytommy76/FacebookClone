202f0ce825bfa2ca051e5a085e142ce7d15f33b2
nagytommy76 <nagytommy76@gmail.com>
2023-10-08 14:54:32 +0200

    Created an answer query fn & using conditionally by isChildComent prop

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/CommentBody/Hook/useGetComment.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/CommentBody/Hook/useGetComment.tsx
@@ -1,16 +1,41 @@
+import { useContext } from 'react'
+import { CommentContext } from '@/CommentContext/CommentContext'
+
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance as axios, AxiosResponse } from '@/src/utils/axiosSetup/AxiosInstance'
 import type { IReactionCount } from '@/src/types/LikeTypes'
 
-const useGetComment = (commentId: string | null, postId: string, isModalOpen: boolean = false) => {
+const useGetComment = (
+   answerId: string,
+   commentId: string | undefined,
+   postId: string,
+   isModalOpen: boolean = false,
+   isChildComment: boolean = false
+) => {
+   const {
+      commentReducer: {
+         singleComment: { _id },
+      },
+   } = useContext(CommentContext)
+
+   const getCommentLikesFn = async () => {
+      return (await axios.post(`/post/get-comment-like-count`, {
+         commentId: answerId,
+         postId,
+      })) as AxiosResponse<IReactionCount>
+   }
+
+   const getAnswerLikesFn = async () => {
+      return (await axios.post(`/post/get-answer-like-count`, {
+         commentId: _id,
+         answerId,
+         postId,
+      })) as AxiosResponse<IReactionCount>
+   }
+
    const { data } = useQuery({
       queryKey: ['GetCommentLikeTypes', commentId],
-      queryFn: async () => {
-         return (await axios.post(`/post/get-comment-like-count`, {
-            commentId,
-            postId,
-         })) as AxiosResponse<IReactionCount>
-      },
+      queryFn: isChildComment ? getAnswerLikesFn : getCommentLikesFn,
       enabled: isModalOpen,
    })
    return { commentLikeCount: data?.data }
