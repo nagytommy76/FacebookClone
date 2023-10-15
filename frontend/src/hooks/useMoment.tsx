590088c72f80aac975b2ed597ce7b9c2c2041982
nagytommy76 <nagytommy76@gmail.com>
2023-10-15 12:34:39 +0200

    Fixed the moment error: set the interval based on the day/hour/second etc.. & using relativeTimeThreshold

--- frontend/src/hooks/useMoment.tsx
+++ frontend/src/hooks/useMoment.tsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react'
 import moment from 'moment'
 import 'moment/locale/hu'
+
+moment.relativeTimeThreshold('s', 59)
+moment.relativeTimeThreshold('m', 59)
+moment.relativeTimeThreshold('h', 24)
+moment.relativeTimeThreshold('d', 29)
+moment.relativeTimeThreshold('M', 11)
+
 moment.updateLocale('hu', {
    relativeTime: {
       s: '1 mp',
-      ss: '%s mp',
+      ss: '%d mp',
       m: '1 p',
       mm: '%d p',
-      h: '1 ó',
-      hh: '%d ó',
+      h: '1 ó.',
+      hh: '%d ó.',
       d: '1 n',
       dd: '%d n',
       M: '1 hó',
@@ -20,19 +27,28 @@ moment.updateLocale('hu', {
 
 const useMoment = (answeredAt: string) => {
    const [currentTime, setCurrentTime] = useState('')
-   const [currentInterval, setCurrentInterval] = useState<number>(6000000)
+   const [currentInterval, setCurrentInterval] = useState<number>(1000)
 
    useEffect(() => {
       setCurrentTime(moment(answeredAt).fromNow(true))
       const interval = setInterval(() => {
-         const elapsedTimeString = moment(answeredAt).fromNow(true)
-         // setCurrentInterval(10000)
-         const regexTime = new RegExp(/d+ ó $/)
-         // console.log(regexTime.test(elapsedTimeString))
-         if (regexTime.test(elapsedTimeString)) {
+         if (currentTime.endsWith('p')) {
+            setCurrentInterval(60000) // 1 minute
          }
+         if (currentTime.endsWith('ó.')) {
+            setCurrentInterval(60000 * 60) // 1 hour
+         }
+         if (currentTime.endsWith('n')) {
+            setCurrentInterval(86400000) // 1 day
+         }
+         if (currentTime.endsWith('hó')) {
+            setCurrentInterval(2629746000) // 1 month
+         }
+
+         const elapsedTimeString = moment(answeredAt).fromNow(true)
          setCurrentTime(elapsedTimeString)
       }, currentInterval)
+
       return () => clearInterval(interval)
    }, [answeredAt, currentInterval, currentTime])
 
