dcb3aeef85e7e19fb264a4a31f37d21e06971131
nagytommy76 <nagytommy76@gmail.com>
2023-10-17 19:24:57 +0200

    Working on the image blur effect

--- frontend/src/components/Posts/SinglePost/Includes/ImageContainer/ImageContainer.tsx
+++ frontend/src/components/Posts/SinglePost/Includes/ImageContainer/ImageContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
    StyledImageGridContainer,
    StyledImage,
@@ -13,20 +13,37 @@ const ImageContainer: React.FC<{
    setCurrentPicIndex: React.Dispatch<React.SetStateAction<number>>
    setIsImgSliderOpen: React.Dispatch<React.SetStateAction<boolean>>
 }> = ({ postedPicturesPath, setCurrentPicIndex, setIsImgSliderOpen }) => {
+   const [loading, setLoading] = useState(true)
    const setPicIndexAndOpenModal = (index: number) => {
       setCurrentPicIndex(index)
       setIsImgSliderOpen(true)
    }
+
+   const buffer = async () => {
+      const result = fetch(postedPicturesPath[0]).then(async (result) => {
+         return Buffer.from(await result.arrayBuffer()).toString('base64')
+      })
+      return result
+   }
+
+   //https://www.youtube.com/watch?v=6zDb1kh52nM&ab_channel=OlivierLarose
+   // https://dev.to/nicolaserny/blurred-image-placeholder-with-nextjs-image-and-cloudinary-4dhm
+
+   // const data = await buffer()
+   // console.log(data)
    return (
       <StyledImageGridContainer>
          {postedPicturesPath.map((image, index) =>
             index === 0 ? (
                <FirstGridImage
+                  loading='lazy'
+                  // placeholder='blur'
+                  onLoadingComplete={() => setLoading(false)}
                   onClick={() => setPicIndexAndOpenModal(index)}
                   key={index}
                   src={image == '' ? StockImage : image}
                   alt='Kép'
-                  width={500}
+                  width={800}
                   height={500}
                />
             ) : (
@@ -38,12 +55,15 @@ const ImageContainer: React.FC<{
                   </OverlayedContainer>
                ) : (
                   <StyledImage
+                     loading='lazy'
+                     // placeholder='blur'
+                     onLoadingComplete={() => setLoading(false)}
                      onClick={() => setPicIndexAndOpenModal(index)}
                      key={index}
                      src={image == '' ? StockImage : image}
                      alt='Kép'
-                     width={500}
-                     height={500}
+                     width={400}
+                     height={250}
                   />
                ))
             )
