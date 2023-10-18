34f02bf4b0e35e8965f8fa432440eb0806fcd664
nagytommy76 <nagytommy76@gmail.com>
2023-10-18 19:59:41 +0200

    Created an ImageSlide skeleton component

---
+++ frontend/src/skeletons/SinglePost/ImageSlide.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Skeleton from '@mui/material/Skeleton'
+
+const ImageSlide = () => {
+   return <Skeleton animation='wave' height={700} width={'100%'} />
+}
+
+export default ImageSlide
