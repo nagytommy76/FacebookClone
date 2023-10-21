61ceb3d6c84c6198a1e721b51322924eee56ec51
nagytommy76 <nagytommy76@gmail.com>
2023-10-21 15:54:27 +0200

    Created a removePostController

---
+++ backend/controllers/posts/removePost.ts
@@ -0,0 +1,22 @@
+import type { Response } from 'express'
+import { Posts as PostModel } from '../../models/posts/posts'
+import BasePostController from './Base/basePost'
+import type { IJWTUserType } from '../../middlewares/accessTokenRefresh'
+
+interface IRemovePostRequest extends IJWTUserType {
+   body: {
+      postId: string
+   }
+}
+
+export default class RemovePostsController extends BasePostController {
+   removePostController = async (request: IRemovePostRequest, response: Response) => {
+      const postId = request.body.postId
+      try {
+         response.status(200).json({ msg: 'TÖRlés poszt' })
+      } catch (error) {
+         console.log(error)
+         response.status(500).json(error)
+      }
+   }
+}
