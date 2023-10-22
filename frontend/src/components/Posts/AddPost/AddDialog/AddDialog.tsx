2dd777bb9032b12261aaf579b03333596572db22
nagytommy76 <nagytommy76@gmail.com>
2023-10-22 12:31:26 +0200

    Moved the hooks from AddPost to AddDialog

--- frontend/src/components/Posts/AddPost/AddDialog/AddDialog.tsx
+++ frontend/src/components/Posts/AddPost/AddDialog/AddDialog.tsx
@@ -1,6 +1,10 @@
-import { useState } from 'react'
-import type { AxiosResponse } from 'axios'
-import type { UseMutateFunction } from '@tanstack/react-query'
+import { useState, Dispatch, SetStateAction } from 'react'
+import dynamic from 'next/dynamic'
+import useDialog from '../Hooks/useDialog'
+import usePostMutate from './Hooks/usePostMutate'
+import useSnack from '../Hooks/useSnack'
+
+import type { IPost } from '@/src/types/PostTypes'
 
 import TextInputField from './Includes/TextInputField'
 import AddImage from '../../../Base/ImagePreview/AddImage'
@@ -12,52 +16,66 @@ import DialogContent from '@mui/material/DialogContent'
 import LoadingButton from '@mui/lab/LoadingButton'
 import SendIcon from '@mui/icons-material/Send'
 
+const InformSnackbar = dynamic(() => import('../Includes/InformSnackbar'))
+
 const AddDialog: React.FC<{
    openAddDialog: boolean
-   uploadedPictures: FileList | null
-   postDescription: string
-   isLoading: boolean
-   handleCloseAddDialog: () => void
-   setUploadedPictures: React.Dispatch<React.SetStateAction<FileList | null>>
-   setPostDescription: React.Dispatch<React.SetStateAction<string>>
-   postMutate: UseMutateFunction<AxiosResponse<any, any>, unknown, React.FormEvent<Element>, unknown>
-}> = ({
-   handleCloseAddDialog,
-   openAddDialog,
-   setUploadedPictures,
-   setPostDescription,
-   postDescription,
-   uploadedPictures,
-   postMutate,
-   isLoading,
-}) => {
+   addNewPost: (newPost: IPost) => void
+   setAddDialogOpen: Dispatch<SetStateAction<boolean>>
+}> = ({ openAddDialog, addNewPost, setAddDialogOpen }) => {
    const [isSendBtnDisabled, setIsSendBtnDisabled] = useState<boolean>(true)
+   const {
+      postDescription,
+      uploadedPictures,
+      setPostDescription,
+      setUploadedPictures,
+      handleDialogCloseOnSuccess,
+      handleDialogClose,
+   } = useDialog(setAddDialogOpen)
+   const { handleSnackClose, handleSnackOpenIfSuccess, isSnackOpen } = useSnack()
+   const { isLoading, postMutate } = usePostMutate(
+      postDescription,
+      uploadedPictures,
+      handleSnackOpenIfSuccess,
+      handleDialogCloseOnSuccess,
+      setIsSendBtnDisabled,
+      addNewPost
+   )
+
    const chancgeTextField = (event: React.ChangeEvent<HTMLInputElement>) => {
       setPostDescription(event.target.value)
       setIsSendBtnDisabled(event.target.value.length <= 1)
    }
 
    return (
-      <Dialog fullWidth maxWidth='sm' open={openAddDialog} onClose={handleCloseAddDialog}>
-         <DialogHeader handleCloseAddDialog={handleCloseAddDialog} />
-         <DialogContent>
-            <TextInputField onChange={chancgeTextField} textValue={postDescription} />
-            <AddImage setUploadedPictures={setUploadedPictures} uploadedPictures={uploadedPictures} />
-         </DialogContent>
-         <DialogActions>
-            <LoadingButton
-               disabled={isSendBtnDisabled}
-               onClick={(event) => postMutate(event)}
-               loading={isLoading}
-               loadingPosition='start'
-               startIcon={<SendIcon />}
-               fullWidth
-               variant='contained'
-               color='primary'>
-               Közzététel
-            </LoadingButton>
-         </DialogActions>
-      </Dialog>
+      <>
+         <Dialog fullWidth maxWidth='sm' open={openAddDialog} onClose={handleDialogClose}>
+            <DialogHeader handleCloseAddDialog={handleDialogClose} />
+            <DialogContent>
+               <TextInputField onChange={chancgeTextField} textValue={postDescription} />
+               <AddImage setUploadedPictures={setUploadedPictures} uploadedPictures={uploadedPictures} />
+            </DialogContent>
+            <DialogActions>
+               <LoadingButton
+                  disabled={isSendBtnDisabled}
+                  onClick={(event) => postMutate(event)}
+                  loading={isLoading}
+                  loadingPosition='start'
+                  startIcon={<SendIcon />}
+                  fullWidth
+                  variant='contained'
+                  color='primary'
+               >
+                  Közzététel
+               </LoadingButton>
+            </DialogActions>
+         </Dialog>
+         <InformSnackbar
+            handleClose={handleSnackClose}
+            message={isSnackOpen.msg}
+            isOpen={isSnackOpen.isOpen}
+         />
+      </>
    )
 }
 
--- frontend/src/components/Posts/AddPost/AddPost.tsx
+++ frontend/src/components/Posts/AddPost/AddPost.tsx
@@ -1,39 +1,18 @@
+import { useState } from 'react'
 import dynamic from 'next/dynamic'
 import { useAppSelector } from '../../../utils/redux/store'
-
-import usePostMutate from './AddDialog/Hooks/usePostMutate'
-import useSnack from './Hooks/useSnack'
-import useDialog from './Hooks/useDialog'
 
 import StockImage from '@/assets/facebook-profile.jpg'
 import { AddPostStyle, CustomAddPostButton, CustomNextImage } from './AddPostStyle'
 import type { IPost } from '@/types/PostTypes'
 
 const AddPostDialog = dynamic(() => import('./AddDialog/AddDialog'))
-const InformSnackbar = dynamic(() => import('./Includes/InformSnackbar'))
 
 const AddPost: React.FC<{
    addNewPost: (newPost: IPost) => void
 }> = ({ addNewPost }) => {
    const profilePicture = useAppSelector((state) => state.auth.currentImage)
-   const {
-      addDialogOpen,
-      postDescription,
-      uploadedPictures,
-      setPostDescription,
-      setUploadedPictures,
-      handleDialogCloseOnSuccess,
-      handleDialogClickOpen,
-      handleDialogClose,
-   } = useDialog()
-   const { handleSnackClose, handleSnackOpenIfSuccess, isSnackOpen } = useSnack()
-   const { isLoading, postMutate } = usePostMutate(
-      postDescription,
-      uploadedPictures,
-      handleSnackOpenIfSuccess,
-      handleDialogCloseOnSuccess,
-      addNewPost
-   )
+   const [addDialogOpen, setAddDialogOpen] = useState<boolean>(false)
 
    return (
       <>
@@ -43,24 +22,19 @@ const AddPost: React.FC<{
             ) : (
                <CustomNextImage src={StockImage} width={35} height={35} alt='Sajat kép' />
             )}
-            <CustomAddPostButton onClick={handleDialogClickOpen} disableRipple variant='text' color='primary'>
+            <CustomAddPostButton
+               onClick={() => setAddDialogOpen(true)}
+               disableRipple
+               variant='text'
+               color='primary'
+            >
                Mi jár a fejedben?
             </CustomAddPostButton>
          </AddPostStyle>
          <AddPostDialog
-            isLoading={isLoading}
-            postDescription={postDescription}
+            addNewPost={addNewPost}
+            setAddDialogOpen={setAddDialogOpen}
             openAddDialog={addDialogOpen}
-            uploadedPictures={uploadedPictures}
-            postMutate={postMutate}
-            setPostDescription={setPostDescription}
-            setUploadedPictures={setUploadedPictures}
-            handleCloseAddDialog={handleDialogClose}
-         />
-         <InformSnackbar
-            handleClose={handleSnackClose}
-            message={isSnackOpen.msg}
-            isOpen={isSnackOpen.isOpen}
          />
       </>
    )
