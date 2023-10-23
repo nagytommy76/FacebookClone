d450dd1b1e54034b31891a2c6a78d1af33ca6635
nagytommy76 <nagytommy76@gmail.com>
2023-10-23 16:19:55 +0200

    Added a StyledRemoveIcon

--- frontend/src/components/Base/ImagePreview/Style.ts
+++ frontend/src/components/Base/ImagePreview/Style.ts
@@ -1,10 +1,17 @@
 import { styled } from '@mui/material'
 import Image from 'next/image'
+import IconButton from '@mui/material/IconButton'
 
 export const StyledImagePreviewContainer = styled('div')({
    height: '50%',
    display: 'flex',
    flexDirection: 'column',
+})
+
+export const StyledRemoveIcon = styled(IconButton)({
+   position: 'absolute',
+   right: 5,
+   top: 5,
 })
 
 export const StyledImagePreview = styled(Image)({
