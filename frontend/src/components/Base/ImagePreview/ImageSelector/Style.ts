f65ec7c68ab4078b0bd562bb3e1af920722b1cf8
nagytommy76 <nagytommy76@gmail.com>
2023-10-25 19:56:04 +0200

    Removed the AddImage component & modified the StyledInput

--- frontend/src/components/Base/ImagePreview/ImageSelector/Style.ts
+++ frontend/src/components/Base/ImagePreview/ImageSelector/Style.ts
@@ -1,29 +1,34 @@
 import { styled } from '@mui/material'
 export const StyledImageInputField = styled('input')({
-   opacity: 0,
-   width: 0.1,
-   height: 0.1,
+   clip: 'rect(0 0 0 0)',
+   clipPath: 'inset(50%)',
+   height: 1,
+   overflow: 'hidden',
    position: 'absolute',
+   bottom: 0,
+   left: 0,
+   whiteSpace: 'nowrap',
+   width: 1,
 })
 
-export const StyledLabelAsButton = styled('label')(({ theme }) => ({
-   position: 'relative',
-   width: '210px',
-   height: '45px',
-   borderRadius: '5px',
+// export const StyledLabelAsButton = styled('label')(({ theme }) => ({
+//    position: 'relative',
+//    width: '210px',
+//    height: '45px',
+//    borderRadius: '5px',
 
-   display: 'flex',
-   alignItems: 'center',
-   justifyContent: 'center',
+//    display: 'flex',
+//    alignItems: 'center',
+//    justifyContent: 'center',
 
-   background: theme.palette.info.main,
-   boxShadow: '0 4px 7px rgba(0,0,0,0.4)',
-   color: theme.palette.text.primary,
+//    background: theme.palette.info.main,
+//    boxShadow: '0 4px 7px rgba(0,0,0,0.4)',
+//    color: theme.palette.text.primary,
 
-   cursor: 'pointer',
-   transition: 'box-shadow .2s ease',
+//    cursor: 'pointer',
+//    transition: 'box-shadow .2s ease',
 
-   ['&:hover']: {
-      boxShadow: '0 8px 14px rgba(0,0,0,0.4)',
-   },
-}))
+//    ['&:hover']: {
+//       boxShadow: '0 8px 14px rgba(0,0,0,0.4)',
+//    },
+// }))
