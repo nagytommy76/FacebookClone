919e17ebb6f01757a4a402d2c500c1cbddb09729
nagytommy76 <nagytommy76@gmail.com>
2023-10-25 19:49:13 +0200

    Added IconsFooterStyle

---
+++ frontend/src/components/Posts/AddPost/AddDialog/Styles.ts
@@ -0,0 +1,8 @@
+import { styled } from '@mui/material'
+
+export const IconsFooterStyle = styled('div')({
+   minWidth: '150px',
+   display: 'flex',
+   flexDirection: 'row',
+   marginBottom: '1rem',
+})
