51bd672715cea412883eba6985ee65fc518dad26
nagytommy76 <nagytommy76@gmail.com>
2023-10-25 19:54:56 +0200

    Using the UseEffect in this component instead of ImageSelector

--- frontend/src/components/Base/ImagePreview/ImagePreview.tsx
+++ frontend/src/components/Base/ImagePreview/ImagePreview.tsx
@@ -1,44 +1,62 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import CloseIcon from '@mui/icons-material/Close'
 import { StyledImagePreview, StyledImagePreviewContainer, StyledRemoveIcon } from './Style'
 
 const ImagePreview: React.FC<{
-   selectedFilePreview: string[]
-   setSelectedFilePreview: Dispatch<SetStateAction<string[] | undefined>>
-}> = ({ selectedFilePreview, setSelectedFilePreview }) => {
+   uploadedPictures: FileList | null
+}> = ({ uploadedPictures }) => {
+   const [selectedFilePreview, setSelectedFilePreview] = useState<string[] | undefined>(undefined)
    const removeSingleImgOnClick = (filePreview: string) => {
       setSelectedFilePreview((prev) => prev?.filter((image) => image != filePreview))
    }
 
+   useEffect(() => {
+      if (!uploadedPictures) {
+         setSelectedFilePreview(undefined)
+         return
+      }
+      let objectUrl: string[] = []
+      for (let index = 0; index < uploadedPictures.length; index++) {
+         objectUrl.push(URL.createObjectURL(uploadedPictures[index]))
+      }
+      setSelectedFilePreview(objectUrl)
+   }, [uploadedPictures])
+
    return (
-      <StyledImagePreviewContainer>
-         {selectedFilePreview.map((filePreview, index) => (
-            <div
-               key={index}
-               style={{
-                  position: 'relative',
-               }}
-            >
-               <StyledImagePreview
-                  key={index}
-                  width={500}
-                  height={200}
-                  alt={filePreview}
-                  src={filePreview}
-               />
-               <StyledRemoveIcon
-                  onClick={() => removeSingleImgOnClick(filePreview)}
-                  color='info'
-                  sx={{
-                     backgroundColor: '#555',
-                  }}
-               >
-                  <CloseIcon fontSize='inherit' />
-               </StyledRemoveIcon>
-            </div>
-         ))}
-      </StyledImagePreviewContainer>
+      <>
+         {!selectedFilePreview ? (
+            <></>
+         ) : (
+            <StyledImagePreviewContainer>
+               {selectedFilePreview.map((filePreview, index) => (
+                  <div
+                     key={index}
+                     style={{
+                        position: 'relative',
+                     }}
+                  >
+                     <StyledImagePreview
+                        key={index}
+                        width={500}
+                        height={200}
+                        alt={filePreview}
+                        src={filePreview}
+                     />
+                     <StyledRemoveIcon
+                        onClick={() => removeSingleImgOnClick(filePreview)}
+                        color='info'
+                        sx={{
+                           backgroundColor: '#555',
+                        }}
+                     >
+                        <CloseIcon fontSize='inherit' />
+                     </StyledRemoveIcon>
+                  </div>
+               ))}
+            </StyledImagePreviewContainer>
+         )}
+      </>
    )
 }
 
