3644d71de2caa179dcc7854c8171e5f469819189
nagytommy76 <nagytommy76@gmail.com>
2023-10-30 18:39:39 +0100

    Added the removeSinglePostById fn to the post context

--- frontend/src/components/MainPage/Context/PostContextProvider.tsx
+++ frontend/src/components/MainPage/Context/PostContextProvider.tsx
@@ -5,10 +5,12 @@ import { IPost } from '@/types/PostTypes'
 interface IPostsContext {
    postsDispatch: React.Dispatch<IPostsAction>
    postsReducer: InitialPostsState
+   removeSinglePostById: (toDeletePostId: string) => void
 }
 
 export const PostContext = createContext<IPostsContext>({
    postsDispatch: () => {},
+   removeSinglePostById(toDeletePostId) {},
    postsReducer: {
       singlePost: initialPostsState.singlePost,
       commentsLength: initialPostsState.commentsLength,
@@ -18,9 +20,11 @@ export const PostContext = createContext<IPostsContext>({
 export default function PostContextProvider({
    children,
    singlePost,
+   removeSinglePostById,
 }: {
    children: React.ReactNode
    singlePost: IPost
+   removeSinglePostById: (toDeletePostId: string) => void
 }) {
    const [postsReducer, postsDispatch] = useReducer(PostsReducer, initialPostsState)
    useEffect(() => {
@@ -29,5 +33,9 @@ export default function PostContextProvider({
          postsDispatch({ type: 'SET_COMMENTS_LENGTH', payload: singlePost.comments.length })
       }
    }, [singlePost])
-   return <PostContext.Provider value={{ postsDispatch, postsReducer }}>{children}</PostContext.Provider>
+   return (
+      <PostContext.Provider value={{ postsDispatch, postsReducer, removeSinglePostById }}>
+         {children}
+      </PostContext.Provider>
+   )
 }
