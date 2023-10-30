b3f81b5d19cea1fe63be83f7b2177f9de366ace1
nagytommy76 <nagytommy76@gmail.com>
2023-10-30 18:42:04 +0100

    Using the ConfirmDeletePost component

--- frontend/src/components/Posts/SinglePost/Includes/PostHeader/Includes/OptionsMenu.tsx
+++ frontend/src/components/Posts/SinglePost/Includes/PostHeader/Includes/OptionsMenu.tsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import useDeletePost from '../Hooks/useDeletePost'
+import dynamic from 'next/dynamic'
 
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import IconButton from '@mui/material/IconButton'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 
-const OptionsMenu: React.FC<{ removeSinglePostById: (toDeletePostId: string) => void }> = ({
-   removeSinglePostById,
-}) => {
+const ConfirmDeletePost = dynamic(() => import('./ConfirmDeletePost'), {
+   loading: () => <h1>Töltés egyelőre</h1>,
+})
+
+const OptionsMenu = () => {
    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false)
    const open = Boolean(anchorEl)
    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
       setAnchorEl(event.currentTarget)
@@ -17,23 +20,24 @@ const OptionsMenu: React.FC<{ removeSinglePostById: (toDeletePostId: string) => 
    const handleClose = () => {
       setAnchorEl(null)
    }
-   const { postDeleteMutation } = useDeletePost(removeSinglePostById)
-
-   const handleDeleteClick = () => {
-      postDeleteMutation()
-      handleClose()
+   const openConfirmDialog = () => {
+      setIsDialogOpen(true)
+      setAnchorEl(null)
    }
 
    return (
-      <div style={{ position: 'absolute', right: 5 }}>
-         <IconButton onClick={handleClick}>
-            <MoreHorizIcon fontSize='inherit' />
-         </IconButton>
-         <Menu id='delete-post-menu' anchorEl={anchorEl} open={open} onClose={handleClose}>
-            <MenuItem onClick={handleClose}>Módosítás</MenuItem>
-            <MenuItem onClick={handleDeleteClick}>Törlés</MenuItem>
-         </Menu>
-      </div>
+      <>
+         <div style={{ position: 'absolute', right: 5 }}>
+            <IconButton onClick={handleClick}>
+               <MoreHorizIcon fontSize='inherit' />
+            </IconButton>
+            <Menu id='delete-post-menu' anchorEl={anchorEl} open={open} onClose={handleClose}>
+               <MenuItem onClick={handleClose}>Módosítás</MenuItem>
+               <MenuItem onClick={openConfirmDialog}>Törlés</MenuItem>
+            </Menu>
+         </div>
+         <ConfirmDeletePost isOpen={isDialogOpen} setIsDialogOpen={setIsDialogOpen} />
+      </>
    )
 }
 
