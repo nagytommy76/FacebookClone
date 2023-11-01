25c473f0160054d53081a9226feaad7867073fac
nagytommy76 <nagytommy76@gmail.com>
2023-11-01 10:50:05 +0100

    Using the imageView componet & the postedPicturesPath context prop

--- frontend/src/components/Base/ImagePreview/ImagePreview.tsx
+++ frontend/src/components/Base/ImagePreview/ImagePreview.tsx
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { PostContext } from '@/PostContext/PostContextProvider'
 
-import CloseIcon from '@mui/icons-material/Close'
-import { StyledImagePreview, StyledImagePreviewContainer, StyledRemoveIcon } from './Style'
+import ImageView from './Includes/ImageView'
 
 const ImagePreview: React.FC<{
    uploadedPictures: FileList | null
 }> = ({ uploadedPictures }) => {
-   const [selectedFilePreview, setSelectedFilePreview] = useState<string[] | undefined>(undefined)
-   const removeSingleImgOnClick = (filePreview: string) => {
-      setSelectedFilePreview((prev) => prev?.filter((image) => image != filePreview))
-   }
+   const {
+      postsReducer: {
+         singlePost: { postedPicturesPath },
+      },
+   } = useContext(PostContext)
+   const [selectedFilePreview, setSelectedFilePreview] = useState<string[] | null>(null)
+   const [currentFilePreview, setCurrentFilePreview] = useState<string[] | null>(postedPicturesPath)
 
    useEffect(() => {
       if (!uploadedPictures) {
-         setSelectedFilePreview(undefined)
+         setSelectedFilePreview(null)
          return
       }
       let objectUrl: string[] = []
@@ -25,36 +28,17 @@ const ImagePreview: React.FC<{
 
    return (
       <>
-         {!selectedFilePreview ? (
-            <></>
-         ) : (
-            <StyledImagePreviewContainer>
-               {selectedFilePreview.map((filePreview, index) => (
-                  <div
-                     key={index}
-                     style={{
-                        position: 'relative',
-                     }}
-                  >
-                     <StyledImagePreview
-                        key={index}
-                        width={500}
-                        height={200}
-                        alt={filePreview}
-                        src={filePreview}
-                     />
-                     <StyledRemoveIcon
-                        onClick={() => removeSingleImgOnClick(filePreview)}
-                        color='info'
-                        sx={{
-                           backgroundColor: '#555',
-                        }}
-                     >
-                        <CloseIcon fontSize='inherit' />
-                     </StyledRemoveIcon>
-                  </div>
-               ))}
-            </StyledImagePreviewContainer>
+         {selectedFilePreview !== null && (
+            <ImageView
+               selectedFilePreview={selectedFilePreview}
+               setSelectedFilePreview={setSelectedFilePreview}
+            />
+         )}
+         {currentFilePreview !== null && (
+            <ImageView
+               selectedFilePreview={currentFilePreview}
+               setSelectedFilePreview={setCurrentFilePreview}
+            />
          )}
       </>
    )
