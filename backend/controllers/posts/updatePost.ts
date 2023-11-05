eea7ab9c2f302b5c3087b7a72fae15409c7ef9ef
nagytommy76 <nagytommy76@gmail.com>
2023-11-05 14:03:46 +0100

    Working on the post update controller

--- backend/controllers/posts/updatePost.ts
+++ backend/controllers/posts/updatePost.ts
@@ -4,13 +4,38 @@ import { Posts as PostModel } from '../../models/posts/posts'
 
 interface IPostUpdateRequest extends IJWTUserType {
    body: {
+      postId: string
       postDescription: string
-      modifiedImageLinks: string[]
+      modifiedImageLinks: string[] | null
+      newAddedImageLinks: string[] | null
    }
 }
 
-export const updatePostController = (request: IPostUpdateRequest, response: Response) => {
-   const { modifiedImageLinks, postDescription } = request.body
+export const updatePostController = async (request: IPostUpdateRequest, response: Response) => {
+   const userId = request.user?.userId
+   const { postId, modifiedImageLinks, newAddedImageLinks, postDescription } = request.body
+   try {
+      let mergedImages: string[] | null = []
 
-   response.status(200).json({ msg: 'Sikeres poszt módosítása' })
+      if (modifiedImageLinks !== null) {
+         console.log(modifiedImageLinks)
+         mergedImages = mergedImages.concat(modifiedImageLinks)
+      } else if (newAddedImageLinks !== null) {
+         console.log(newAddedImageLinks)
+         mergedImages = mergedImages.concat(newAddedImageLinks)
+      } else {
+         console.log(mergedImages)
+         mergedImages = null
+      }
+
+      const foundPost = await PostModel.updateOne({ _id: postId, userId: userId }, [
+         { $set: { description: postDescription } },
+         { $set: { postedPicturesPath: mergedImages } },
+      ])
+
+      response.status(200).json({ modifiedImageLinks, postDescription, foundPost })
+   } catch (error) {
+      console.log(error)
+      response.status(500).json({ error })
+   }
 }
