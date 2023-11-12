436c378995b2e8619e9210705adab69ce2f01821
nagytommy76 <nagytommy76@gmail.com>
2023-11-12 11:52:23 +0100

    Working on the upload image before axios sent functionality

--- frontend/src/components/Posts/HandlePosts/ModifyPost/Hooks/useModifyPostFn.tsx
+++ frontend/src/components/Posts/HandlePosts/ModifyPost/Hooks/useModifyPostFn.tsx
@@ -1,30 +1,35 @@
-import { useContext } from 'react'
+import { useContext, Dispatch, SetStateAction } from 'react'
 import { PostContext } from '@/PostContext/PostContextProvider'
+import { ImageContext } from '../../Context/ImageContextProvider'
 import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
 
 import useUploadFirebase from '../../Hooks/useUploadFirebase'
 
 const useModifyPostFn = (
    modifiedImageLinks: string[] | null,
-   postDescription: string | null,
-   newUploadedImages: File[] | null
+   modifiedDescription: string | null,
+   newUploadedImages: File[] | null,
+   setNewFirebaseImageLinks: Dispatch<SetStateAction<string[] | null>>
 ) => {
    const {
-      postsDispatch,
+      // postsDispatch,
       postsReducer: {
          singlePost: { _id },
       },
    } = useContext(PostContext)
+   const { imageDispatch } = useContext(ImageContext)
    const { handleMultipleImageUploadToFirebase } = useUploadFirebase()
    const handlePostMutateFn = async () => {
       let returnedImagePaths: string[] | null = null
       if (newUploadedImages) {
          returnedImagePaths = await handleMultipleImageUploadToFirebase(newUploadedImages)
-         postsDispatch({ type: 'ADD_UPLOADED_IMAGES', payload: returnedImagePaths })
+         // postsDispatch({ type: 'ADD_UPLOADED_IMAGES', payload: returnedImagePaths })
+         setNewFirebaseImageLinks(returnedImagePaths)
+         // imageDispatch({ type: 'SET_FIREBASE_IMAGES', payload: returnedImagePaths })
       }
       return await axios.put('/post/edit/update-post', {
          postId: _id,
-         postDescription,
+         postDescription: modifiedDescription,
          modifiedImageLinks,
          newAddedImageLinks: returnedImagePaths,
       })
