96bd399af08cd7e221ecb2c213b72a3af143741b
nagytommy76 <nagytommy76@gmail.com>
2023-11-13 19:25:29 +0100

    Using Image reducer instead of props

--- frontend/src/components/Posts/HandlePosts/ModifyPost/Hooks/useModifyPostFn.tsx
+++ frontend/src/components/Posts/HandlePosts/ModifyPost/Hooks/useModifyPostFn.tsx
@@ -1,38 +1,32 @@
-import { useContext, Dispatch, SetStateAction } from 'react'
+import { useContext } from 'react'
 import { PostContext } from '@/PostContext/PostContextProvider'
 import { ImageContext } from '../../Context/ImageContextProvider'
 import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
 
 import useUploadFirebase from '../../Hooks/useUploadFirebase'
 
-const useModifyPostFn = (
-   modifiedImageLinks: string[] | null,
-   modifiedDescription: string | null,
-   newUploadedImages: File[] | null,
-   setNewFirebaseImageLinks: Dispatch<SetStateAction<string[] | null>>
-) => {
+const useModifyPostFn = (modifiedDescription: string | null) => {
    const {
-      // postsDispatch,
       postsReducer: {
          singlePost: { _id },
       },
    } = useContext(PostContext)
-   const { imageDispatch } = useContext(ImageContext)
+   const {
+      imageReducer: { newUploadedImages, uploadedImages },
+   } = useContext(ImageContext)
+
    const { handleMultipleImageUploadToFirebase } = useUploadFirebase()
    const handlePostMutateFn = async () => {
       let returnedImagePaths: string[] | null = null
       if (newUploadedImages) {
          returnedImagePaths = await handleMultipleImageUploadToFirebase(newUploadedImages)
-         // postsDispatch({ type: 'ADD_UPLOADED_IMAGES', payload: returnedImagePaths })
-         setNewFirebaseImageLinks(returnedImagePaths)
-         // imageDispatch({ type: 'SET_FIREBASE_IMAGES', payload: returnedImagePaths })
       }
-      return await axios.put('/post/edit/update-post', {
+      return (await axios.put('/post/edit/update-post', {
          postId: _id,
          postDescription: modifiedDescription,
-         modifiedImageLinks,
+         modifiedImageLinks: uploadedImages,
          newAddedImageLinks: returnedImagePaths,
-      })
+      })) as AxiosResponse<{ newImagesLinks: string[] | null }>
    }
    return { handlePostMutateFn }
 }
