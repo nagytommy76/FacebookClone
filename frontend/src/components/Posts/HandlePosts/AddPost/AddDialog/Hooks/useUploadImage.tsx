d912bae949b1aad5af3fd86e5690eb10b8619309
nagytommy76 <nagytommy76@gmail.com>
2023-11-19 11:33:20 +0100

    Created a handleUploadNewImages fn to upload images after post created

---
+++ frontend/src/components/Posts/HandlePosts/AddPost/AddDialog/Hooks/useUploadImage.tsx
@@ -0,0 +1,31 @@
+import { useContext } from 'react'
+import { axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+
+import type { IPost } from '@/src/types/PostTypes'
+import { ImageContext } from '../../../Context/ImageContextProvider'
+import useUploadFirebase from '../../../Hooks/useUploadFirebase'
+
+const useUploadImage = () => {
+   const {
+      imageReducer: { newUploadedImages },
+   } = useContext(ImageContext)
+   const { handleMultipleImageUploadToFirebase } = useUploadFirebase()
+
+   const handleUploadNewImages = async (createdPost: IPost) => {
+      if (newUploadedImages) {
+         let uploadedImagesPaths: string[] | null = null
+         uploadedImagesPaths = await handleMultipleImageUploadToFirebase(newUploadedImages, createdPost._id)
+         // Itt kell egy újabb AXIOS call /post/save-post-image
+         // Majd hozzá kell fűzni a kép[]-t a data.data.createdPost-hoz és ezt az obj-t átadni a addNewPost-nrk
+
+         const response = await axios.put('/post/save-post-image', {
+            postId: createdPost._id,
+            postedPicturesPath: uploadedImagesPaths,
+         })
+         if (response.status === 201) createdPost.postedPicturesPath = uploadedImagesPaths
+      }
+   }
+   return handleUploadNewImages
+}
+
+export default useUploadImage
