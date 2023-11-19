3be664685c9b101ad6e07685055710b69e498a88
nagytommy76 <nagytommy76@gmail.com>
2023-11-19 14:56:33 +0100

    Separated the commentMutationFn

---
+++ frontend/src/components/Posts/SinglePost/AddComment/Hook/useCommentMutation.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { AxiosResponse, axiosInstance as axios } from '@/axios/AxiosInstance'
+import useUploadFirebase from '../../../HandlePosts/Hooks/useUploadFirebase'
+import type { IPostComment } from '@/src/types/LikeTypes'
+
+const useCommentMutation = (commentImagePath: FileList | null, commentText: string, postID: string) => {
+   const { handleSingleImageUploadToFirebase } = useUploadFirebase()
+   const commentMutationFn = async () => {
+      let imageUrl: string | null = null
+      if (commentImagePath) {
+         imageUrl = await handleSingleImageUploadToFirebase(commentImagePath[0], postID, true)
+      }
+      const response = (await axios.post('/post/post-comment-add', {
+         answeredAt: new Date(),
+         comment: commentText,
+         commentImage: imageUrl,
+         postId: postID,
+      })) as AxiosResponse<{ comments: IPostComment[] }>
+      return response.data
+   }
+   return commentMutationFn
+}
+
+export default useCommentMutation
