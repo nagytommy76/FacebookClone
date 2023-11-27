95a6fb22f2f8ff3dcf94dff11592f3fc675874fd
nagytommy76 <nagytommy76@gmail.com>
2023-11-27 19:18:39 +0100

    Separated the removeBtn & added a fade transition

---
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/CommentImage/Includes/RemoveImgBtn.tsx
@@ -0,0 +1,38 @@
+import { useContext } from 'react'
+import { CommentContext } from '../../../../../Context/CommentContext'
+
+import { IconButtonStyle } from '../Styles'
+import IconButton from '@mui/material/IconButton'
+import DeleteIcon from '@mui/icons-material/Delete'
+import Fade from '@mui/material/Fade'
+
+const RemoveImgBtn: React.FC<{
+   isUpdateActive: boolean
+   commentImage: string | null
+}> = ({ commentImage, isUpdateActive }) => {
+   const { commentDispatch } = useContext(CommentContext)
+   const removeExistedImg = () => {
+      commentDispatch({ type: 'REMOVE_COMMENT_IMAGE', payload: null })
+   }
+
+   return (
+      <>
+         {isUpdateActive && commentImage !== null && (
+            <Fade in={isUpdateActive && commentImage !== null}>
+               <IconButtonStyle>
+                  <IconButton
+                     onClick={removeExistedImg}
+                     color='error'
+                     size='medium'
+                     aria-label='delete comment image'
+                  >
+                     <DeleteIcon fontSize='inherit' />
+                  </IconButton>
+               </IconButtonStyle>
+            </Fade>
+         )}
+      </>
+   )
+}
+
+export default RemoveImgBtn
