4a09552c8e6ad6c4cf26ecb4029c7c42e1c9fd0c
nagytommy76 <nagytommy76@gmail.com>
2023-12-07 19:21:42 +0100

    Added an onError & solved the img delete after update & passed the answerId

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Hooks/useUpdateAnswer.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Hooks/useUpdateAnswer.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, Dispatch, SetStateAction } from 'react'
 import { useMutation } from '@tanstack/react-query'
 import { AxiosResponse, axiosInstance as axios } from '@/axios/AxiosInstance'
 
@@ -9,18 +9,21 @@ import useDeleteImage from '@/src/hooks/useDeleteImage'
 const useUpdateAnswer = (
    modifiedText: string,
    modifiedImageLink: FileList | null,
-   setStatesToDefault: () => void
+   answerId: string,
+   setStatesToDefault: () => void,
+   setIsError: Dispatch<SetStateAction<boolean>>
 ) => {
    const { handleSingleImageUploadToFirebase } = useUploadFirebase()
    const deleteImageFromFirebase = useDeleteImage()
 
    const {
       answerDispatch,
+      getAnswerImageById,
       answerReducer: { removedAnswerImageLink, postId, commentId },
    } = useContext(AnswerContext)
    let newUploadedImage: string | null = null
 
-   const updateAnswerMutateFn = async (answerId: string) => {
+   const updateAnswerMutateFn = async () => {
       if (modifiedImageLink !== null) {
          newUploadedImage = await handleSingleImageUploadToFirebase(modifiedImageLink[0], postId, true)
       }
@@ -37,18 +40,26 @@ const useUpdateAnswer = (
    const { mutate } = useMutation({
       mutationKey: ['updateCommentAnswer'],
       mutationFn: updateAnswerMutateFn,
-      onSuccess: async (data, variables) => {
+      onSuccess: async (data) => {
          // Feltölteni a képet mert van új és kicserélni ha nem törli a régit!?
          if (removedAnswerImageLink !== null) deleteImageFromFirebase([removedAnswerImageLink])
          answerDispatch({ type: 'SET_REMOVED_ANSWER_IMG_LINK', payload: null })
          // Itt letörlöm a meglévő képet ha esetleg nem törölte volna le a user
-         //   if (commentImage !== null) await deleteImageFromFirebase([commentImage])
+         const answerImage = getAnswerImageById(answerId)
+         if (answerImage !== null && answerImage !== undefined) await deleteImageFromFirebase([answerImage])
 
          answerDispatch({
             type: 'UPDATE_SINGLE_COMMENT_ANSWER',
-            payload: { modifiedText, answerID: variables, commentImage: data.uploadedImageLink },
+            payload: { modifiedText, answerID: answerId, commentImage: data.uploadedImageLink },
          })
          setStatesToDefault()
+      },
+      onError: async () => {
+         //  Esetleg itt egy kép törlése firebase-ről, ha nem sikerül a backend-en valami
+         setIsError(true)
+         if (newUploadedImage !== null) await deleteImageFromFirebase([newUploadedImage])
+         setStatesToDefault()
+         setTimeout(() => setIsError(false), 7500)
       },
    })
 
