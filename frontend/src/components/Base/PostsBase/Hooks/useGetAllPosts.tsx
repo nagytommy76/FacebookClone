0a3de878f577e023add7b8ea57fa5f42a6954205
nagytommy76 <nagytommy76@gmail.com>
2023-12-13 19:13:51 +0100

    Working on the load posts on url change & added getUserPosts fn

--- frontend/src/components/Base/PostsBase/Hooks/useGetAllPosts.tsx
+++ frontend/src/components/Base/PostsBase/Hooks/useGetAllPosts.tsx
@@ -2,6 +2,16 @@ import { useQuery } from '@tanstack/react-query'
 import { axiosInstance as axios, AxiosResponse } from '@/utils/axiosSetup/AxiosInstance'
 import { IPost } from '@/types/PostTypes'
 
+const getUserPosts = async () => {
+   try {
+      const response = await axios.get('/post/get-user-posts')
+      return response as AxiosResponse<{ allPosts: IPost[] }>
+   } catch (error) {
+      console.log(error)
+      return Promise.reject(new Error())
+   }
+}
+
 const fetchAllQueries = async () => {
    try {
       const response = await axios.get('/post/get-posts')
@@ -11,10 +21,14 @@ const fetchAllQueries = async () => {
    }
 }
 
-const useGetAllPosts = (setAllPosts: React.Dispatch<React.SetStateAction<IPost[]>>) => {
+const useGetAllPosts = (
+   setAllPosts: React.Dispatch<React.SetStateAction<IPost[]>>,
+   isGetUsersPosts: boolean = false,
+   isUrlChanged: boolean = false
+) => {
    const { isLoading } = useQuery({
-      queryKey: ['getAllPosts'],
-      queryFn: fetchAllQueries,
+      queryKey: ['getAllPosts', isUrlChanged],
+      queryFn: isGetUsersPosts ? getUserPosts : fetchAllQueries,
       onSuccess(data) {
          if (data) setAllPosts(data.data.allPosts)
       },
