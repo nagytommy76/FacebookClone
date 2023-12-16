e70a0ef986914e58e62e0491c182853a42939851
nagytommy76 <nagytommy76@gmail.com>
2023-12-16 09:28:05 +0100

    Separated the handle posts logic

---
+++ frontend/src/components/Base/PostsBase/Hooks/useHandlePosts.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import type { IPost } from '@/src/types/PostTypes'
+/**
+ * @returns allPosts
+ * @type IPost[]
+ */
+const useHandlePosts = () => {
+   const [allPosts, setAllPosts] = useState<IPost[]>([])
+   const addNewPost = (newPost: IPost) => {
+      setAllPosts([...allPosts, newPost])
+   }
+   const removeSinglePostById = (toDeletePostId: string) => {
+      setAllPosts((prevPosts) => {
+         return prevPosts.filter((post) => post._id !== toDeletePostId)
+      })
+   }
+
+   return {
+      allPosts,
+      setAllPosts,
+      addNewPost,
+      removeSinglePostById,
+   }
+}
+
+export default useHandlePosts
