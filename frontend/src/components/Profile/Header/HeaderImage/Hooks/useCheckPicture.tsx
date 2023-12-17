30b5e2a9e92219eee9a9f89e7e01bb975223df86
nagytommy76 <nagytommy76@gmail.com>
2023-12-17 13:21:50 +0100

    Moved the handleSetUploadPictures & using the profileReducer's _id

--- frontend/src/components/Profile/Header/HeaderImage/Hooks/useCheckPicture.tsx
+++ frontend/src/components/Profile/Header/HeaderImage/Hooks/useCheckPicture.tsx
@@ -1,8 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { ProfileContext } from '../../../Context/ProfileContextProvider'
+import usePictureMutate from './usePictureMutate'
+import useUploadFirebase from '@/hooks/useUploadFirebase'
 
 const useCheckPicture = () => {
    const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true)
    const [uploadedPictures, setUploadedPictures] = useState<FileList | null>(null)
+   const mutationFunction = usePictureMutate()
+   const { handleSingleProfileImageUploadToFirebase } = useUploadFirebase()
+   const {
+      profileReducer: {
+         initialUserDataState: { _id },
+      },
+   } = useContext(ProfileContext)
 
    useEffect(() => {
       if (!uploadedPictures) setIsButtonDisabled(true)
@@ -11,11 +21,23 @@ const useCheckPicture = () => {
    const addPictures = (e: React.ChangeEvent<HTMLInputElement>) => {
       setUploadedPictures(e.target.files)
    }
+
+   const handleSetUploadPictures = async () => {
+      if (!uploadedPictures) return
+      // Először feltöltöm az 1DB képet firebase/userId/profileImage mappába ha ez megvan ->
+      const uploadedPicturePath = await handleSingleProfileImageUploadToFirebase(
+         uploadedPictures.item(0) as File,
+         _id
+      )
+      // Elküldöm az adatbázisnak a visszakapott url-t
+      mutationFunction(uploadedPicturePath as string)
+      setUploadedPictures(null)
+   }
+
    return {
-      uploadedPictures,
       isButtonDisabled,
       addPictures,
-      setUploadedPictures,
+      handleSetUploadPictures,
    }
 }
 
