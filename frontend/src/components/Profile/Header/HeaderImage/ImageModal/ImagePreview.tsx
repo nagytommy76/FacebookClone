ac1ba474f036dbf42fc3229742402ce5c91df9ec
nagytommy76 <nagytommy76@gmail.com>
2023-12-18 10:29:17 +0100

    Added a tooltip + Icon button to delete a selected image

--- frontend/src/components/Profile/Header/HeaderImage/ImageModal/ImagePreview.tsx
+++ frontend/src/components/Profile/Header/HeaderImage/ImageModal/ImagePreview.tsx
@@ -3,14 +3,31 @@ import Image from 'next/image'
 
 import useGetImgURL from './useGetImgURL'
 
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import DeleteIcon from '@mui/icons-material/Delete'
 import DialogContent from '@mui/material/DialogContent'
 
-const ImagePreview: React.FC<{ uploadedPictures: FileList | null }> = ({ uploadedPictures = null }) => {
+const ImagePreview: React.FC<{ uploadedPictures: FileList | null; removePicture: () => void }> = ({
+   uploadedPictures = null,
+   removePicture,
+}) => {
    const imagePreview = useGetImgURL(uploadedPictures)
    return (
       <>
          {imagePreview && uploadedPictures && (
-            <DialogContent dividers>
+            <DialogContent dividers sx={{ position: 'relative' }}>
+               <Tooltip title='Fénykép törlése' arrow placement='top'>
+                  <IconButton
+                     onClick={removePicture}
+                     sx={{ position: 'absolute', right: 15, top: 15 }}
+                     aria-label='delete'
+                     size='large'
+                     color='warning'
+                  >
+                     <DeleteIcon fontSize='inherit' />
+                  </IconButton>
+               </Tooltip>
                <Image
                   style={{ objectFit: 'cover' }}
                   src={imagePreview}
