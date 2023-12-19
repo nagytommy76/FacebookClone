7fbf339be712c76b5b666c0297a1b42b75bf596c
nagytommy76 <nagytommy76@gmail.com>
2023-12-19 15:23:56 +0100

    Removed the selectSelectedProfilePicture fn

--- frontend/src/components/Profile/Context/ProfileContextProvider.tsx
+++ frontend/src/components/Profile/Context/ProfileContextProvider.tsx
@@ -2,7 +2,6 @@ import React, { createContext, useState, useEffect, useReducer } from 'react'
 import useGetUserData from '../Hooks/useGetUserData'
 
 import UserDetailsReducer, { initialProfileState, InitialState, IBaseListAction } from './ProfileReducer'
-import type { IProfilePicture } from '@/types/PostTypes'
 
 interface IProfileContext {
    profileReducer: InitialState
@@ -10,14 +9,12 @@ interface IProfileContext {
    tabValue: number
    handleTabChange: (event: React.SyntheticEvent, newValue: number) => void
    isDataLoading: boolean
-   selectSelectedProfilePicture: () => IProfilePicture | undefined
 }
 
 export const ProfileContext = createContext<IProfileContext>({
    profileReducer: initialProfileState,
    profileDispatch: () => {},
    handleTabChange: () => {},
-   selectSelectedProfilePicture: () => undefined,
    tabValue: 0,
    isDataLoading: true,
 })
@@ -26,12 +23,6 @@ const ProfileContextProvider: React.FC<{ children: React.ReactNode }> = ({ child
    const [profileReducer, profileDispatch] = useReducer(UserDetailsReducer, initialProfileState)
    const [tabValue, setTabValue] = useState<number>(0)
    const { data, isLoading } = useGetUserData()
-
-   const selectSelectedProfilePicture = (): IProfilePicture | undefined => {
-      return profileReducer.initialUserDataState.userDetails?.profilePicturePath.find(
-         (image) => image.isSelected
-      )
-   }
 
    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
       setTabValue(newValue)
@@ -48,7 +39,6 @@ const ProfileContextProvider: React.FC<{ children: React.ReactNode }> = ({ child
          value={{
             profileDispatch,
             profileReducer,
-            selectSelectedProfilePicture,
             handleTabChange,
             tabValue,
             isDataLoading: isLoading,
