eae5e01ebb459e44c24aaa13788b949615e49fda
nagytommy76 <nagytommy76@gmail.com>
2023-12-20 13:48:58 +0100

    Created the socket.IO instance

---
+++ frontend/src/utils/socketIo.ts
@@ -0,0 +1,8 @@
+import { io } from 'socket.io-client'
+
+// "undefined" means the URL will be computed from the `window.location` object
+// const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:6060'
+
+export const socket = io('http://localhost:3001', {
+   withCredentials: true,
+})
