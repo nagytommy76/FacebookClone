6e36dd7b264f0baeb2da4ded8311fd7a8740adac
nagytommy76 <nagytommy76@gmail.com>
2023-12-22 14:56:10 +0100

    Added the onlineFriends & getUser props to ISocketRequest

--- backend/types.ts
+++ backend/types.ts
@@ -1,7 +1,10 @@
-import { Request } from 'express'
+import type { Request } from 'express'
+import type { IOnlineFriends } from 'config/socketIo'
 import type { Server } from 'socket.io'
 import type { DefaultEventsMap } from 'socket.io/dist/typed-events'
 
 export interface ISocketRequest extends Request {
    ioSocket?: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
+   onlineFriends?: IOnlineFriends[]
+   getUser?: (userId: string) => IOnlineFriends | undefined
 }
