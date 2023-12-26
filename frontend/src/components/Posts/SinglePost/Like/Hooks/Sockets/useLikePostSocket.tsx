7b0988d245de235760a28630e67185e5a6ea88aa
nagytommy76 <nagytommy76@gmail.com>
2023-12-26 11:52:46 +0100

    Renamed & moved to the hooks folder

--- frontend/src/components/Posts/SinglePost/Like/Hooks/Sockets/useLikePostSocket.tsx
+++ frontend/src/components/Posts/SinglePost/Like/Hooks/Sockets/useLikePostSocket.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect } from 'react'
+import { useContext, useEffect } from 'react'
 import { PostContext } from '@/PostContext/PostContextProvider'
 import { socket } from '@/src/utils/socketIo'
 import type { IPostLike } from '@/src/types/LikeTypes'
@@ -6,22 +6,19 @@ import type { IPostLike } from '@/src/types/LikeTypes'
 const useSocket = () => {
    const { postsDispatch } = useContext(PostContext)
 
-   const onLikePost = useCallback(
-      (args: { likeType: IPostLike; postData: { _id: string } }[]) => {
+   useEffect(() => {
+      const onLikePost = (args: { likeType: IPostLike; postData: { _id: string } }[]) => {
          postsDispatch({
             type: 'ADD_SINGLE_SOCKET_POST_LIKE',
             payload: { likes: args[0].likeType, toModifyPostId: args[0].postData._id },
          })
-      },
-      [postsDispatch]
-   )
+      }
 
-   useEffect(() => {
       socket.on('likedPost', onLikePost)
       return () => {
          socket.off('likedPost', onLikePost)
       }
-   }, [onLikePost])
+   }, [postsDispatch])
 
    return null
 }
