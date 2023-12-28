edcb28761a4a51bc9c3c9417fc7c16f3aa7a8a8f
nagytommy76 <nagytommy76@gmail.com>
2023-12-28 14:16:13 +0100

    Added 3 styles

--- frontend/src/components/Navbar/Includes/Notification/NotificationsMenu/Style.tsx
+++ frontend/src/components/Navbar/Includes/Notification/NotificationsMenu/Style.tsx
@@ -1,5 +1,31 @@
 import { styled } from '@mui/material'
 import Image from 'next/image'
+import MenuItem from '@mui/material/MenuItem'
+
+export const StyledMenuItem = styled(MenuItem)({
+   width: '450px',
+   position: 'relative',
+})
+
+export const StyledTextArea = styled('div')({
+   width: '100%',
+   display: 'flex',
+   flexDirection: 'column',
+})
+
+export const StyledIsActive = styled('span', {
+   shouldForwardProp: (prop) => prop !== 'isRead',
+})(({ isRead = false }: { isRead: boolean }) => ({
+   position: 'absolute',
+   top: 10,
+   right: 10,
+
+   width: '10px',
+   height: '10px',
+   backgroundColor: 'green',
+   borderRadius: '50%',
+   display: `${isRead ? 'none' : 'block'}`,
+}))
 
 export const StyledImage = styled(Image)({
    borderRadius: '50%',
