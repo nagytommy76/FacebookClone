ff2c3686a411f10f560c317936072773753256df
nagytommy76 <nagytommy76@gmail.com>
2024-01-01 12:40:09 +0100

    Added an image to the right side

--- frontend/src/components/Auth/Styles.ts
+++ frontend/src/components/Auth/Styles.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 import Paper from '@mui/material/Paper'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export const StyledAuthContainer = styled('section')({
    height: '100%',
@@ -24,11 +25,33 @@ export const StyledRegisterPaper = styled(Paper)({
 })
 
 // Login
-export const StyledLoginPaper = styled(Paper)({
+export const StyledLoginPaper = styled('div')({
+   width: '1200px',
+   height: '700px',
+   display: 'flex',
+})
+
+export const StyledLeftContainer = styled(Paper)({
+   height: '100%',
+   display: 'flex',
+   alignItems: 'center',
+})
+
+export const StyledLoginForm = styled('form')({
    ...BaseStyledPaper,
    width: '400px',
    height: '340px',
    alignItems: 'center',
+
+   flex: 1,
+})
+
+export const StyledImage = styled(Image)({
+   width: '600px',
+   height: '100%',
+   objectFit: 'cover',
+
+   flex: 2,
 })
 
 export const StyledLink = styled(Link)({
