1824fd8be4622f45172cf152ebf78a1cc2ecfeaa
nagytommy76 <nagytommy76@gmail.com>
2024-01-02 12:02:18 +0100

    Using the RemoveNotification

--- frontend/src/components/Navbar/Includes/Notification/NotificationsMenu/NotificationsMenu.tsx
+++ frontend/src/components/Navbar/Includes/Notification/NotificationsMenu/NotificationsMenu.tsx
@@ -4,10 +4,11 @@ import { NotificationsContext } from '../Context/NotificationContextProvider'
 
 import useSetActiveNotifications from '../Hooks/useSetActiveNotifications'
 
-import { StyledImage, StyledMenuItem, StyledIsActive, StyledTextArea } from './Style'
+import { StyledImage, StyledMenuItem, StyledIsActive, StyledTextArea, StyledMenuContainer } from './Style'
 import Menu from '@mui/material/Menu'
 import Typography from '@mui/material/Typography'
 
+import RemoveNotification from '../Includes/RemoveNotification'
 import TimeAgo from '../Includes/TimeAgo'
 
 const NotificationsMenu: React.FC<{
@@ -31,15 +32,14 @@ const NotificationsMenu: React.FC<{
                id='notifications-menu'
                open={open}
                onClose={handleClose}
-               onClick={handleClose}
+               // onClick={handleClose}
                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
             >
                {notifications.map((notification) => (
-                  <Link key={notification._id} href={`#${notification.postData?.postId}`}>
+                  <StyledMenuContainer key={notification._id}>
                      <StyledMenuItem
-                        key={notification._id}
-                        onClick={() => handleSetInactiveAndClose(notification._id, notification.isRead)}
+                        // onClick={() => handleSetInactiveAndClose(notification._id, notification.isRead)}
                         sx={{ maxWidth: '400px', marginBottom: 2 }}
                      >
                         <StyledImage
@@ -48,21 +48,26 @@ const NotificationsMenu: React.FC<{
                            height={65}
                            width={65}
                         />
-                        <StyledTextArea>
-                           <Typography variant='h6' gutterBottom>
-                              {notification.userDetails.firstName} {notification.userDetails.sureName}
-                           </Typography>
-                           <Typography variant='caption' gutterBottom>
-                              Likeolta a bejegyzésedet:
-                           </Typography>
-                           <Typography variant='caption' noWrap sx={{ width: '280px' }}>
-                              {notification.postData.description}
-                           </Typography>
-                           <TimeAgo createdAt={notification.createdAt} />
-                        </StyledTextArea>
+                        <Link href={`#${notification.postData?.postId}`}>
+                           <StyledTextArea
+                              onClick={() => handleSetInactiveAndClose(notification._id, notification.isRead)}
+                           >
+                              <Typography variant='h6' gutterBottom>
+                                 {notification.userDetails.firstName} {notification.userDetails.sureName}
+                              </Typography>
+                              <Typography variant='caption' gutterBottom>
+                                 Likeolta a bejegyzésedet:
+                              </Typography>
+                              <Typography variant='caption' noWrap sx={{ width: '280px' }}>
+                                 {notification.postData.description}
+                              </Typography>
+                              <TimeAgo createdAt={notification.createdAt} />
+                           </StyledTextArea>
+                        </Link>
                         <StyledIsActive isRead={notification.isRead} />
                      </StyledMenuItem>
-                  </Link>
+                     <RemoveNotification notificationId={notification._id} />
+                  </StyledMenuContainer>
                ))}
             </Menu>
          )}
