174ebf84236a472e54c50d045072e92ab5a95b7a
nagytommy76 <nagytommy76@gmail.com>
2024-01-03 11:54:26 +0100

    Added a tooltip & using the mutate fn

--- frontend/src/components/Navbar/Includes/Notification/Includes/RemoveNotification.tsx
+++ frontend/src/components/Navbar/Includes/Notification/Includes/RemoveNotification.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import useRemoveNotification from '../Hooks/useRemoveNotification'
 
 import IconButton from '@mui/material/IconButton'
 import DeleteIcon from '@mui/icons-material/Delete'
+import Tooltip from '@mui/material/Tooltip'
 
 const RemoveNotification: React.FC<{ notificationId: string }> = ({ notificationId }) => {
+   const removeMutate = useRemoveNotification(notificationId)
    return (
-      <IconButton aria-label='delete' sx={{ position: 'absolute', right: 5, top: 5 }}>
-         <DeleteIcon />
-      </IconButton>
+      <Tooltip title='Értesítés törlése'>
+         <IconButton
+            onClick={() => removeMutate()}
+            aria-label='delete'
+            sx={{ position: 'absolute', right: 5, top: 5 }}
+         >
+            <DeleteIcon />
+         </IconButton>
+      </Tooltip>
    )
 }
 
