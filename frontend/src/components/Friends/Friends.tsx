546bfebc5ed3e6e68de8b540c9a89d298cfc8f4d
nagytommy76 <nagytommy76@gmail.com>
2024-01-05 12:41:25 +0100

    Working on the friends display

--- frontend/src/components/Friends/Friends.tsx
+++ frontend/src/components/Friends/Friends.tsx
@@ -1,18 +1,42 @@
+'use client'
 import React from 'react'
+import useGetFriends from './Hooks/useGetFriends'
 
-// import { styled } from '@mui/material'
-// import Card from '@mui/material/Card'
-// import CardContent from '@mui/material/CardContent'
-// import CardMedia from '@mui/material/CardMedia'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
 
-// const StyledContainer = styled('section')({
-//    height: '100vh',
-// })
+import Grid from '@mui/material/Grid'
 
 const Friends = () => {
+   const friends = useGetFriends()
+   console.log(friends[0])
    return (
-      <div>
+      <div style={{ minHeight: '100vh' }}>
          <h1>Barátok komponens, szintén refreshTokennel látható. !!!!!!!444</h1>
+         <Grid container spacing={2}>
+            {friends.map((friend) => (
+               <Grid key={friend._id} item xs={2}>
+                  <Card sx={{ maxWidth: 345 }}>
+                     <CardMedia
+                        sx={{ height: 140 }}
+                        image={friend.userDetails.profilePicturePath[0].path}
+                        title={`${friend.firstName} ${friend.sureName}`}
+                     />
+                     <CardContent>
+                        <Typography variant='h5'>
+                           {friend.sureName} {friend.firstName}
+                        </Typography>
+                     </CardContent>
+                     {/* <CardActions>
+                        <Button size='small'>Share</Button>
+                        <Button size='small'>Learn More</Button>
+                     </CardActions> */}
+                  </Card>
+               </Grid>
+            ))}
+         </Grid>
       </div>
    )
 }
