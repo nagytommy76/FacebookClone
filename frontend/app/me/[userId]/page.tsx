73d23e03fe8c4dfcd8e4fade61b8e7574e441472
nagytommy76 <nagytommy76@gmail.com>
2024-01-06 11:24:32 +0100

    Added a dynamic route [userId] to the profile

---
+++ frontend/app/me/[userId]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ProfileMe from '@/components/Profile/ProfileMe'
+
+const page = () => {
+   return <ProfileMe />
+}
+
+export default page
--- frontend/app/me/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import ProfileMe from '../../src/components/Profile/ProfileMe'
-
-const me = () => {
-   return <ProfileMe />
-}
-
-export default me
