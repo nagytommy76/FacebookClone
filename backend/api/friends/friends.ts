dd9799eccf92d6b2080ccd53203710f31d756539
nagytommy76 <nagytommy76@gmail.com>
2024-01-08 18:43:22 +0100

    Added the makeFriendshipController to the API route

--- backend/api/friends/friends.ts
+++ backend/api/friends/friends.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { authenticateAccessTokenForApi } from '../../middlewares/accessTokenRefresh'
-import { getUsers } from '../../controllers/friends/friends'
+import { getUsers, makeFriendshipController } from '../../controllers/friends/friends'
 
 const router = Router()
 
 router.get('/get-friends', getUsers)
+
+router.post('/make-friendship', makeFriendshipController, authenticateAccessTokenForApi)
 
 module.exports = router
