c4a13fd9cb00a087864f7959d9defe6b7540c992
nagytommy76 <nagytommy76@gmail.com>
2024-01-08 18:43:09 +0100

    Working on the makeFriendshipController

--- backend/controllers/friends/friends.ts
+++ backend/controllers/friends/friends.ts
@@ -1,15 +1,34 @@
 import { Response, Request } from 'express'
 import { User as UserModel } from '../../models/user/user'
+import { IJWTUserType } from '../../middlewares/accessTokenRefresh'
 
 export const getUsers = async (request: Request, response: Response) => {
    try {
       const users = await UserModel.find({
          'userDetails.profilePicturePath': { $elemMatch: { isSelected: { $eq: true } } },
-      }).select(['firstName', 'sureName', 'email', 'createdAt', 'userDetails.profilePicturePath'])
+      }).select(['firstName', 'sureName', 'email', 'createdAt', 'userDetails.profilePicturePath', 'friends'])
 
       return response.status(200).json(users)
    } catch (error) {
       console.log(error)
       return response.status(500).json(error)
+   }
+}
+
+export const makeFriendshipController = async (request: IMakeFriends, response: Response) => {
+   const loggedInUserId = request.user?.userId as string
+   const friendId = request.body.friendId
+
+   try {
+      response.status(200).json({ msg: 'makeFriendship' })
+   } catch (error) {
+      console.log(error)
+      response.json(500).json(error)
+   }
+}
+
+interface IMakeFriends extends IJWTUserType {
+   body: {
+      friendId: string
    }
 }
