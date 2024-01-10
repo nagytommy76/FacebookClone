018144ccfd8afea735bd964fbd068a58abf2830e
nagytommy76 <nagytommy76@gmail.com>
2024-01-10 18:42:21 +0100

    Added a loading state for the loading button

--- frontend/src/components/Friends/Hooks/useFriendRequest.tsx
+++ frontend/src/components/Friends/Hooks/useFriendRequest.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { useState } from 'react'
 import { axiosInstance as axios } from '@/axios/AxiosInstance'
 import { useMutation } from '@tanstack/react-query'
 
 const useFriendRequest = (friendId: string) => {
+   const [loading, setLoading] = useState<boolean>(false)
+
    const mutationFunction = async () => {
       return await axios.post('/friends/make-friendship', { friendId })
    }
@@ -10,11 +12,16 @@ const useFriendRequest = (friendId: string) => {
    const { mutate } = useMutation({
       mutationKey: ['makeFriendship'],
       mutationFn: mutationFunction,
+      onMutate(variables) {
+         setLoading(true)
+         console.log('MUTATE INDUL')
+      },
       onSuccess(data, variables, context) {
          console.log(data.data)
+         setLoading(false)
       },
    })
-   return mutate
+   return { friendRequestMutate: mutate, loading }
 }
 
 export default useFriendRequest
