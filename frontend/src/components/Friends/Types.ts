947aca0fc1e14e5179f6adfc975339adcff8f6e1
nagytommy76 <nagytommy76@gmail.com>
2024-01-10 18:42:57 +0100

    Created an IFriends interface

--- frontend/src/components/Friends/Types.ts
+++ frontend/src/components/Friends/Types.ts
@@ -1,11 +1,17 @@
 import { IProfilePicture } from '@/src/types/PostTypes'
 
 export interface IFriends {
+   userId: string
+   isAccepted: boolean
+   createdAt: string
+}
+
+export interface IFriendsResponse {
    firstName: string
    sureName: string
    _id: string
    email: string
    createdAt: string
-   friends: string[]
+   friends: IFriends[]
    userDetails: { profilePicturePath: IProfilePicture[] }
 }
