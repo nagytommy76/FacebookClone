ca7d0d92e5af870a69a3990bf534a0be3a7e457a
nagytommy76 <nagytommy76@gmail.com>
2024-01-10 18:45:04 +0100

    Using a loading button & added another button to delete the friend request

--- frontend/src/components/Friends/Includes/CardButton.tsx
+++ frontend/src/components/Friends/Includes/CardButton.tsx
@@ -1,27 +1,52 @@
 'use client'
 import React from 'react'
 import Link from 'next/link'
-import { useAppSelector } from '@/src/utils/redux/store'
+import { useAppSelector } from '@/reduxStore/store'
+import useFriendRequest from '../Hooks/useFriendRequest'
 
 import CardActions from '@mui/material/CardActions'
 import Button from '@mui/material/Button'
+import LoadingButton from '@mui/lab/LoadingButton'
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove'
 
 const CardButton: React.FC<{ friendId: string }> = ({ friendId }) => {
    const userId = useAppSelector((state) => state.auth.userId)
+   const { friendRequestMutate, loading } = useFriendRequest(friendId)
    return (
       <CardActions>
          {userId == friendId ? (
             <Link style={{ width: '100%' }} href={`/me/${userId}`}>
-               <Button fullWidth variant='outlined' color='success' startIcon={<AccountCircleIcon />}>
+               <Button fullWidth variant='outlined' color='success' endIcon={<AccountCircleIcon />}>
                   Profilom
                </Button>
             </Link>
          ) : (
-            <Button fullWidth variant='outlined' color='info' startIcon={<PersonAddAlt1Icon />}>
-               Jelölés
-            </Button>
+            <div>
+               <LoadingButton
+                  onClick={() => friendRequestMutate()}
+                  endIcon={<PersonAddAlt1Icon />}
+                  loading={loading}
+                  loadingPosition='end'
+                  variant='outlined'
+                  color='info'
+                  fullWidth
+               >
+                  <span>Jelölés</span>
+               </LoadingButton>
+               <LoadingButton
+                  onClick={() => {}}
+                  endIcon={<PersonRemoveIcon />}
+                  loading={loading}
+                  loadingPosition='end'
+                  variant='outlined'
+                  color='error'
+                  fullWidth
+               >
+                  <span>Jelölés törlése</span>
+               </LoadingButton>
+            </div>
          )}
       </CardActions>
    )
