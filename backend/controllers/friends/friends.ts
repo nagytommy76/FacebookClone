847328c3e2dfd4269eac092f75e0792547d5a083
nagytommy76 <nagytommy76@gmail.com>
2024-01-14 14:24:08 +0100

    Working on the makeFriendship controller

--- backend/controllers/friends/friends.ts
+++ backend/controllers/friends/friends.ts
@@ -1,6 +1,6 @@
 import { Response, Request } from 'express'
 import { User as UserModel } from '../../models/user/user'
-import { IJWTUserType } from '../../middlewares/accessTokenRefresh'
+import { IMakeFriends } from './Types'
 
 export const getUsers = async (request: Request, response: Response) => {
    try {
@@ -27,19 +27,50 @@ export const makeFriendshipController = async (request: IMakeFriends, response: 
    const friendId = request.body.friendId
 
    try {
-      const senderUser = await UserModel.findOne({ _id: loggedInUserId }).select('friends')
-      const receiverUser = await UserModel.findOne({ _id: friendId }).select('friends')
+      const senderUser = await UserModel.getUserByUserIdAndSelect(loggedInUserId)
+      const receiverUser = await UserModel.findOne({ _id: friendId }).select([
+         'friends',
+         'firstName',
+         'sureName',
+         'notifications',
+      ])
+      if (!senderUser) return response.status(404).json({ msg: 'Sender user not found' })
+      if (!receiverUser) return response.status(404).json({ msg: 'Receiver user not found' })
 
-      senderUser?.friends.push({
-         createdAt: new Date(),
-         isAccepted: false,
-         userId: receiverUser?.id,
-      })
-      receiverUser?.friends.push({
+      senderUser.friends.push({
          createdAt: new Date(),
          isAccepted: true,
-         userId: senderUser?.id,
+         userId: receiverUser.id,
       })
+
+      receiverUser.friends.push({
+         createdAt: new Date(),
+         isAccepted: false,
+         userId: senderUser.id,
+      })
+      receiverUser.notifications.push({
+         createdAt: new Date(),
+         isRead: false,
+         notificationType: 'isFriend',
+         userDetails: {
+            firstName: senderUser.firstName,
+            sureName: senderUser.sureName,
+            userId: senderUser.id,
+            profilePicture: senderUser.userDetails.profilePicturePath[0].path,
+         },
+      })
+
+      if (request.getUser !== undefined) {
+         const toSendUser = request.getUser(friendId) as any
+         if (toSendUser !== undefined) {
+            request.ioSocket?.to(toSendUser.socketId).emit('makeFriendship', {
+               notifications: receiverUser.notifications,
+            })
+         }
+      }
+
+      await receiverUser.save()
+      await senderUser.save()
 
       response.status(200).json({ msg: 'makeFriendship', senderUser, receiverUser })
    } catch (error) {
@@ -48,8 +79,4 @@ export const makeFriendshipController = async (request: IMakeFriends, response: 
    }
 }
 
-interface IMakeFriends extends IJWTUserType {
-   body: {
-      friendId: string
-   }
-}
+export const removeFriendRequestController = async () => {}
