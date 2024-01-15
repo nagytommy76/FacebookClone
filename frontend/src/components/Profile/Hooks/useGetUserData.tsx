7a19378153b835dcbce67c3c4484e71cf77c79cb
nagytommy76 <nagytommy76@gmail.com>
2024-01-15 19:28:51 +0100

    Sending the userId as parameter

--- frontend/src/components/Profile/Hooks/useGetUserData.tsx
+++ frontend/src/components/Profile/Hooks/useGetUserData.tsx
@@ -1,18 +1,22 @@
+'use client'
+import { useParams } from 'next/navigation'
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance as axios } from '@/utils/axiosSetup/AxiosInstance'
 import { IUserPopulatedPosts } from '@/types/AuthTypes'
 
-const getUserData = async () => {
-   try {
-      const response = await axios.get('/user/get-details')
-      return response.data as IUserPopulatedPosts
-   } catch (error) {
-      console.log(error)
-      return Promise.reject(new Error())
-   }
-}
-
 const useGetUserData = () => {
+   const params = useParams() as { userId: string }
+
+   const getUserData = async () => {
+      try {
+         const response = await axios.get('/user/get-details', { params: { userId: params.userId } })
+         return response.data as IUserPopulatedPosts
+      } catch (error) {
+         console.log(error)
+         return Promise.reject(new Error())
+      }
+   }
+
    const { data, isLoading, isError } = useQuery({
       queryKey: ['userData'],
       queryFn: getUserData,
