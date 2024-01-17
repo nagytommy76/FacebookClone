b816c44a1a3f9ee4bb2ce5234a1d22f733c137ab
nagytommy76 <nagytommy76@gmail.com>
2024-01-17 19:40:40 +0100

    Using the url param userId

--- frontend/src/components/Base/PostsBase/Hooks/useGetAllPosts.tsx
+++ frontend/src/components/Base/PostsBase/Hooks/useGetAllPosts.tsx
@@ -1,31 +1,36 @@
+import { useParams } from 'next/navigation'
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance as axios, AxiosResponse } from '@/utils/axiosSetup/AxiosInstance'
 import { IPost } from '@/types/PostTypes'
-
-const getUserPosts = async () => {
-   try {
-      const response = await axios.get('/post/get-user-posts')
-      return response as AxiosResponse<{ allPosts: IPost[] }>
-   } catch (error) {
-      console.log(error)
-      return Promise.reject(new Error())
-   }
-}
-
-const fetchAllQueries = async () => {
-   try {
-      const response = await axios.get('/post/get-posts')
-      return response as AxiosResponse<{ allPosts: IPost[] }>
-   } catch (error) {
-      console.log(error)
-   }
-}
 
 const useGetAllPosts = (
    setAllPosts: React.Dispatch<React.SetStateAction<IPost[]>>,
    isGetUsersPosts: boolean = false,
    isUrlChanged: boolean = false
 ) => {
+   const params = useParams() as { userId: string }
+
+   const getUserPosts = async () => {
+      try {
+         const response = await axios.get('/post/get-user-posts', {
+            params: { userId: params.userId },
+         })
+         return response as AxiosResponse<{ allPosts: IPost[] }>
+      } catch (error) {
+         console.log(error)
+         return Promise.reject(new Error())
+      }
+   }
+
+   const fetchAllQueries = async () => {
+      try {
+         const response = await axios.get('/post/get-posts')
+         return response as AxiosResponse<{ allPosts: IPost[] }>
+      } catch (error) {
+         console.log(error)
+      }
+   }
+
    const { isLoading } = useQuery({
       queryKey: ['getAllPosts', isUrlChanged],
       queryFn: isGetUsersPosts ? getUserPosts : fetchAllQueries,
