78a42bc0f5208726bfafd9a1f8fa498d38b1a3a9
nagytommy76 <nagytommy76@gmail.com>
2024-01-22 19:15:08 +0100

    Disabled the modal when an other user on the profile page

--- frontend/src/components/Profile/Header/HeaderImage/Hooks/useModalControl.tsx
+++ frontend/src/components/Profile/Header/HeaderImage/Hooks/useModalControl.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import { useParams } from 'next/navigation'
+import { useAppSelector } from '@/reduxStore/store'
 
 const useModalControl = () => {
+   const params = useParams() as { userId: string }
+   const loggedInUserId = useAppSelector((state) => state.auth.userId)
+
    const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
    const handleOpenDialog = () => {
-      setIsModalOpen(true)
+      if (loggedInUserId == params.userId) {
+         setIsModalOpen(true)
+      }
    }
    const handleCloseDialog = () => {
       setIsModalOpen(false)
