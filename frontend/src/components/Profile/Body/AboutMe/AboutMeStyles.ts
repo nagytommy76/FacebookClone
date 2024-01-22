1f3e6e6580c7c97ad3a3c86b08ce56c7211db1cf
nagytommy76 <nagytommy76@gmail.com>
2024-01-22 19:12:05 +0100

    Created a new Style file for Work & school

--- frontend/src/components/Profile/Body/AboutMe/AboutMeStyles.ts
+++ frontend/src/components/Profile/Body/AboutMe/AboutMeStyles.ts
@@ -10,20 +10,3 @@ export const StyledTabContainer = styled(BodyPaper)(({ theme }) => ({
       flexDirection: 'column',
    },
 }))
-
-// WorkSchool --------------------------
-export const WorkSchoolSection = styled('section')({
-   width: '100%',
-   minHeight: '400px',
-   display: 'flex',
-   flexDirection: 'column',
-})
-
-export const WorkSchoolItems = styled('div')({
-   width: '100%',
-   height: '160px',
-   display: 'flex',
-   flexDirection: 'column',
-   justifyContent: 'space-evenly',
-   alignItems: 'flex-start',
-})
---
+++ frontend/src/components/Profile/Body/AboutMe/WorkSchool/Styles.ts
@@ -0,0 +1,20 @@
+import { styled } from '@mui/system'
+
+// WorkSchool --------------------------
+export const WorkSchoolSection = styled('section')({
+   width: '100%',
+   minHeight: '400px',
+   marginLeft: 13,
+
+   display: 'flex',
+   flexDirection: 'column',
+})
+
+export const WorkSchoolItems = styled('div')({
+   width: '100%',
+   minHeight: '160px',
+   display: 'flex',
+   flexDirection: 'column',
+   justifyContent: 'space-evenly',
+   alignItems: 'flex-start',
+})
