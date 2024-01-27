fdc48bfd0bb472bac657da76bc1f57a1a9297660
nagytommy76 <nagytommy76@gmail.com>
2024-01-27 14:48:09 +0100

    Using the SelectDate hook

--- frontend/src/components/Profile/Body/AboutMe/WorkSchool/Includes/SelectContainer.tsx
+++ frontend/src/components/Profile/Body/AboutMe/WorkSchool/Includes/SelectContainer.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
-import useYear from '../Hooks/useYear'
+import useSelectDate from '../Hooks/useSelectDate'
 
 import SelectYear from './SelectDate/SelectYear'
 import SelectMonth from './SelectDate/SelectMonth'
 import SelectDay from './SelectDate/SelectDay'
 
 const SelectContainer = () => {
+   const { handleChangeYear, handleChangeMonth, handleChangeDay } = useSelectDate()
    return (
       <div style={{ width: '100%', display: 'flex' }}>
          <SelectYear />
