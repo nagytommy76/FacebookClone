0f2904e5207198731a966276b8ae81ec5ff5d5a6
nagytommy76 <nagytommy76@gmail.com>
2024-01-29 19:15:05 +0100

    Using the useInputs hook

--- frontend/src/components/Profile/Body/AboutMe/WorkSchool/Includes/WorkSchoolInputs.tsx
+++ frontend/src/components/Profile/Body/AboutMe/WorkSchool/Includes/WorkSchoolInputs.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import useInputs from './SelectDate/Hooks/useInputs'
 
-import SelectContainer from './SelectContainer'
+import DateSelector from './DateSelector'
 
 import { WorkSchoolInputContainer } from '../Styles'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 
 const WorkSchoolInputs = () => {
+   const {
+      setCity,
+      setCompany,
+      setPost,
+      setFromDate,
+      setToDate,
+      handleChangeCity,
+      handleChangeCompany,
+      handleChangePost,
+   } = useInputs()
+
    return (
       <WorkSchoolInputContainer>
-         <TextField label='Vállalat' id='companyName' size='medium' />
-         <TextField label='Pozíció' id='post' size='medium' />
-         <TextField label='Város' id='city' size='medium' />
-         <SelectContainer />
+         <TextField onChange={handleChangeCompany} label='Vállalat' id='companyName' size='medium' />
+         <TextField onChange={handleChangePost} label='Pozíció' id='post' size='medium' />
+         <TextField onChange={handleChangeCity} label='Város' id='city' size='medium' />
+         <DateSelector setFromDate={setFromDate} setToDate={setToDate} />
          <Button color='warning' variant='outlined'>
             Küldés
          </Button>
