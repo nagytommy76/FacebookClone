d88d4976a4a2c93702ed7bd6f656c43711b07f90
nagytommy76 <nagytommy76@gmail.com>
2024-02-03 16:55:51 +0100

    Created a ValidateAddWorkplace validation

---
+++ backend/api/user/validators/userDataValidator.ts
@@ -0,0 +1,15 @@
+import { body } from 'express-validator'
+
+export const ValidateAddWorkplace = [
+   body('city').trim().isLength({ min: 2 }).withMessage('A Város mezőnek minimum 2 karakternek kell lennie!'),
+   body('company').trim().isLength({ min: 2 }).withMessage('A Cég mező kitöltése kötelező!'),
+   body('post').trim().isLength({ min: 3 }).withMessage('A Pozíció mezőnek min 3 karakternek kell lennie!'),
+   body('fromDate').isLength({ min: 5 }).withMessage('A kezdeti idő kitöltése kötelező!'),
+   //    body('endDateChecked', 'toDate').custom((value: { endDateChecked: boolean; toDate: string }) => {
+   //       console.log(value)
+   //       if (value.endDateChecked === false && value.toDate === undefined) {
+   //          throw new Error('A befejezés idő mező kitöltése kötelező!')
+   //       }
+   //       return true
+   //    }),
+]
