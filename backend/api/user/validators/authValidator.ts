01d71c40db54465f313d0515e2f149a2107af2d2
nagytommy76 <nagytommy76@gmail.com>
2024-02-05 19:22:00 +0100

    Fixed the validation order

--- backend/api/user/validators/authValidator.ts
+++ backend/api/user/validators/authValidator.ts
@@ -1,23 +1,22 @@
 import { body } from 'express-validator'
 import { User } from '../../../models/user/user'
 
-const customEmailValidator = async (email: string, isForLogin: boolean = false) => {
-   const checkUserRegisteredWithEmail = await User.findOne({ email })
-   if (!isForLogin && checkUserRegisteredWithEmail !== null)
-      throw new Error('Ezzel az email címmel már regisztráltak!')
-   if (isForLogin && checkUserRegisteredWithEmail === null)
-      throw new Error('Ez az email cím még nincs regisztrálva!')
-   return true
-}
-
 export const ValidateRegister = [
-   body('email').custom((email: string) => customEmailValidator(email)),
-   body('email')
-      .isEmail()
-      .normalizeEmail()
-      .withMessage('Az email cím nem megfelelő formátumú vagy üres a mező!'),
    body('sureName').trim().isLength({ min: 1 }).withMessage('A Vezetéknév mező kitöltése kötelező!'),
    body('firstName').trim().isLength({ min: 1 }).withMessage('A Keresztnév mező kitöltése kötelező!'),
+   body('email').custom(async (email: string) => {
+      const checkUserRegisteredWithEmail = await User.findOne({ email })
+      if (email.length <= 3) {
+         throw new Error('Az email mező kitöltése kötelező!')
+      } else if (checkUserRegisteredWithEmail !== null) {
+         throw new Error('Ezzel az email címmel már regisztráltak!')
+      }
+      return true
+   }),
+   // body('email')
+   //    .isEmail()
+   //    .normalizeEmail()
+   //    .withMessage('Az email cím nem megfelelő formátumú vagy üres a mező!'),
    body('password').trim().isLength({ min: 3 }).withMessage('A jelszónak min 3 karakternek kell lennie!'),
    body('dateOfBirth').custom((value: { day: number | ''; month: number | ''; year: number | '' }) => {
       if (value.day === '' || value.month === '' || value.year === '') {
@@ -29,7 +28,17 @@ export const ValidateRegister = [
 
 export const ValidateLogin = [
    body('email')
+      .custom(async (email: string) => {
+         const checkUserRegisteredWithEmail = await User.findOne({ email })
+         if (email.length <= 3) {
+            throw new Error('Az email mező kitöltése kötelező!')
+         } else if (checkUserRegisteredWithEmail === null) {
+            throw new Error('Ez az email cím még nincs regisztrálva!')
+         }
+         return true
+      })
+      .normalizeEmail()
       .isEmail()
-      .custom((email: string) => customEmailValidator(email, true)),
+      .withMessage('Az email cím nem megfelelő formátumú!'),
    body('password').trim().isLength({ min: 3 }).withMessage('A jelszónak min 3 karakternek kell lennie!'),
 ]
