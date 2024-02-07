3d6f4800e05376c5e770c163d52aaf771c336f13
nagytommy76 <nagytommy76@gmail.com>
2024-02-07 19:32:57 +0100

    Added a handleChangeErrors  fn

--- frontend/src/components/Profile/Body/AboutMe/WorkSchool/Includes/SelectDate/Hooks/useInputs.tsx
+++ frontend/src/components/Profile/Body/AboutMe/WorkSchool/Includes/SelectDate/Hooks/useInputs.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, useState } from 'react'
 import useAddWork from './useAddWork'
-import type { Error } from '../Types'
+import type { Error, DateError } from '../Types'
+import type { ErrorResponse } from '@/types/AuthTypes'
 
 const textWithError: Error = {
    value: '',
@@ -8,37 +9,74 @@ const textWithError: Error = {
    errorMsg: '',
 }
 
+const dateWithError: DateError = {
+   error: false,
+   errorMsg: '',
+   value: undefined,
+}
+
 const useInputs = () => {
    const [company, setCompany] = useState<Error>(textWithError)
    const [post, setPost] = useState<Error>(textWithError)
    const [city, setCity] = useState<Error>(textWithError)
-   const [fromDate, setFromDate] = useState<Date | undefined>(undefined)
-   const [toDate, setToDate] = useState<Date | undefined>(undefined)
+   const [fromDate, setFromDate] = useState<DateError>(dateWithError)
+   const [toDate, setToDate] = useState<DateError>(dateWithError)
    const [endDateChecked, setEndDateChecked] = useState<boolean>(true)
 
    const setToDefault = () => {
       setCompany(textWithError)
       setPost(textWithError)
       setCity(textWithError)
-      setFromDate(undefined)
-      setToDate(undefined)
+      setFromDate(dateWithError)
+      setToDate(dateWithError)
       setEndDateChecked(true)
    }
-   const addWorkMutation = useAddWork(company, post, city, fromDate, toDate, endDateChecked, setToDefault)
+
+   const handleChangeErrors = (errorResponseArray: ErrorResponse) => {
+      errorResponseArray.map((errorResponse) => {
+         switch (errorResponse.path) {
+            case 'city':
+               setCity({ ...city, error: true, errorMsg: errorResponse.msg })
+               break
+            case 'company':
+               setCompany({ ...company, error: true, errorMsg: errorResponse.msg })
+               break
+            case 'post':
+               setPost({ ...post, error: true, errorMsg: errorResponse.msg })
+               break
+            case 'fromDate':
+               setFromDate({ ...fromDate, error: true, errorMsg: errorResponse.msg })
+               break
+         }
+      })
+   }
+   const addWorkMutation = useAddWork(
+      company,
+      post,
+      city,
+      fromDate,
+      toDate,
+      endDateChecked,
+      setToDefault,
+      handleChangeErrors
+   )
 
    const handleChangeChecked = (event: React.ChangeEvent<HTMLInputElement>) => {
       setEndDateChecked(event.target.checked)
    }
 
-   const handleChangeCompanyError = () => {}
    const handleChangeCompanyValue = (event: ChangeEvent<HTMLInputElement>) => {
       setCompany({ ...company, value: event.target.value })
+      if (event.target.value.length >= 3)
+         setCompany({ error: false, errorMsg: '', value: event.target.value })
    }
    const handleChangePostValue = (event: ChangeEvent<HTMLInputElement>) => {
       setPost({ ...post, value: event.target.value })
+      if (event.target.value.length >= 3) setPost({ error: false, errorMsg: '', value: event.target.value })
    }
    const handleChangeCityValue = (event: ChangeEvent<HTMLInputElement>) => {
       setCity({ ...city, value: event.target.value })
+      if (event.target.value.length >= 2) setCity({ error: false, errorMsg: '', value: event.target.value })
    }
 
    return {
@@ -46,6 +84,8 @@ const useInputs = () => {
       post,
       city,
       endDateChecked,
+      fromDate,
+      toDate,
       setFromDate,
       setToDate,
       handleChangeChecked,
