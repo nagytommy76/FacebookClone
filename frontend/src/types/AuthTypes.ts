b77b047fe77b98c6b7ff4bdd0954cd706cc89ada
nagytommy76 <nagytommy76@gmail.com>
2024-02-11 14:15:57 +0100

    Separated the IUserDetails & removed the unused types

--- frontend/src/types/AuthTypes.ts
+++ frontend/src/types/AuthTypes.ts
@@ -1,5 +1,39 @@
 import type { IPost, IProfilePicture } from './PostTypes'
 
+export interface IUserDetails {
+   dateOfBirth: { day: number; month: number; year: number }
+   gender: 'female' | 'male'
+   profilePicturePath: IProfilePicture[]
+   birthTown: string
+   homeTown: string
+   relationShip: { isAlone: boolean; inRelation: boolean }
+   studies: {
+      elementary: {
+         from: number
+         to: number
+         name: string
+      }
+      highSchool: {
+         from: number
+         to: number
+         name: string
+      }
+      university: {
+         from: number
+         to: number
+         name: string
+      }
+   }
+   workPlaces: {
+      companyName: string
+      position: string
+      description: string
+      city: string
+      startDate: string
+      endDate?: string | null
+   }[]
+}
+
 export interface IUserTypes {
    _id: string
    email: string
@@ -8,38 +42,7 @@ export interface IUserTypes {
    password: string
    isEmailConfirmed: boolean
    friends: { userId: string }[]
-   userDetails: {
-      dateOfBirth: { day: number; month: number; year: number }
-      gender: 'female' | 'male'
-      profilePicturePath: IProfilePicture[]
-      birthTown: string
-      homeTown: string
-      relationShip: { isAlone: boolean; inRelation: boolean }
-      studies: {
-         elementary: {
-            from: number
-            to: number
-            name: string
-         }
-         highSchool: {
-            from: number
-            to: number
-            name: string
-         }
-         university: {
-            from: number
-            to: number
-            name: string
-         }
-      }
-      workPlaces: {
-         companyName: string
-         post: string
-         city: string
-         from: number
-         to: number
-      }[]
-   }
+   userDetails: IUserDetails
    posts: IPost[] | string[] | any
    createdAt: number
    updatedAt: number
--- frontend/src/types/PostTypes.ts
+++ frontend/src/types/PostTypes.ts
@@ -1,4 +1,5 @@
 import type { IPostComment, IPostLike } from './LikeTypes'
+import { IUserDetails } from './AuthTypes'
 
 export interface IPopulatedUserId<T = IUserDetails> {
    _id: string
@@ -17,45 +18,6 @@ export interface IPost {
    postedPicturesPath: string[] | null
    createdAt: string
    updatedAt: string
-}
-
-export interface IUserTypes {
-   _id: string
-   email: string
-   firstName: string
-   sureName: string
-   password: string
-   isEmailConfirmed: boolean
-   friends: { userId: string }[]
-   userDetails: IUserDetails
-   createdAt: number
-   updatedAt: number
-}
-
-export interface IUserDetails {
-   dateOfBirth: { day: number; month: number; year: number }
-   gender: 'male' | 'female'
-   profilePicturePath: IProfilePicture[]
-   birthTown?: string
-   homeTown?: string
-   studies?: {
-      elementary: {
-         from: number
-         to: number
-         name: string
-      }
-      highSchool: {
-         from: number
-         to: number
-         name: string
-      }
-      university: {
-         from: number
-         to: number
-         name: string
-      }
-   }
-   relationShip?: { isAlone: boolean; inRelation: boolean }
 }
 
 export interface IProfilePicture {
