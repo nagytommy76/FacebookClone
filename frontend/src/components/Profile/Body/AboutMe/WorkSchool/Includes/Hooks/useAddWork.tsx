e55c9df454ea0cb00eee163df85dc2c377f241e4
nagytommy76 <nagytommy76@gmail.com>
2024-02-12 19:31:10 +0100

    Added a params to axios call & using profileDispatch

--- frontend/src/components/Profile/Body/AboutMe/WorkSchool/Includes/Hooks/useAddWork.tsx
+++ frontend/src/components/Profile/Body/AboutMe/WorkSchool/Includes/Hooks/useAddWork.tsx
@@ -1,5 +1,9 @@
+import { useContext } from 'react'
 import { useMutation } from '@tanstack/react-query'
 import { axiosInstance as axios, AxiosResponse, AxiosError } from '@/axios/AxiosInstance'
+import { useParams } from 'next/navigation'
+import { ProfileContext } from '@/ProfileContext/ProfileContextProvider'
+
 import type { DateError, Error } from '../SelectDate/Types'
 import type { ErrorResponse } from '@/types/AuthTypes'
 
@@ -7,27 +11,44 @@ const useAddWork = (
    company: Error,
    post: Error,
    city: Error,
+   description: string,
    fromDate: DateError,
    toDate: DateError,
    endDateChecked: boolean,
    setToDefault: () => void,
    handleChangeErrors: (errorResponseArray: ErrorResponse) => void
 ) => {
+   const { profileDispatch } = useContext(ProfileContext)
+   const params = useParams() as { userId: string }
+
    const workMutationFn = async () => {
-      return await axios.post('/user/save-workplace', {
-         company: company.value,
-         post: post.value,
-         city: city.value,
-         endDateChecked,
-         fromDate: fromDate.value,
-         toDate: toDate.value,
-      })
+      return await axios.post(
+         '/user/save-workplace',
+         {
+            company: company.value,
+            post: post.value,
+            city: city.value,
+            description,
+            endDateChecked,
+            fromDate: fromDate.value,
+            toDate: toDate.value,
+         },
+         {
+            params: {
+               userToModify: params.userId,
+            },
+         }
+      )
    }
 
    const { mutate, isError, error } = useMutation({
       mutationKey: ['addNewWorkplace'],
       mutationFn: workMutationFn,
       onSuccess(data, variables, context) {
+         profileDispatch({
+            type: 'ADD_WORKPLACE',
+            payload: data.data.savedUserWorlplace,
+         })
          setToDefault()
          console.log(data)
       },
