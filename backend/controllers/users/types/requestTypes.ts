57620e567b503f7ab8bcbc1dc54c9b460eb65e84
nagytommy76 <nagytommy76@gmail.com>
2024-02-24 14:44:47 +0100

    Separated the Request types/interfaces

---
+++ backend/controllers/users/types/requestTypes.ts
@@ -0,0 +1,37 @@
+import { ObjectId } from 'mongoose'
+import type { Request } from 'express'
+import type { IJWTUserType } from '../../../middlewares/accessTokenRefresh'
+import type { IFriends, IProfilePicturePath } from './ModelTypes'
+
+export interface IRegisterRequest extends Request {
+   body: {
+      email: string
+      sureName: string
+      firstName: string
+      password: string
+      gender: 'male' | 'female'
+      dateOfBirth: { day: number; month: number; year: number }
+   }
+}
+
+export interface ILoginRequest extends Request {
+   body: {
+      email: string
+      password: string
+   }
+}
+
+export interface IGetUserDetailsRequest extends IJWTUserType {
+   query: {
+      userId: string | undefined
+   }
+}
+
+export type SelectUserByIdType = {
+   _id: string | ObjectId
+   firstName: string
+   sureName: string
+   email: string
+   friends: IFriends[]
+   selectedProfilePicturePath: IProfilePicturePath
+}
