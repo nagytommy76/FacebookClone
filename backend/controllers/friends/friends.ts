a9f64958e2f963eb204f6272700d93ef0a37e5a9
nagytommy76 <nagytommy76@gmail.com>
2024-02-25 14:57:42 +0100

    Using a findOne query insetad of aggregate

--- backend/controllers/friends/friends.ts
+++ backend/controllers/friends/friends.ts
@@ -49,7 +49,11 @@ export const makeFriendshipController = async (request: IMakeFriends, response: 
    const loggedInUserId = request.user?.userId as string
    const friendId = request.body.friendId
    try {
-      const senderUser = await UserModel.getUserByUserIdAndSelect(loggedInUserId)
+      const senderUser = await UserModel.findOne({
+         _id: loggedInUserId,
+         'userDetails.profilePicturePath': { $elemMatch: { isSelected: { $eq: true } } },
+      }).select(['friends', 'firstName', 'sureName', 'notifications', 'userDetails.profilePicturePath.$'])
+
       const receiverUser = await UserModel.findOne({ _id: friendId }).select([
          'friends',
          'firstName',
@@ -59,26 +63,32 @@ export const makeFriendshipController = async (request: IMakeFriends, response: 
       if (!senderUser) return response.status(404).json({ msg: 'Sender user not found' })
       if (!receiverUser) return response.status(404).json({ msg: 'Receiver user not found' })
 
-      senderUser[0].friends.push({
+      console.log(senderUser.userDetails.profilePicturePath[0].path)
+
+      senderUser.friends.push({
          createdAt: new Date(),
-         isAccepted: true,
          userId: receiverUser.id,
+         isAccepted: true,
+         isSender: true,
+         isReceiver: false,
       })
 
       receiverUser.friends.push({
          createdAt: new Date(),
+         userId: senderUser._id,
          isAccepted: false,
-         userId: senderUser[0]._id,
+         isSender: false,
+         isReceiver: true,
       })
       receiverUser.notifications.push({
          createdAt: new Date(),
          isRead: false,
          notificationType: 'isFriend',
          userDetails: {
-            firstName: senderUser[0].firstName,
-            sureName: senderUser[0].sureName,
-            userId: senderUser[0]._id,
-            profilePicture: senderUser[0].selectedProfilePicturePath.path,
+            firstName: senderUser.firstName,
+            sureName: senderUser.sureName,
+            userId: senderUser._id,
+            profilePicture: senderUser.userDetails.profilePicturePath[0].path,
          },
       })
 
@@ -94,7 +104,7 @@ export const makeFriendshipController = async (request: IMakeFriends, response: 
       // await receiverUser.save()
       // await senderUser.save()
 
-      response.status(200).json({ msg: 'makeFriendship', senderUser, receiverUser })
+      response.status(200).json({ senderUser, receiverUser })
    } catch (error) {
       console.log(error)
       response.json(500).json(error)
