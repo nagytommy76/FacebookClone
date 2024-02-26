bc9bf889fae3225dbb77e073e4cadb0e2a3bfe19
nagytommy76 <nagytommy76@gmail.com>
2024-02-26 19:34:51 +0100

    Using the new hooks

--- frontend/src/components/Friends/Includes/CardButton.tsx
+++ frontend/src/components/Friends/Includes/CardButton.tsx
@@ -2,7 +2,10 @@
 import React from 'react'
 import Link from 'next/link'
 import { useAppSelector } from '@/reduxStore/store'
+
 import useFriendRequest from '../Hooks/useFriendRequest'
+import useFriendWithdraw from '../Hooks/useFriendWithdraw'
+import useFriendConfirm from '../Hooks/useFriendConfirm'
 
 import CardActions from '@mui/material/CardActions'
 import Button from '@mui/material/Button'
@@ -13,13 +16,9 @@ import ButtonTypes from './ButtonTypes'
 
 const CardButton: React.FC<{ friend: IFriendsResponse }> = ({ friend }) => {
    const userId = useAppSelector((state) => state.auth.userId)
-   const { friendRequestMutate, loading, cardButtonType } = useFriendRequest(friend._id)
-
-   const isFriendRequestSendByMeAndNotAccepted = friend.friends.find(
-      (item) => item.userId == userId && item.isAccepted === false
-   )
-
-   const myFriendRequest = friend.friends.find((item) => item.userId === userId)
+   const { friendRequestMutate, setCardButtonType, loading, cardButtonType } = useFriendRequest(friend._id)
+   useFriendWithdraw(friend.friends, setCardButtonType)
+   useFriendConfirm(friend.friends, setCardButtonType)
 
    return (
       <CardActions>
