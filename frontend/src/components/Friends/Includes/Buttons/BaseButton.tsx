5de069331d4fd32274c9630dc869707d9762a8dc
nagytommy76 <nagytommy76@gmail.com>
2024-03-03 14:14:11 +0100

    Created a BaseButton component

---
+++ frontend/src/components/Friends/Includes/Buttons/BaseButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import LoadingButton from '@mui/lab/LoadingButton'
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1'
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove'
+
+const BaseButton: React.FC<{
+   buttonText: string
+   isLoading: boolean
+   iconType?: 'addIcon' | 'removeIcon'
+   color?: 'error' | 'warning' | 'info'
+   onClickEvent: () => void
+}> = ({ onClickEvent, buttonText = 'Jelölés', isLoading, iconType = 'addIcon', color = 'info' }) => {
+   return (
+      <LoadingButton
+         onClick={onClickEvent}
+         endIcon={iconType === 'addIcon' ? <PersonAddAlt1Icon /> : <PersonRemoveIcon />}
+         loading={isLoading}
+         loadingPosition='end'
+         variant='outlined'
+         color={color}
+         fullWidth
+      >
+         <span>{buttonText}</span>
+      </LoadingButton>
+   )
+}
+
+export default BaseButton
