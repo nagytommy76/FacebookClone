32f37f1a0ef5b1c9c36587437dcafb6700d44008
nagytommy76 <nagytommy76@gmail.com>
2024-03-07 18:59:26 +0100

    Using the setCardTypeDeleteFriend function on succes

--- frontend/src/components/Friends/Hooks/useFriendCornfirmRequest.tsx
+++ frontend/src/components/Friends/Hooks/useFriendCornfirmRequest.tsx
@@ -1,11 +1,15 @@
 import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
 import { useMutation } from '@tanstack/react-query'
+import useFriendDelete from './ButtonType/useFriendDelete'
+
 import type { FriendButtonType, IFriendResponse, IFriends } from '../Types'
 
 const useFriendCornfirmRequest = (
    friendId: string,
-   setLoading: React.Dispatch<React.SetStateAction<boolean>>
+   setLoading: React.Dispatch<React.SetStateAction<boolean>>,
+   setCardButtonType: React.Dispatch<React.SetStateAction<FriendButtonType>>
 ) => {
+   const { setCardTypeDeleteFriend } = useFriendDelete(setCardButtonType)
    const confirmMutate = async () => {
       return await axios.post('/friends/confirm-friendship', { friendId })
    }
@@ -19,6 +23,7 @@ const useFriendCornfirmRequest = (
       onSuccess(data) {
          console.log(data.data)
          setLoading(false)
+         setCardTypeDeleteFriend(data.data.friends)
       },
    })
 
