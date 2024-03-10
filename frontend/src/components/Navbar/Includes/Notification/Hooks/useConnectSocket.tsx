940c24e96e040a860cd4701178548a454553cdc1
nagytommy76 <nagytommy76@gmail.com>
2024-03-10 14:22:59 +0100

    Added a setNotificationContext fn & 2 listeners

--- frontend/src/components/Navbar/Includes/Notification/Hooks/useConnectSocket.tsx
+++ frontend/src/components/Navbar/Includes/Notification/Hooks/useConnectSocket.tsx
@@ -1,8 +1,22 @@
-import { useEffect, useContext } from 'react'
+import { useEffect, useContext, Dispatch } from 'react'
 import { NotificationsContext } from '../Context/NotificationContextProvider'
 import { socket } from '@/src/utils/socketIo'
+import type { NotificationType } from '../Types'
+import type { INotificationsAction } from '../Context/NotificationReducer'
 
 import { useAppSelector } from '@/src/utils/redux/store'
+
+interface INotificationArguments {
+   notifications: NotificationType
+}
+
+const setNotificationContext = (
+   args: INotificationArguments,
+   notificationsDispatch: Dispatch<INotificationsAction>
+) => {
+   notificationsDispatch({ type: 'SET_ALL_NOTIFICATIONS', payload: args.notifications })
+   notificationsDispatch({ type: 'SET_ACTIVE_NOTIFICATIONS_COUNT', payload: args.notifications })
+}
 
 const useConnectSocket = () => {
    const userId = useAppSelector((state) => state.auth.userId)
@@ -16,18 +30,27 @@ const useConnectSocket = () => {
 
          socket.on('likedPost', (args) => {
             console.log(args)
-            notificationsDispatch({ type: 'SET_ALL_NOTIFICATIONS', payload: args.notifications })
-            notificationsDispatch({ type: 'SET_ACTIVE_NOTIFICATIONS_COUNT', payload: args.notifications })
+            setNotificationContext(args, notificationsDispatch)
          })
          socket.on('addComment', (args) => {
             console.log(args)
-            notificationsDispatch({ type: 'SET_ALL_NOTIFICATIONS', payload: args.notifications })
-            notificationsDispatch({ type: 'SET_ACTIVE_NOTIFICATIONS_COUNT', payload: args.notifications })
+            setNotificationContext(args, notificationsDispatch)
+         })
+
+         socket.on('makeFriendship', (args) => {
+            console.log(args)
+            setNotificationContext(args, notificationsDispatch)
+         })
+         socket.on('confirmFriendship', (args) => {
+            console.log(args)
+            setNotificationContext(args, notificationsDispatch)
          })
       })
       return () => {
          socket.off('likedPost')
          socket.off('addComment')
+         socket.off('makeFriendship')
+         socket.off('confirmFriendship')
          socket.disconnect()
       }
    }, [userId, notificationsDispatch])
