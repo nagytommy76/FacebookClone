2cc01b148284f70672dc02221165084f43d21639
nagytommy76 <nagytommy76@gmail.com>
2024-03-15 16:00:29 +0100

    Added the remove friend controller to API route

--- backend/api/friends/friends.ts
+++ backend/api/friends/friends.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { authenticateAccessTokenForApi } from '../../middlewares/accessTokenRefresh'
-import { makeFriendshipController } from '../../controllers/friends/friends'
+
 import { getUsers } from '../../controllers/friends/getFriends'
+import { makeFriendshipController } from '../../controllers/friends/friends'
 import { confirmFriendshipController } from '../../controllers/friends/confirmFriend'
+import { removeFriendController } from '../../controllers/friends/removeFriend'
 
 const router = Router()
 
@@ -10,5 +12,6 @@ router.get('/get-friends', getUsers)
 
 router.post('/make-friendship', authenticateAccessTokenForApi, makeFriendshipController)
 router.post('/confirm-friendship', authenticateAccessTokenForApi, confirmFriendshipController)
+router.delete('/remove-friend', authenticateAccessTokenForApi, removeFriendController)
 
 module.exports = router
