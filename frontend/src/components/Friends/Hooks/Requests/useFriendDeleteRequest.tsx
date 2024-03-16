af6837cb891259a89cdb1a0831af1d81de5561ba
nagytommy76 <nagytommy76@gmail.com>
2024-03-16 15:16:29 +0100

    Added an isFriendRequestWithdraw prop to decide friend remove or withdraw

--- frontend/src/components/Friends/Hooks/Requests/useFriendDeleteRequest.tsx
+++ frontend/src/components/Friends/Hooks/Requests/useFriendDeleteRequest.tsx
@@ -1,18 +1,36 @@
 import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
 import { useMutation } from '@tanstack/react-query'
+import type { FriendButtonType, IFriends } from '../../Types'
 
-const useFriendDeleteRequest = (friendId: string) => {
-   const removeFriendMutation = async () => {
-      await axios.delete('/friends/remove-friend', { data: { friendId } })
+const useFriendDeleteRequest = (
+   friendId: string,
+   setLoading: React.Dispatch<React.SetStateAction<boolean>>,
+   setCardButtonType: React.Dispatch<React.SetStateAction<FriendButtonType>>
+) => {
+   const removeFriendMutation = async (isFriendRequestWithdraw: boolean = false) => {
+      return (await axios.delete(`/friends/remove-friend`, {
+         data: { friendId, isFriendRequestWithdraw },
+      })) as AxiosResponse<{
+         loggedInUserFriends: IFriends[]
+      }>
    }
 
    const { mutate } = useMutation({
       mutationKey: ['removeFriend'],
       mutationFn: removeFriendMutation,
+      onMutate: () => {
+         setLoading(true)
+      },
+      onSuccess(data, variables, context) {
+         setLoading(false)
+         setCardButtonType('makeFriend')
+      },
    })
 
+   const deleteFriendMutate = (isFriendRequestWithdraw?: boolean) => mutate(isFriendRequestWithdraw)
+
    return {
-      deleteFriendMutate: () => mutate(),
+      deleteFriendMutate,
    }
 }
 
