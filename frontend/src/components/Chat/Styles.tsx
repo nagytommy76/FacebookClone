8ba3684b23f0373acd1ae93cb08a40805f3c47fc
nagytommy76 <nagytommy76@gmail.com>
2024-03-17 14:40:35 +0100

    Added a ChatMenuContainer style

---
+++ frontend/src/components/Chat/Styles.tsx
@@ -0,0 +1,46 @@
+import { styled } from '@mui/material/styles'
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip'
+
+import Menu from '@mui/material/Menu'
+
+export const StyledButtonContainer = styled('div')({
+   position: 'absolute',
+   right: 25,
+   bottom: 30,
+})
+
+export const ChatMenuContainer = styled(Menu)(({ theme }) => ({
+   '& .MuiPaper-root': {
+      width: '350px',
+      minHeight: '550px',
+
+      borderRadius: 6,
+      padding: '12px',
+      marginBottom: theme.spacing(15),
+      minWidth: 180,
+      color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+      boxShadow:
+         'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+      '& .MuiMenu-list': {
+         padding: '4px 0',
+      },
+      '& .MuiMenuItem-root': {
+         display: 'flex',
+         flexDirection: 'row',
+
+         height: '80px',
+         borderRadius: 7,
+      },
+   },
+}))
+
+export const CustomizedTooltip = styled(({ className, ...props }: TooltipProps) => (
+   <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+   [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.background.paper,
+      boxShadow: theme.shadows[1],
+      fontSize: 15,
+   },
+}))
