057e033d08600bee9000280173de4f411e0377be
nagytommy76 <nagytommy76@gmail.com>
2024-03-17 14:41:26 +0100

    Added a chatImg & using the chat button

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,15 @@
 'use client'
 import Link from 'next/link'
+import dynamic from 'next/dynamic'
 import { useAppSelector } from '../../utils/redux/store'
 
 import Menu from './Includes/Menu/Menu'
 import Avatar from './Includes/Avatar/Avatar'
 import NotificationContextProvider from './Includes/Notification/Context/NotificationContextProvider'
 import LeftSide from './Includes/LeftSide/LeftSide'
+
+const ChatButton = dynamic(() => import('../Chat/ChatButton'))
+
 import { StyledNavbarToolbar, RightSideContainer } from './Style'
 
 import AppBar from '@mui/material/AppBar'
@@ -14,22 +18,25 @@ import Button from '@mui/material/Button'
 const Navbar = () => {
    const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
    return (
-      <AppBar position='sticky'>
-         <StyledNavbarToolbar disableGutters>
-            <LeftSide />
-            <Menu />
-            {!isLoggedIn ? (
-               <Link href='/login'>
-                  <Button color='inherit'>Belépés</Button>
-               </Link>
-            ) : (
-               <RightSideContainer>
-                  <NotificationContextProvider />
-                  <Avatar />
-               </RightSideContainer>
-            )}
-         </StyledNavbarToolbar>
-      </AppBar>
+      <>
+         <AppBar position='sticky'>
+            <StyledNavbarToolbar disableGutters>
+               <LeftSide />
+               <Menu />
+               {!isLoggedIn ? (
+                  <Link href='/login'>
+                     <Button color='inherit'>Belépés</Button>
+                  </Link>
+               ) : (
+                  <RightSideContainer>
+                     <NotificationContextProvider />
+                     <Avatar />
+                  </RightSideContainer>
+               )}
+            </StyledNavbarToolbar>
+         </AppBar>
+         <ChatButton />
+      </>
    )
 }
 
