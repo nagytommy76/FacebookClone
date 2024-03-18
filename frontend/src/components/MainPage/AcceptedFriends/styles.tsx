d8261260200f2c2c27e2c5f00d095ff26e1f5cd5
nagytommy76 <nagytommy76@gmail.com>
2024-03-18 19:19:16 +0100

    Created a style for AcceptedFriends component

---
+++ frontend/src/components/MainPage/AcceptedFriends/styles.tsx
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material/styles'
+
+export const AcceptedFriendsStyles = styled('aside')({
+   width: '300px',
+   height: '500px',
+   marginLeft: 25,
+   backgroundColor: 'blue',
+
+   position: 'sticky',
+   // right: 80,
+   top: '80px',
+})
