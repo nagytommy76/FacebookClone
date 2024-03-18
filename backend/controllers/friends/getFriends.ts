2d84d3ae70ba57541d8ce4ac17bad740f6cb3122
nagytommy76 <nagytommy76@gmail.com>
2024-03-18 19:17:09 +0100

    Added a getAcceptedUsers controller

--- backend/controllers/friends/getFriends.ts
+++ backend/controllers/friends/getFriends.ts
@@ -1,5 +1,7 @@
 import { Response, Request } from 'express'
+import { Types } from 'mongoose'
 import { User as UserModel } from '../../models/user/user'
+import { IJWTUserType } from '../../middlewares/accessTokenRefresh'
 
 // https://www.mongodb.com/docs/manual/reference/operator/aggregation/filter/
 // https://www.mongodb.com/docs/manual/reference/operator/aggregation/match/
@@ -38,6 +40,35 @@ export const getUsers = async (request: Request, response: Response) => {
       ])
 
       return response.status(200).json(users)
+   } catch (error) {
+      console.log(error)
+      return response.status(500).json(error)
+   }
+}
+
+// https://stackoverflow.com/questions/50363220/modelling-for-friends-schema-in-mongoose
+
+export const getAcceptedUsers = async (request: IJWTUserType, response: Response) => {
+   const userId = new Types.ObjectId(request.user?.userId)
+   try {
+      const acceptedFriends = await UserModel.aggregate([
+         {
+            $match: { _id: userId },
+         },
+         {
+            $project: {
+               friends: {
+                  $filter: {
+                     input: '$friends',
+                     as: 'acceptedFriends',
+                     cond: { $eq: ['$$acceptedFriends.isAccepted', true] },
+                  },
+               },
+            },
+         },
+      ])
+
+      return response.status(200).json(acceptedFriends)
    } catch (error) {
       console.log(error)
       return response.status(500).json(error)
