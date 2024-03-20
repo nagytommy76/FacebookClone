c3c7348d52bacf960b9165545763a3ad5cd0357d
nagytommy76 <nagytommy76@gmail.com>
2024-03-20 19:23:33 +0100

    Saving the ids && FriendsModel created

--- backend/controllers/friends/friends.ts
+++ backend/controllers/friends/friends.ts
@@ -1,5 +1,6 @@
 import { Response } from 'express'
 import { User as UserModel } from '../../models/user/user'
+import { FriendsModel } from '../../models/friends/friends'
 import { IMakeFriends } from './Types'
 
 export const makeFriendshipController = async (request: IMakeFriends, response: Response) => {
@@ -17,26 +18,18 @@ export const makeFriendshipController = async (request: IMakeFriends, response: 
          'sureName',
          'notifications',
       ])
+
       if (!senderUser) return response.status(404).json({ msg: 'Sender user not found' })
       if (!receiverUser) return response.status(404).json({ msg: 'Receiver user not found' })
-
-      senderUser.friends.push({
-         createdAt: new Date(),
-         receiverUserId: receiverUser._id,
-         senderUserId: senderUser._id,
-         isAccepted: true,
-         isSender: true,
-         isReceiver: false,
+      await FriendsModel.create({
+         receiverUser: receiverUser._id,
+         senderUser: senderUser._id,
+         status: 'pending',
       })
 
-      receiverUser.friends.push({
-         createdAt: new Date(),
-         receiverUserId: receiverUser._id,
-         senderUserId: senderUser._id,
-         isAccepted: false,
-         isSender: false,
-         isReceiver: true,
-      })
+      senderUser.friends.push(receiverUser._id)
+      receiverUser.friends.push(senderUser._id)
+
       receiverUser.notifications.push({
          createdAt: new Date(),
          isRead: false,
@@ -48,6 +41,7 @@ export const makeFriendshipController = async (request: IMakeFriends, response: 
             profilePicture: senderUser.userDetails.profilePicturePath[0].path,
          },
       })
+
       await receiverUser.save()
       await senderUser.save()
 
