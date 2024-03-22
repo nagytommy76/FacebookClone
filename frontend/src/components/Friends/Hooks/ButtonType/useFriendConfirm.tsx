a79211cb1d5a355b07fca3437f47b4111d6eadce
nagytommy76 <nagytommy76@gmail.com>
2024-03-22 16:02:29 +0100

    Find the receiver user

--- frontend/src/components/Friends/Hooks/ButtonType/useFriendConfirm.tsx
+++ frontend/src/components/Friends/Hooks/ButtonType/useFriendConfirm.tsx
@@ -1,33 +1,32 @@
 import { useEffect, useCallback } from 'react'
 import { useAppSelector } from '@/reduxStore/store'
-import type { FriendButtonType, IFriends } from '../../Types'
+import type { FriendButtonType, IConnectedFriends } from '../../Types'
 
 import useFriendSocket from '../Sockets/useFriendSocket'
 
 const useFriendConfirm = (
    friendId: string,
-   friends: IFriends[],
+   connectedFriends: IConnectedFriends[],
    setCardButtonType: (value: React.SetStateAction<FriendButtonType>) => void
 ) => {
    const userId = useAppSelector((state) => state.auth.userId)
 
    const setButtonTypeToConfirmFriend = useCallback(
-      (friends: IFriends[], friendId: string) => {
+      (/*friends: IFriends[], friendId: string*/) => {
          // Ebben az esetben megtaláltam a nekem ( belépett user ) küldött requesteket
-         const myFriendRequest = friends.find(
-            (item) => item.senderUserId === friendId && item.receiverUserId === userId
-         )
+         const myFriendRequest = connectedFriends.find((friend) => friend.receiverUser == userId)
          if (myFriendRequest) {
             setCardButtonType('confirmFriend')
          }
+         return myFriendRequest
       },
-      [setCardButtonType, userId]
+      [setCardButtonType, userId, connectedFriends]
    )
    useFriendSocket(friendId, setButtonTypeToConfirmFriend)
 
    useEffect(() => {
-      setButtonTypeToConfirmFriend(friends, friendId)
-   }, [setButtonTypeToConfirmFriend, friends, friendId])
+      setButtonTypeToConfirmFriend()
+   }, [setButtonTypeToConfirmFriend, friendId])
 
    return setButtonTypeToConfirmFriend
 }
