d90c064b183cd95f79d13e8a8e5c344ca9af0f1c
nagytommy76 <nagytommy76@gmail.com>
2024-03-26 11:50:52 +0100

    Working on the confirm socket hook

--- frontend/src/components/Friends/Hooks/Sockets/useConfirmSocket.tsx
+++ frontend/src/components/Friends/Hooks/Sockets/useConfirmSocket.tsx
@@ -1,22 +1,40 @@
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
+import { FriendContext } from '../../Context/FriendContext'
+import { useAppSelector } from '@/reduxStore/store'
+
 import { socket } from '@/src/utils/socketIo'
-import type { IFriends } from '../../Types'
+import type { IConnectedFriends } from '../../Types'
 import type { IMakeFriendshipArgs } from './Types'
 
-const useConfirmSocket = (
-   friendId: string,
-   setCardTypeDeleteFriendReceiver: (friends: IFriends[]) => IFriends | undefined
-) => {
+const useConfirmSocket = (setBtnTypeDeleteFriendSender: (connectedFriend: IConnectedFriends) => void) => {
+   const {
+      friendReducer: { selectedConnectedFriend },
+      setCardButtonType,
+   } = useContext(FriendContext)
+   const userId = useAppSelector((state) => state.auth.userId)
+
    useEffect(() => {
       const setButtonType = (args: IMakeFriendshipArgs) => {
-         setCardTypeDeleteFriendReceiver(args.userFriends)
+         setBtnTypeDeleteFriendSender(args.foundFriendsModel)
+         const connectedFriend = args.foundFriendsModel
+
+         // Át kéne tenni ide, nem a useFriendDelete hookban
+         if (
+            connectedFriend.senderUser == userId ||
+            (connectedFriend.receiverUser && connectedFriend.status === 'friends')
+         ) {
+            setCardButtonType('isFriend')
+         }
+
+         setCardButtonType('isFriend')
+         console.log(args)
       }
       socket.on('confirmFriendship', setButtonType)
 
       return () => {
          socket.off('confirmFriendship', setButtonType)
       }
-   }, [setCardTypeDeleteFriendReceiver, friendId])
+   }, [setBtnTypeDeleteFriendSender, setCardButtonType])
 
    return null
 }
