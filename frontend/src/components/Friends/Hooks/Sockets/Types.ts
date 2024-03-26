96f2710964ee9cf5be817660f9dcc09e12a8e5d6
nagytommy76 <nagytommy76@gmail.com>
2024-03-26 11:50:26 +0100

    Added a foundFriendsModel type

--- frontend/src/components/Friends/Hooks/Sockets/Types.ts
+++ frontend/src/components/Friends/Hooks/Sockets/Types.ts
@@ -1,7 +1,8 @@
 import type { NotificationType } from '@/src/components/Navbar/Includes/Notification/Types'
-import type { IFriends } from '../../Types'
+import type { IConnectedFriends, IFriends } from '../../Types'
 
 export interface IMakeFriendshipArgs {
    notifications: NotificationType
    userFriends: IFriends[]
+   foundFriendsModel: IConnectedFriends
 }
