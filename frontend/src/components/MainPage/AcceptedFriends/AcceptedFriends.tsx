3f56303afd5f372c485c90ea7238fcb687daf3bc
nagytommy76 <nagytommy76@gmail.com>
2024-03-29 15:48:49 +0100

    Stlyed the friends aside

--- frontend/src/components/MainPage/AcceptedFriends/AcceptedFriends.tsx
+++ frontend/src/components/MainPage/AcceptedFriends/AcceptedFriends.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import Image from 'next/image'
 import useGetAccepted from './Hooks/useGetAccepted'
 
-import { AcceptedFriendsStyles } from './styles'
+import { AcceptedFriendsStyles, FriendMenuItemStyle } from './styles'
+import Typography from '@mui/material/Typography'
+import Avatar from '@mui/material/Avatar'
 
 const AcceptedFriends = () => {
-   useGetAccepted()
+   const myFriends = useGetAccepted()
    return (
       <AcceptedFriendsStyles>
-         <h1>Ide jönnek a már elfogadott barátok</h1>
+         <Typography sx={{ mb: 5 }} variant='h5'>
+            Ismerősök
+         </Typography>
+         {myFriends.map((friend) => (
+            <FriendMenuItemStyle onClick={() => console.log('Chat indítása ')} key={friend._id}>
+               <Avatar sx={{ width: 56, height: 56, mr: 2 }}>
+                  <Image
+                     width={56}
+                     height={56}
+                     alt={`${friend.sureName} ${friend.firstName}`}
+                     src={friend.selectedProfilePicture[0].path}
+                  />
+               </Avatar>
+               <Typography variant='h6'>{`${friend.firstName} ${friend.sureName}`}</Typography>
+            </FriendMenuItemStyle>
+         ))}
       </AcceptedFriendsStyles>
    )
 }
