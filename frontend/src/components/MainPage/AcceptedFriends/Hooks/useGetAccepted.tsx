7ac35a8be8c76b49da4c5c70ad54f294f48616a7
nagytommy76 <nagytommy76@gmail.com>
2024-03-29 15:49:17 +0100

    Return a state with myFriends

--- frontend/src/components/MainPage/AcceptedFriends/Hooks/useGetAccepted.tsx
+++ frontend/src/components/MainPage/AcceptedFriends/Hooks/useGetAccepted.tsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+import { AxiosResponse, axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+import type { IProfilePicture } from '@/src/types/PostTypes'
+
+interface IFriendResponse {
+   _id: string
+   firstName: string
+   sureName: string
+   selectedProfilePicture: IProfilePicture[]
+}
 
 const useGetAccepted = () => {
+   const [myFriends, setMyFriends] = useState<IFriendResponse[]>([])
    const queryFn = async () => {
-      return await axios.get('/friends/get-accepted-friends')
+      return (await axios.get('/friends/get-accepted-friends')) as AxiosResponse<{
+         myFoundFriendsData: IFriendResponse[]
+      }>
    }
 
    const { data } = useQuery({
       queryKey: ['getAcceptedFriends'],
       queryFn,
       onSuccess(data) {
-         console.log(data.data)
+         console.log(data.data.myFoundFriendsData[0])
+         setMyFriends(data.data.myFoundFriendsData)
       },
    })
 
-   return null
+   return myFriends
 }
 
 export default useGetAccepted
