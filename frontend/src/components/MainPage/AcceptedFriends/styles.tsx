4fe144f289f05da7a0ede3c8e40368290158e9ef
nagytommy76 <nagytommy76@gmail.com>
2024-03-29 15:49:40 +0100

    Added FriendMenuItemStyle

--- frontend/src/components/MainPage/AcceptedFriends/styles.tsx
+++ frontend/src/components/MainPage/AcceptedFriends/styles.tsx
@@ -1,12 +1,22 @@
 import { styled } from '@mui/material/styles'
+import MenuItem from '@mui/material/MenuItem'
 
 export const AcceptedFriendsStyles = styled('aside')({
    width: '300px',
-   height: '500px',
+   height: '600px',
    marginLeft: 25,
-   backgroundColor: 'blue',
-
    position: 'sticky',
-   // right: 80,
    top: '80px',
+
+   padding: 18,
 })
+
+export const FriendContainerStyle = styled('section')({})
+
+export const FriendMenuItemStyle = styled(MenuItem)(({ theme }) => ({
+   width: '100%',
+   marginBottom: 10,
+   padding: '.6rem .3rem',
+   borderRadius: 5,
+   transition: 'all .2s ease',
+}))
