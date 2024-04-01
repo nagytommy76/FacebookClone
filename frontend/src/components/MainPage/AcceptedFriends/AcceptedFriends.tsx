a4eac1876c3c02fde1e11b32046e454c15a03d73
nagytommy76 <nagytommy76@gmail.com>
2024-04-01 14:47:23 +0200

    Using the ChatAvatar component

--- frontend/src/components/MainPage/AcceptedFriends/AcceptedFriends.tsx
+++ frontend/src/components/MainPage/AcceptedFriends/AcceptedFriends.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Image from 'next/image'
+import dynamic from 'next/dynamic'
 import useGetAccepted from './Hooks/useGetAccepted'
 
 import { AcceptedFriendsStyles, FriendMenuItemStyle } from './styles'
-import { StyledIsActive } from '@/styles/BaseStyles'
 
 import Typography from '@mui/material/Typography'
-import Avatar from '@mui/material/Avatar'
+
+const ChatAvatar = dynamic(() => import('@/Base/ChatAvatar/ChatAvatar'))
 
 const AcceptedFriends = () => {
    const myFriends = useGetAccepted()
@@ -17,21 +16,12 @@ const AcceptedFriends = () => {
          </Typography>
          {myFriends.map((friend) => (
             <FriendMenuItemStyle onClick={() => console.log('Chat indítása ')} key={friend._id}>
-               <Avatar sx={{ width: 56, height: 56, mr: 2, position: 'relative' }}>
-                  <Image
-                     width={56}
-                     height={56}
-                     alt={`${friend.sureName} ${friend.firstName}`}
-                     src={friend.selectedProfilePicture[0].path}
-                  />
-                  <StyledIsActive
-                     isRead={false}
-                     rightPosition='1px'
-                     bottomPosition='10px'
-                     topPosition='unset'
-                  />
-               </Avatar>
-               <Typography variant='h6'>{`${friend.firstName} ${friend.sureName}`}</Typography>
+               <ChatAvatar
+                  firstName={friend.firstName}
+                  sureName={friend.sureName}
+                  selectedProfilePicturePath={friend.selectedProfilePicture[0].path}
+               />
+               <Typography variant='body1'>{`${friend.firstName} ${friend.sureName}`}</Typography>
             </FriendMenuItemStyle>
          ))}
       </AcceptedFriendsStyles>
