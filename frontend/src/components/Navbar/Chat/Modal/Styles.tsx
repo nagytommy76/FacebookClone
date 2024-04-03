650cf4aaf8a9cc1e5d24d855532390711c70d75b
nagytommy76 <nagytommy76@gmail.com>
2024-04-03 14:21:22 +0200

    Added styles to ChatModal component

---
+++ frontend/src/components/Navbar/Chat/Modal/Styles.tsx
@@ -0,0 +1,45 @@
+import { styled } from '@mui/material/styles'
+import Paper from '@mui/material/Paper'
+import Tab from '@mui/material/Tab'
+import TabList from '@mui/lab/TabList'
+
+export const StyledPaper = styled(Paper)(({ theme }) => ({
+   width: '80%',
+   minHeight: '700px',
+   position: 'absolute',
+   top: '50%',
+   left: '50%',
+   transform: 'translate(-50%, -50%)',
+
+   display: 'flex',
+   flexDirection: 'row',
+
+   [theme.breakpoints.down('md')]: {
+      width: '95%',
+      minHeight: '85%',
+   },
+}))
+
+export const StyledChatAside = styled('aside')(({ theme }) => ({
+   flex: 2,
+}))
+
+export const StyledChatMessagesContainer = styled('section')(({ theme }) => ({
+   flex: 6,
+}))
+
+export const StyledTab = styled(Tab)({
+   display: 'flex',
+   flexDirection: 'row',
+   textTransform: 'none',
+   height: '80px',
+})
+export const StyledTabList = styled(TabList)({
+   borderRight: 1,
+   borderColor: 'divider',
+   height: '100%',
+   '& .MuiTabs-indicator': {
+      backgroundColor: '#f5ab0b',
+      width: '5px',
+   },
+})
