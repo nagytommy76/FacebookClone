72bd540551d941dcf7a350208a158321bf27315d
nagytommy76 <nagytommy76@gmail.com>
2024-04-05 16:59:45 +0200

    Using the ChatAside & ChatMessages components

--- frontend/src/components/Navbar/Chat/Modal/ChatModal.tsx
+++ frontend/src/components/Navbar/Chat/Modal/ChatModal.tsx
@@ -1,20 +1,19 @@
 import dynamic from 'next/dynamic'
 import { useAppDispatch, useAppSelector } from '@/reduxStore/store'
-import { setChatModalOpen, setTabValue } from '@/reduxStore/slices/ChatSlice'
+import { setChatModalOpen } from '@/reduxStore/slices/ChatSlice'
 
-import { StyledTab, StyledTabList, StyledPaper, StyledChatAside, StyledChatMessagesContainer } from './Styles'
+import { StyledPaper } from './Styles'
 import Modal from '@mui/material/Modal'
-import TabPanel from '@mui/lab/TabPanel'
 import TabContext from '@mui/lab/TabContext'
 
-import AddTextBase from '@/Base/AddTextBase/AddTextBase'
-const TabLabel = dynamic(() => import('./Includes/TabLabel'))
+const ChatAside = dynamic(() => import('./Includes/ChatAside/ChatAside'))
+const ChatMessages = dynamic(() => import('./Includes/ChatMessages/ChatMessages'))
 
 const ChatModal = () => {
    const chatModalOpen = useAppSelector((state) => state.chat.isChatModalOpen)
    const tabValue = useAppSelector((state) => state.chat.tabValue)
-   const messageLabels = useAppSelector((state) => state.chat.messageLabels)
    const dispatch = useAppDispatch()
+
    return (
       <Modal
          open={chatModalOpen}
@@ -24,47 +23,8 @@ const ChatModal = () => {
       >
          <StyledPaper>
             <TabContext value={tabValue}>
-               <StyledChatAside>
-                  <StyledTabList
-                     variant='scrollable'
-                     indicatorColor='primary'
-                     orientation='vertical'
-                     onChange={(event: React.SyntheticEvent, newValue: string) => {
-                        dispatch(setTabValue(newValue))
-                     }}
-                     aria-label='lab API tabs example'
-                  >
-                     {messageLabels?.map((chatMenuItem) => (
-                        <StyledTab
-                           key={chatMenuItem._id}
-                           value={chatMenuItem._id}
-                           label={
-                              <TabLabel
-                                 captionText={chatMenuItem.captionText}
-                                 fullName={chatMenuItem.fullName}
-                                 selectedProfilePicturePath={chatMenuItem.selectedProfilePicturePath}
-                              />
-                           }
-                        />
-                     ))}
-                  </StyledTabList>
-               </StyledChatAside>
-               <StyledChatMessagesContainer>
-                  <TabPanel value='658569424d27aad220f6e887'>
-                     <div>
-                        <h1>Teszt Béla</h1>
-                        <AddTextBase
-                           value=''
-                           placeholderText='...'
-                           setImagePath={() => {}}
-                           onClickFunction={() => {}}
-                           handleChangeValue={() => {}}
-                           handleChangeValueWithEmoji={() => {}}
-                        />
-                     </div>
-                  </TabPanel>
-                  <TabPanel value='64777ef1c3038faf5e1a41c6'>Teszt János</TabPanel>
-               </StyledChatMessagesContainer>
+               <ChatAside />
+               <ChatMessages />
             </TabContext>
          </StyledPaper>
       </Modal>
