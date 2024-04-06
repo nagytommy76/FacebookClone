8b0f5d8087b5f5e6036a8adee01603cf64bdf9e2
nagytommy76 <nagytommy76@gmail.com>
2024-04-06 15:25:22 +0200

    Added a width & height optional property

--- frontend/src/components/Base/ChatAvatar/ChatAvatar.tsx
+++ frontend/src/components/Base/ChatAvatar/ChatAvatar.tsx
@@ -3,14 +3,16 @@ import Image from 'next/image'
 import { StyledIsActive } from '@/styles/BaseStyles'
 import Avatar from '@mui/material/Avatar'
 
-const ChatAvatar: React.FC<{ fullName: string; selectedProfilePicturePath: string }> = ({
-   fullName,
-   selectedProfilePicturePath,
-}) => {
+const ChatAvatar: React.FC<{
+   fullName: string
+   selectedProfilePicturePath: string
+   width?: number
+   height?: number
+}> = ({ fullName, selectedProfilePicturePath, width = 56, height = 56 }) => {
    return (
       <>
-         <Avatar sx={{ width: 50, height: 50, mr: 2, position: 'relative' }}>
-            <Image width={56} height={56} alt={fullName} src={selectedProfilePicturePath} />
+         <Avatar sx={{ width, height, mr: 2, position: 'relative' }}>
+            <Image width={width} height={height} alt={fullName} src={selectedProfilePicturePath} />
             <StyledIsActive isRead={false} rightPosition='1px' bottomPosition='10px' topPosition='unset' />
          </Avatar>
       </>
