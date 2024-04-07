1b4ef4fe252d6649c2ab111f215fd6e4741fb21c
nagytommy76 <nagytommy76@gmail.com>
2024-04-07 14:06:02 +0200

    Using isRightContent prop to decide the orientation

---
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/Styles.tsx
@@ -0,0 +1,30 @@
+import { styled } from '@mui/material/styles'
+import Paper from '@mui/material/Paper'
+
+export const StyledTextContainer = styled('div', {
+   shouldForwardProp: (prop) => prop !== 'isRightContent',
+})<{ isRightContent: boolean }>(({ isRightContent, theme }) => ({
+   width: '48%',
+   alignSelf: isRightContent ? 'end' : 'start',
+   marginBottom: 15,
+}))
+
+export const StyledTextBoxHead = styled('span', {
+   shouldForwardProp: (prop) => prop !== 'isRightContent',
+})<{ isRightContent: boolean }>(({ isRightContent, theme }) => ({
+   display: 'flex',
+   alignItems: 'center',
+   flexDirection: isRightContent ? 'row' : 'row-reverse',
+   justifyContent: 'space-between',
+}))
+
+export const StyledTextBox = styled(Paper, {
+   shouldForwardProp: (prop) => prop !== 'isRightContent',
+})<{ isRightContent: boolean }>(({ isRightContent, theme }) => ({
+   padding: '.85rem',
+   textAlign: 'justify',
+
+   borderRadius: '25px',
+   borderTopRightRadius: isRightContent ? 0 : '25px',
+   borderTopLeftRadius: isRightContent ? '25px' : 0,
+}))
