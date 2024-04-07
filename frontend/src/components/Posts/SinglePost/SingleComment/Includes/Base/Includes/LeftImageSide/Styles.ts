9a5479fa91930ccad98b29d253c3ef73f1e288e1
nagytommy76 <nagytommy76@gmail.com>
2024-04-07 14:12:00 +0200

    Added shouldForwardProp

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/LeftImageSide/Styles.ts
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/LeftImageSide/Styles.ts
@@ -7,24 +7,24 @@ export const StyledLeftSide = styled('aside')({
    alignItems: 'center',
 })
 
-export const HorizontalLineStyle = styled('span')<{ isChildComment: boolean }>(
-   ({ isChildComment, theme }) => ({
-      display: isChildComment ? 'block' : 'none',
-      position: 'absolute',
-      // cursor: 'pointer',
+export const HorizontalLineStyle = styled('span', {
+   shouldForwardProp: (prop) => prop !== 'isChildComment',
+})<{ isChildComment: boolean }>(({ isChildComment, theme }) => ({
+   display: isChildComment ? 'block' : 'none',
+   position: 'absolute',
+   // cursor: 'pointer',
 
-      top: '50px',
-      width: '6px',
-      height: 'calc(100% - 50px)',
+   top: '50px',
+   width: '6px',
+   height: 'calc(100% - 50px)',
 
-      borderLeft: '2px solid transparent',
-      borderRight: '2px solid transparent',
+   borderLeft: '2px solid transparent',
+   borderRight: '2px solid transparent',
 
-      backgroundColor: 'rgba(100, 100, 100, .55)',
-      backgroundClip: 'padding-box',
-      transition: 'all .05s ease',
-      ['&:hover']: {
-         backgroundColor: theme.palette.grey[300],
-      },
-   })
-)
+   backgroundColor: 'rgba(100, 100, 100, .55)',
+   backgroundClip: 'padding-box',
+   transition: 'all .05s ease',
+   ['&:hover']: {
+      backgroundColor: theme.palette.grey[300],
+   },
+}))
