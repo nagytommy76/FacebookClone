baa1fed86a29de988203ef55755ef99c9f3f3b21
nagytommy76 <nagytommy76@gmail.com>
2024-04-07 14:05:18 +0200

    Created a text display component

---
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/MessageItem.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+import moment from 'moment'
+
+import { StyledTextContainer, StyledTextBox, StyledTextBoxHead } from './Styles'
+import Typography from '@mui/material/Typography'
+
+const ChatAvatar = dynamic(() => import('@/Base/ChatAvatar/ChatAvatar'))
+
+const MessageItem: React.FC<{
+   message: {
+      _id: string
+      createdAt: string
+      updatedAt: string
+      isRead: boolean
+      userId: string
+      message: string
+      image: string
+   }
+   isRightContent?: boolean
+}> = ({ message, isRightContent = false }) => {
+   return (
+      <StyledTextContainer isRightContent={isRightContent}>
+         <StyledTextBoxHead isRightContent={isRightContent}>
+            <Typography variant='caption'>{moment(message.createdAt).format('h:mm:ss')}</Typography>
+            <ChatAvatar
+               fullName='Saját Neve'
+               selectedProfilePicturePath={message.image}
+               width={30}
+               height={30}
+            />
+         </StyledTextBoxHead>
+         <StyledTextBox isRightContent={isRightContent}>
+            <Typography fontWeight={350} variant='caption'>
+               {message.message}
+            </Typography>
+         </StyledTextBox>
+      </StyledTextContainer>
+   )
+}
+
+export default MessageItem
