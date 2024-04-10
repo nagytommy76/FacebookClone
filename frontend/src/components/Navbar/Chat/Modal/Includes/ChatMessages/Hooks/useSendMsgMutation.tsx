d77fc66a8f0573c5dd5d9b9e2cfd1a03cbf82180
nagytommy76 <nagytommy76@gmail.com>
2024-04-10 19:06:07 +0200

    Import the useMessage -> export the props

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Hooks/useSendMsgMutation.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Hooks/useSendMsgMutation.tsx
@@ -1,18 +1,34 @@
 import React from 'react'
+import useMessage from './useMessage'
+
 import { useMutation } from '@tanstack/react-query'
 import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
 
 const useSendMsgMutation = () => {
+   const { chatMsg, chatRef, chatImagePath, setChatImagePath, handleChatMsg, handleChangeTextWithEmoji } =
+      useMessage()
+
    const chatMutateFn = async () => {
-      return await axios.post('')
+      return await axios.post('/chat/add-chat-msg', { chatMsg })
    }
 
-   const {} = useMutation({
+   const { mutate } = useMutation({
       mutationKey: ['addChatMessage'],
       mutationFn: chatMutateFn,
+      onSuccess(data, variables, context) {
+         console.log(data.data)
+      },
    })
 
-   return null
+   return {
+      chatMsg,
+      chatRef,
+      chatImagePath,
+      setChatImagePath,
+      handleChatMsg,
+      handleChangeTextWithEmoji,
+      handleAddChatMutate: () => mutate(),
+   }
 }
 
 export default useSendMsgMutation
