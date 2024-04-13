fb2db3ca315bf6d98eab9e4e3a28da9eab89addd
nagytommy76 <nagytommy76@gmail.com>
2024-04-13 15:01:14 +0200

    Created a getUsersChatMessagesController

--- backend/api/chat/chat.ts
+++ backend/api/chat/chat.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { authenticateAccessTokenForApi } from '../../middlewares/accessTokenRefresh'
-import { saveChatController } from '../../controllers/chat/chat'
+import { saveChatController, getUsersChatMessagesController } from '../../controllers/chat/chat'
 
 const router = Router()
+
+router.get('/get-user-chat', authenticateAccessTokenForApi, getUsersChatMessagesController)
 
 router.post('/add-chat-msg', authenticateAccessTokenForApi, saveChatController)
 
--- backend/controllers/chat/chat.ts
+++ backend/controllers/chat/chat.ts
@@ -30,3 +30,24 @@ export const saveChatController = (request: ISaveChatRequest, response: Response
       response.status(500).json(error)
    }
 }
+
+interface IGetUsersChatRequest extends IJWTUserType {
+   query: {
+      chatWithUserId: string
+   }
+}
+
+export const getUsersChatMessagesController = async (request: IGetUsersChatRequest, response: Response) => {
+   const userId = request.user?.userId
+   const chatWithUserId = request.query.chatWithUserId
+
+   console.log(chatWithUserId)
+   console.log(userId)
+   try {
+      const foundChat = await ChatModel.findOne({ participants: { $all: [userId, chatWithUserId] } })
+      response.status(200).json(foundChat)
+   } catch (error) {
+      console.log(error)
+      response.status(500).json(error)
+   }
+}
