345ad2d318b01fd84bf94f0782292c58a02322e5
nagytommy76 <nagytommy76@gmail.com>
2024-04-14 15:34:35 +0200

    Query the user's chat

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Hooks/useGetMessages.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Hooks/useGetMessages.tsx
@@ -1,24 +1,51 @@
+import { useState } from 'react'
 import { useAppSelector } from '@/src/utils/redux/store'
 
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
 
+interface IMessages {
+   _id: string
+   createdAt: string
+   updatedAt: string
+   image: string
+   isRead: boolean
+   message: string
+   userId: string
+}
+
+interface IChatResponse {
+   _id: string
+   createdAt: string
+   updatedAt: string
+   messages: IMessages[]
+   participants: string[]
+}
+
 const useGetMessages = () => {
+   const [messages, setMessages] = useState<IMessages[] | null>(null)
    const { chatWithUserId } = useAppSelector((state) => state.chat)
 
    const getMessagesFunction = async () => {
-      return await axios.get('/chat/get-user-chat', { params: { chatWithUserId } })
+      return (await axios.get('/chat/get-user-chat', {
+         params: { chatWithUserId },
+      })) as AxiosResponse<IChatResponse | null>
    }
 
-   const {} = useQuery({
-      queryKey: ['getUsersChatMessgaes', chatWithUserId],
+   const { data, isLoading } = useQuery({
+      queryKey: ['getUsersChatMessgaes', { chatWithUserId }],
       queryFn: getMessagesFunction,
       onSuccess(data) {
-         console.log(data.data)
+         const receivedData = data.data
+         // console.log(receivedData)
+         if (receivedData) {
+            console.log(receivedData.messages)
+            setMessages(receivedData.messages)
+         }
       },
    })
 
-   return null
+   return { messages, isLoading }
 }
 
 export default useGetMessages
