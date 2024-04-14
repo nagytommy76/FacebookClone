0aca84d844eed323a545a2301b4eb75e209cbb56
nagytommy76 <nagytommy76@gmail.com>
2024-04-14 15:31:52 +0200

    Added createNewChatController to api route

--- backend/api/chat/chat.ts
+++ backend/api/chat/chat.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express'
 import { authenticateAccessTokenForApi } from '../../middlewares/accessTokenRefresh'
-import { saveChatController, getUsersChatMessagesController } from '../../controllers/chat/chat'
+import {
+   saveChatController,
+   getUsersChatMessagesController,
+   createNewChatController,
+} from '../../controllers/chat/chat'
 
 const router = Router()
 
 router.get('/get-user-chat', authenticateAccessTokenForApi, getUsersChatMessagesController)
 
 router.post('/add-chat-msg', authenticateAccessTokenForApi, saveChatController)
+router.post('/create-chat', authenticateAccessTokenForApi, createNewChatController)
 
 module.exports = router
