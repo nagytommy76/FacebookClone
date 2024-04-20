dc06069a77aca8d7118bbd268af16679b4a93447
nagytommy76 <nagytommy76@gmail.com>
2024-04-20 14:29:53 +0200

    Created useFillMessageLabels to query all the users chats

---
+++ frontend/src/components/Navbar/Chat/ChatButton/Hooks/useFillMessageLabels.tsx
@@ -0,0 +1,27 @@
+import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
+
+import { useAppDispatch } from '@/reduxStore/store'
+import { setMessageLabels } from '@/reduxStore/slices/ChatSlice'
+import { useQuery } from '@tanstack/react-query'
+
+import type { IChat } from '@/src/components/Navbar/Chat/Types'
+
+const useFillMessageLabels = () => {
+   const dispatch = useAppDispatch()
+
+   const queryFunction = async () => {
+      return (await axios.get('/chat/get-all-chats')) as AxiosResponse<{ foundChat: IChat[] }>
+   }
+
+   const {} = useQuery({
+      queryKey: ['getMessageLabels'],
+      queryFn: queryFunction,
+      onSuccess(data) {
+         console.log(data.data)
+         dispatch(setMessageLabels(data.data.foundChat))
+      },
+   })
+   return null
+}
+
+export default useFillMessageLabels
