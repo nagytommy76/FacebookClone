f44eeb55bd56a9e59860231fa2cb593bebb69d6f
nagytommy76 <nagytommy76@gmail.com>
2024-04-20 14:29:04 +0200

    Added the getChatMessageLabels to the API route

--- backend/api/chat/chat.ts
+++ backend/api/chat/chat.ts
@@ -5,10 +5,12 @@ import {
    getUsersChatMessagesController,
    createNewChatController,
 } from '../../controllers/chat/chat'
+import { getChatMessageLabels } from '../../controllers/chat/getChats'
 
 const router = Router()
 
 router.get('/get-user-chat', authenticateAccessTokenForApi, getUsersChatMessagesController)
+router.get('/get-all-chats', authenticateAccessTokenForApi, getChatMessageLabels)
 
 router.post('/add-chat-msg', authenticateAccessTokenForApi, saveChatController)
 router.post('/create-chat', authenticateAccessTokenForApi, createNewChatController)
