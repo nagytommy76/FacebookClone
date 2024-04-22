90005f0fe9f174bfee374a56f21914fabec23e82
nagytommy76 <nagytommy76@gmail.com>
2024-04-22 19:24:50 +0200

    Check the status code

--- frontend/src/components/MainPage/AcceptedFriends/Hooks/useChatModal.tsx
+++ frontend/src/components/MainPage/AcceptedFriends/Hooks/useChatModal.tsx
@@ -1,50 +1,30 @@
 import { useAppDispatch } from '@/reduxStore/store'
-import {
-   setChatModalOpen,
-   setChatWithUserId,
-   setMessageLabels,
-   setChatId,
-} from '@/reduxStore/slices/ChatSlice'
+import { setChatModalOpen, setSingleMessageLabel, setChatId } from '@/reduxStore/slices/ChatSlice'
 
 import { useMutation } from '@tanstack/react-query'
 import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
+import type { IChat } from '@/src/components/Navbar/Chat/Types'
 
 const useChatModal = () => {
    const dispatch = useAppDispatch()
 
-   const mutationFunction = async ({
-      userId,
-      fullName,
-      selectedProfilePicturePath,
-   }: {
-      userId: string
-      fullName: string
-      selectedProfilePicturePath: string
-   }) => {
-      return await axios.post('/chat/create-chat', { chatUserId: userId })
-   }
-
-   const hadleOpenChatModal = (userId: string, fullName: string, selectedProfilePicturePath: string) => {
-      dispatch(
-         setMessageLabels({
-            _id: userId,
-            captionText: '',
-            fullName: fullName,
-            selectedProfilePicturePath: selectedProfilePicturePath,
-         })
-      )
-      dispatch(setChatWithUserId(userId))
-      dispatch(setChatModalOpen(true))
+   const mutationFunction = async ({ userId }: { userId: string }) => {
+      return (await axios.post('/chat/create-chat', { chatUserId: userId })) as AxiosResponse<{
+         createdChatModel: IChat
+      }>
    }
 
    const { mutate } = useMutation({
       mutationKey: ['createNewChat'],
       mutationFn: mutationFunction,
       onSuccess(data, variables, context) {
-         console.log(data.data)
-         const { fullName, selectedProfilePicturePath, userId } = variables
-         hadleOpenChatModal(userId, fullName, selectedProfilePicturePath)
-         dispatch(setChatId(data.data.createdChatModel._id))
+         if (data.status === 200) {
+            dispatch(setChatModalOpen(true))
+            dispatch(setChatId(data.data.createdChatModel._id))
+         } else if (data.status === 201) {
+            // Itt hozzá kell adom egy setSingleMessageLabel-t
+            dispatch(setSingleMessageLabel(data.data.createdChatModel))
+         }
       },
    })
 
