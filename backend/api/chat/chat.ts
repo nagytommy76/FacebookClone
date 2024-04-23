88f23351d2606279b335bce3107b55f6990de5a6
nagytommy76 <nagytommy76@gmail.com>
2024-04-23 14:15:25 +0200

    Finished the getChatMessageLabels controller with aggregate query

--- backend/api/chat/chat.ts
+++ backend/api/chat/chat.ts
@@ -1,18 +1,14 @@
 import { Router } from 'express'
 import { authenticateAccessTokenForApi } from '../../middlewares/accessTokenRefresh'
-import {
-   saveChatController,
-   getUsersChatMessagesController,
-   createNewChatController,
-} from '../../controllers/chat/chat'
+import { createNewChatController } from '../../controllers/chat/chat'
+import { saveChatMessageController } from '../../controllers/chat/chatMessages'
 import { getChatMessageLabels } from '../../controllers/chat/getChats'
 
 const router = Router()
 
-router.get('/get-user-chat', authenticateAccessTokenForApi, getUsersChatMessagesController)
 router.get('/get-all-chats', authenticateAccessTokenForApi, getChatMessageLabels)
 
-router.post('/add-chat-msg', authenticateAccessTokenForApi, saveChatController)
+router.post('/add-chat-msg', authenticateAccessTokenForApi, saveChatMessageController)
 router.post('/create-chat', authenticateAccessTokenForApi, createNewChatController)
 
 module.exports = router
--- backend/controllers/chat/getChats.ts
+++ backend/controllers/chat/getChats.ts
@@ -1,44 +1,52 @@
+import { Response } from 'express'
+import { Types } from 'mongoose'
+
 import { ChatModel } from '../../models/chat/chatModel'
-import { Response, Request } from 'express'
 import type { IJWTUserType } from '../../middlewares/accessTokenRefresh'
 
 export const getChatMessageLabels = async (request: IJWTUserType, response: Response) => {
-   const loggedInUserId = request.user?.userId
+   const loggedInUserId = new Types.ObjectId(request.user?.userId)
    try {
-      const foundChat = await ChatModel.find({
-         $in: [{ 'participants.participant': loggedInUserId }],
-      }).populate({
-         path: 'participants.participant',
-         select: ['_id', 'sureName', 'firstName', 'userDetails.profilePicturePath.$'],
-         match: {
-            'userDetails.profilePicturePath': { $elemMatch: { isSelected: { $eq: true } } },
+      const foundChat = await ChatModel.aggregate([
+         {
+            $match: {
+               'participants.participant': loggedInUserId,
+            },
          },
-      })
-
-      // const foundChat1 = await ChatModel.aggregate([
-      //    {
-      //       // $match: { 'participants.participant': { loggedInUserId } },
-      //       $match: {
-      //          participants: {
-      //             $all: [
-      //                {
-      //                   $elemMatch: {
-      //                      participant: loggedInUserId,
-      //                   },
-      //                },
-      //             ],
-      //          },
-      //       },
-      //    },
-      //    // {
-      //    //    $project: {
-      //    //       'participants.participant': '$participants.participant',
-      //    //       testing: {
-      //    //          $in: [loggedInUserId, '$participants.participant'],
-      //    //       },
-      //    //    },
-      //    // },
-      // ])
+         {
+            $lookup: {
+               from: 'users',
+               localField: 'participants.participant',
+               foreignField: '_id',
+               as: 'chatWithParticipant',
+               pipeline: [
+                  {
+                     $match: {
+                        _id: { $ne: loggedInUserId },
+                     },
+                  },
+                  {
+                     $project: {
+                        firstName: 1,
+                        sureName: 1,
+                        selectedProfilePicture: {
+                           $filter: {
+                              input: '$userDetails.profilePicturePath',
+                              as: 'profilePic',
+                              cond: { $eq: ['$$profilePic.isSelected', true] },
+                           },
+                        },
+                     },
+                  },
+               ],
+            },
+         },
+         {
+            $unwind: {
+               path: '$chatWithParticipant',
+            },
+         },
+      ])
 
       response.status(200).json({ foundChat })
    } catch (error) {
