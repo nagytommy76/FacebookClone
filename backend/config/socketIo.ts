0fbf1d60155733bfa315b08b79d4b3b221bacc4c
nagytommy76 <nagytommy76@gmail.com>
2024-04-28 14:02:26 +0200

    Check the user logged in/out

--- backend/config/socketIo.ts
+++ backend/config/socketIo.ts
@@ -1,7 +1,10 @@
-import type { Application, NextFunction, Response } from 'express'
+import type { Application } from 'express'
 import { createServer } from 'https'
-import { Server } from 'socket.io'
-import { ISocketRequest } from 'types'
+import { Server, Socket } from 'socket.io'
+
+import { getAcceptedFriendsModel } from '@/controllers/friends/getFriends'
+import { Types } from 'mongoose'
+import { DefaultEventsMap } from 'socket.io/dist/typed-events'
 
 export interface IOnlineFriends {
    userId: string
@@ -23,20 +26,33 @@ export const initSocketIO = (app: Application) => {
    let onlineFriends: IOnlineFriends[] = []
 
    const addNewUser = (userId: string, socketId: string) => {
-      if (!onlineFriends.some((user) => user.userId === userId)) {
+      if (userId !== '' && !onlineFriends.some((user) => user.userId === userId)) {
          onlineFriends.push({ userId, socketId })
       }
    }
-   const removeUser = (socketId: string) => {
-      onlineFriends = onlineFriends.filter((user) => user.socketId !== socketId)
+   const removeUser = (
+      socketId: string,
+      socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
+   ) => {
+      let loggedInUserId: string | null = null
+      onlineFriends = onlineFriends.filter((user) => {
+         if (user.socketId !== socketId) {
+            return true
+         } else {
+            loggedInUserId = user.userId
+            return false
+         }
+      })
+      socket.broadcast.emit('offline:friends', { userId: loggedInUserId })
    }
    const getUser = (userId: string) => {
       return onlineFriends.find((user) => user.userId === userId)
    }
 
    socketIo.on('connection', (socket) => {
-      socket.on('newUser', (userId) => {
+      socket.on('newUser', async (userId) => {
          addNewUser(userId, socket.id)
+         socket.broadcast.emit('online:friends', { userId, socketId: socket.id })
       })
 
       socket.on('join_room', (args: { chatRoomId: string[] }) => {
@@ -44,7 +60,7 @@ export const initSocketIO = (app: Application) => {
       })
 
       socket.on('disconnect', () => {
-         removeUser(socket.id)
+         removeUser(socket.id, socket)
       })
    })
    return { io: socketIo, onlineFriends, getUser }
