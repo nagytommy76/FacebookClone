2616bf8120977645fe83c563a35005133d782713
nagytommy76 <nagytommy76@gmail.com>
2024-05-05 14:08:22 +0200

    Displayed the message image

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/MessageItem.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/MessageItem.tsx
@@ -1,43 +1,43 @@
-import React from 'react'
+import Image from 'next/image'
 import dynamic from 'next/dynamic'
-import moment from 'moment'
-import { useAppSelector } from '@/reduxStore/store'
 import type { IMessages } from '@/Chat/Types'
 
-import { StyledTextContainer, StyledTextBox, StyledTextBoxHead } from './Styles'
+import { StyledTextContainer, StyledTextBox } from './Styles'
 import Typography from '@mui/material/Typography'
 
-const ChatAvatar = dynamic(() => import('@/Base/ChatAvatar/ChatAvatar'))
+const MessageItemHead = dynamic(() => import('./Includes/MessageItemHead'))
 
 const MessageItem: React.FC<{
    message: IMessages
    isRightContent?: boolean
 }> = ({ message, isRightContent = false }) => {
-   const loggedInUserImg = useAppSelector((state) => state.auth.currentImage)
-   const chatId = useAppSelector((state) => state.chat.chatId)
-   const messageLabels = useAppSelector((state) => state.chat.messageLabels)
-   const chatWithParticipantImage =
-      messageLabels && chatId && messageLabels[chatId].chatWithParticipant.selectedProfilePicture[0].path
-
    return (
       <StyledTextContainer isRightContent={isRightContent}>
-         <StyledTextBoxHead isRightContent={isRightContent}>
-            <Typography variant='caption'>{moment(message.createdAt).format('MMM D ddd H:mm')}</Typography>
-            <ChatAvatar
-               isRead={true}
-               fullName='Saját Neve'
-               selectedProfilePicturePath={
-                  isRightContent ? loggedInUserImg.path : chatWithParticipantImage || ''
-               }
-               width={30}
-               height={30}
-            />
-         </StyledTextBoxHead>
-         <StyledTextBox isRightContent={isRightContent}>
-            <Typography fontWeight={350} variant='caption'>
-               {message.message}
-            </Typography>
-         </StyledTextBox>
+         <MessageItemHead isRightContent={isRightContent} msgCreatedAt={message.createdAt} />
+         {message.image.length > 1 && (
+            <div
+               style={{
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: isRightContent ? 'end' : 'start',
+               }}
+            >
+               <Image
+                  style={{ objectFit: 'cover' }}
+                  src={message.image}
+                  alt='Message image'
+                  width={300}
+                  height={250}
+               />
+            </div>
+         )}
+         {message.message.length !== 0 && (
+            <StyledTextBox isRightContent={isRightContent}>
+               <Typography fontWeight={350} fontSize={13} variant='caption'>
+                  {message.message}
+               </Typography>
+            </StyledTextBox>
+         )}
       </StyledTextContainer>
    )
 }
