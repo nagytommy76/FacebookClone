913e19ea0534d3be5be2c6a6ed882e281d713fe2
nagytommy76 <nagytommy76@gmail.com>
2024-05-06 15:13:48 +0200

    set-read-messages API route was added

--- backend/api/chat/chat.ts
+++ backend/api/chat/chat.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { authenticateAccessTokenForApi } from '../../middlewares/accessTokenRefresh'
 import { createNewChatController } from '../../controllers/chat/chat'
-import { saveChatMessageController } from '../../controllers/chat/chatMessages'
+import { saveChatMessageController, setMessagesRead } from '../../controllers/chat/chatMessages'
 import { getChatMessageLabels } from '../../controllers/chat/getChats'
 
 const router = Router()
@@ -10,5 +10,7 @@ router.get('/get-all-chats', authenticateAccessTokenForApi, getChatMessageLabels
 
 router.post('/add-chat-msg', authenticateAccessTokenForApi, saveChatMessageController)
 router.post('/create-chat', authenticateAccessTokenForApi, createNewChatController)
+
+router.put('/set-read-messages', authenticateAccessTokenForApi, setMessagesRead)
 
 module.exports = router
