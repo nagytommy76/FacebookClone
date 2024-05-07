03f03948735cf1c01a653c9734af319784b91dbe
nagytommy76 <nagytommy76@gmail.com>
2024-05-07 12:13:44 +0200

    Set the messages.isRead to true

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatAside/Hooks/useSetReadMsg.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatAside/Hooks/useSetReadMsg.tsx
@@ -1,22 +1,27 @@
 import { useMutation } from '@tanstack/react-query'
-import { axiosInstance as axios } from '@/axios/AxiosInstance'
+import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
 
-import { useAppSelector } from '@/reduxStore/store'
+import { useAppSelector, useAppDispatch } from '@/reduxStore/store'
+import { setTotalUnreadMsgCount } from '@/reduxStore/slices/ChatSlice'
 
 const useSetReadMsg = () => {
+   const dispatch = useAppDispatch()
    const messageLabels = useAppSelector((state) => state.chat.messageLabels)
    const currentChatId = useAppSelector((state) => state.chat.chatId)
-   const loggedInUserId = useAppSelector((state) => state.auth.userId)
 
    const setMessageIsRead = async () => {
-      return await axios.put(`/chat/set-read-messages`, { currentChatId })
+      return (await axios.put(`/chat/set-read-messages`, { currentChatId })) as AxiosResponse<{
+         totalUnreadMsgCount: number
+      }>
    }
 
    const { mutate } = useMutation({
       mutationKey: ['setUnreadMessages', messageLabels],
       mutationFn: setMessageIsRead,
       onSuccess(data, variables, context) {
-         console.log(data.data)
+         if (currentChatId) {
+            dispatch(setTotalUnreadMsgCount({ currentChatId, count: data.data.totalUnreadMsgCount }))
+         }
       },
    })
 
