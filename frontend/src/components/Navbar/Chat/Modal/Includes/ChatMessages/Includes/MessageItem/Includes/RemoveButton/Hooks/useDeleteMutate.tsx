ea5f9a79f626cc64988c83144f81b00e8611c9f6
nagytommy76 <nagytommy76@gmail.com>
2024-05-10 14:49:09 +0200

    Message delete mutation was created

---
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/Includes/RemoveButton/Hooks/useDeleteMutate.tsx
@@ -0,0 +1,30 @@
+import { useAppSelector, useAppDispatch } from '@/reduxStore/store'
+import { setNewMessages } from '@/reduxStore/slices/ChatSlice'
+
+import { useMutation } from '@tanstack/react-query'
+import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
+import type { IMessages } from '@/Chat/Types'
+
+const useDeleteMutate = (messageId: string) => {
+   const chatId = useAppSelector((state) => state.chat.chatId)
+   const dispatch = useAppDispatch()
+
+   const deleteMutationFn = async () => {
+      return (await axios.patch('/chat/delete-message', { messageId, chatId })) as AxiosResponse<{
+         updatedMessages: IMessages[]
+      }>
+   }
+
+   const { mutate } = useMutation({
+      mutationKey: ['DeleteSingleMessage'],
+      mutationFn: deleteMutationFn,
+      onSuccess(data, variables, context) {
+         console.log(data.data)
+         if (chatId) dispatch(setNewMessages({ updatedMessages: data.data.updatedMessages, chatId }))
+      },
+   })
+
+   return mutate
+}
+
+export default useDeleteMutate
