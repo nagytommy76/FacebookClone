425ecf4f3505316781cfb9d9302504a4eb0a2c09
nagytommy76 <nagytommy76@gmail.com>
2024-05-11 15:00:44 +0200

    Listening to chat:sendMsgResponse on the receiver client's side

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Hooks/Sockets/useSendMsgSocket.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Hooks/Sockets/useSendMsgSocket.tsx
@@ -1,36 +1,31 @@
 import { useEffect, Dispatch, SetStateAction } from 'react'
 import { socket } from '@/src/utils/socketIo'
 
-import { useAppDispatch, useAppSelector } from '@/reduxStore/store'
+import { useAppDispatch } from '@/reduxStore/store'
 import { setChatMessage, incrementTotalUnreadMsgCount } from '@/reduxStore/slices/ChatSlice'
 import type { IMessages } from '@/Chat/Types'
 
 interface IChatArgs {
-   addedMessages: IMessages
+   addedMessage: IMessages
    foundChatId: string
    socketId?: string
 }
 
 const useSendMsgSocket = (setTypingStatus: Dispatch<SetStateAction<boolean>>) => {
    const dispatch = useAppDispatch()
-   const loggedInUserId = useAppSelector((state) => state.auth.userId)
    useEffect(() => {
       const sendChatMsg = (args: IChatArgs) => {
-         // loggedInUserId == args.addedMessages.receiverUserId -> azért kell, hogy ha egyezik csak akkor dispatch->setChatMessage
-         // Különben 2szer írja be a msg-t, broadcast-tel backenden megoldani!!!
-         // A fogadó félnek állítom be ->
-         if (loggedInUserId == args.addedMessages.receiverUserId) {
-            setTypingStatus(false)
-            dispatch(setChatMessage({ addedMessage: args.addedMessages, foundChatId: args.foundChatId }))
-            dispatch(incrementTotalUnreadMsgCount({ count: 1, currentChatId: args.foundChatId }))
-         }
+         setTypingStatus(false)
+         dispatch(setChatMessage({ addedMessage: args.addedMessage, foundChatId: args.foundChatId }))
+         dispatch(incrementTotalUnreadMsgCount({ count: 1, currentChatId: args.foundChatId }))
       }
 
-      socket.on('chat:sendMsg', sendChatMsg)
+      socket.on('chat:sendMsgResponse', sendChatMsg)
+
       return () => {
-         socket.off('chat:sendMsg', sendChatMsg)
+         socket.off('chat:sendMsgResponse', sendChatMsg)
       }
-   }, [dispatch, loggedInUserId, setTypingStatus])
+   }, [dispatch, setTypingStatus])
 
    return null
 }
