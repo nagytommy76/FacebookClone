bc1d93f2a8895841c507a3bc296fb945dfe7e8a3
nagytommy76 <nagytommy76@gmail.com>
2024-05-11 14:57:48 +0200

    Refactored the IMessage interface

--- backend/models/chat/Types.ts
+++ backend/models/chat/Types.ts
@@ -1,16 +1,17 @@
 import { Model, ObjectId } from 'mongoose'
 
+export interface IMessage {
+   _id?: ObjectId | string
+   createdAt?: Date | string
+   updatedAt?: Date | string
+   receiverUserId: string // ez is legyen ref
+   isRead?: boolean
+   message: string
+   image?: string
+}
 export interface IChatSchema {
    participants: { participant: string | ObjectId; _id: string | ObjectId }[] // ref to users
-   messages: {
-      _id?: ObjectId | string
-      createdAt?: Date | string
-      updatedAt?: Date | string
-      receiverUserId: string // ez is legyen ref
-      isRead?: boolean
-      message: string
-      image?: string
-   }[]
+   messages: IMessage[]
 }
 
 export interface IChatModel extends Model<IChatSchema> {}
