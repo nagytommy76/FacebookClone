76ddff7af66977550f6c9a2942c54196881054c6
nagytommy76 <nagytommy76@gmail.com>
2024-05-18 14:06:31 +0200

    likeMessageController added to API route

--- backend/api/chat/chat.ts
+++ backend/api/chat/chat.ts
@@ -4,6 +4,7 @@ import { createNewChatController } from '../../controllers/chat/chat'
 import { saveChatMessageController, setMessagesRead } from '../../controllers/chat/chatMessages'
 import { getChatMessageLabels } from '../../controllers/chat/getChats'
 import { deleteMessageController } from '../../controllers/chat/removeMessage'
+import { likeMessageController } from '../../controllers/chat/likeMessage'
 
 const router = Router()
 
@@ -11,6 +12,8 @@ router.get('/get-all-chats', authenticateAccessTokenForApi, getChatMessageLabels
 
 router.post('/add-chat-msg', authenticateAccessTokenForApi, saveChatMessageController)
 router.post('/create-chat', authenticateAccessTokenForApi, createNewChatController)
+// Like
+router.post('/like-message', authenticateAccessTokenForApi, likeMessageController)
 
 router.put('/set-read-messages', authenticateAccessTokenForApi, setMessagesRead)
 
