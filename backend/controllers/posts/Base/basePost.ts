1c1ee5970b50d0d358345ea21cbaef57f4c28ced
nagytommy76 <nagytommy76@gmail.com>
2024-05-20 14:16:07 +0200

    Added reaction type to IMessage interface

--- backend/controllers/posts/Base/basePost.ts
+++ backend/controllers/posts/Base/basePost.ts
@@ -1,41 +1,10 @@
 import { Posts as PostModel } from '../../../models/posts/posts'
-import type { IReactionTypes, LikeTypes, IPostLike } from '../types/PostTypes'
+import BaseLikeController from '../../Base/BaseLikeController'
 
-export default abstract class BasePostController {
+export default abstract class BasePostController extends BaseLikeController {
    public async findPostModelByPostId(postId: string) {
       return await PostModel.findById(postId)
    }
-
-   private findPreviousReactionType(reactionType: IReactionTypes) {
-      return Object.keys(reactionType).filter((key) => reactionType[key])[0] as LikeTypes
-   }
-
-   findUsersLikeByUserID(likes: IPostLike[], userId: string) {
-      return likes.find((like) => like.userId.toString() === userId.toString())
-   }
-
-   checkUserLike(userLike: IPostLike | undefined, reactionType: LikeTypes, likes: IPostLike[], userId: any) {
-      if (userLike) {
-         let previousReaction = this.findPreviousReactionType(userLike.reactionType)
-         userLike.reactionType[previousReaction] = false
-         userLike.reactionType[reactionType] = true
-      } else {
-         likes.push({
-            userId,
-            reactionType: {
-               isLike: false,
-               isAngry: false,
-               isCare: false,
-               isHaha: false,
-               isLove: false,
-               isSad: false,
-               isWow: false,
-               [reactionType]: true,
-            },
-         })
-      }
-   }
-
    async returnPostModelWithPopulated(userId: string | null = null) {
       // const allPosts = await PostModel.aggregate([
       //    { $match: userId ? { userId } : {} },
--- backend/models/chat/Types.ts
+++ backend/models/chat/Types.ts
@@ -1,4 +1,5 @@
 import { Model, ObjectId } from 'mongoose'
+import type { ILike } from '@/controllers/Base/Types'
 
 export interface IMessage {
    _id?: ObjectId | string
@@ -8,6 +9,7 @@ export interface IMessage {
    isRead?: boolean
    message: string
    image?: string
+   reaction: ILike[]
 }
 export interface IChatSchema {
    participants: { participant: string | ObjectId; _id: string | ObjectId }[] // ref to users
