9d5beb57c51795ab709d4b9df80ceaffc942cea1
nagytommy76 <nagytommy76@gmail.com>
2024-05-23 19:31:41 +0200

    Created a separated component for Reaction modal

---
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/Includes/MsgReactionModal/MsgReactionModal.tsx
@@ -0,0 +1,43 @@
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+import useGetReactionCount from './Hook/useGetReactionCount'
+import type { ILike } from '@/src/types/LikeTypes'
+
+const LikeModal = dynamic(() => import('@/Base/LikeReactions/LikeModal/LikeModal'))
+const Reactions = dynamic(() => import('@/Base/LikeReactions/Reactions'))
+
+const MsgReactionModal: React.FC<{ reactions: ILike[]; messageId: string }> = ({ reactions, messageId }) => {
+   const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+   useGetReactionCount(messageId, isModalOpen)
+   return (
+      <>
+         {reactions.length !== 0 && (
+            <Reactions setIsModalOpen={() => setIsModalOpen(true)} likes={reactions}>
+               {/* <LikeModal
+                  isModalOpen={isModalOpen}
+                  likeCount={{
+                     totalReactionCount: 1,
+                     reactionTypes: {
+                        isLike: {
+                           count: 1,
+                           reactors: [
+                              {
+                                 firstName: 'dssda',
+                                 sureName: 'sdafdfsdf',
+                                 userDetails: {
+                                    profilePicturePath: [{ id: '12121', isSelected: true, path: '' }],
+                                 },
+                              },
+                           ],
+                        },
+                     },
+                  }}
+                  setIsModalOpen={setIsModalOpen}
+               /> */}
+            </Reactions>
+         )}
+      </>
+   )
+}
+
+export default MsgReactionModal
