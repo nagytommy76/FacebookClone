39ecf3af9131dbf67de3b09eb72528e092d27f57
nagytommy76 <nagytommy76@gmail.com>
2024-05-29 19:17:45 +0200

    Using StyledBadge

--- frontend/src/components/Base/ChatAvatar/ChatAvatar.tsx
+++ frontend/src/components/Base/ChatAvatar/ChatAvatar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import { StyledIsActive } from '@/styles/BaseStyles'
+import { StyledBadge } from './Styles'
 import Avatar from '@mui/material/Avatar'
 
 const ChatAvatar: React.FC<{
@@ -11,12 +11,16 @@ const ChatAvatar: React.FC<{
    isRead?: boolean
 }> = ({ fullName, selectedProfilePicturePath, width = 56, height = 56, isRead = false }) => {
    return (
-      <>
-         <Avatar sx={{ width, height, mr: 2, position: 'relative' }}>
+      <StyledBadge
+         overlap='circular'
+         anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+         variant='dot'
+         invisible={isRead}
+      >
+         <Avatar sx={{ width, height }}>
             <Image width={width} height={height} alt={fullName} src={selectedProfilePicturePath} />
-            <StyledIsActive isRead={isRead} rightPosition='1px' bottomPosition='10px' topPosition='unset' />
          </Avatar>
-      </>
+      </StyledBadge>
    )
 }
 
