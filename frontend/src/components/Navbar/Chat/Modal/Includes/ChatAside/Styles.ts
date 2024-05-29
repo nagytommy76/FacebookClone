94ac7710f2cb065e5f475ced79a379b89f111ad6
nagytommy76 <nagytommy76@gmail.com>
2024-05-29 19:18:28 +0200

    Set mindWidth to 60px

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatAside/Styles.ts
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatAside/Styles.ts
@@ -2,6 +2,14 @@ import { styled } from '@mui/material/styles'
 import Tab from '@mui/material/Tab'
 import TabList from '@mui/lab/TabList'
 
+export const StyledChatAside = styled('aside')(({ theme }) => ({
+   flex: 1,
+
+   [theme.breakpoints.down('md')]: {
+      width: '70px',
+   },
+}))
+
 export const StyledTabList = styled(TabList)(({ theme }) => ({
    borderRight: 1,
    borderColor: 'divider',
@@ -9,6 +17,10 @@ export const StyledTabList = styled(TabList)(({ theme }) => ({
    '& .MuiTabs-indicator': {
       backgroundColor: '#f5ab0b',
       width: '5px',
+   },
+
+   [theme.breakpoints.down('md')]: {
+      width: '70px',
    },
 }))
 
@@ -24,15 +36,7 @@ export const StyledTab = styled(Tab)(({ theme }) => ({
    [theme.breakpoints.down('md')]: {
       justifyContent: 'center',
       height: '60px',
-      width: '60px',
-      padding: '.5rem',
-   },
-}))
-
-export const StyledChatAside = styled('aside')(({ theme }) => ({
-   flex: 1,
-
-   [theme.breakpoints.down('md')]: {
-      width: '70px',
+      minWidth: '60px',
+      padding: '0',
    },
 }))
