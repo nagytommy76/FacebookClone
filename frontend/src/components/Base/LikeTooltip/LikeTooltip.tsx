e12163686d1677fa073d059861721f256d901ed2
nagytommy76 <nagytommy76@gmail.com>
2024-06-01 14:52:04 +0200

    Using isMobile state to show the clickable like tooltip

--- frontend/src/components/Base/LikeTooltip/LikeTooltip.tsx
+++ frontend/src/components/Base/LikeTooltip/LikeTooltip.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
-
 import CustomTooltipTitle from './CustomTooltipTitle'
 import Reactions from './Reactions'
 import { LikeTypes } from '@/types/LikeTypes'
+import { useAppSelector } from '@/reduxStore/store'
+
+import ClickAwayListener from '@mui/material/ClickAwayListener'
 
 const LikeTooltip: React.FC<{
    setLikeFunction: (likeType: LikeTypes) => void
    LikeCommentButtonComponent: React.ReactElement
-}> = ({ setLikeFunction, LikeCommentButtonComponent }) => {
-   return (
+   handleClose?: () => void
+   open?: boolean
+}> = ({ setLikeFunction, LikeCommentButtonComponent, open = false, handleClose = () => {} }) => {
+   const isMobile = useAppSelector((state) => state.theme.isMobileView)
+
+   return isMobile ? (
+      <ClickAwayListener onClickAway={handleClose}>
+         <div>
+            <CustomTooltipTitle
+               open={open}
+               onClose={handleClose}
+               disableFocusListener
+               disableHoverListener
+               disableTouchListener
+               placement='top'
+               title={<Reactions setLike={setLikeFunction} />}
+            >
+               {LikeCommentButtonComponent}
+            </CustomTooltipTitle>
+         </div>
+      </ClickAwayListener>
+   ) : (
       <CustomTooltipTitle placement='top' title={<Reactions setLike={setLikeFunction} />}>
          {LikeCommentButtonComponent}
       </CustomTooltipTitle>
