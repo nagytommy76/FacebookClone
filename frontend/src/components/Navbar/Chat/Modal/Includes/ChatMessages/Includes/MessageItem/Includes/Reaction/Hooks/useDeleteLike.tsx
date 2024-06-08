0239f39a6f8b2a5d2c55ada6a82e4957f8d9ebb0
nagytommy76 <nagytommy76@gmail.com>
2024-06-08 14:00:59 +0200

    Wrote the mutation function

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/Includes/Reaction/Hooks/useDeleteLike.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/Includes/Reaction/Hooks/useDeleteLike.tsx
@@ -1,13 +1,41 @@
 import { useAppSelector, useAppDispatch } from '@/reduxStore/store'
-import { addNewReactionsToMessage } from '@/reduxStore/slices/ChatSlice'
+import { removeReactionFromMessage } from '@/reduxStore/slices/ChatSlice'
 import { socket } from '@/src/utils/socketIo'
 
 import { useMutation } from '@tanstack/react-query'
-import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
-import type { ILike, LikeTypes } from '@/src/types/LikeTypes'
+import { axiosInstance as axios } from '@/axios/AxiosInstance'
 
-const useDeleteLike = () => {
-   return null
+// Route -> /delete-message-like
+const useDeleteLike = (messageId: string, likeIdToDelete: string) => {
+   const dispatch = useAppDispatch()
+   const chatId = useAppSelector((state) => state.chat.chatId)
+
+   const deleteReaction = async () => {
+      return await axios.delete('chat/delete-message-like', {
+         data: {
+            chatId,
+            messageId,
+            likeIdToDelete,
+         },
+      })
+   }
+
+   const { mutate } = useMutation({
+      mutationKey: ['deleteLike'],
+      mutationFn: deleteReaction,
+      onSuccess: (data) => {
+         if (data.status === 200) {
+            console.log(data.data)
+            dispatch(removeReactionFromMessage({ chatId, likeIdToDelete, messageId }))
+            // socket.emit('deleteLike', {
+            //    messageId: 'messageId',
+            //    likeId: 'likeId',
+            // })
+         }
+      },
+   })
+
+   return mutate
 }
 
 export default useDeleteLike
