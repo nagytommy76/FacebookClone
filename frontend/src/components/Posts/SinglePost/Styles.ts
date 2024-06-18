1373dc487a81c628c5b25069e449e0192d659761
nagytommy76 <nagytommy76@gmail.com>
2024-06-18 19:46:20 +0200

    Added theme breakpoint styles

--- frontend/src/components/Posts/SinglePost/Styles.ts
+++ frontend/src/components/Posts/SinglePost/Styles.ts
@@ -1,21 +1,40 @@
 import { styled } from '@mui/material'
 import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
 
 export const StyledPaper = styled(Paper)(({ theme }) => ({
    margin: '1rem 0',
-   pb: '.3rem',
+   paddingBottom: '.3rem',
    minHeight: '100px',
    position: 'relative',
 
    [theme.breakpoints.down('md')]: {
-      margin: '0',
+      margin: '1.5rem 0',
    },
 }))
 
 // Body section -----------------
-export const BodyDescriptionSection = styled('div')({ padding: '.6rem 1rem' })
+export const BodyDescriptionSection = styled('div')(({ theme }) => ({
+   padding: '.5rem 1rem 1.5rem 1rem',
+   textAlign: 'justify',
+   [theme.breakpoints.down('md')]: {
+      textTransform: 'capitalize',
+   },
+}))
+
+export const StyledTypography = styled(Typography)(({ theme }) => ({
+   [theme.breakpoints.down('md')]: {
+      fontSize: '.8rem',
+   },
+}))
 
 // "Footer" section
+
+export const LikeAndCommentContainer = styled('div')({
+   width: '100%',
+   display: 'flex',
+   justifyContent: 'space-between',
+})
 
 export const ButtonGroupStyle = styled('div')({
    display: 'flex',
@@ -23,16 +42,14 @@ export const ButtonGroupStyle = styled('div')({
    gap: '2px',
 })
 
-export const FooterSectionStyle = styled('footer')({
+export const FooterSectionStyle = styled('footer')(({ theme }) => ({
    width: '100%',
    padding: '1rem 1rem .5rem 1rem',
-})
-
-export const LikeAndCommentContainer = styled('div')({
-   width: '100%',
-   display: 'flex',
-   justifyContent: 'space-between',
-})
+   [theme.breakpoints.down('md')]: {
+      paddingTop: '1rem',
+      paddingLeft: '.2rem',
+   },
+}))
 
 export const CommentsParagraph = styled('p')({
    transition: 'all .3s ease',
