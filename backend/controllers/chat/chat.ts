7021d3b96ffb7cba6d17f25b5a09fb2cf0083d5a
nagytommy76 <nagytommy76@gmail.com>
2024-06-30 13:46:21 +0200

    Using the aggregateMessageLabels function

--- backend/controllers/chat/chat.ts
+++ backend/controllers/chat/chat.ts
@@ -1,6 +1,8 @@
 import { ChatModel } from '../../models/chat/chatModel'
-import { Response, Request } from 'express'
+import { Types } from 'mongoose'
+import { Response } from 'express'
 import type { IJWTUserType } from '../../middlewares/accessTokenRefresh'
+import { aggregateMessageLabels } from './getChats'
 
 interface ICreateChatRequest extends IJWTUserType {
    body: {
@@ -14,27 +16,19 @@ export const createNewChatController = async (request: ICreateChatRequest, respo
    try {
       const foundChat = await ChatModel.findOne({
          $and: [{ 'participants.participant': chatUserId }, { 'participants.participant': loggedInUserId }],
-      }).populate({
-         path: 'participants.participant',
-         select: ['_id', 'sureName', 'firstName', 'userDetails.profilePicturePath.$'],
-         match: {
-            'userDetails.profilePicturePath': { $elemMatch: { isSelected: { $eq: true } } },
-         },
-      })
+      }).select('_id')
 
       if (foundChat === null) {
          const createdChatModel = await ChatModel.create({
             participants: [{ participant: chatUserId }, { participant: loggedInUserId }],
             messages: [],
          })
-         createdChatModel.populate({
-            path: 'participants.participant',
-            select: ['_id', 'sureName', 'firstName', 'userDetails.profilePicturePath.$'],
-            match: {
-               'userDetails.profilePicturePath': { $elemMatch: { isSelected: { $eq: true } } },
-            },
-         })
-         return response.status(201).json({ createdChatModel })
+         const aggregatedChatLabels = await aggregateMessageLabels(
+            new Types.ObjectId(loggedInUserId),
+            new Types.ObjectId(createdChatModel._id)
+         )
+
+         return response.status(201).json({ createdChatModel: aggregatedChatLabels[0] })
       } else {
          return response.status(200).json({ createdChatModel: foundChat, msg: 'Chat already exist' })
       }
