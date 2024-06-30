6738b94260c70d850d79d6214220e206c3cbd1b0
nagytommy76 <nagytommy76@gmail.com>
2024-06-30 13:45:41 +0200

    Separated the aggregation to aggregateMessageLabels function

--- backend/controllers/chat/getChats.ts
+++ backend/controllers/chat/getChats.ts
@@ -4,70 +4,74 @@ import { Types } from 'mongoose'
 import { ChatModel } from '../../models/chat/chatModel'
 import type { IJWTUserType } from '../../middlewares/accessTokenRefresh'
 
-export const getChatMessageLabels = async (request: IJWTUserType, response: Response) => {
-   const loggedInUserId = new Types.ObjectId(request.user?.userId)
-   try {
-      const foundChat = await ChatModel.aggregate([
-         {
-            $match: {
-               'participants.participant': loggedInUserId,
-            },
+export const aggregateMessageLabels = async (loggedInUserId: Types.ObjectId, chatId?: Types.ObjectId) => {
+   const foundChat = await ChatModel.aggregate([
+      {
+         $match: {
+            'participants.participant': loggedInUserId,
          },
-         {
-            $lookup: {
-               from: 'users',
-               localField: 'participants.participant',
-               foreignField: '_id',
-               as: 'chatWithParticipant',
-               pipeline: [
-                  {
-                     $match: {
-                        _id: { $ne: loggedInUserId },
-                     },
+      },
+      {
+         $lookup: {
+            from: 'users',
+            localField: 'participants.participant',
+            foreignField: '_id',
+            as: 'chatWithParticipant',
+            pipeline: [
+               {
+                  $match: {
+                     _id: { $ne: loggedInUserId },
                   },
-                  {
-                     $project: {
-                        firstName: 1,
-                        sureName: 1,
-                        selectedProfilePicture: {
-                           $filter: {
-                              input: '$userDetails.profilePicturePath',
-                              as: 'profilePic',
-                              cond: { $eq: ['$$profilePic.isSelected', true] },
-                           },
+               },
+               {
+                  $project: {
+                     firstName: 1,
+                     sureName: 1,
+                     selectedProfilePicture: {
+                        $filter: {
+                           input: '$userDetails.profilePicturePath',
+                           as: 'profilePic',
+                           cond: { $eq: ['$$profilePic.isSelected', true] },
                         },
                      },
                   },
-               ],
-            },
+               },
+            ],
          },
-         {
-            $addFields: {
-               totalUnreadMsgCount: {
-                  $sum: {
-                     $size: {
-                        $filter: {
-                           input: '$messages',
-                           as: 'message',
-                           cond: {
-                              $and: [
-                                 { $eq: ['$$message.isRead', false] },
-                                 { $eq: ['$$message.receiverUserId', loggedInUserId] },
-                              ],
-                           },
+      },
+      {
+         $addFields: {
+            totalUnreadMsgCount: {
+               $sum: {
+                  $size: {
+                     $filter: {
+                        input: '$messages',
+                        as: 'message',
+                        cond: {
+                           $and: [
+                              { $eq: ['$$message.isRead', false] },
+                              { $eq: ['$$message.receiverUserId', loggedInUserId] },
+                           ],
                         },
                      },
                   },
                },
             },
          },
-         {
-            $unwind: {
-               path: '$chatWithParticipant',
-            },
+      },
+      {
+         $unwind: {
+            path: '$chatWithParticipant',
          },
-      ])
+      },
+   ])
+   return foundChat
+}
 
+export const getChatMessageLabels = async (request: IJWTUserType, response: Response) => {
+   const loggedInUserId = new Types.ObjectId(request.user?.userId)
+   try {
+      const foundChat = await aggregateMessageLabels(loggedInUserId)
       response.status(200).json({ foundChat })
    } catch (error) {
       console.log(error)
