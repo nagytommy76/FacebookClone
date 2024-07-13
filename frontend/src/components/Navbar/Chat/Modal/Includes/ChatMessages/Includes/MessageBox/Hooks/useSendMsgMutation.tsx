9b1ae143949c7e9a8dcb47131999c6223b8fc30e
nagytommy76 <nagytommy76@gmail.com>
2024-07-13 13:51:01 +0200

    Using the useMsgSocket

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Hooks/useSendMsgMutation.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Hooks/useSendMsgMutation.tsx
@@ -1,15 +1,16 @@
 import { useAppSelector, useAppDispatch } from '@/reduxStore/store'
 import { setChatMessage } from '@/reduxStore/slices/ChatSlice'
-
-import { socket } from '@/src/utils/socketIo'
-
 import { useMutation } from '@tanstack/react-query'
 import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
+
+import useMsgSocket from './Sockets/useMsgSocket'
+
 import type { IMessages } from '@/Chat/Types'
 
 const useSendMsgMutation = (chatMsg: string, restoreTextField: () => void) => {
    const dispatch = useAppDispatch()
    const { selectedChatWithUserId, chatId } = useAppSelector((state) => state.chat)
+   const msgSocket = useMsgSocket()
 
    const chatMutateFn = async (chatImagePath: string = '') => {
       return (await axios.post('/chat/add-chat-msg', {
@@ -27,10 +28,7 @@ const useSendMsgMutation = (chatMsg: string, restoreTextField: () => void) => {
          dispatch(
             setChatMessage({ addedMessage: data.data.addedMessages, foundChatId: data.data.foundChatId })
          )
-         socket.emit('chat:sendMsg', {
-            addedMessage: data.data.addedMessages,
-            foundChatId: data.data.foundChatId,
-         })
+         msgSocket(data.data.addedMessages, data.data.foundChatId)
          restoreTextField()
       },
    })
