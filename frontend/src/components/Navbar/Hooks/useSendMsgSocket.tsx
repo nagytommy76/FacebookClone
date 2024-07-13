e77f9ec9248c67e3376328646e2ce11ac584b1be
nagytommy76 <nagytommy76@gmail.com>
2024-07-13 13:52:57 +0200

    Using useNotification & profileImage/fullName added to IChatArgs

--- frontend/src/components/Navbar/Hooks/useSendMsgSocket.tsx
+++ frontend/src/components/Navbar/Hooks/useSendMsgSocket.tsx
@@ -3,20 +3,26 @@ import { socket } from '@/src/utils/socketIo'
 
 import { useAppDispatch } from '@/reduxStore/store'
 import { setChatMessage, incrementTotalUnreadMsgCount } from '@/reduxStore/slices/ChatSlice'
+
+import useNotification from './useNotification'
 import type { IMessages } from '@/Chat/Types'
 
 interface IChatArgs {
    addedMessage: IMessages
    foundChatId: string
+   profileImage: string
+   fullName: string
    socketId?: string
 }
 
 const useSendMsgSocket = (setTypingStatus?: Dispatch<SetStateAction<boolean>>) => {
    const [chatAudio] = useState(new Audio('/sounds/facebook_messenger.mp3'))
+   const notification = useNotification()
    const dispatch = useAppDispatch()
 
    useEffect(() => {
       const sendChatMsg = (args: IChatArgs) => {
+         notification(args.addedMessage.message, args.profileImage, args.fullName)
          if (setTypingStatus) setTypingStatus(false)
          dispatch(setChatMessage({ addedMessage: args.addedMessage, foundChatId: args.foundChatId }))
          dispatch(incrementTotalUnreadMsgCount({ count: 1, currentChatId: args.foundChatId }))
@@ -28,7 +34,7 @@ const useSendMsgSocket = (setTypingStatus?: Dispatch<SetStateAction<boolean>>) =
       return () => {
          socket.off('chat:sendMsgResponse', sendChatMsg)
       }
-   }, [dispatch, setTypingStatus, chatAudio])
+   }, [dispatch, setTypingStatus, chatAudio, notification])
 
    return null
 }
