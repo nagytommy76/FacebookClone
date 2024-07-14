dd3112dd003e16f9d56c2542a12be1c2a0e491db
nagytommy76 <nagytommy76@gmail.com>
2024-07-14 12:15:16 +0200

    Using deleteChatMutate

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Profile/DeleteMessage/DeleteMessage.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Profile/DeleteMessage/DeleteMessage.tsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import dynamic from 'next/dynamic'
+
 import IconButton from '@mui/material/IconButton'
 import DeleteIcon from '@mui/icons-material/Delete'
 import Tooltip from '@mui/material/Tooltip'
+
+import useDeleteChat from './Hooks/useDeleteChat'
 
 const ConfirmDelete = dynamic(() => import('@/Base/ConfirmDelete/ConfirmDelete'))
 
 const DeleteMessage: React.FC<{ fullName: string }> = ({ fullName }) => {
    const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false)
+   const deleteChatMutate = useDeleteChat(setIsDialogOpen)
+
    return (
       <>
          <Tooltip title='Beszélgetés törlése'>
@@ -18,7 +23,7 @@ const DeleteMessage: React.FC<{ fullName: string }> = ({ fullName }) => {
          <ConfirmDelete
             isOpen={isDialogOpen}
             otherTextToDisplay={`A beszélgetésed ${fullName} felhasználóval törölve lesz. Biztosan folytatod?`}
-            handleCloseAndDelete={() => {}}
+            handleCloseAndDelete={() => deleteChatMutate()}
             setIsDialogOpen={setIsDialogOpen}
          />
       </>
