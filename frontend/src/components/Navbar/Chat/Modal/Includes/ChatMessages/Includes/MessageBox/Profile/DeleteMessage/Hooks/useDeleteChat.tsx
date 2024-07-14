545924225315f342fb13fb70e8441d50b13791bc
nagytommy76 <nagytommy76@gmail.com>
2024-07-14 12:13:58 +0200

    Created a delete mutation hook

---
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Profile/DeleteMessage/Hooks/useDeleteChat.tsx
@@ -0,0 +1,30 @@
+import { useAppSelector, useAppDispatch } from '@/reduxStore/store'
+import { useMutation } from '@tanstack/react-query'
+import { axiosInstance as axios } from '@/axios/AxiosInstance'
+import { deleteChat, setChatId } from '@/reduxStore/slices/ChatSlice'
+import type { Dispatch, SetStateAction } from 'react'
+
+const useDeleteChat = (setIsDialogOpen: Dispatch<SetStateAction<boolean>>) => {
+   const dispatch = useAppDispatch()
+   const { chatId, messageLabels } = useAppSelector((state) => state.chat)
+   const deleteChatAxios = async () => {
+      const response = await axios.delete(`/chat/delete-chat`, { data: { chatId } })
+      return response
+   }
+
+   const { mutate } = useMutation({
+      mutationKey: ['deleteChat'],
+      mutationFn: deleteChatAxios,
+      onSuccess: async () => {
+         setIsDialogOpen(false)
+         if (messageLabels) {
+            dispatch(setChatId(Object.keys(messageLabels)[0]))
+         }
+         if (chatId) dispatch(deleteChat({ chatId }))
+      },
+   })
+
+   return mutate
+}
+
+export default useDeleteChat
