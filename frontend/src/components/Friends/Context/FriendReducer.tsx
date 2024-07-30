626efe6051a8d55547e080f4e8cd9eba532239c0
nagytommy76 <nagytommy76@gmail.com>
2024-07-30 19:39:17 +0200

    Added SET_SENDER_FRIENDS

--- frontend/src/components/Friends/Context/FriendReducer.tsx
+++ frontend/src/components/Friends/Context/FriendReducer.tsx
@@ -8,13 +8,17 @@ export default function FriendReducer(state: FriendStateType, { payload, type }:
             ...state,
             friendId: payload,
          }
-      case 'SET_SELECTED_CONNECTED_FRIEND':
-         return produce(state, (draft) => {
-            draft.selectedConnectedFriend = payload
-         })
       case 'SET_FRIENDS_ARRAY':
          return produce(state, (draft) => {
             draft.friend.friends = payload
+         })
+      case 'SET_SENDER_FRIENDS':
+         return produce(state, (draft) => {
+            const { receiverFriendId, receiverFriends } = payload
+            const foundModifyFriendIndex = draft.friend.friends.findIndex(
+               (friend) => friend.friend === receiverFriendId
+            )
+            draft.friend.friends[foundModifyFriendIndex] = receiverFriends
          })
       case 'SET_FRIEND':
          const friend = produce(state, (draft) => {
