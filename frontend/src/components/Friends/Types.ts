f726a4f2ea0ad61c929b03ea520ad98a5bbbe1de
nagytommy76 <nagytommy76@gmail.com>
2024-07-30 19:37:16 +0200

    Added IFriends interface

--- frontend/src/components/Friends/Types.ts
+++ frontend/src/components/Friends/Types.ts
@@ -5,17 +5,19 @@ import type { NotificationType } from '../Navbar/Includes/Notification/Types'
 export type FriendButtonType = 'isFriend' | 'withdrawRequest' | 'makeFriend' | 'confirmFriend'
 export type StatusTypes = 'pending' | 'friends' | 'rejected'
 
+export interface IFriends {
+   _id: string
+   friend: string
+   isSender: boolean
+   friendSince?: Date | string
+   status?: 'pending' | 'friends' | 'rejected'
+}
+
 export interface IFriendResponse {
    _id: string
    firstName: string
    sureName: string
-   friends: {
-      _id: string
-      friend: string
-      isSender: boolean
-      friendSince?: Date | string
-      status?: 'pending' | 'friends' | 'rejected'
-   }[]
+   friends: IFriends[]
    notification: NotificationType[]
 }
 
