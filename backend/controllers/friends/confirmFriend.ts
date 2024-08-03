ab20910b08faba0e9d65486f8f0fbfd1c3fb8cb9
nagytommy76 <nagytommy76@gmail.com>
2024-08-03 15:18:26 +0200

    Working on the makeFriendship controller

--- backend/controllers/friends/confirmFriend.ts
+++ backend/controllers/friends/confirmFriend.ts
@@ -1,52 +1,56 @@
 import { Response } from 'express'
 import { User as UserModel } from '../../models/user/user'
-import { FriendsModel } from '../../models/friends/friends'
 import { IMakeFriends } from './Types'
 
 export const confirmFriendshipController = async (request: IMakeFriends, response: Response) => {
    const loggedInUserId = request.user?.userId as string
    const friendId = request.body.friendId
-   const connectedFriendId = request.body.connectedFriendId
 
    try {
-      const foundFriendsModel = await FriendsModel.findOne({ _id: connectedFriendId })
-      const foundReceiver = await UserModel.findOne({ _id: friendId })
-      const foundSender = await UserModel.findOne({ _id: loggedInUserId })
+      const foundSender = await UserModel.findOne({
+         _id: loggedInUserId,
+         friends: { $elemMatch: { friend: { $eq: friendId } } },
+      }).select(['friends.$', 'firstName', 'sureName', 'notifications', 'userDetails.profilePicturePath'])
+
+      const foundReceiver = await UserModel.findOne({
+         _id: friendId,
+         friends: { $elemMatch: { friend: { $eq: loggedInUserId } } },
+      }).select(['friends.$', 'firstName', 'sureName', 'notifications'])
 
       if (!foundReceiver) return response.status(404)
       if (!foundSender) return response.status(404)
-      if (!foundFriendsModel) return response.status(404)
 
-      foundFriendsModel.status = 'friends'
+      foundReceiver.friends[0].status = 'friends'
+      foundSender.friends[0].status = 'friends'
 
       foundSender.notifications.push({
          createdAt: new Date(),
          isRead: false,
          notificationType: 'isFriendConfirm',
          userDetails: {
-            firstName: foundReceiver.firstName,
-            sureName: foundReceiver.sureName,
-            userId: foundReceiver._id,
-            profilePicture: foundReceiver.userDetails.profilePicturePath[0].path,
+            firstName: foundSender.firstName,
+            sureName: foundSender.sureName,
+            userId: foundSender._id,
+            profilePicture: foundSender.userDetails.profilePicturePath[0].path,
          },
       })
 
       await foundSender.save()
-      await foundFriendsModel.save()
+      await foundReceiver.save()
 
       if (request.getUser !== undefined) {
          const toSendUser = request.getUser(friendId) as any
          if (toSendUser !== undefined) {
             request.ioSocket?.to(toSendUser.socketId).emit('confirmFriendship', {
                notifications: foundSender.notifications,
-               userFriends: foundReceiver.friends,
-               foundFriendsModel,
+               userFriends: foundSender.friends[0],
             })
          }
       }
 
       response.status(200).json({
-         foundFriendsModel,
+         receiverFriendId: foundReceiver.friends[0].friend,
+         receiverFriends: foundReceiver.friends[0],
       })
    } catch (error) {
       console.log(error)
