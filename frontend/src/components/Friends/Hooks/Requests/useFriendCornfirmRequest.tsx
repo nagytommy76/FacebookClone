d4c89e100d0b51f7bedebf3b27ac0ef7aca048f7
nagytommy76 <nagytommy76@gmail.com>
2024-08-03 15:16:18 +0200

    Removed unnecessary lines

--- frontend/src/components/Friends/Hooks/Requests/useFriendCornfirmRequest.tsx
+++ frontend/src/components/Friends/Hooks/Requests/useFriendCornfirmRequest.tsx
@@ -1,12 +1,9 @@
 import { useContext } from 'react'
 import { FriendContext } from '../../Context/FriendContext'
-// import type { IConnectedFriends } from '../../Types'
 import type { IFriends } from '../../Types'
 
 import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
 import { useMutation } from '@tanstack/react-query'
-
-// import useFriendDelete from '../ButtonType/useFriendDelete'
 import useConfirmSocket from '../Sockets/useConfirmSocket'
 
 const useFriendCornfirmRequest = () => {
@@ -16,7 +13,6 @@ const useFriendCornfirmRequest = () => {
       setCardButtonType,
       setLoading,
    } = useContext(FriendContext)
-   // const { setBtnTypeToDeleteFriend } = useFriendDelete()
    const confirmMutate = async () => {
       return (await axios.post('/friends/confirm-friendship', {
          friendId,
@@ -31,11 +27,9 @@ const useFriendCornfirmRequest = () => {
          setLoading(true)
       },
       onSuccess(data) {
-         console.log(data.data.receiverFriendId)
          friendDispatch({ type: 'SET_SENDER_FRIENDS', payload: data.data })
          setCardButtonType('isFriend')
          setLoading(false)
-         // setBtnTypeToDeleteFriend()
       },
    })
 
