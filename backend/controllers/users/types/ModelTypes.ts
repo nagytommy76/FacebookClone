1d10578ed61c9d642c8dca24634a72dd3f752efd
nagytommy76 <nagytommy76@gmail.com>
2024-08-11 12:23:54 +0200

    Removed the UserModel interface

--- backend/controllers/users/types/ModelTypes.ts
+++ backend/controllers/users/types/ModelTypes.ts
@@ -1,6 +1,5 @@
-import { Model, ObjectId, Document } from 'mongoose'
-import type { INotifications, NotificationType } from '../../notifications/types/notificationTypes'
-import type { SelectUserByIdType } from './requestTypes'
+import { ObjectId } from 'mongoose'
+import type { INotifications } from '../../notifications/types/notificationTypes'
 
 export interface IProfilePicturePath {
    _id?: ObjectId | string
@@ -59,53 +58,4 @@ export interface IUserTypes {
    posts: ObjectId[]
    createdAt: number
    updatedAt: number
-}
-
-export interface UserModel extends Model<IUserTypes> {
-   getUserByUserIdAndSelect: (userId: string | ObjectId) => Promise<
-      Document<unknown, any, SelectUserByIdType[]> &
-         Omit<
-            SelectUserByIdType[] &
-               Required<{
-                  _id: ObjectId
-               }>,
-            never
-         >
-   >
-   getSaveNotification: (
-      foundPostUserId: string | ObjectId,
-      foundPostDescription: string,
-      firstName: string,
-      sureName: string,
-      likedUserId: string | ObjectId,
-      profilePicture: string,
-      notificationType: NotificationType
-   ) => Promise<
-      Document<unknown, any, IUserTypes> &
-         Omit<
-            IUserTypes &
-               Required<{
-                  _id: ObjectId
-               }>,
-            never
-         >
-   >
-   checkRegisterEmail(email: string): Promise<void>
-   encryptPassword(nativePass: string): Promise<string>
-   comparePassword(
-      email: string,
-      plainPass: string
-   ): Promise<{ isPasswordCorrect: boolean; foundUser: IUserTypes }>
-   jwtAccessTokenSign(
-      userId: string | ObjectId,
-      email: string,
-      ACCESS_TOKEN_SECRET: string,
-      expiresIn?: string
-   ): string
-   jwtRefreshTokenSign(
-      userId: string | ObjectId,
-      email: string,
-      REFRESH_TOKEN_SECRET: string,
-      expiresIn?: string
-   ): string
 }
