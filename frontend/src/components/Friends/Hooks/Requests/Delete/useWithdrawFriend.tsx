de1b467a7029fed7a278a5c8b5f3f8825fb3986b
nagytommy76 <nagytommy76@gmail.com>
2024-08-18 12:25:30 +0200

    emitting friend:withdrawFriend socket

--- frontend/src/components/Friends/Hooks/Requests/Delete/useWithdrawFriend.tsx
+++ frontend/src/components/Friends/Hooks/Requests/Delete/useWithdrawFriend.tsx
@@ -1,16 +1,14 @@
 import { useContext } from 'react'
 import { FriendContext } from '../../../Context/FriendContext'
 import { useMutation } from '@tanstack/react-query'
+import { useAppSelector } from '@/reduxStore/store'
 import useRemoveMutation from './useRemoveMutation'
 import { socket } from '@/src/utils/socketIo'
 
 const useWithdrawFriend = () => {
+   const { userId, userName, currentImage } = useAppSelector((state) => state.auth)
    const removeFriendMutation = useRemoveMutation()
-   const {
-      setLoading,
-      setCardButtonType,
-      friendReducer: { friend },
-   } = useContext(FriendContext)
+   const { setLoading, setCardButtonType } = useContext(FriendContext)
 
    const { mutate } = useMutation({
       mutationKey: ['removeFriend'],
@@ -21,7 +19,11 @@ const useWithdrawFriend = () => {
       onSuccess(data, variables, context) {
          setLoading(false)
          setCardButtonType('makeFriend')
-         socket.emit('friend:withdrawFriend', { friend })
+         // My userId needed -> I'm the sender -> my id is the room_id
+         socket.emit('friend:withdrawFriend', {
+            friend: { userName, currentImage: currentImage.path },
+            friendId: userId,
+         })
       },
    })
    return { withdrawFriend: () => mutate() }
