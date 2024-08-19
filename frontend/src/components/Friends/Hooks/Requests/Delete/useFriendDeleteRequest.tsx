0865239efe05b46e1d63c09afb047d3f88e0dcbf
nagytommy76 <nagytommy76@gmail.com>
2024-08-19 12:18:20 +0200

    Emit friend:rejectFriend

--- frontend/src/components/Friends/Hooks/Requests/Delete/useFriendDeleteRequest.tsx
+++ frontend/src/components/Friends/Hooks/Requests/Delete/useFriendDeleteRequest.tsx
@@ -1,12 +1,20 @@
 import { useContext } from 'react'
 import { FriendContext } from '../../../Context/FriendContext'
-
+import { useAppSelector } from '@/reduxStore/store'
 import { useMutation } from '@tanstack/react-query'
+import { socket } from '@/src/utils/socketIo'
+
 import useRemoveMutation from './useRemoveMutation'
 
 const useFriendDeleteRequest = () => {
    const removeFriendMutation = useRemoveMutation()
-   const { setLoading, setCardButtonType } = useContext(FriendContext)
+   const { userId, userName, currentImage } = useAppSelector((state) => state.auth)
+   const {
+      setLoading,
+      setCardButtonType,
+      friendDispatch,
+      friendReducer: { friendId },
+   } = useContext(FriendContext)
 
    // Ide kéne 3 mutation ->
    // - Amikor törlöm a már meglévő friendet
@@ -22,6 +30,12 @@ const useFriendDeleteRequest = () => {
       },
       onSuccess(data, variables, context) {
          setLoading(false)
+         friendDispatch({ type: 'REMOVE_SINGLE_FRIEND', payload: userId as string })
+         socket.emit('friend:rejectFriend', {
+            friend: { userName, currentImage: currentImage.path },
+            friendId: userId,
+            roomId: friendId,
+         })
          setCardButtonType('makeFriend')
       },
    })
