6093063cb3eafab62172e44a2a8da107c46f5702
nagytommy76 <nagytommy76@gmail.com>
2024-08-19 12:16:36 +0200

    Using aggregate to find the sender

--- backend/controllers/friends/confirmFriend.ts
+++ backend/controllers/friends/confirmFriend.ts
@@ -1,22 +1,52 @@
 import { Response } from 'express'
+import { Types } from 'mongoose'
 import { User as UserModel } from '../../models/user/user'
-import { IMakeFriends } from './Types'
+import type { IMakeFriends } from './Types'
+import type { IFriends } from '../users/types/ModelTypes'
 
 export const confirmFriendshipController = async (request: IMakeFriends, response: Response) => {
-   const loggedInUserId = request.user?.userId as string
-   const friendId = request.body.friendId
+   const loggedInUserId = new Types.ObjectId(request.user?.userId)
+   const friendId = new Types.ObjectId(request.body.friendId)
 
    try {
       await UserModel.updateOne(
          { _id: loggedInUserId, friends: { $elemMatch: { friend: { $eq: friendId } } } },
          { $set: { 'friends.$.status': 'friends', 'friends.$.friendSince': new Date() } }
       )
-      const foundSender = await UserModel.findOne({
-         _id: loggedInUserId,
-         friends: { $elemMatch: { friend: { $eq: friendId } } },
-      })
-         .select(['friends.$', 'firstName', 'sureName', 'notifications'])
-         .byGetSelectedProfilePicture()
+
+      const foundSender = await UserModel.aggregate<{
+         _id: string
+         firstName: string
+         sureName: string
+         selectedProfilePicture: [{ path: string }]
+         foundFriend: IFriends[]
+      }>([
+         {
+            $match: {
+               _id: loggedInUserId,
+            },
+         },
+         {
+            $project: {
+               firstName: 1,
+               sureName: 1,
+               selectedProfilePicture: {
+                  $filter: {
+                     input: '$userDetails.profilePicturePath',
+                     as: 'profilePic',
+                     cond: { $eq: ['$$profilePic.isSelected', true] },
+                  },
+               },
+               foundFriend: {
+                  $filter: {
+                     input: '$friends',
+                     as: 'friend',
+                     cond: { $eq: ['$$friend.friend', friendId] },
+                  },
+               },
+            },
+         },
+      ])
 
       await UserModel.updateOne(
          { _id: friendId, friends: { $elemMatch: { friend: { $eq: loggedInUserId } } } },
@@ -28,10 +58,10 @@ export const confirmFriendshipController = async (request: IMakeFriends, respons
                   isRead: false,
                   notificationType: 'isFriendConfirm',
                   userDetails: {
-                     firstName: foundSender?.firstName,
-                     sureName: foundSender?.sureName,
-                     userId: foundSender?._id,
-                     profilePicture: foundSender?.userDetails.profilePicturePath[0].path,
+                     firstName: foundSender[0].firstName,
+                     sureName: foundSender[0].sureName,
+                     userId: foundSender[0]._id,
+                     profilePicture: foundSender[0].selectedProfilePicture[0].path,
                   },
                },
             },
@@ -44,11 +74,11 @@ export const confirmFriendshipController = async (request: IMakeFriends, respons
       }).select(['friends.$', 'notifications'])
 
       if (request.getUser !== undefined) {
-         const toSendUser = request.getUser(friendId) as any
+         const toSendUser = request.getUser(String(friendId)) as any
          if (toSendUser !== undefined) {
             request.ioSocket?.to(toSendUser.socketId).emit('confirmFriendship', {
                notifications: foundReceiver?.notifications,
-               userFriends: foundSender?.friends[0],
+               userFriends: foundSender[0].foundFriend[0],
             })
          }
       }
