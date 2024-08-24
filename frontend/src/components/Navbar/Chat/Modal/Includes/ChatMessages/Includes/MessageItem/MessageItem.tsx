22057daaa459e918fe33c36c5f0d9a95c9a9ab78
nagytommy76 <nagytommy76@gmail.com>
2024-08-24 15:04:02 +0200

    Using Fade to display reaction & delete onHover & added date tooltip

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/MessageItem.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/MessageItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import moment from 'moment'
 import dynamic from 'next/dynamic'
 import type { IMessages } from '@/Chat/Types'
 
@@ -6,29 +8,49 @@ import { StyledTextContainer, StyledTextBox, StyledTextBoxContainer, StyledTypog
 import RemoveButton from './Includes/RemoveButton/RemoveButton'
 import Reaction from './Includes/Reaction/Reaction'
 
+import Fade from '@mui/material/Fade'
+import Tooltip from '@mui/material/Tooltip'
+
 const MsgReactionModal = dynamic(() => import('./Includes/MsgReactionModal/MsgReactionModal'))
 const ImageDisplay = dynamic(() => import('./Includes/ImageDisplay'))
-const MessageItemHead = dynamic(() => import('./Includes/MessageItemHead'))
+const MessageItemHead = dynamic(() => import('./Includes/SenderAvatar'))
 
 const MessageItem: React.FC<{
    message: IMessages
    isRightContent?: boolean
 }> = ({ message, isRightContent = false }) => {
+   const [isReactionOpen, setIsReactionOpen] = useState<boolean>(false)
+
    return (
       <>
          <StyledTextContainer isRightContent={isRightContent}>
-            <MessageItemHead isRightContent={isRightContent} msgCreatedAt={message.createdAt} />
             {message.image.length > 1 && (
                <ImageDisplay imagePath={message.image} isRightContent={isRightContent} />
             )}
             {message.message.length !== 0 && (
-               <StyledTextBoxContainer isRightContent={isRightContent}>
-                  <Reaction reactions={message.reaction} messageId={message._id} />
-                  {isRightContent && <RemoveButton messageImage={message.image} messageId={message._id} />}
-                  <StyledTextBox isRightContent={isRightContent}>
-                     <StyledTypography variant='caption'>{message.message}</StyledTypography>
-                  </StyledTextBox>
-               </StyledTextBoxContainer>
+               <Tooltip
+                  placement={isRightContent ? 'left' : 'right'}
+                  title={moment(message.createdAt).format('YYYY MMMM D k:mm:ss')}
+               >
+                  <StyledTextBoxContainer
+                     isRightContent={isRightContent}
+                     onMouseEnter={() => setIsReactionOpen(true)}
+                     onMouseLeave={() => setIsReactionOpen(false)}
+                  >
+                     <Fade timeout={5} in={isReactionOpen}>
+                        <div style={{ display: 'flex' }}>
+                           <Reaction reactions={message.reaction} messageId={message._id} />
+                           {isRightContent && (
+                              <RemoveButton messageImage={message.image} messageId={message._id} />
+                           )}
+                        </div>
+                     </Fade>
+                     <StyledTextBox isRightContent={isRightContent}>
+                        <StyledTypography variant='caption'>{message.message}</StyledTypography>
+                     </StyledTextBox>
+                     {!isRightContent && <MessageItemHead isRightContent={isRightContent} />}
+                  </StyledTextBoxContainer>
+               </Tooltip>
             )}
             {message.reaction && <MsgReactionModal messageId={message._id} reactions={message.reaction} />}
          </StyledTextContainer>
