60f7e389565055e3cb376d37c0c3338eea286cb5
nagytommy76 <nagytommy76@gmail.com>
2024-08-24 15:03:18 +0200

    Added margin & removed Head style

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/Styles.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageItem/Styles.tsx
@@ -7,25 +7,13 @@ export const StyledTextContainer = styled('div', {
 })<{ isRightContent: boolean }>(({ isRightContent, theme }) => ({
    width: '43%',
    alignSelf: isRightContent ? 'end' : 'start',
-   marginBottom: 15,
-
+   marginBottom: 8,
    position: 'relative',
 
    [theme.breakpoints.down('md')]: {
       width: '100%',
       marginBottom: 25,
    },
-}))
-
-export const StyledTextBoxHead = styled('span', {
-   shouldForwardProp: (prop) => prop !== 'isRightContent',
-})<{ isRightContent: boolean }>(({ isRightContent, theme }) => ({
-   display: 'flex',
-   alignItems: 'center',
-   flexDirection: isRightContent ? 'row' : 'row-reverse',
-   justifyContent: 'space-between',
-
-   marginBottom: 10,
 }))
 
 export const StyledTextBoxContainer = styled('div', {
@@ -42,6 +30,7 @@ export const StyledTextBox = styled(Paper, {
    width: '100%',
    padding: '.85rem',
    textAlign: 'justify',
+   marginLeft: isRightContent ? 0 : 5,
 
    borderRadius: '25px',
    borderTopRightRadius: isRightContent ? 0 : '25px',
