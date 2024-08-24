626ba49845a3592da24c3153d61c6f8a566124ba
nagytommy76 <nagytommy76@gmail.com>
2024-08-24 15:02:34 +0200

    Removed useEffect & using memo to get all messages

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/MessageItems/Hooks/useFormatMsg.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/MessageItems/Hooks/useFormatMsg.tsx
@@ -1,22 +1,17 @@
-import { useState, useEffect } from 'react'
+import { useMemo } from 'react'
 import { useAppSelector } from '@/reduxStore/store'
 import { selectMessagesByChatId } from '@/reduxStore/slices/ChatSlice'
 
 import useFormatMessageDate from './useFormatMessageDate'
-import type { IMessages } from '@/src/components/Navbar/Chat/Types'
 
 const useFormatMsg = () => {
    const messages = useAppSelector(selectMessagesByChatId)
-   const [allMessages, setAllMessages] = useState<{ [key: string]: IMessages[] } | null>(null)
    const groupMessagesByInterval = useFormatMessageDate()
 
-   useEffect(() => {
-      if (messages) {
-         const grouppedMessagedByDate = groupMessagesByInterval(messages, 'hour')
-         setAllMessages(grouppedMessagedByDate)
-         console.log(grouppedMessagedByDate)
-      }
-   }, [messages])
+   const allMessages = useMemo(
+      () => groupMessagesByInterval(messages, 'hour'),
+      [messages, groupMessagesByInterval]
+   )
 
    return allMessages
 }
