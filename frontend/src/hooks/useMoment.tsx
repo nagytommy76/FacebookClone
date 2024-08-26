22709a26b6f3c23405a87b6c05e46ca96c8aea0f
nagytommy76 <nagytommy76@gmail.com>
2024-08-26 14:20:52 +0200

    Reworked the time ago display hook

--- frontend/src/hooks/useMoment.tsx
+++ frontend/src/hooks/useMoment.tsx
@@ -8,70 +8,71 @@ moment.relativeTimeThreshold('h', 24)
 moment.relativeTimeThreshold('d', 29)
 moment.relativeTimeThreshold('M', 11)
 
-/**
- *  moment.updateLocale('hu', {
-         relativeTime: {
-            s: '1 másodperce',
-            ss: '%d másodperce',
-            m: '1 perce',
-            mm: '%d perce',
-            h: '1 órája.',
-            hh: '%d órája.',
-            d: '1 napja',
-            dd: '%d napja',
-            M: '1 hónapja',
-            MM: '%d hónapja',
-            y: '1 éve',
-            yy: '%d éve',
-         },
-      })
- */
-
 moment.updateLocale('hu', {
    relativeTime: {
-      s: '1 mp',
-      ss: '%d mp',
-      m: '1 p',
-      mm: '%d p',
-      h: '1 ó.',
-      hh: '%d ó.',
-      d: '1 n',
-      dd: '%d n',
-      M: '1 hó',
-      MM: '%d hó',
-      y: '1 é',
-      yy: '%d é',
+      s: '1 másodperce',
+      ss: '%d másodperce',
+      m: '1 perce',
+      mm: '%d perce',
+      h: '1 órája',
+      hh: '%d órája',
+      d: '1 napja',
+      dd: '%d napja',
+      M: '1 hónapja',
+      MM: '%d hónapja',
+      y: '1 éve',
+      yy: '%d éve',
    },
 })
 
+// moment.updateLocale('hu', {
+//    relativeTime: {
+//       s: '1 mp',
+//       ss: '%d mp',
+//       m: '1 p',
+//       mm: '%d p',
+//       h: '1 ó.',
+//       hh: '%d ó.',
+//       d: '1 n',
+//       dd: '%d n',
+//       M: '1 hó',
+//       MM: '%d hó',
+//       y: '1 é',
+//       yy: '%d é',
+//    },
+// })
+
 const useMoment = (answeredAt: string) => {
-   const [currentTime, setCurrentTime] = useState('')
-   const [currentInterval, setCurrentInterval] = useState<number>(1000)
+   const [tinmeFromNow, setTimeFromNow] = useState(moment(answeredAt).fromNow(true))
 
    useEffect(() => {
-      setCurrentTime(moment(answeredAt).fromNow(true))
-      const interval = setInterval(() => {
-         if (currentTime.endsWith('p')) {
-            setCurrentInterval(60000) // 1 minute
-         }
-         if (currentTime.endsWith('ó.')) {
-            setCurrentInterval(60000 * 60) // 1 hour
-         }
-         if (currentTime.endsWith('n')) {
-            setCurrentInterval(86400000) // 1 day
-         }
-         if (currentTime.endsWith('hó')) {
-            setCurrentInterval(2629746000) // 1 month
-         }
+      const updateTimeFromNow = () => {
+         setTimeFromNow(moment(answeredAt).fromNow(true))
+      }
 
-         const elapsedTimeString = moment(answeredAt).fromNow(true)
-         setCurrentTime(elapsedTimeString)
-      }, currentInterval)
-
+      // Calculate the interval dynamically based on the current time difference
+      const getRefreshInterval = () => {
+         const diffInSeconds = moment().diff(answeredAt, 'seconds')
+         switch (true) {
+            case diffInSeconds < 60: // within 1 minute
+               return 30000 // refresh every 30 seconds
+            case diffInSeconds < 60 && diffInSeconds > 600: // between 1 and 10 minutes
+               return 60000 // Refresh every minute
+            case diffInSeconds < 600 && diffInSeconds > 1800: // between 10 and 30 minutes
+               return 1800000 // Refresh every 30 minutes
+            default:
+               return 3600000
+         }
+      }
+      // Update the time initially
+      updateTimeFromNow()
+      // Set the interval
+      const interval = setInterval(updateTimeFromNow, getRefreshInterval())
+      // Cleanup the interval on component unmount
       return () => clearInterval(interval)
-   }, [answeredAt, currentInterval, currentTime])
+   }, [answeredAt])
 
-   return currentTime
+   return tinmeFromNow
 }
 
 export default useMoment
