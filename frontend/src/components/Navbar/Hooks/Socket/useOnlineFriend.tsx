05599b99b18d3daebef5122330097669e0005482
nagytommy76 <nagytommy76@gmail.com>
2024-09-14 11:31:45 +0200

    Friend is online -> using InfoSnack & setOnlineStatus

--- frontend/src/components/Navbar/Hooks/Socket/useOnlineFriend.tsx
+++ frontend/src/components/Navbar/Hooks/Socket/useOnlineFriend.tsx
@@ -1,28 +1,35 @@
 import { useEffect } from 'react'
 import { useAppDispatch } from '@/reduxStore/store'
-import { setOnlineStatus } from '@/reduxStore/slices/ChatSlice'
+import { setOnlineStatus, setOnlineFriends } from '@/reduxStore/slices/ChatSlice'
+import { setHeadText, setImageSrc, setIsInfoSnackOpen } from '@/reduxStore/slices/InfoSnack'
 import { socket } from '@/src/utils/socketIo'
 import type { IRawOnlineFriends } from '@/types/FriendTypes'
 
 const useOnlineFriend = () => {
-   // This hook checks if a friend opens the app and if he is online
+   // This hook checks if a friend opens the app or loggs in -> is online
    const dispatch = useAppDispatch()
 
    useEffect(() => {
-      socket.on('online:friend', (args: IRawOnlineFriends) => {
-         console.log('OnLINE FRIENDS', args)
-         dispatch(
-            setOnlineStatus({
-               userId: args.userId,
-               isActive: args.isActive == '1' ? true : false,
-               lastSeen: Number(args.lastSeen),
-            })
-         )
-      })
+      socket.on(
+         'online:friend',
+         (args: { onlineUserData: IRawOnlineFriends; userName: string; profilePicture: string }) => {
+            const { onlineUserData, userName, profilePicture } = args
+            dispatch(
+               setOnlineFriends({
+                  userId: onlineUserData.userId,
+                  isActive: onlineUserData.isActive == '1' ? true : false,
+                  lastSeen: Number(onlineUserData.lastSeen),
+               })
+            )
+            dispatch(setIsInfoSnackOpen(onlineUserData.isActive == '1' ? true : false))
+            dispatch(setImageSrc(profilePicture))
+            dispatch(setHeadText(`${userName} elérhető!`))
+            dispatch(setHeadText(`${userName} újra elérhető!`))
+         }
+      )
 
       socket.on('offline:friend', (args: { userId: string }) => {
-         console.log('OFFLINE FRIENDS', args)
-         dispatch(setOnlineStatus({ userId: args.userId, isActive: false, lastSeen: 0 }))
+         dispatch(setOnlineStatus({ userId: args.userId, isActive: false }))
       })
       return () => {
          socket.off('online:friends')
