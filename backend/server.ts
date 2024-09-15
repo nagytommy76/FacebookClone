e24923b7c1f511eccc245557901ce8924b591cb4
nagytommy76 <nagytommy76@gmail.com>
2024-09-15 14:55:09 +0200

    Working on the middleware that extends the request object

--- backend/server.ts
+++ backend/server.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
-import { initSocketIO } from './config/socketIo'
+import initSocketIO from './config/socketIo'
 
 import morgan from 'morgan'
 import path, { resolve } from 'path'
@@ -16,7 +16,8 @@ const app: Application = express()
 
 const PORT = process.env.PORT || 5050
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-connectDB().then(() => {
+
+connectDB().then(async () => {
    app.listen(PORT, () => {
       console.log(`The app started: ${PORT}`)
    })
@@ -28,22 +29,24 @@ app.use(
       origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:6060'],
    })
 )
-
 app.use(helmet())
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(morgan('combined', { stream: accessLogStream }))
 
 initSocketIO(app).then(({ getUserById, getUser, io, onlineFriends, pubClient }) => {
-   app.use('/', (request: Request, response: Response, next: NextFunction) => {
+   //https://dev.to/omardiaa48/how-to-make-realtime-apis-with-nodejs-and-reactjs-using-socket-io-6ja
+   app.use((request: Request, response: Response, next: NextFunction) => {
+      console.log('APP:USE')
       request.ioSocket = io
       request.onlineFriends = onlineFriends
       request.getUser = getUser
       request.getUserById = getUserById
-      request.redisClient = pubClient
+      request.redisClient = pubClient as any
       next()
    })
 })
+
 app.use('/api/user', require('./api/user/userData'))
 app.use('/api/auth', require('./api/user/user'))
 app.use('/api/friends', require('./api/friends/friends'))
