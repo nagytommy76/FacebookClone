24b6474bf4b266717fdbc5383b315318673bf70f
nagytommy76 <nagytommy76@gmail.com>
2024-09-15 14:56:15 +0200

    Send the profilePicture

--- frontend/src/components/Navbar/Hooks/useSocketIoConnect.tsx
+++ frontend/src/components/Navbar/Hooks/useSocketIoConnect.tsx
@@ -3,14 +3,14 @@ import { useAppSelector } from '@/reduxStore/store'
 import { socket } from '@/src/utils/socketIo'
 
 const useSocketIoConnect = () => {
-   const { userId, userName } = useAppSelector((state) => state.auth)
+   const { userId, userName, currentImage } = useAppSelector((state) => state.auth)
    const messageLabels = useAppSelector((state) => state.chat.messageLabels)
 
    useEffect(() => {
       if (userId !== null || userId !== '') {
-         socket.emit('newUser', { userName, userId })
+         socket.emit('newUser', { userName, userId, profilePicture: currentImage.path })
       }
-   }, [userId, userName])
+   }, [userId, userName, currentImage.path])
 
    useEffect(() => {
       if (messageLabels) {
