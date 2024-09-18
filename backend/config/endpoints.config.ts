ec05749c56dc9574e10f22443202c858a1da9b4c
nagytommy76 <nagytommy76@gmail.com>
2024-09-18 13:43:06 +0200

    Added REDIS_PASSWORD & using dotenv config (fixed .env path)

--- backend/config/endpoints.config.ts
+++ backend/config/endpoints.config.ts
@@ -1,2 +1,9 @@
+import { config } from 'dotenv'
+import { resolve } from 'path'
+
+// Load environment variables before anything else
+config({ path: resolve(__dirname, '../.env') })
+
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET as string
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET as string
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD
