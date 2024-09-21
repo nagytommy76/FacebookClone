23747c30f9301a982bbd6b31854204b951a5c301
nagytommy76 <nagytommy76@gmail.com>
2024-09-21 14:06:05 +0200

    Set online friends on get-accepted-friends api call

--- frontend/src/components/MainPage/AcceptedFriends/Hooks/useGetAccepted.tsx
+++ frontend/src/components/MainPage/AcceptedFriends/Hooks/useGetAccepted.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { AxiosResponse, axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+import { useAppDispatch } from '@/reduxStore/store'
+import { setOnlineFriends } from '@/reduxStore/slices/ChatSlice'
 
 import type { IProfilePicture } from '@/src/types/PostTypes'
+import type { IIndexedRawOnlineFriendsRedis } from '@/types/FriendTypes'
 
 interface IFriendResponse {
    _id: string
@@ -12,10 +15,12 @@ interface IFriendResponse {
 }
 
 const useGetAccepted = () => {
+   const dispatch = useAppDispatch()
    const [myFriends, setMyFriends] = useState<IFriendResponse[]>([])
    const queryFn = async () => {
       return (await axios.get('/friends/get-accepted-friends')) as AxiosResponse<{
          myFoundFriendsData: IFriendResponse[]
+         allOnlineFriends: IIndexedRawOnlineFriendsRedis
       }>
    }
 
@@ -23,6 +28,16 @@ const useGetAccepted = () => {
       queryKey: ['getAcceptedFriends'],
       queryFn,
       onSuccess(data) {
+         Object.entries(data.data.allOnlineFriends).map(([key, value]) => {
+            dispatch(
+               setOnlineFriends({
+                  isActive: value.isActive === '0' ? false : true,
+                  lastSeen: Number(value.lastSeen),
+                  userId: key,
+                  socketId: value.socketId,
+               })
+            )
+         })
          setMyFriends(data.data.myFoundFriendsData)
       },
    })
