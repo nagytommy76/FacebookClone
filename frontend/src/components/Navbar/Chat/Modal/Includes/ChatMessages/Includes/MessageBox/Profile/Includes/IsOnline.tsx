3fd0fe4ad4b8c272accf071b4c2a5c0b3d8f9164
nagytommy76 <nagytommy76@gmail.com>
2024-10-02 18:25:16 +0200

    Added a last seen text to offline chat partner

--- frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Profile/Includes/IsOnline.tsx
+++ frontend/src/components/Navbar/Chat/Modal/Includes/ChatMessages/Includes/MessageBox/Profile/Includes/IsOnline.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useAppSelector } from '@/reduxStore/store'
+import moment from 'moment'
 
 import Typography from '@mui/material/Typography'
 
@@ -7,14 +8,21 @@ const IsOnline: React.FC<{ chatFirendId: string }> = ({ chatFirendId }) => {
    const isOnlineFriends = useAppSelector((state) => state.chat.isOnlineFriends)
    return (
       <>
-         {isOnlineFriends && isOnlineFriends[chatFirendId] ? (
+         {isOnlineFriends && isOnlineFriends[chatFirendId]?.isActive ? (
             <Typography sx={{ color: '#77c70e' }} variant='body1'>
                online
             </Typography>
          ) : (
-            <Typography sx={{ color: '#fd2b27' }} variant='body1'>
-               offline
-            </Typography>
+            <>
+               <Typography sx={{ color: '#fd2b27' }} variant='body1'>
+                  offline
+               </Typography>
+               <Typography variant='caption'>
+                  Utoljára elérhető volt:{' '}
+                  {isOnlineFriends &&
+                     moment(isOnlineFriends[chatFirendId]?.lastSeen).format('YYYY MM. DD. k:mm')}
+               </Typography>
+            </>
          )}
       </>
    )
