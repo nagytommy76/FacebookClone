b531573a86d6fd08b6a6b6c9648152c146556c9b
nagytommy76 <nagytommy76@gmail.com>
2024-11-09 14:41:52 +0100

    Removed MainPage and using AllPostsComponent & AcceptedFriends

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,29 @@
-import MainPage from '../src/components/MainPage/MainPage'
-const HomePage = () => {
-   return <MainPage />
+import dynamic from 'next/dynamic'
+
+const AllPostsComponent = dynamic(() => import('@/Base/PostsBase/PostsBase'))
+const AcceptedFriends = dynamic(() => import('@/components/MainPage/AcceptedFriends/AcceptedFriends'))
+
+const HomePage = async () => {
+   return (
+      <section
+         style={{
+            position: 'relative',
+            width: '100%',
+            minHeight: '100%',
+            margin: 'auto',
+
+            display: 'flex',
+            justifyContent: 'center',
+
+            overflowX: 'hidden',
+         }}
+      >
+         <div style={{ maxWidth: '800px' }}>
+            <AllPostsComponent />
+         </div>
+         <AcceptedFriends />
+      </section>
+   )
 }
 
 export default HomePage
