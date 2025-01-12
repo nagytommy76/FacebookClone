2975a888e578b17e5d3ffc32beee672751130e8d
nagytommy76 <nagytommy76@gmail.com>
2025-01-12 11:07:10 +0100

    useEffect
    Using a useEffect to dispatch context & using userId as querykey

--- frontend/src/components/Navbar/Includes/Notification/Hooks/useGetNotifications.tsx
+++ frontend/src/components/Navbar/Includes/Notification/Hooks/useGetNotifications.tsx
@@ -1,25 +1,30 @@
+import { useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance as axios, AxiosResponse } from '@/axios/AxiosInstance'
+import { useAppSelector } from '@/reduxStore/store'
+
 import type { NotificationType } from '../Types'
 import type { INotificationsAction } from '../Context/NotificationReducer'
 
 const useGetNotifications = (notificationsDispatch: React.Dispatch<INotificationsAction>) => {
+   const userId = useAppSelector((state) => state.auth.userId)
    const queryFunction = async () => {
       const data = (await axios.get('/user/notifications')) as AxiosResponse<{
          notifications: NotificationType[]
       }>
       return data
    }
-   const {} = useQuery({
-      queryKey: ['notifications'],
+   const { data } = useQuery({
+      queryKey: ['notifications', userId],
       queryFn: queryFunction,
-      onSuccess: (data) => {
-         notificationsDispatch({ type: 'SET_ALL_NOTIFICATIONS', payload: data.data.notifications })
-         notificationsDispatch({ type: 'SET_ACTIVE_NOTIFICATIONS_COUNT', payload: data.data.notifications })
-      },
    })
 
-   return null
+   useEffect(() => {
+      if (data) {
+         notificationsDispatch({ type: 'SET_ALL_NOTIFICATIONS', payload: data.data.notifications })
+         notificationsDispatch({ type: 'SET_ACTIVE_NOTIFICATIONS_COUNT', payload: data.data.notifications })
+      }
+   }, [data, notificationsDispatch])
 }
 
 export default useGetNotifications
