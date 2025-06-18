51cf95d86aa048634b9916e784eb288e22a7b2c8
nagytommy76 <nagytommy76@gmail.com>
2025-06-18 19:47:35 +0200

    Add REDIS_HOST REDIS_PORT endpoints

--- backend/config/endpoints.config.ts
+++ backend/config/endpoints.config.ts
@@ -6,4 +6,7 @@ config({ path: resolve(__dirname, '../.env') })
 
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET as string
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET as string
+
 export const REDIS_PASSWORD = process.env.REDIS_PASSWORD
+export const REDIS_HOST = process.env.REDIS_HOST
+export const REDIS_PORT = process.env.REDIS_PORT
