8d3a179d0cf3d866b2ffb57f95d96a5a4f2e3bec
nagytommy76 <nagytommy76@gmail.com>
2025-06-22 12:06:40 +0200

    Add getUsersFriends controller
    Query my friends -> lookup and project friend details

--- backend/controllers/friends/getFriends.ts
+++ backend/controllers/friends/getFriends.ts
@@ -50,6 +50,67 @@ export const getUsers = async (request: Request, response: Response) => {
    }
 }
 
+export const getUsersFriends = async (request: Request, response: Response) => {
+   try {
+      const queryUserId = request.query.userId as string
+      const userId = new Types.ObjectId(queryUserId)
+      const myFriends = await UserModel.aggregate([
+         {
+            $match: { _id: userId },
+         },
+         {
+            $project: {
+               friends: {
+                  $filter: {
+                     input: '$friends',
+                     as: 'friend',
+                     cond: { $eq: ['$$friend.status', 'friends'] },
+                  },
+               },
+            },
+         },
+         {
+            $lookup: {
+               from: 'users',
+               localField: 'friends.friend',
+               foreignField: '_id',
+               as: 'myFoundFriendsData',
+               pipeline: [
+                  {
+                     $project: {
+                        firstName: 1,
+                        sureName: 1,
+                        email: 1,
+                        createdAt: 1,
+                        friends: 1,
+                        dateOfBirth: 1,
+                        selectedProfilePicture: {
+                           $filter: {
+                              input: '$userDetails.profilePicturePath',
+                              as: 'profilePic',
+                              cond: { $eq: ['$$profilePic.isSelected', true] },
+                           },
+                        },
+                        lastWorkPlace: {
+                           $filter: {
+                              input: '$userDetails.workPlaces',
+                              as: 'workPlace',
+                              cond: { $eq: ['$$workPlace.endDate', null] },
+                           },
+                        },
+                     },
+                  },
+               ],
+            },
+         },
+      ])
+      return response.status(200).json(myFriends[0].myFoundFriendsData)
+   } catch (error) {
+      console.log(error)
+      return response.status(500).json(error)
+   }
+}
+
 // https://stackoverflow.com/questions/50363220/modelling-for-friends-schema-in-mongoose
 
 export const getAcceptedFriendsModel = async (userId: Types.ObjectId) => {
