e59acc7661e52e59ce7569e33d0ffabcd7dea63f
nagytommy76 <nagytommy76@gmail.com>
2025-06-22 10:30:35 +0200

    Add matcher
    Add new matcher config & check if refreshtoken is valid -> return to home page

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,10 +1,16 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+const authRoutes = ['/login', '/register']
+
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
    const refreshToken = request.cookies.get('refreshToken')?.value
    if (!refreshToken) return NextResponse.redirect(new URL('/login', request.url))
+
+   const nextUrl = request.nextUrl
+   const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
    //  Ha van refresh token megnézem, hogy valid-e fetch-el, axios nem müxik
    const response = await fetch('http://localhost:5050/api/auth/refresh-token', {
       method: 'POST',
@@ -21,10 +27,22 @@ export async function middleware(request: NextRequest) {
       redirectResponse.cookies.delete('refreshToken')
       return redirectResponse
    }
+
+   if (refreshToken) {
+      if (isAuthRoute) return Response.redirect(new URL('/', nextUrl))
+      return
+   }
    return NextResponse.next()
 }
 
 // Itt tudom a route-okat hozzáadni -> mire legyen érvényes
+// export const config = {
+//    matcher: ['/', /*'/friends',*/ '/groups', '/me'],
+// }
 export const config = {
-   matcher: ['/', /*'/friends',*/ '/groups', '/me'],
+   matcher: [
+      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+      // Always run for API routes
+      '/(api|trpc)(.*)',
+   ],
 }
