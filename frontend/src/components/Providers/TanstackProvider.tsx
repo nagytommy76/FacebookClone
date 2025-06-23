ed4c0d5a7fa539d47f3779b0bf2d852c3381fef9
nagytommy76 <nagytommy76@gmail.com>
2025-06-23 19:24:24 +0200

    Removed unused Hydrate

--- frontend/src/components/Providers/TanstackProvider.tsx
+++ frontend/src/components/Providers/TanstackProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const TanstackProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
