6abd21e4aeb35ec9407572d8031ca8ca82d9873c
nagytommy76 <nagytommy76@gmail.com>
2025-06-24 10:42:43 +0200

    Using ProfileImg PostImg components

---
+++ frontend/src/components/Profile/Body/PostProfileImage/PostProfileImage.tsx
@@ -0,0 +1,12 @@
+import ProfileImg from './ProfileImg'
+import PostImg from './PostImg'
+import { BodyPaper } from '../Styles'
+
+export default function PostProfileImage() {
+   return (
+      <BodyPaper>
+         <ProfileImg />
+         <PostImg />
+      </BodyPaper>
+   )
+}
