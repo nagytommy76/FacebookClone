cea4a14d1e70684ea2b3b949ca3c8b5089f77bca
nagytommy76 <nagytommy76@gmail.com>
2025-06-29 10:56:38 +0200

    Add img slider
    Add img slider & using GridImg style

--- frontend/src/components/Profile/Body/PostProfileImage/ProfileImg.tsx
+++ frontend/src/components/Profile/Body/PostProfileImage/ProfileImg.tsx
@@ -1,10 +1,13 @@
 import { useContext } from 'react'
+import ImageSlider from '@/Base/ImageSlider/ImageSlider'
 import { ProfileContext } from '../../Context/ProfileContextProvider'
-import Image from 'next/image'
+import { GridImage, StyledImage } from './Styles'
+import useImageSlider from './Hooks/useImageSlider'
 
 import Typography from '@mui/material/Typography'
 
 export default function ProfileImg() {
+   const { imageSliderOpen, selectedImgUrl, onClickImage, setImageSliderOpen } = useImageSlider()
    const {
       profileReducer: {
          initialUserDataState: {
@@ -12,14 +15,32 @@ export default function ProfileImg() {
          },
       },
    } = useContext(ProfileContext)
+
    return (
       <>
          <Typography variant='h4' align='center' gutterBottom>
             Profil képek
          </Typography>
-         {profilePicturePath.map((image) => (
-            <Image key={image._id} src={image.path} alt={image.path} width={200} height={200} />
-         ))}
+         <GridImage>
+            {profilePicturePath.map((image) => (
+               <StyledImage
+                  key={image._id}
+                  src={image.path}
+                  alt={image.path}
+                  width={200}
+                  height={200}
+                  onClick={() => onClickImage(image.path)}
+               />
+            ))}
+         </GridImage>
+         <ImageSlider
+            currentPicIndex={0}
+            isImgSliderOpen={imageSliderOpen}
+            postedPicturesPath={selectedImgUrl}
+            nextImage={() => {}}
+            previousImage={() => {}}
+            setIsImgSliderOpen={setImageSliderOpen}
+         />
       </>
    )
 }
