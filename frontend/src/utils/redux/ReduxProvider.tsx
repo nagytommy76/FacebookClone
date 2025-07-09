21ed06a355baae41877eab91ee49f7493a218be5
nagytommy76 <nagytommy76@gmail.com>
2025-07-09 18:19:23 +0200

    Reworked provider
    using persistStore & removed useRef

--- frontend/src/utils/redux/ReduxProvider.tsx
+++ frontend/src/utils/redux/ReduxProvider.tsx
@@ -1,21 +1,10 @@
 'use client'
-import { useRef } from 'react'
 import { Provider } from 'react-redux'
-import { makeStore, AppStore } from './store'
+import { store } from './store'
 import { persistStore } from 'redux-persist'
 
-import { PersistGate } from 'redux-persist/integration/react'
+persistStore(store) // persist the store
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
-   const storeRef = useRef<AppStore>()
-   if (!storeRef.current) {
-      // Create the store instance the first time this renders
-      storeRef.current = makeStore()
-   }
-   let persistor = persistStore(storeRef.current)
-   return (
-      <Provider store={storeRef.current}>
-         <PersistGate persistor={persistor}>{children}</PersistGate>
-      </Provider>
-   )
+   return <Provider store={store}>{children}</Provider>
 }
