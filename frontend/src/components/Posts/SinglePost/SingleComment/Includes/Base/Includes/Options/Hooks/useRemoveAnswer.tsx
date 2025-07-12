ed342825d9b6bbff59bcca89f4a864e5ea1951d8
nagytommy76 <nagytommy76@gmail.com>
2025-07-12 13:21:00 +0200

    Get post,comment,answer ids

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/Options/Hooks/useRemoveAnswer.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/Options/Hooks/useRemoveAnswer.tsx
@@ -1,22 +1,19 @@
+import { useContext } from 'react'
+import { AnswerContext } from '@/AnswersContext/AnswersContext'
+
 import { useMutation } from '@tanstack/react-query'
-import { axiosInstance as axios } from '@/src/utils/axiosSetup/AxiosInstance'
+import { axiosInstance as axios } from '@/axios/AxiosInstance'
 
-const mutationFunction = async ({
-   answerId,
-   commentId,
-   postId,
-}: {
-   answerId: string
-   commentId: string
-   postId: string
-}) => {
-   const response = await axios.delete('/post/post-comment-answer-delete', {
-      data: { answerId, postId, commentId },
-   })
-   return response.data
-}
-
-const useRemoveAnswer = () => {
+const useRemoveAnswer = (postId: string, commentId: string, answerId: string) => {
+   const { getAllChildRepliesToDelete } = useContext(AnswerContext)
+   const mutationFunction = async () => {
+      // getAllChildRepliesToDelete(answerId)
+      const response = await axios.delete('/post/post-comment-answer-delete', {
+         data: { answerId, postId, commentId },
+      })
+      console.log(response.data)
+      return response.data
+   }
    const { mutate } = useMutation({
       mutationKey: ['deleteCommentAnswer'],
       mutationFn: mutationFunction,
