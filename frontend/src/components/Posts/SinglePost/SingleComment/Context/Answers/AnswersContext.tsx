bf0775cf37b4bac2caed4332c1b4fbca4b1028a3
nagytommy76 <nagytommy76@gmail.com>
2025-07-12 13:21:34 +0200

    Removed unused/commented lines

--- frontend/src/components/Posts/SinglePost/SingleComment/Context/Answers/AnswersContext.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Context/Answers/AnswersContext.tsx
@@ -9,6 +9,7 @@ interface IAnswerContext {
    getAnswerReplies(parentId: string): ICommentAnswers[]
    getAnswerImageById: (answerId: string) => string | null | undefined
    // getAnswersChildAnswers: (answerId: string) => ICommentAnswers[] | null
+   getAllChildRepliesToDelete: (toDeleteAnswerId: string) => void
 }
 
 export const AnswerContext = createContext<IAnswerContext>({
@@ -24,9 +25,7 @@ export const AnswerContext = createContext<IAnswerContext>({
    getAnswerImageById: () => {
       return null
    },
-   // getAnswersChildAnswers: (answerId: string) => {
-   //    return null
-   // },
+   getAllChildRepliesToDelete: (toDeleteAnswerId: string) => {},
    getAnswerReplies: (parentId: string) => {
       return []
    },
@@ -61,51 +60,7 @@ const AnswersContextProvider: React.FC<{
       return getCommentsByParentId[parentId]
    }
 
-   // Végigmegyek a 65102dcb9f91c84b6d20f47c-n, majd ezekből ki kell gíyűjtenem az id-kat,
-   // és azokkal ellenőrizni getCommentsByParentId[id],
-   // Ha ez undefined vége,
-   // Ha nem undefined akkor van még childComment -> azoknak az ID-jét is begyűjteni majd azokon is végigmenni és így tovább....
-
-   const test = () => {}
-
-   const getAllChildRepliesToDelete = (toDeleteAnswerId: string) => {
-      if (getCommentsByParentId[toDeleteAnswerId] !== undefined) {
-         // Ezzel így megkapom azt amit törölni szeretnék, majd megnézem,
-         // hogy az answer._id (a trölendő) benne van-e a getCommentsByParentId[parentId]
-         let toDeleteIds: string[] = []
-         let hasChildCommentIds: string[] = []
-         for (const answer of Object.values(getCommentsByParentId[toDeleteAnswerId])) {
-            toDeleteIds.push(answer._id)
-            let toWatchAnswerId: string = ''
-
-            if (getCommentsByParentId[answer._id] !== undefined) {
-               hasChildCommentIds.push(answer._id)
-               // Ebben az esetben nincs child -> uccsó a comment ágon
-            }
-         }
-         // console.log(hasChildCommentIds)
-         // let isWhileRun = true
-         // while (isWhileRun) {
-         //    let newchildCommentIds: string[] = []
-         //    for (const commentId of hasChildCommentIds) {
-         //       for (const answer of Object.values(getCommentsByParentId[commentId])) {
-         //          if (getCommentsByParentId[answer._id] !== undefined) {
-         //             newchildCommentIds.push(commentId)
-         //          }
-         //          console.log(answer)
-         //       }
-         //       toDeleteIds.push(commentId)
-         //       // console.log(newchildCommentIds)
-         //    }
-         //    hasChildCommentIds = []
-
-         //    isWhileRun = false
-         // }
-      }
-   }
-   // console.log(getCommentsByParentId['jolsdflks'])
-   // console.log(getCommentsByParentId)
-   // getAllChildRepliesToDelete('65102dcb9f91c84b6d20f47c')
+   const getAllChildRepliesToDelete = (toDeleteAnswerId: string) => {}
 
    const getAnswerImageById = (answerId: string) => {
       return answerReducer.commentAnswers.find((answer) => answer._id === answerId)?.commentImage
@@ -117,7 +72,7 @@ const AnswersContextProvider: React.FC<{
             getAnswerReplies,
             answerDispatch,
             getAnswerImageById,
-            // getAnswersChildAnswers,
+            getAllChildRepliesToDelete,
             parentRootAnswers: getCommentsByParentId['null'],
             answerReducer,
          }}
