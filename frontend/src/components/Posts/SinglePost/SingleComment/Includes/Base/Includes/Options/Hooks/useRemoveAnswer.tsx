51b7560e7842ffffd2efd37ad035da389fef5289
nagytommy76 <nagytommy76@gmail.com>
2025-07-13 11:39:09 +0200

    using mutation
    Using InfoSnack to display the success delete in onSuccess fn

--- frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/Options/Hooks/useRemoveAnswer.tsx
+++ frontend/src/components/Posts/SinglePost/SingleComment/Includes/Base/Includes/Options/Hooks/useRemoveAnswer.tsx
@@ -1,22 +1,30 @@
 import { useContext } from 'react'
 import { AnswerContext } from '@/AnswersContext/AnswersContext'
+import { useAppDispatch } from '@/reduxStore/store'
+import { setHeadText, setIsInfoSnackOpen, setMessage } from '@/reduxStore/slices/InfoSnack'
 
 import { useMutation } from '@tanstack/react-query'
 import { axiosInstance as axios } from '@/axios/AxiosInstance'
 
 const useRemoveAnswer = (postId: string, commentId: string, answerId: string) => {
-   const { getAllChildRepliesToDelete } = useContext(AnswerContext)
+   const { answerDispatch } = useContext(AnswerContext)
+   const dispatch = useAppDispatch()
+
    const mutationFunction = async () => {
-      // getAllChildRepliesToDelete(answerId)
       const response = await axios.delete('/post/post-comment-answer-delete', {
          data: { answerId, postId, commentId },
       })
-      console.log(response.data)
       return response.data
    }
    const { mutate } = useMutation({
       mutationKey: ['deleteCommentAnswer'],
       mutationFn: mutationFunction,
+      onSuccess() {
+         dispatch(setIsInfoSnackOpen(true))
+         dispatch(setHeadText('Komment törlése sikeres'))
+         dispatch(setMessage('Sikeresen törölted a kommentet!'))
+         answerDispatch({ type: 'SET_ANSWER_DELETED', payload: answerId })
+      },
    })
    return mutate
 }
