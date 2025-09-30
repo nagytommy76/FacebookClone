f022967ff5094e8fa37a59b3b85f72b077f625bd
nagytommy76 <nagytommy76@gmail.com>
2025-09-30 19:54:35 +0200

    Add production url

--- frontend/app/friends/page.tsx
+++ frontend/app/friends/page.tsx
@@ -1,8 +1,11 @@
 import FriendsComponent from '@/components/Friends/Friends'
 import { IFriendsResponse } from '@/components/Friends/Types'
 
+const DEV_URL = process.env.NEXT_PUBLIC_DEV_API_URL
+const PRODUCTION_URL = process.env.NEXT_PUBLIC_PROD_API_URL
+
 async function getAllUsers(): Promise<IFriendsResponse[]> {
-   const res = await fetch('http://localhost:5050/api/friends/get-friends')
+   const res = await fetch(`${PRODUCTION_URL}/api/friends/get-friends`)
    if (!res.ok) {
       // This will activate the closest `error.js` Error Boundary
       throw new Error('Nem sikerült betölteni az ismerősöket')
