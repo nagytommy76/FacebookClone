8c4a2f3e7f843ff45e3b2a4f4506a69c5f1a2a8c
nagytommy76 <nagytommy76@gmail.com>
2025-10-01 18:09:24 +0200

    Fixed get-friends url

--- frontend/app/friends/page.tsx
+++ frontend/app/friends/page.tsx
@@ -5,7 +5,7 @@ const DEV_URL = process.env.NEXT_PUBLIC_DEV_API_URL
 const PRODUCTION_URL = process.env.NEXT_PUBLIC_PROD_API_URL
 
 async function getAllUsers(): Promise<IFriendsResponse[]> {
-   const res = await fetch(`${PRODUCTION_URL}/api/friends/get-friends`)
+   const res = await fetch(`${PRODUCTION_URL}friends/get-friends`)
    if (!res.ok) {
       // This will activate the closest `error.js` Error Boundary
       throw new Error('Nem sikerült betölteni az ismerősöket')
