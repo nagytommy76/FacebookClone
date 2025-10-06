3c703013665c18a79860e8e64328b2ee03733d16
nagytommy76 <nagytommy76@gmail.com>
2025-10-06 18:49:45 +0200

    Set domain
    Set domain in cookies for .vercel.app

--- backend/controllers/users/login.ts
+++ backend/controllers/users/login.ts
@@ -19,14 +19,14 @@ export const loginUserController = async (req: ILoginRequest, res: Response) => 
          httpOnly: true,
          secure: true,
          sameSite: 'none',
-         domain: 'up.railway.app',
+         domain: '.vercel.app',
          maxAge: REFRESH_TOKEN_EXPIRES_IN_MILLISEC,
       })
       res.cookie('accessToken', accessToken, {
          httpOnly: true,
          secure: true,
          sameSite: 'none',
-         domain: 'up.railway.app',
+         domain: '.vercel.app',
          maxAge: ACCESS_TOKEN_EXPIRES_IN_MILLISEC,
       })
          .status(200)
