4f9caed236e35770f148f100fbdedb52467ef225
nagytommy76 <nagytommy76@gmail.com>
2025-10-11 10:26:07 +0200

    Change fetch path
    Send request to auth/refresh-token

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,29 +3,26 @@ import type { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
    const refreshToken = request.cookies.get('refreshToken')?.value
-   /**
-    * Próba: Küldök egy requestet a backend felé, hogy hogy be tudjam állítani a next.js backendjében a cookiekat
-    * A middleware a next.js backendjén fut le, ezért nem látom a böngészőben lévő cookikat.
-    * Mert a kommunikáció az expres.js backend és next.js fornted (AXIOS) között zajlik le.
-    * A middleware nek fogalma sincs a böngészőben lévő cookiekról.
-    *
-    */
 
    if (!refreshToken) {
-      //  Ha van refresh token megnézem, hogy valid-e fetch-el, axios nem müxik
-      const response = await fetch(`${process.env.NEXT_PUBLIC_PROD_API_URL}auth/check-refresh-token`, {
-         method: 'POST',
-         headers: {
-            'Content-Type': 'application/json',
-         },
-         credentials: 'include',
-      })
-      if (response.status === 403) {
-         let redirectResponse = NextResponse.redirect(new URL('/login', request.url))
-         redirectResponse.cookies.delete('accessToken')
-         redirectResponse.cookies.delete('refreshToken')
-         return redirectResponse
-      }
+      return NextResponse.redirect(new URL('/login', request.url))
+   }
+
+   //  Ha van refresh token megnézem, hogy valid-e fetch-el, axios nem müxik
+   const response = await fetch(`${process.env.NEXT_PUBLIC_PROD_API_URL}auth/refresh-token`, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ refreshToken }),
+      credentials: 'include',
+   })
+
+   if (response.status === 403) {
+      let redirectResponse = NextResponse.redirect(new URL('/login', request.url))
+      redirectResponse.cookies.delete('accessToken')
+      redirectResponse.cookies.delete('refreshToken')
+      return redirectResponse
    }
 
    return NextResponse.next()
